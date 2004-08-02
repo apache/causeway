fdfb0ec4acc3d5716811d187db90d2ab8f6ee7db
rcm <dev-null@apache.org>
2004-08-02 13:00:01 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1006378 13f79535-47bb-0310-9956-ffa450edef68

--- example-expenses/src/org/nakedobjects/example/expenses/ExampleStories.java
+++ example-expenses/src/org/nakedobjects/example/expenses/ExampleStories.java
@@ -1,60 +1,33 @@
-/*
-    Naked Objects - a framework that exposes behaviourally complete
-    business objects directly to the user.
-    Copyright (C) 2000 - 2003  Naked Objects Group Ltd
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-    The authors can be contacted via www.nakedobjects.org (the
-    registered address of Naked Objects Group is Kingsway House, 123 Goldworth
-    Road, Woking GU21 1NR, UK).
-*/
-
 package org.nakedobjects.example.expenses;
-import java.util.Locale;
-
-import org.nakedobjects.testing.AcceptanceTest;
-import org.nakedobjects.testing.View;
+import org.nakedobjects.object.value.Money;
 import org.nakedobjects.utility.ComponentException;
 import org.nakedobjects.utility.ConfigurationException;
+import org.nakedobjects.xat.AcceptanceTestCase;
+import org.nakedobjects.xat.TestObject;
 
-public class ExampleStories extends AcceptanceTest {
+import java.util.Locale;
+
+public class ExampleStories extends AcceptanceTestCase {
 
 	public ExampleStories(String name) {
 		super(name);
 	}
 
-	public void runStories() {
-		story1();
-	}
-
 	private void story1() {
-		story("Make claim for new employee");
+		subtitle("Make claim for new employee");
 
-		step("Set up employee");
-		View robert = getClassView("Employees").newInstance();
+		nextStep("Set up employee");
+		TestObject robert = getTestClass("Employees").newInstance();
 		robert.fieldEntry("Surname", "Matthews");
 
-		step("Set up project");
-		View ait = getClassView("Projects").newInstance();
+		nextStep("Set up project");
+		TestObject ait = getTestClass("Projects").newInstance();
 		ait.fieldEntry("Name", "AIT phone system");
-		robert.drop("Projects", ait.drag());
+		robert.invokeAction("Projects", ait);
 		
-		step("Create claim for employee");
-		View claim = getClassView("Claims").drop(robert.drag());
-		claim.checkField("Total", "$0.00");
+		nextStep("Create claim for employee");
+		TestObject claim = getTestClass("Claims").invokeAction("New Claim", robert);
+		claim.assertFieldContains("Total", new Money(0.0));
 	}
 
 
@@ -67,11 +40,31 @@ public class ExampleStories extends AcceptanceTest {
 
 	}
 
-
-
 	public static void main(String[] args) throws ConfigurationException, ComponentException {
-        ExampleStories st = new ExampleStories("Example Stories");
-
-        st.start();
+        junit.textui.TestRunner.run(ExampleStories.class);
 	}
 }
+
+/*
+Naked Objects - a framework that exposes behaviourally complete
+business objects directly to the user.
+Copyright (C) 2000 - 2003  Naked Objects Group Ltd
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+The authors can be contacted via www.nakedobjects.org (the
+registered address of Naked Objects Group is Kingsway House, 123 Goldworth
+Road, Woking GU21 1NR, UK).
+*/
