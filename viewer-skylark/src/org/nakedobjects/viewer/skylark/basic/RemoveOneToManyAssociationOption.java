2a464c59ec54fb3e3d5b3811f71ae921c72be09b
rcm <dev-null@apache.org>
2004-09-10 12:56:23 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1006394 13f79535-47bb-0310-9956-ffa450edef68

--- viewer-skylark/src/org/nakedobjects/viewer/skylark/basic/RemoveOneToManyAssociationOption.java
+++ viewer-skylark/src/org/nakedobjects/viewer/skylark/basic/RemoveOneToManyAssociationOption.java
@@ -1,29 +1,5 @@
-/*
-    Naked Objects - a framework that exposes behaviourally complete
-    business objects directly to the user.
-    Copyright (C) 2000 - 2003  Naked Objects Group Ltd
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-    The authors can be contacted via www.nakedobjects.org (the
-    registered address of Naked Objects Group is Kingsway House, 123 Goldworth
-    Road, Woking GU21 1NR, UK).
-*/
 package org.nakedobjects.viewer.skylark.basic;
 
-import org.apache.log4j.Logger;
 import org.nakedobjects.object.NakedObject;
 import org.nakedobjects.object.collection.InternalCollection;
 import org.nakedobjects.object.control.About;
@@ -35,9 +11,11 @@ import org.nakedobjects.security.Session;
 import org.nakedobjects.viewer.skylark.Location;
 import org.nakedobjects.viewer.skylark.MenuOption;
 import org.nakedobjects.viewer.skylark.ObjectContent;
-import org.nakedobjects.viewer.skylark.OneToManyField;
+import org.nakedobjects.viewer.skylark.OneToManyElement;
 import org.nakedobjects.viewer.skylark.View;
 import org.nakedobjects.viewer.skylark.Workspace;
+
+import org.apache.log4j.Logger;
 
 
 public class RemoveOneToManyAssociationOption extends MenuOption {
@@ -52,7 +30,7 @@ public class RemoveOneToManyAssociationOption extends MenuOption {
     	NakedObject parentObject = collection.forParent();
 
 		// associated object
-    	OneToManyField content = (OneToManyField) view.getContent();
+    	OneToManyElement content = (OneToManyElement) view.getContent();
 		OneToManyAssociation association = content.getOneToManyAssociation();
         NakedObject associatedObject = content.getObject();
         
@@ -74,7 +52,7 @@ public class RemoveOneToManyAssociationOption extends MenuOption {
     	NakedObject parentObject = ((ObjectContent) view.getParent().getParent().getContent()).getObject();
 
 		// associated object
-    	OneToManyField content = (OneToManyField) view.getContent();
+    	OneToManyElement content = (OneToManyElement) view.getContent();
 		OneToManyAssociation association = content.getOneToManyAssociation();
         NakedObject associatedObject = content.getObject();
         
@@ -82,3 +60,27 @@ public class RemoveOneToManyAssociationOption extends MenuOption {
         association.clearAssociation(parentObject, associatedObject);
     }
 }
+
+/*
+Naked Objects - a framework that exposes behaviourally complete
+business objects directly to the user.
+Copyright (C) 2000 - 2003  Naked Objects Group Ltd
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+The authors can be contacted via www.nakedobjects.org (the
+registered address of Naked Objects Group is Kingsway House, 123 Goldworth
+Road, Woking GU21 1NR, UK).
+*/
