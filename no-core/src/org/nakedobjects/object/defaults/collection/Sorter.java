6659d2982b16b910fbe5cee737c7e6d5880f86c4
rcm <dev-null@apache.org>
2004-12-10 17:09:54 +0000

    Created a sortable internal collection - InternalCollectionSortableVector - and an interface to define the sort order.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1006522 13f79535-47bb-0310-9956-ffa450edef68

---
+++ no-core/src/org/nakedobjects/object/defaults/collection/Sorter.java
@@ -0,0 +1,38 @@
+package org.nakedobjects.object.defaults.collection;
+
+import org.nakedobjects.object.NakedObject;
+
+
+public interface Sorter {
+
+    /**
+     * Returns: 0 if the two objects are equal; a negative number if object1
+     * comes before object2; and a positive number if object2 comes before object1.
+     */
+    int compare(NakedObject object1, NakedObject object2);
+
+}
+
+/*
+ * Naked Objects - a framework that exposes behaviourally complete business
+ * objects directly to the user. Copyright (C) 2000 - 2004 Naked Objects Group
+ * Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * The authors can be contacted via www.nakedobjects.org (the registered address
+ * of Naked Objects Group is Kingsway House, 123 Goldworth Road, Woking GU21
+ * 1NR, UK).
+ */
