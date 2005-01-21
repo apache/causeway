c846f680c83cc4e1159e85145955dd58cc64fbf6
rcm <dev-null@apache.org>
2005-01-21 14:31:57 +0000

    Modfied the error dialog so that the message is displayed within a scrollable window allowing all text to be more easily seen.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1006548 13f79535-47bb-0310-9956-ffa450edef68

--- no-core/src/org/nakedobjects/utility/ErrorDialog.java
+++ no-core/src/org/nakedobjects/utility/ErrorDialog.java
@@ -4,13 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Frame;
-import java.awt.Graphics;
-import java.awt.Panel;
+import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.StringTokenizer;
 
 
 public abstract class ErrorDialog extends Frame {
@@ -22,7 +20,12 @@ public abstract class ErrorDialog extends Frame {
     }
     
     public void show(final String message, final String text) {
-        add(new MessagePanel(message, text), BorderLayout.CENTER);
+        TextArea messagePanel = new TextArea();
+        messagePanel.setText(message + "\n\n" + text);
+        messagePanel.setForeground(Color.red);
+        messagePanel.setEditable(false);
+        
+        add(messagePanel, BorderLayout.CENTER);
         Button button = new Button("OK");
         button.addActionListener(new ActionListener() {
 
@@ -38,36 +41,6 @@ public abstract class ErrorDialog extends Frame {
             }
         });
         show();
-    }
-
-}
-
-class MessagePanel extends Panel {
-    private final String message;
-    private final String stackTrace;
-
-    public MessagePanel(final String message, final String stackTtrace) {
-        this.message = message;
-        this.stackTrace = stackTtrace;
-    }
-
-    public void paint(Graphics g) {
-        int lineHeight = g.getFontMetrics().getHeight();
-
-        g.setColor(Color.red);
-
-        int x = 10;
-        int y = lineHeight;
-        g.drawString(message, x, y);
-
-        y += lineHeight + 14;
-
-        StringTokenizer st = new StringTokenizer(stackTrace, "\n\r");
-        while (st.hasMoreTokens()) {
-            String line = st.nextToken();
-            g.drawString(line, x, y);
-            y += lineHeight;
-        }
     }
 
 }
