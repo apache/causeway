88f91dd6e1a08557775a44f64ca0232c589a2fc0
rcm <dev-null@apache.org>
2005-02-18 19:48:22 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1006665 13f79535-47bb-0310-9956-ffa450edef68

---
+++ example-clue/src/Assertion.java
@@ -0,0 +1,54 @@
+
+
+public class Assertion {
+    private Weapon weapon;
+    public Room getRoom() {
+        return room;
+    }
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+    public Suspect getSuspect() {
+        return suspect;
+    }
+    public void setSuspect(Suspect suspect) {
+        this.suspect = suspect;
+    }
+    public Weapon getWeapon() {
+        return weapon;
+    }
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+    private Suspect suspect;
+    private Room room;
+   
+     public String toString() {
+        return suspect + " in " + room + " with " + weapon;
+    }
+}
+
+
+/*
+ * Naked Objects - a framework that exposes behaviourally complete business
+ * objects directly to the user. Copyright (C) 2000 - 2005 Naked Objects Group
+ * Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * The authors can be contacted via www.nakedobjects.org (the registered address
+ * of Naked Objects Group is Kingsway House, 123 Goldworth Road, Woking GU21
+ * 1NR, UK).
+ */
---
+++ example-clue/src/ExamplePersistent.java
@@ -0,0 +1,180 @@
+import org.nakedobjects.container.configuration.Configuration;
+import org.nakedobjects.container.configuration.ConfigurationException;
+import org.nakedobjects.container.configuration.ConfigurationFactory;
+import org.nakedobjects.object.NakedObject;
+import org.nakedobjects.object.NakedObjectContext;
+import org.nakedobjects.object.OidGenerator;
+import org.nakedobjects.object.defaults.LoadedObjectsHashtable;
+import org.nakedobjects.object.defaults.LocalObjectManager;
+import org.nakedobjects.object.defaults.LocalReflectionFactory;
+import org.nakedobjects.object.defaults.NakedObjectSpecificationImpl;
+import org.nakedobjects.object.defaults.NakedObjectSpecificationLoaderImpl;
+import org.nakedobjects.object.defaults.TimeBasedOidGenerator;
+import org.nakedobjects.object.reflect.PojoAdapter;
+import org.nakedobjects.persistence.file.XmlObjectStore;
+import org.nakedobjects.reflector.java.JavaBusinessObjectContainer;
+import org.nakedobjects.reflector.java.JavaObjectFactory;
+import org.nakedobjects.reflector.java.SimpleExplorationSetup;
+import org.nakedobjects.reflector.java.reflect.JavaReflectorFactory;
+import org.nakedobjects.system.AboutNakedObjects;
+import org.nakedobjects.system.SplashWindow;
+import org.nakedobjects.viewer.ObjectViewingMechanismListener;
+import org.nakedobjects.viewer.skylark.InteractionSpy;
+import org.nakedobjects.viewer.skylark.RootObject;
+import org.nakedobjects.viewer.skylark.View;
+import org.nakedobjects.viewer.skylark.ViewUpdateNotifier;
+import org.nakedobjects.viewer.skylark.Viewer;
+import org.nakedobjects.viewer.skylark.ViewerAssistant;
+import org.nakedobjects.viewer.skylark.ViewerFrame;
+import org.nakedobjects.viewer.skylark.special.RootWorkspaceSpecification;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+
+public class ExamplePersistent {
+    //    private static final Logger LOG = Logger.getLogger(XmlClient.class);
+    private static final String DEFAULT_CONFIG = "nakedobjects.properties";
+    private static final String SHOW_EXPLORATION_OPTIONS = "viewer.lightweight.show-exploration";
+
+    public static void main(String[] args) throws ConfigurationException {
+        BasicConfigurator.configure();
+
+        ConfigurationFactory.setConfiguration(new Configuration(DEFAULT_CONFIG));
+        if (ConfigurationFactory.getConfiguration().getString(SHOW_EXPLORATION_OPTIONS) == null) {
+            ConfigurationFactory.getConfiguration().add(SHOW_EXPLORATION_OPTIONS, "yes");
+        }
+        PropertyConfigurator.configure(ConfigurationFactory.getConfiguration().getProperties("log4j"));
+
+        Logger log = Logger.getLogger("Naked Objects");
+        log.info(AboutNakedObjects.getName());
+        log.info(AboutNakedObjects.getVersion());
+        log.info(AboutNakedObjects.getBuildId());
+
+        SplashWindow splash = null;
+        boolean noSplash = ConfigurationFactory.getConfiguration().getBoolean("nosplash", false);
+        if (!noSplash) {
+            splash = new SplashWindow();
+        }
+
+        try {
+            JavaBusinessObjectContainer container = new JavaBusinessObjectContainer();
+
+            ViewUpdateNotifier updateNotifier = new ViewUpdateNotifier();
+
+            LoadedObjectsHashtable loadedObjectsHashtable = new LoadedObjectsHashtable();
+
+            JavaObjectFactory objectFactory = new JavaObjectFactory();
+            objectFactory.setContainer(container);
+
+            container.setObjectFactory(objectFactory);
+
+
+            
+            XmlObjectStore objectStore = new XmlObjectStore();
+            objectStore.setLoadedObjects(loadedObjectsHashtable);
+
+            OidGenerator oidGenerator = new TimeBasedOidGenerator();            
+
+            LocalObjectManager objectManager = new LocalObjectManager();
+            objectManager.setObjectStore(objectStore);
+            objectManager.setNotifier(updateNotifier);
+            objectManager.setFactory(objectFactory);
+            objectManager.setOidGenerator(oidGenerator);
+            objectManager.setLoadedObjects(loadedObjectsHashtable);
+
+            container.setObjectManger(objectManager);
+
+            new NakedObjectSpecificationLoaderImpl();
+
+            LocalReflectionFactory reflectionFactory = new LocalReflectionFactory();
+
+            JavaReflectorFactory reflectorFactory = new JavaReflectorFactory();
+
+            //    new NakedObjectSpecificationImpl();
+            NakedObjectSpecificationImpl.setReflectionFactory(reflectionFactory);
+            NakedObjectSpecificationLoaderImpl.setReflectorFactory(reflectorFactory);
+
+            reflectorFactory.setObjectFactory(objectFactory);
+
+            NakedObjectContext context = new NakedObjectContext(objectManager);
+
+            SimpleExplorationSetup explorationSetup = new SimpleExplorationSetup();
+            explorationSetup.setContext(context);
+            
+     //       explorationSetup.addFixture(new EcsFixture());
+ 
+            ViewerFrame frame = new ViewerFrame();
+            frame.setTitle("ECS");
+
+            Viewer viewer = new Viewer();
+            viewer.setRenderingArea(frame);
+
+            frame.setViewer(viewer);
+
+            viewer.setListener(new ObjectViewingMechanismListener() {
+                public void viewerClosing() {}
+            });
+
+            InteractionSpy spy = new InteractionSpy();
+
+            ViewerAssistant viewerAssistant = new ViewerAssistant();
+            viewerAssistant.setViewer(viewer);
+            viewerAssistant.setDebugFrame(spy);
+            viewerAssistant.setUpdateNotifier(updateNotifier);
+
+            viewer.setUpdateNotifier(updateNotifier);
+            viewer.setSpy(spy);
+
+            viewer.start();
+
+            ExampleContext applicationContext = new ExampleContext();
+            applicationContext.created();
+
+            NakedObject rootObject = PojoAdapter.createAdapter(applicationContext);
+            RootWorkspaceSpecification spec = new RootWorkspaceSpecification();
+            View view = spec.createView(new RootObject(rootObject), null);
+            viewer.setRootView(view);
+
+            frame.setBounds(10, 10, 800, 600);
+
+            viewer.sizeChange();
+
+            frame.show();
+            
+            explorationSetup.installFixtures();
+
+
+        } finally {
+            if (splash != null) {
+                splash.toFront();
+                splash.removeAfterDelay(3);
+            }
+        }
+    }
+}
+
+/*
+ * Naked Objects - a framework that exposes behaviourally complete business
+ * objects directly to the user. Copyright (C) 2000 - 2005 Naked Objects Group
+ * Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * The authors can be contacted via www.nakedobjects.org (the registered address
+ * of Naked Objects Group is Kingsway House, 123 Goldworth Road, Woking GU21
+ * 1NR, UK).
+ */
