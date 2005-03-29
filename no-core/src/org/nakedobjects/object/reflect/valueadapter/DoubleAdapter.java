285946703fd463d5f12b31c546189e5d0d27500c
rcm <dev-null@apache.org>
2005-03-29 12:16:09 +0000

    Added value classes for the rest of the primitive types: byte, short, long and double.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1006719 13f79535-47bb-0310-9956-ffa450edef68

---
+++ no-core/src/org/nakedobjects/object/reflect/valueadapter/DoubleAdapter.java
@@ -0,0 +1,81 @@
+package org.nakedobjects.object.reflect.valueadapter;
+
+import org.nakedobjects.object.InvalidEntryException;
+import org.nakedobjects.object.TextEntryParseException;
+import org.nakedobjects.object.value.DoubleFloatingPointValue;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+
+public class DoubleAdapter extends AbstractNakedValue implements DoubleFloatingPointValue {
+    private static NumberFormat FORMAT = NumberFormat.getNumberInstance();
+
+    private double value;
+
+    public byte[] asEncodedString() {
+        return null;
+    }
+
+    public double doubleValue() {
+        return value;
+    }
+
+    public String getIconName() {
+        return "double";
+    }
+
+    public Object getObject() {
+        return new Double(value);
+    }
+
+    public void parseTextEntry(String entry) throws InvalidEntryException {
+        if (entry == null || entry.trim().equals("")) {
+            throw new InvalidEntryException();
+        } else {
+            try {
+                value = FORMAT.parse(entry).doubleValue();
+            } catch (ParseException e) {
+                throw new TextEntryParseException("Invalid number", e);
+            }
+        }
+    }
+
+    public void restoreFromEncodedString(byte[] data) {}
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String titleString() {
+        return FORMAT.format(value);
+    }
+
+    public String getValueClass() {
+        return double.class.getName();
+    }
+}
+
+/*
+ * Naked Objects - a framework that exposes behaviourally complete business
+ * objects directly to the user. Copyright (C) 2000 - 2005 Naked Objects Group
+ * Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * The authors can be contacted via www.nakedobjects.org (the registered address
+ * of Naked Objects Group is Kingsway House, 123 Goldworth Road, Woking GU21
+ * 1NR, UK).
+ */
---
+++ no-core/src/org/nakedobjects/object/reflect/valueadapter/ShortAdapter.java
@@ -0,0 +1,81 @@
+package org.nakedobjects.object.reflect.valueadapter;
+
+import org.nakedobjects.object.InvalidEntryException;
+import org.nakedobjects.object.TextEntryParseException;
+import org.nakedobjects.object.value.ShortValue;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+
+public class ShortAdapter extends AbstractNakedValue implements ShortValue {
+    private static NumberFormat FORMAT = NumberFormat.getNumberInstance();
+
+    private short value;
+
+    public byte[] asEncodedString() {
+        return null;
+    }
+
+    public short shortValue() {
+       return value;
+    }
+
+    public String getIconName() {
+        return "short";
+    }
+
+    public Object getObject() {
+        return new Short(value);
+    }
+
+    public void parseTextEntry(String entry) throws InvalidEntryException {
+        if (entry == null || entry.trim().equals("")) {
+            throw new InvalidEntryException();
+        } else {
+            try {
+                value = FORMAT.parse(entry).shortValue();
+            } catch (ParseException e) {
+                throw new TextEntryParseException("Invalid number", e);
+            }
+        }
+    }
+
+    public void restoreFromEncodedString(byte[] data) {}
+
+    public void setValue(short value) {
+        this.value = value;
+    }
+
+    public String titleString() {
+        return FORMAT.format(value);
+    }
+
+    public String getValueClass() {
+        return short.class.getName();
+    }
+}
+
+/*
+ * Naked Objects - a framework that exposes behaviourally complete business
+ * objects directly to the user. Copyright (C) 2000 - 2005 Naked Objects Group
+ * Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * The authors can be contacted via www.nakedobjects.org (the registered address
+ * of Naked Objects Group is Kingsway House, 123 Goldworth Road, Woking GU21
+ * 1NR, UK).
+ */
