e2da0285eebd3654f5e1726d3ee67a380bb94084
rcm <dev-null@apache.org>
2005-05-12 15:00:04 +0000

    Added a utility - DebugFileDump - to dump the details from a DebugInfo object to a file, with an automatically generated name.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1006917 13f79535-47bb-0310-9956-ffa450edef68

---
+++ no-core/src/org/nakedobjects/utility/DebugFileDump.java
@@ -0,0 +1,53 @@
+package org.nakedobjects.utility;
+
+import org.nakedobjects.object.NakedObjectRuntimeException;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class DebugFileDump {
+    private static final DateFormat FORMAT = new SimpleDateFormat("yyyyMMdd-HHmmss-SSS");
+
+    public static void dump(DebugInfo object) {
+        String dateStamp = FORMAT.format(new Date());
+        String fileName = object.getClass().getName() + "-" + dateStamp + ".txt";
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(fileName));
+            writer.println(object.getDebugTitle());
+            writer.println();
+            writer.println(object.getDebugData());
+            writer.close();
+        } catch (IOException e) {
+            throw new NakedObjectRuntimeException(e);
+        }
+    }
+}
+
+/*
+ * Naked Objects - a framework that exposes behaviourally complete business
+ * objects directly to the user. Copyright (C) 2000 - 2005 Naked Objects Group
+ * Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * The authors can be contacted via www.nakedobjects.org (the registered address
+ * of Naked Objects Group is Kingsway House, 123 Goldworth Road, Woking GU21
+ * 1NR, UK).
+ */
