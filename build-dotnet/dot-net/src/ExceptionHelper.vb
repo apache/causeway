55af092496461ba4580bc377df0a6b78c0d5c8d3
rcm <dev-null@apache.org>
2005-07-12 18:35:28 +0000

    Refactoring to tidy up resolving and combine object factory and manager - VI
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1007288 13f79535-47bb-0310-9956-ffa450edef68

--- build-dotnet/dot-net/src/ExceptionHelper.vb
+++ build-dotnet/dot-net/src/ExceptionHelper.vb
@@ -7,11 +7,17 @@ namespace org.nakedobjects.dotnet
 
 Public Class ExceptionHelper
         Public Shared Sub dumpException(ByVal ex As Exception)
-            Dim trace As StringBuilder = New StringBuilder
-            dumpExceptionRecursively(trace, ex, 0)
-            org.nakedobjects.utility.ExceptionHelper.submitLog(ex.Message, trace.ToString())
+            Dim trace as String
+            trace = exceptionAsString(ex)
+            org.nakedobjects.utility.ExceptionHelper.submitLog(ex.Message, trace)
             Console.WriteLine(trace)
         End Sub
+        
+        Public Shared Function exceptionAsString(ByVal ex As Exception) as String
+            Dim trace As StringBuilder = New StringBuilder
+            dumpExceptionRecursively(trace, ex, 0)
+            return trace.ToString()
+        End Function
 
         Private Shared Sub dumpExceptionRecursively( _
             ByVal trace As StringBuilder, ByVal ex As Exception, ByVal indent As Integer)
@@ -26,9 +32,9 @@ Public Class ExceptionHelper
             If ex.InnerException Is Nothing Then Return
 
             For i As Integer = 1 To indent
-                trace.Append("*")
+                trace.Append("* ")
             Next
-            trace.Append(" INNER EXCEPTION >>>")
+            trace.Append("INNER EXCEPTION >>>")
             trace.Append(Environment.NewLine)
             dumpExceptionRecursively(trace, ex.InnerException, indent + 3)
         End Sub
