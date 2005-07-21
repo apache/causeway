e3eb74e5d1ba7aa08e23d07986184ec5b7564eea
rcm <dev-null@apache.org>
2005-07-21 16:13:45 +0000

    Extracted interface for Canvas and Image.  Created dummy version of Canvas for testing.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1007378 13f79535-47bb-0310-9956-ffa450edef68

--- viewer-skylark/src/org/nakedobjects/viewer/skylark/Canvas.java
+++ viewer-skylark/src/org/nakedobjects/viewer/skylark/Canvas.java
@@ -1,173 +1,67 @@
 package org.nakedobjects.viewer.skylark;
 
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Rectangle;
+public interface Canvas {
+    Canvas createSubcanvas();
 
+    Canvas createSubcanvas(Bounds bounds);
 
-public class Canvas {
-    private java.awt.Color color;
-    private Font font;
-    private Graphics graphics;
+    Canvas createSubcanvas(int x, int y, int width, int height);
 
-    protected Canvas(Graphics graphics) {
-        this.graphics = graphics;
-    }
+    void draw3DRectangle(int x, int y, int width, int height, boolean raised);
 
-    protected Canvas(Graphics bufferGraphic, int x, int y, int width, int height) {
-        graphics = bufferGraphic;
-        graphics.clipRect(x, y, width, height);
-    }
+    void clearBackground(View view, Color color);
 
-    public Canvas createSubcanvas() {
-        return new Canvas(graphics.create());
-    }
+    void drawIcon(Image icon, int x, int y);
 
-    public Canvas createSubcanvas(Bounds bounds) {
-        return createSubcanvas(bounds.x, bounds.y, bounds.width, bounds.height);
-    }
+    void drawIcon(Image icon, int x, int y, int width, int height);
 
-    public Canvas createSubcanvas(int x, int y, int width, int height) {
-        Graphics g = graphics.create();
-        // this form of clipping must go here!
-        g.translate(x, y);
-        return new Canvas(g, 0, 0, width, height);
-    }
+    void drawLine(int x, int y, int x2, int y2, Color color);
 
-    public void draw3DRectangle(int x, int y, int width, int height, boolean raised) {
-        graphics.draw3DRect(x, y, width, height, raised);
-    }
+    void drawLine(Location start, int xExtent, int yExtent, Color color);
 
-    public void clearBackground(View view, Color color) {
-	        Bounds bounds = view.getBounds();
-	        drawSolidRectangle(0, 0, bounds.getWidth() - 1, bounds.getHeight() - 1, color);
-    }
+    void drawRectangle(int x, int y, int width, int height, Color color);
 
-    public void drawIcon(Image icon, int x, int y) {
-        graphics.drawImage(icon.getAwtImage(), x, y, null);
-    }
+    void drawRectangleAround(View view, Color color);
 
-    public void drawIcon(Image icon, int x, int y, int width, int height) {
-        graphics.drawImage(icon.getAwtImage(), x, y, width, height, null);
-    }
+    void drawRoundedRectangle(int x, int y, int width, int height, int arcWidth, int arcHeight, Color color);
 
-    public void drawLine(int x, int y, int x2, int y2, Color color) {
-        useColor(color);
-        graphics.drawLine(x, y, x2, y2);
-    }
+    void drawShape(Shape shape, Color color);
 
-    public void drawLine(Location start, int xExtent, int yExtent, Color color) {
-        drawLine(start.getX(), start.getY(), start.getX() + xExtent, start.getY() + yExtent, color);
-    }
+    void drawShape(Shape shape, int x, int y, Color color);
 
-    public void drawRectangle(int x, int y, int width, int height, Color color) {
-        useColor(color);
-        graphics.drawRect(x, y, width, height);
-    }
+    void drawSolidOval(int x, int y, int width, int height, Color color);
 
-    public void drawRectangleAround(View view, Color color) {
-        Bounds bounds = view.getBounds();
-        drawRectangle(0, 0, bounds.getWidth() - 1, bounds.getHeight() - 1, color);
-    }
+    void drawSolidRectangle(int x, int y, int width, int height, Color color);
 
-    public void drawRoundedRectangle(int x, int y, int width, int height, int arcWidth, int arcHeight, Color color) {
-        useColor(color);
-        graphics.drawRoundRect(x, y, width, height, arcWidth, arcHeight);
-    }
+    void drawSolidShape(Shape shape, Color color);
 
-    public void drawShape(Shape shape, Color color) {
-        useColor(color);
-        graphics.drawPolygon(shape.getX(), shape.getY(), shape.count());
-    }
+    void drawSolidShape(Shape shape, int x, int y, Color color);
 
-    public void drawShape(Shape shape, int x, int y, Color color) {
-        Shape copy = new Shape(shape);
-        copy.translate(x, y);
-        drawShape(copy, color);
-    }
+    void drawText(String text, int x, int y, Color color, Text style);
 
-    public void drawSolidOval(int x, int y, int width, int height, Color color) {
-        useColor(color);
-        graphics.fillOval(x, y, width, height);
-    }
-
-    public void drawSolidRectangle(int x, int y, int width, int height, Color color) {
-        useColor(color);
-        graphics.fillRect(x, y, width, height);
-    }
-
-    public void drawSolidShape(Shape shape, Color color) {
-        useColor(color);
-        graphics.fillPolygon(shape.getX(), shape.getY(), shape.count());
-    }
-
-    public void drawSolidShape(Shape shape, int x, int y, Color color) {
-        Shape copy = new Shape(shape);
-        copy.translate(x, y);
-        drawSolidShape(copy, color);
-    }
-
-    public void drawText(String text, int x, int y, Color color, Text style) {
-        useColor(color);
-        useFont(style);
-        graphics.drawString(text, x, y + 1);
-    }
-
-    public void offset(int x, int y) {
-        graphics.translate(x, y);
-    }
-
-    public boolean overlaps(Bounds bounds) {
-        // return activeArea.intersects(bounds);
-        Rectangle clip = graphics.getClipBounds();
-        Bounds activeArea = new Bounds(clip.x, clip.y, clip.width, clip.height);
-        return bounds.intersects(activeArea);
-    }
-
-    public String toString() {
-        Rectangle cb = graphics.getClipBounds();
-        return "Canvas [area=" + cb.x + "," + cb.y + " " + cb.width + "x" + cb.height + ",color=" + color + ",font=" + font + "]";
-    }
-
-    private void useColor(Color color) {
-        java.awt.Color awtColor = color.getAwtColor();
-
-        if (this.color != awtColor) {
-            this.color = awtColor;
-            graphics.setColor(awtColor);
-        }
-    }
-
-    private void useFont(Text style) {
-        Font font = style.getAwtFont();
-
-        if (this.font != font) {
-            this.font = font;
-            graphics.setFont(font);
-        }
-    }
+    void offset(int x, int y);
 }
 
 /*
- * Naked Objects - a framework that exposes behaviourally complete business
- * objects directly to the user. Copyright (C) 2000 - 2005 Naked Objects Group
- * Ltd
- * 
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
- * 
- * The authors can be contacted via www.nakedobjects.org (the registered address
- * of Naked Objects Group is Kingsway House, 123 Goldworth Road, Woking GU21
- * 1NR, UK).
+ Naked Objects - a framework that exposes behaviourally complete
+ business objects directly to the user.
+ Copyright (C) 2000 - 2005  Naked Objects Group Ltd
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ The authors can be contacted via www.nakedobjects.org (the
+ registered address of Naked Objects Group is Kingsway House, 123 Goldworth
+ Road, Woking GU21 1NR, UK).
  */
--- viewer-skylark/src/org/nakedobjects/viewer/skylark/Image.java
+++ viewer-skylark/src/org/nakedobjects/viewer/skylark/Image.java
@@ -1,55 +1,29 @@
 package org.nakedobjects.viewer.skylark;
 
+public interface Image {
+    int getHeight();
 
-public class Image {
-    java.awt.Image iconImage;
+    int getWidth();
 
-    public Image(java.awt.Image iconImage) {
-        if (iconImage == null) {
-            throw new NullPointerException();
-        }
-
-        this.iconImage = iconImage;
-    }
-
-    public int getHeight() {
-        return iconImage.getHeight(null);
-    }
-
-    public int getWidth() {
-        return iconImage.getWidth(null);
-    }
-
-    public Size getSize() {
-        return new Size(getWidth(), getHeight());
-    }
-    
-    java.awt.Image getAwtImage() {
-        return iconImage;
-    }
+    Size getSize();
 }
 
-
 /*
-Naked Objects - a framework that exposes behaviourally complete
-business objects directly to the user.
-Copyright (C) 2000 - 2005  Naked Objects Group Ltd
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-The authors can be contacted via www.nakedobjects.org (the
-registered address of Naked Objects Group is Kingsway House, 123 Goldworth
-Road, Woking GU21 1NR, UK).
-*/
+ * Naked Objects - a framework that exposes behaviourally complete business objects directly to the
+ * user. Copyright (C) 2000 - 2005 Naked Objects Group Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ * 
+ * The authors can be contacted via www.nakedobjects.org (the registered address of Naked Objects
+ * Group is Kingsway House, 123 Goldworth Road, Woking GU21 1NR, UK).
+ */
