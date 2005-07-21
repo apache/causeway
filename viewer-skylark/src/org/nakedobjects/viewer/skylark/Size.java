28058a19d00d287fabfb1e06bf9154358e880be3
rcm <dev-null@apache.org>
2005-07-21 15:51:33 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1007363 13f79535-47bb-0310-9956-ffa450edef68

--- viewer-skylark/src/org/nakedobjects/viewer/skylark/Size.java
+++ viewer-skylark/src/org/nakedobjects/viewer/skylark/Size.java
@@ -1,26 +1,3 @@
-/*
-        Naked Objects - a framework that exposes behaviourally complete
-        business objects directly to the user.
-        Copyright (C) 2000 - 2005  Naked Objects Group Ltd
-
-        This program is free software; you can redistribute it and/or modify
-        it under the terms of the GNU General Public License as published by
-        the Free Software Foundation; either version 2 of the License, or
-        (at your option) any later version.
-
-        This program is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        GNU General Public License for more details.
-
-        You should have received a copy of the GNU General Public License
-        along with this program; if not, write to the Free Software
-        Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-        The authors can be contacted via www.nakedobjects.org (the
-        registered address of Naked Objects Group is Kingsway House, 123 Goldworth
-        Road, Woking GU21 1NR, UK).
-*/
 package org.nakedobjects.viewer.skylark;
 
 import java.awt.Dimension;
@@ -48,7 +25,7 @@ public class Size {
         width = size.width;
         height = size.height;
     }
-    
+
     public void contract(int width, int height) {
         this.width -= width;
         this.height -= height;
@@ -63,10 +40,10 @@ public class Size {
         this.height -= height;
     }
 
-	public void contract(Padding padding) {
-		height -= padding.top + padding.bottom;
-		width -= padding.left + padding.right;
-	}
+    public void contract(Padding padding) {
+        height -= padding.top + padding.bottom;
+        width -= padding.left + padding.right;
+    }
 
     public void contractWidth(int width) {
         this.width -= width;
@@ -79,30 +56,30 @@ public class Size {
     public void ensureWidth(int width) {
         this.width = Math.max(this.width, width);
     }
-    
-	public boolean equals(Object obj) {
-		 if (obj == this) {
-			 return true;
-		 }
 
-		 if (obj instanceof Size) {
-			Size object = (Size) obj;
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
 
-			 return object.width == this.width && object.height == this.height;
-		 }
+        if (obj instanceof Size) {
+            Size object = (Size) obj;
 
-		 return false;
-	 }
+            return object.width == this.width && object.height == this.height;
+        }
+
+        return false;
+    }
 
     public void extend(int width, int height) {
         this.width += width;
         this.height += height;
     }
-    
-	public void extend(Padding padding) {
+
+    public void extend(Padding padding) {
         this.width += padding.getLeftRight();
         this.height += padding.getTopBottom();
-	}
+    }
 
     public void extend(Size size) {
         this.width += size.width;
@@ -138,3 +115,23 @@ public class Size {
     }
 
 }
+
+/*
+ * Naked Objects - a framework that exposes behaviourally complete business objects directly to the
+ * user. Copyright (C) 2000 - 2005 Naked Objects Group Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ * 
+ * The authors can be contacted via www.nakedobjects.org (the registered address of Naked Objects
+ * Group is Kingsway House, 123 Goldworth Road, Woking GU21 1NR, UK).
+ */
