7c6e1049ac6337fbc4e2cea76b18b28973a1d1c6
rcm <dev-null@apache.org>
2005-07-21 15:56:37 +0000

    Changed TitleText so that can have a maximum width specified.  This limits the amount of text that is drawn.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1007366 13f79535-47bb-0310-9956-ffa450edef68

--- viewer-skylark/src/org/nakedobjects/viewer/skylark/basic/TitleText.java
+++ viewer-skylark/src/org/nakedobjects/viewer/skylark/basic/TitleText.java
@@ -11,103 +11,111 @@ import org.nakedobjects.viewer.skylark.core.AbstractView;
 
 
 public abstract class TitleText {
-     private View view;
-    private Text style;
+    private final View view;
+    private final Text style;
+    private final int ellipsisWidth;
 
-    public TitleText(View view, Text style) {
+    public TitleText(final View view, final Text style) {
         this.view = view;
-         this.style = style;
+        this.style = style;
+
+        ellipsisWidth = style.stringWidth("...");
     }
 
     public Size getSize() {
         int height = View.VPADDING + style.getTextHeight() + View.VPADDING;
         int width = View.HPADDING + style.stringWidth(title()) + View.HPADDING;
-
         return new Size(width, height);
     }
 
-    public void draw(Canvas canvas, int x, int baseline) {
+    /**
+     * Draw this TitleText's text stating from the specified x coordination and on the specified
+     * baseline.
+     */
+    public void draw(final Canvas canvas, final int x, final int baseline) {
+        draw(canvas, x, baseline, -1);
+    }
+
+    /**
+     * Draw this TitleText's text stating from the specified x coordination and on the specified
+     * baseline. If a maximum width is specified (ie it is positive) then the text drawn will not
+     * extend past that width.
+     * 
+     * @param maxWidth
+     *                 the maximum width to display the text within; if negative no limit is imposed
+     */
+    public void draw(final Canvas canvas, final int x, final int baseline, final int maxWidth) {
         Color color;
-   
-        if (getState().canDrop()) {
+        ViewState state = view.getState();
+        if (state.canDrop()) {
             color = Style.VALID;
-        } else if (getState().cantDrop()) {
+        } else if (state.cantDrop()) {
             color = Style.INVALID;
-        } else if (getState().isObjectIdentified()) {
+        } else if (state.isObjectIdentified()) {
             color = Style.PRIMARY1;
- /*       } else if (getState().isRootViewIdentified()) {
-            color = Style.PRIMARY2;
-    */    } else {
+        } else {
             color = Style.BLACK;
         }
 
-        int maxWidth = view.getSize().getWidth() - view.getPadding().getLeft() - view.getPadding().getRight();
+        final int xt = x + View.HPADDING;
+        final int yt = baseline;
 
-         String text = title();
-        int xt = x + View.HPADDING;
-        int yt = baseline;
-
-        if (style.stringWidth(text) > maxWidth) {
-            int elip = style.stringWidth("...");
-
-            do {
-                int last = text.lastIndexOf(' ');
-
-                if (last == -1) {
-                    for (int i = text.length() - 1; i > 5; i--) {
-                        if ((style.stringWidth(text.substring(0, i)) + elip) < maxWidth) {
-                            text = text.substring(0, i);
-
-                            break;
-                        }
-                    }
-
-                    break;
-                }
-
-                text = text.substring(0, last);
-            } while ((style.stringWidth(text) + elip) > maxWidth);
-
+        String text = title();
+        if (maxWidth > 0 && style.stringWidth(text) > maxWidth) {
+            int lastCharacterWithinAllowedWidth = 0;
+            for(int textWidth = ellipsisWidth; textWidth <= maxWidth;) {
+                char character = text.charAt(lastCharacterWithinAllowedWidth);
+                textWidth += style.charWidth(character);
+                lastCharacterWithinAllowedWidth++;
+            }
+            
+            int space = text.lastIndexOf(' ', lastCharacterWithinAllowedWidth - 1);
+            if(space > 0) {
+	            while (space >= 0) {
+	                char character = text.charAt(space - 1);
+	                if(Character.isLetterOrDigit(character)) {
+	                    break;
+	                }
+	                space --;
+	            }
+	            
+                text = text.substring(0, space);
+            } else {
+                text = text.substring(0, lastCharacterWithinAllowedWidth - 1);
+            }
             text += "...";
         }
 
         canvas.drawText(text, xt, yt, color, style);
 
         if (AbstractView.debug) {
-        	int x2 = style.stringWidth(text) - 1;
+            int x2 = style.stringWidth(text) - 1;
             canvas.drawRectangle(xt, yt - style.getAscent(), x2, style.getTextHeight() - 1, Color.DEBUG_DRAW_BOUNDS);
-            canvas.drawLine(xt, yt - style.getAscent() - style.getTextHeight() / 2, xt +x2,  yt - style.getAscent() - style.getTextHeight() / 2, Color.DEBUG_DRAW_BOUNDS);
-           canvas.drawLine(xt, baseline, xt + x2, baseline, Color.DEBUG_BASELINE);
+            canvas.drawLine(xt, yt - style.getAscent() - style.getTextHeight() / 2, xt + x2, yt - style.getAscent()
+                    - style.getTextHeight() / 2, Color.DEBUG_DRAW_BOUNDS);
+            canvas.drawLine(xt, baseline, xt + x2, baseline, Color.DEBUG_BASELINE);
         }
-    }
-
-    private ViewState getState() {
-        return view.getState();
     }
 
     protected abstract String title();
 }
 
 /*
-Naked Objects - a framework that exposes behaviourally complete
-business objects directly to the user.
-Copyright (C) 2000 - 2005  Naked Objects Group Ltd
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-The authors can be contacted via www.nakedobjects.org (the
-registered address of Naked Objects Group is Kingsway House, 123 Goldworth
-Road, Woking GU21 1NR, UK).
-*/
+ * Naked Objects - a framework that exposes behaviourally complete business objects directly to the
+ * user. Copyright (C) 2000 - 2005 Naked Objects Group Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ * 
+ * The authors can be contacted via www.nakedobjects.org (the registered address of Naked Objects
+ * Group is Kingsway House, 123 Goldworth Road, Woking GU21 1NR, UK).
+ */
