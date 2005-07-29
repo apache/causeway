e5b53384f98a0f6544163a9f7ae5dbd91aa897f3
rcm <dev-null@apache.org>
2005-07-29 16:34:20 +0000

    Tidied up the Naked class hierachy
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1007425 13f79535-47bb-0310-9956-ffa450edef68

--- no-default-reflector/src/org/nakedobjects/reflector/java/reflect/InternalCollectionAdapter.java
+++ no-default-reflector/src/org/nakedobjects/reflector/java/reflect/InternalCollectionAdapter.java
@@ -8,50 +8,41 @@ import org.nakedobjects.object.NakedObjectSpecification;
 import org.nakedobjects.object.control.Hint;
 import org.nakedobjects.object.persistence.Oid;
 import org.nakedobjects.object.reflect.Action;
-import org.nakedobjects.object.reflect.NakedObjectAssociation;
 import org.nakedobjects.object.reflect.NakedObjectField;
-import org.nakedobjects.object.security.Session;
 import org.nakedobjects.utility.NotImplementedException;
 
 import java.util.Enumeration;
 import java.util.Hashtable;
 
+
 public class InternalCollectionAdapter implements InternalCollection {
     protected static Hashtable pojos = new Hashtable();
-    private NakedObjectSpecification specification;
-    private org.nakedobjects.application.collection.InternalCollection collection;
-    private NakedObjectSpecification elementSpecification;
-    private Oid oid;
-    private long version;
 
-
-    public long getVersion() {
-        return version;
-    }
-    public void setVersion(long version) {
-        this.version = version;
-    }
     public static Naked createAdapter(org.nakedobjects.application.collection.InternalCollection pojo, Class type) {
-        if(pojo == null) {
+        if (pojo == null) {
             return null;
         }
         InternalCollectionAdapter nakedObject;
-        if(false && pojos.containsKey(pojo)) {
+        if (false && pojos.containsKey(pojo)) {
             nakedObject = (InternalCollectionAdapter) pojos.get(pojo);
         } else {
             nakedObject = new InternalCollectionAdapter(pojo, type);
         }
-        
-        return nakedObject;       
+
+        return nakedObject;
     }
-    
-    
-    
+
+    private org.nakedobjects.application.collection.InternalCollection collection;
+    private NakedObjectSpecification elementSpecification;
+    private Oid oid;
+    private NakedObjectSpecification specification;
+    private long version;
+
     private InternalCollectionAdapter(org.nakedobjects.application.collection.InternalCollection vector, Class type) {
         this.collection = vector;
-        
+
         pojos.put(vector, this);
-        
+
         Class t = type == null ? Object.class : type;
         elementSpecification = NakedObjects.getSpecificationLoader().loadSpecification(t);
     }
@@ -67,7 +58,7 @@ public class InternalCollectionAdapter implements InternalCollection {
 
     public Enumeration elements() {
         final Enumeration elements = collection.elements();
-        
+
         return new Enumeration() {
             public boolean hasMoreElements() {
                 return elements.hasMoreElements();
@@ -75,17 +66,25 @@ public class InternalCollectionAdapter implements InternalCollection {
 
             public Object nextElement() {
                 Object element = elements.nextElement();
-                return element instanceof NakedObject ? element : NakedObjects.getObjectLoader().getAdapterForElseCreateAdapterForTransient(element);
+                return element instanceof NakedObject ? element : NakedObjects.getObjectLoader()
+                        .getAdapterForElseCreateAdapterForTransient(element);
             }
         };
     }
 
-    public Enumeration oids() {
-        throw new NotImplementedException();
+    public NakedObjectSpecification getElementSpecification() {
+        if (elementSpecification == null) {
+            return NakedObjects.getSpecificationLoader().loadSpecification(Object.class);
+        }
+        return elementSpecification;
     }
 
-    public int size() {
-        return collection.size();
+    public Hint getHint(Action action, Naked[] parameters) {
+        return null;
+    }
+
+    public Hint getHint(NakedObjectField field, Naked value) {
+        return null;
     }
 
     public String getIconName() {
@@ -100,76 +99,53 @@ public class InternalCollectionAdapter implements InternalCollection {
         return oid;
     }
 
-    public void setOid(Oid oid) {
-       this.oid = oid;
-    }
-
-    public void setResolved() {}
-
-    public void copyObject(Naked object) {}
-
     public NakedObjectSpecification getSpecification() {
-        if(specification == null) {
+        if (specification == null) {
             specification = NakedObjects.getSpecificationLoader().loadSpecification(getObject().getClass());
         }
         return specification;
     }
 
-    public String titleString() {
-        return "vector...";
-    }
-    
-    public NakedObjectSpecification getElementSpecification() {
-        if(elementSpecification == null) {
-            return NakedObjects.getSpecificationLoader().loadSpecification(Object.class);
-        }
-        return elementSpecification;
-    }
-
-    public NakedObject parent() {
-        return null;
+    public long getVersion() {
+        return version;
     }
 
     public boolean isAggregated() {
         return false;
     }
 
-    public void clear() {}
-    
-    public void clearAssociation(NakedObjectAssociation specification, NakedObject ref) {}
-
-    public boolean canAccess(Session session, NakedObjectField specification) {
-        return false;
+    public Enumeration oids() {
+        throw new NotImplementedException();
     }
 
-    public boolean canAccess(Session session, Action action) {
-        return false;
-    }
-
-    public boolean canUse(Session session, NakedObjectField field) {
-        return false;
-    }
-
-    public Naked execute(Action action, Naked[] parameters) {
+    public NakedObject parent() {
         return null;
     }
 
-    public Hint getHint(Action action, Naked[] parameters) {
-        return null;
+    public void setOid(Oid oid) {
+        this.oid = oid;
     }
 
-    public Hint getHint(NakedObjectField field, Naked value) {
-        return null;
+    public void setResolved() {}
+
+    public void setVersion(long version) {
+        this.version = version;
     }
 
-    public boolean isEmpty() {
-        return size() == 0;
+    public int size() {
+        return collection.size();
+    }
+
+    public void sort() {}
+
+    public String titleString() {
+        return "vector...";
     }
 
     public String toString() {
         StringBuffer s = new StringBuffer();
 
-        s.append("POJO (collection) ");
+        s.append("InternalCollectionAdapater");
         // datatype
         NakedObjectSpecification spec = getSpecification();
         s.append(spec == null ? getClass().getName() : spec.getShortName());
@@ -198,41 +174,26 @@ public class InternalCollectionAdapter implements InternalCollection {
         s.append(collection);
         return s.toString();
 
-        
-        
-    
     }
-
-
-    public void clearViewDirty() {}
-
-
-
-    public void sort() {}
 
 }
 
-
 /*
-Naked Objects - a framework that exposes behaviourally complete
-business objects directly to the user.
-Copyright (C) 2000 - 2005  Naked Objects Group Ltd
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-The authors can be contacted via www.nakedobjects.org (the
-registered address of Naked Objects Group is Kingsway House, 123 Goldworth
-Road, Woking GU21 1NR, UK).
-*/
+ * Naked Objects - a framework that exposes behaviourally complete business objects directly to the
+ * user. Copyright (C) 2000 - 2005 Naked Objects Group Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ * 
+ * The authors can be contacted via www.nakedobjects.org (the registered address of Naked Objects
+ * Group is Kingsway House, 123 Goldworth Road, Woking GU21 1NR, UK).
+ */
--- no-default-reflector/src/org/nakedobjects/reflector/java/reflect/VectorCollectionAdapter.java
+++ no-default-reflector/src/org/nakedobjects/reflector/java/reflect/VectorCollectionAdapter.java
@@ -8,55 +8,50 @@ import org.nakedobjects.object.NakedObject;
 import org.nakedobjects.object.NakedObjectSpecification;
 import org.nakedobjects.object.control.Hint;
 import org.nakedobjects.object.persistence.Oid;
-import org.nakedobjects.object.reflect.Action;
-import org.nakedobjects.object.reflect.NakedObjectAssociation;
 import org.nakedobjects.object.reflect.NakedObjectField;
-import org.nakedobjects.object.security.Session;
 import org.nakedobjects.utility.NotImplementedException;
+import org.nakedobjects.utility.ToString;
 
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Vector;
 
+
 public class VectorCollectionAdapter implements InternalCollection {
     protected static Hashtable pojos = new Hashtable();
-    private NakedObjectSpecification specification;
-    private Vector collection;
-    private NakedObjectSpecification elementSpecification;
-    private Oid oid;
-    private long version;
 
-
-    public long getVersion() {
-        return version;
-    }
-    public void setVersion(long version) {
-        this.version = version;
-    }
-    
     public static NakedCollection createAdapter(Vector pojo, Class type) {
-        if(pojo == null) {
+        if (pojo == null) {
             return null;
         }
         VectorCollectionAdapter nakedObject;
-        if(false && pojos.containsKey(pojo)) {
+        if (false && pojos.containsKey(pojo)) {
             nakedObject = (VectorCollectionAdapter) pojos.get(pojo);
         } else {
             nakedObject = new VectorCollectionAdapter(pojo, type);
         }
-        
-        return nakedObject;       
+
+        return nakedObject;
     }
-    
-    
-    
+
+    private Vector collection;
+    private NakedObjectSpecification elementSpecification;
+    private Oid oid;
+    private NakedObjectSpecification specification;
+    private long version;
+
     private VectorCollectionAdapter(Vector vector, Class type) {
         this.collection = vector;
-        
+
         pojos.put(vector, this);
-        
+
         Class t = type == null ? Object.class : type;
         elementSpecification = NakedObjects.getSpecificationLoader().loadSpecification(t);
+    }
+
+    private Object adapter(Object element) {
+        return element instanceof NakedObject ? element : NakedObjects.getObjectLoader()
+                .getAdapterForElseCreateAdapterForTransient(element);
     }
 
     public boolean contains(NakedObject object) {
@@ -70,7 +65,7 @@ public class VectorCollectionAdapter implements InternalCollection {
 
     public Enumeration elements() {
         final Enumeration elements = collection.elements();
-        
+
         return new Enumeration() {
             public boolean hasMoreElements() {
                 return elements.hasMoreElements();
@@ -83,16 +78,12 @@ public class VectorCollectionAdapter implements InternalCollection {
         };
     }
 
-    private Object adapter(Object element) {
-        return element instanceof NakedObject ? element : NakedObjects.getObjectLoader().getAdapterForElseCreateAdapterForTransient(element);
+    public NakedObjectSpecification getElementSpecification() {
+        return elementSpecification;
     }
 
-    public Enumeration oids() {
-        throw new NotImplementedException();
-    }
-
-    public int size() {
-        return collection.size();
+    public Hint getHint(NakedObjectField field, Naked value) {
+        return null;
     }
 
     public String getIconName() {
@@ -107,76 +98,53 @@ public class VectorCollectionAdapter implements InternalCollection {
         return oid;
     }
 
-    public void setOid(Oid oid) {
-       this.oid = oid;
-    }
-
-    public void setResolved() {}
-
-    public void copyObject(Naked object) {}
-
     public NakedObjectSpecification getSpecification() {
-        if(specification == null) {
+        if (specification == null) {
             specification = NakedObjects.getSpecificationLoader().loadSpecification(getObject().getClass());
         }
         return specification;
     }
 
-    public String titleString() {
-        return "vector...";
-    }
-    
-    public NakedObjectSpecification getElementSpecification() {
-        return elementSpecification;
-    }
-
-    public NakedObject parent() {
-        return null;
+    public long getVersion() {
+        return version;
     }
 
     public boolean isAggregated() {
         return false;
     }
 
-    public void clear() {}
-    
-    public void clearAssociation(NakedObjectAssociation specification, NakedObject ref) {}
-
-    public boolean canAccess(Session session, NakedObjectField specification) {
-        return false;
+    public Enumeration oids() {
+        throw new NotImplementedException();
     }
 
-    public boolean canAccess(Session session, Action action) {
-        return false;
-    }
-
-    public boolean canUse(Session session, NakedObjectField field) {
-        return false;
-    }
-
-    public Naked execute(Action action, Naked[] parameters) {
+    public NakedObject parent() {
         return null;
     }
 
-    public Hint getHint(Action action, Naked[] parameters) {
-        return null;
+    public void setOid(Oid oid) {
+        this.oid = oid;
     }
 
-    public Hint getHint(NakedObjectField field, Naked value) {
-        return null;
+    public void setResolved() {}
+
+    public int size() {
+        return collection.size();
     }
 
-    public boolean isEmpty() {
-        return size() == 0;
+    public void sort() {}
+
+    public String titleString() {
+        return "vector...";
     }
 
     public String toString() {
-        StringBuffer s = new StringBuffer();
+        ToString s = new ToString(this);
+//      s.append(resolveState.code());
 
-        s.append("POJO (collection)");
+
         // datatype
         NakedObjectSpecification spec = getSpecification();
-        s.append(spec == null ? getClass().getName() : spec.getShortName());
+        s.append(spec == null ? "any" : spec.getShortName());
         s.append(" [");
 
         // obect identifier
@@ -197,49 +165,31 @@ public class VectorCollectionAdapter implements InternalCollection {
         s.append("'");
         s.append("]");
 
-        s.append("  " + Long.toHexString(super.hashCode()).toUpperCase());
+        s.append("  #" + Long.toHexString(super.hashCode()).toUpperCase());
 
-        s.append(collection);
+        s.append(collection.toString());
         return s.toString();
 
-        
-        
-    
     }
-
-
-
-    public void clearViewDirty() {}
-
-
-
-    public void sort() {}
-    
-
 
 }
 
-
 /*
-Naked Objects - a framework that exposes behaviourally complete
-business objects directly to the user.
-Copyright (C) 2000 - 2005  Naked Objects Group Ltd
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-The authors can be contacted via www.nakedobjects.org (the
-registered address of Naked Objects Group is Kingsway House, 123 Goldworth
-Road, Woking GU21 1NR, UK).
-*/
+ * Naked Objects - a framework that exposes behaviourally complete business objects directly to the
+ * user. Copyright (C) 2000 - 2005 Naked Objects Group Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ * 
+ * The authors can be contacted via www.nakedobjects.org (the registered address of Naked Objects
+ * Group is Kingsway House, 123 Goldworth Road, Woking GU21 1NR, UK).
+ */
