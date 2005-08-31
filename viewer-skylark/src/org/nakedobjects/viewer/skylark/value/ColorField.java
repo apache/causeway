96d7a8231d5de3433d595cae23ff2491edd53a27
rcm <dev-null@apache.org>
2005-08-31 12:44:56 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1007510 13f79535-47bb-0310-9956-ffa450edef68

--- viewer-skylark/src/org/nakedobjects/viewer/skylark/value/ColorField.java
+++ viewer-skylark/src/org/nakedobjects/viewer/skylark/value/ColorField.java
@@ -18,22 +18,23 @@ import org.nakedobjects.viewer.skylark.core.AbstractFieldSpecification;
 
 
 public class ColorField extends AbstractField {
-
-	private int color;
     public static class Specification extends AbstractFieldSpecification {
-	    public String getName() {
-	        return "Color";
-	    }
 
-	    public View createView(Content content, ViewAxis axis) {
-			return new ColorField(content, this, axis);
-	    }
-	    
-	    public boolean canDisplay(Content content) {
-	    	return content.isValue() && content.getNaked() instanceof ColorValue;
-		}
-	}
-	
+        public boolean canDisplay(Content content) {
+            return content.isValue() && content.getNaked() instanceof ColorValue;
+        }
+
+        public View createView(Content content, ViewAxis axis) {
+            return new ColorField(content, this, axis);
+        }
+
+        public String getName() {
+            return "Color";
+        }
+    }
+
+    private int color;
+
     public ColorField(Content content, ViewSpecification specification, ViewAxis axis) {
         super(content, specification, axis);
     }
@@ -51,30 +52,30 @@ public class ColorField extends AbstractField {
             color = Style.SECONDARY1;
         }
 
-    	int top = 0;
-    	int left = 0;
+        int top = 0;
+        int left = 0;
 
-    	Size size = getSize();
-    	int w = size.getWidth() - 1;
-    	int h = size.getHeight() - 1;
-    	canvas.drawRectangle(left, top, w, h, color);
-     	left++;
-    	top++;
-    	w -= 1;
-    	h -= 1;
+        Size size = getSize();
+        int w = size.getWidth() - 1;
+        int h = size.getHeight() - 1;
+        canvas.drawRectangle(left, top, w, h, color);
+        left++;
+        top++;
+        w -= 1;
+        h -= 1;
         canvas.drawSolidRectangle(left, top, w, h, new Color(getColor()));
     }
 
     public void firstClick(Click click) {
-        if(getContent().getHint().canUse().isAllowed()) {
-	        View overlay = new ColorFieldOverlay(this);
-	        Location location = click.getLocation();
-	        // TODO offset by constant amount
-	        location.move(10, 10);
-			overlay.setLocation(location);
-	        overlay.setSize(overlay.getRequiredSize());
-	        overlay.markDamaged();
-	        getViewManager().setOverlayView(overlay);
+        if (getContent().getHint().canUse().isAllowed()) {
+            View overlay = new ColorFieldOverlay(this);
+            Location location = click.getLocation();
+            // TODO offset by constant amount
+            location.move(10, 10);
+            overlay.setLocation(location);
+            overlay.setSize(overlay.getRequiredSize());
+            overlay.markDamaged();
+            getViewManager().setOverlayView(overlay);
         }
     }
 
@@ -82,62 +83,55 @@ public class ColorField extends AbstractField {
         return VPADDING + Style.NORMAL.getAscent();
     }
 
-    public Size getRequiredSize() {
-		Size size = super.getRequiredSize();
-		size.extendWidth(45);
-        return size; 
-  }
-
     int getColor() {
         ValueField content = ((ValueField) getContent());
         ColorValue value = (ColorValue) content.getObject();
         return value.color();
     }
-/*
-    public void refresh() {
-        ValueField content = ((ValueField) getContent());
-        Value field = content.getValueField();
 
-        if (field.isDerived()) {
-            content.getValue().copyObject((Logical) field.get(
-                    ((ObjectContent) getParent().getContent()).getObject()));
-        }
+    public Size getRequiredSize() {
+        Size size = super.getRequiredSize();
+        size.extendWidth(45);
+        return size;
     }
-*/
-	void setColor(int color) {
-	    this.color = color;
-	    initiateSave();
-	}
-	
-	protected void save() {
-		try {
+
+    protected void save() {
+        try {
             parseEntry("" + color);
         } catch (InvalidEntryException e) {
             throw new NotImplementedException();
         }
-	}
+    }
+
+    /*
+     * public void refresh() { ValueField content = ((ValueField) getContent()); Value field =
+     * content.getValueField();
+     * 
+     * if (field.isDerived()) { content.getValue().copyObject((Logical) field.get( ((ObjectContent)
+     * getParent().getContent()).getObject())); } }
+     */
+    void setColor(int color) {
+        this.color = color;
+        initiateSave();
+    }
 }
 
 /*
-Naked Objects - a framework that exposes behaviourally complete
-business objects directly to the user.
-Copyright (C) 2000 - 2005  Naked Objects Group Ltd
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-The authors can be contacted via www.nakedobjects.org (the
-registered address of Naked Objects Group is Kingsway House, 123 Goldworth
-Road, Woking GU21 1NR, UK).
-*/
+ * Naked Objects - a framework that exposes behaviourally complete business objects directly to the
+ * user. Copyright (C) 2000 - 2005 Naked Objects Group Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ * 
+ * The authors can be contacted via www.nakedobjects.org (the registered address of Naked Objects
+ * Group is Kingsway House, 123 Goldworth Road, Woking GU21 1NR, UK).
+ */
