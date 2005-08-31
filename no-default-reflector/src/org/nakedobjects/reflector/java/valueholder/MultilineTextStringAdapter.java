0b215cfdc466313db9aeb2adc0f482a7ddc43d7d
rcm <dev-null@apache.org>
2005-08-31 12:30:29 +0000

    Added multi-line string and password string adapters.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1007494 13f79535-47bb-0310-9956-ffa450edef68

---
+++ no-default-reflector/src/org/nakedobjects/reflector/java/valueholder/MultilineTextStringAdapter.java
@@ -0,0 +1,103 @@
+package org.nakedobjects.reflector.java.valueholder;
+
+import org.nakedobjects.application.valueholder.MultilineTextString;
+import org.nakedobjects.object.reflect.valueadapter.AbstractNakedValue;
+import org.nakedobjects.object.value.MultilineStringValue;
+import org.nakedobjects.object.value.StringValue;
+import org.nakedobjects.utility.ToString;
+
+public class MultilineTextStringAdapter extends AbstractNakedValue implements MultilineStringValue {
+    private final MultilineTextString adaptee;
+    
+    public MultilineTextStringAdapter(final MultilineTextString adaptee) {
+        this.adaptee = adaptee;
+    }
+    
+    public void parseTextEntry(String text) {
+        adaptee.setValue(text);
+    }
+
+    public byte[] asEncodedString() {
+        return adaptee.asEncodedString().getBytes();
+    }
+
+    public void restoreFromEncodedString(byte[] data) {
+        String text = new String(data);
+        adaptee.restoreFromEncodedString(text);
+    }
+
+    public Object getObject() {
+        return adaptee;
+    }
+ 
+    public String getIconName() {
+        return "text";
+    }
+    
+    public String toString() {
+        ToString str = new ToString(this);
+        str.append("string", adaptee.stringValue());
+        return str.toString();
+    }
+
+    public String titleString() {
+        return adaptee.titleString();
+    }
+
+    public String getValueClass() {
+        return adaptee.getClass().getName();
+    }
+    
+    public int getMinumumLength() {
+        return adaptee.getMinimumLength();
+    }
+    
+    public int getMaximumLength() {
+        return adaptee.getMaximumLength();
+    }
+
+    public String stringValue() {
+        return adaptee.stringValue();
+    }
+
+    public void setValue(String value) {
+        adaptee.setValue(value);
+    }
+    
+    public boolean canClear() {
+        return true;
+    }
+
+    public void clear() {
+        adaptee.clear();
+    }
+    
+    public boolean isEmpty() {
+        return adaptee.isEmpty();
+    }
+}
+
+
+/*
+Naked Objects - a framework that exposes behaviourally complete
+business objects directly to the user.
+Copyright (C) 2000 - 2005  Naked Objects Group Ltd
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+The authors can be contacted via www.nakedobjects.org (the
+registered address of Naked Objects Group is Kingsway House, 123 Goldworth
+Road, Woking GU21 1NR, UK).
+*/
