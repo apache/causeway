de6b7bb408a7e1a2e2557d45a7122fe2fbe160f5
rcm <dev-null@apache.org>
2005-09-23 18:54:14 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1007704 13f79535-47bb-0310-9956-ffa450edef68

--- no-core/src/org/nakedobjects/object/persistence/defaults/LocalObjectManager.java
+++ no-core/src/org/nakedobjects/object/persistence/defaults/LocalObjectManager.java
@@ -6,7 +6,6 @@ import org.nakedobjects.object.DirtyObjectSetImpl;
 import org.nakedobjects.object.NakedClass;
 import org.nakedobjects.object.NakedObject;
 import org.nakedobjects.object.NakedObjectLoader;
-import org.nakedobjects.object.NakedObjectRuntimeException;
 import org.nakedobjects.object.NakedObjectSpecification;
 import org.nakedobjects.object.NakedReference;
 import org.nakedobjects.object.NullDirtyObjectSet;
@@ -63,9 +62,8 @@ public class LocalObjectManager extends AbstractNakedObjectManager implements Pe
     }
 
     /**
-     * Removes all the data from the specified object. All associations are set
-     * to nulll; values have clear() call on them; and internal collections are
-     * reset so they have zero elements
+     * Removes all the data from the specified object. All associations are set to nulll; values have clear()
+     * call on them; and internal collections are reset so they have zero elements
      */
     private void clear(NakedObject object) {
         NakedObjectField[] fields = object.getSpecification().getFields();
@@ -88,11 +86,11 @@ public class LocalObjectManager extends AbstractNakedObjectManager implements Pe
 
     public void createObject(NakedObject object) throws ObjectManagerException {
         getTransaction().addCommand(objectStore.createCreateObjectCommand(object));
-     }
+    }
 
     /**
-     * Removes the specified object from the system. The specified object's data
-     * should be removed from the persistence mechanism.
+     * Removes the specified object from the system. The specified object's data should be removed from the
+     * persistence mechanism.
      */
     public void destroyObject(NakedObject object) {
         LOG.info("destroyObject " + object);
@@ -105,7 +103,7 @@ public class LocalObjectManager extends AbstractNakedObjectManager implements Pe
         loader().end(object);
 
         // TODO need to do garbage collection instead
-        //loader().unloaded(object);
+        // loader().unloaded(object);
     }
 
     private NakedObjectLoader loader() {
@@ -114,15 +112,11 @@ public class LocalObjectManager extends AbstractNakedObjectManager implements Pe
 
     public void endTransaction() {
         transactionLevel--;
-        if(transactionLevel == 0) {
-	        try {
-	        	// TODO collate changes before committing
-	            saveChanges();
-	            getTransaction().commit(objectStore);
-	            transaction = null;
-	        } catch (ObjectManagerException e) {
-	            throw new NakedObjectRuntimeException(e);
-	        }
+        if (transactionLevel == 0) {
+            // TODO collate changes before committing
+            saveChanges();
+            getTransaction().commit(objectStore);
+            transaction = null;
         }
     }
 
@@ -143,25 +137,16 @@ public class LocalObjectManager extends AbstractNakedObjectManager implements Pe
 
     protected NakedObject[] getInstances(InstancesCriteria criteria) {
         LOG.info("getInstances matching " + criteria);
-        try {
-            NakedObject[] instances = objectStore.getInstances(criteria);
-            collateChanges();
-            //objectsToBeSaved.remove(instances);
-            return instances;
-        } catch (ObjectManagerException e) {
-            throw new NakedObjectRuntimeException(e);
-        }
+        NakedObject[] instances = objectStore.getInstances(criteria);
+        collateChanges();
+        return instances;
     }
 
     protected NakedObject[] getInstances(NakedObjectSpecification specification, boolean includeSubclasses) {
         LOG.info("getInstances of " + specification.getShortName());
-        try {
-            NakedObject[] instances = objectStore.getInstances(specification, false);
-            collateChanges();
-            return instances;
-        } catch (ObjectManagerException e) {
-            throw new NakedObjectRuntimeException(e);
-        }
+        NakedObject[] instances = objectStore.getInstances(specification, false);
+        collateChanges();
+        return instances;
     }
 
     public NakedClass getNakedClass(NakedObjectSpecification specification) {
@@ -171,11 +156,9 @@ public class LocalObjectManager extends AbstractNakedObjectManager implements Pe
 
         NakedClass spec;
         try {
-            spec = objectStore.getNakedClass(specification.getFullName());
+        spec = objectStore.getNakedClass(specification.getFullName());
         } catch (ObjectNotFoundException e) {
             spec = new NakedClass(specification.getFullName());
-        } catch (ObjectManagerException e) {
-            throw new NakedObjectRuntimeException(e);
         }
         nakedClasses.put(specification, spec);
         return spec;
@@ -184,21 +167,20 @@ public class LocalObjectManager extends AbstractNakedObjectManager implements Pe
     public NakedObject getObject(Oid oid, NakedObjectSpecification specification) {
         Assert.assertNotNull("needs an OID", oid);
         Assert.assertNotNull("needs a specification", specification);
-        
+
         NakedObject object;
         /*
-		if(oid == null) {
-		    object = NakedObjects.getObjectLoader().createTransientInstance(objectType);
-		} else
-		    */
-        if(NakedObjects.getObjectLoader().isIdentityKnown(oid)) {
-		    object = NakedObjects.getObjectLoader().getAdapterFor(oid);
-		} else {
-		    object = objectStore.getObject(oid, specification);
-		}
+         * if(oid == null) { object = NakedObjects.getObjectLoader().createTransientInstance(objectType); }
+         * else
+         */
+        if (NakedObjects.getObjectLoader().isIdentityKnown(oid)) {
+            object = NakedObjects.getObjectLoader().getAdapterFor(oid);
+        } else {
+            object = objectStore.getObject(oid, specification);
+        }
         return object;
     }
-    
+
     private Transaction getTransaction() {
         if (transaction == null) {
             throw new TransactionException("No transaction started");
@@ -207,23 +189,18 @@ public class LocalObjectManager extends AbstractNakedObjectManager implements Pe
     }
 
     /**
-     * Checks whether there are any instances of the specified type. The object
-     * store should look for instances of the type represented by <variable>type
-     * </variable> and return <code>true</code> if there are, or
-     * <code>false</code> if there are not.
+     * Checks whether there are any instances of the specified type. The object store should look for
+     * instances of the type represented by <variable>type </variable> and return <code>true</code> if there
+     * are, or <code>false</code> if there are not.
      */
     public boolean hasInstances(NakedObjectSpecification specification) {
         LOG.info("hasInstances of " + specification.getShortName());
-        try {
-            return objectStore.hasInstances(specification, false);
-        } catch (ObjectManagerException e) {
-            throw new NakedObjectRuntimeException(e);
-        }
+        return objectStore.hasInstances(specification, false);
     }
 
     /**
-     * Initialize the object store so that calls to this object store access
-     * persisted objects and persist changes to the object that are saved.
+     * Initialize the object store so that calls to this object store access persisted objects and persist
+     * changes to the object that are saved.
      */
     public void init() throws StartupException {
         Assert.assertNotNull("persist algorithm required", persistAlgorithm);
@@ -237,90 +214,59 @@ public class LocalObjectManager extends AbstractNakedObjectManager implements Pe
     }
 
     /**
-     * Makes a naked object persistent. The specified object should be stored
-     * away via this object store's persistence mechanism, and have an new and
-     * unique OID assigned to it (by calling the object's <code>setOid</code>
-     * method). The object, should also be added to the cache as the object is
+     * Makes a naked object persistent. The specified object should be stored away via this object store's
+     * persistence mechanism, and have an new and unique OID assigned to it (by calling the object's
+     * <code>setOid</code> method). The object, should also be added to the cache as the object is
      * implicitly 'in use'.
      * 
      * <p>
-     * If the object has any associations then each of these, where they aren't
-     * already persistent, should also be made persistent by recursively calling
-     * this method.
+     * If the object has any associations then each of these, where they aren't already persistent, should
+     * also be made persistent by recursively calling this method.
      * </p>
      * 
      * <p>
-     * If the object to be persisted is a collection, then each element of that
-     * collection, that is not already persistent, should be made persistent by
-     * recursively calling this method.
+     * If the object to be persisted is a collection, then each element of that collection, that is not
+     * already persistent, should be made persistent by recursively calling this method.
      * </p>
-     *  
+     * 
      */
     public void makePersistent(NakedObject object) {
         if (isPersistent(object)) {
             throw new NotPersistableException("Object already persistent");
         }
-        if(object.persistable() == Persistable.TRANSIENT) {
+        if (object.persistable() == Persistable.TRANSIENT) {
             throw new NotPersistableException("Object must be transient");
-       }
- 
+        }
+
         persistAlgorithm.makePersistent(object, this);
     }
+
     /*
-    private void persist(NakedObject object) {
-        if(object.getResolveState().isPersistent() || object.getSpecification().persistable() == Persistable.TRANSIENT) {
-            return;
-        }
-            
-        LOG.info("persist " + object);
-        loader().madePersistent(object, createOid(object));
-        
-
-        NakedObjectField[] fields = object.getFields();
-        for (int i = 0; i < fields.length; i++) {
-            NakedObjectField field = fields[i];
-            if (field.isDerived()) {
-                continue;
-            } else if (field.isValue()) {
-                continue;
-            } else if (field instanceof OneToManyAssociation) {
-                InternalCollection collection = (InternalCollection) object.getField(field);
-                collection.setOid(createOid(collection));
-                collection.setResolved();
-                for (int j = 0; j < collection.size(); j++) {
-                    persist(collection.elementAt(j));
-                }
-            } else {
-                Object fieldValue = object.getField(field);
-                if (fieldValue == null) {
-                    continue;
-                }
-                if (!(fieldValue instanceof NakedObject)) {
-                    throw new NakedObjectRuntimeException();
-                }
-                persist((NakedObject) fieldValue);
-            }
-        }
-
-        try {
-            createObject(object);
-        } catch (ObjectStoreException e) {
-            throw new NakedObjectRuntimeException(e);
-        }
-    }
-        */
+     * private void persist(NakedObject object) { if(object.getResolveState().isPersistent() ||
+     * object.getSpecification().persistable() == Persistable.TRANSIENT) { return; }
+     * 
+     * LOG.info("persist " + object); loader().madePersistent(object, createOid(object));
+     * 
+     * 
+     * NakedObjectField[] fields = object.getFields(); for (int i = 0; i < fields.length; i++) {
+     * NakedObjectField field = fields[i]; if (field.isDerived()) { continue; } else if (field.isValue()) {
+     * continue; } else if (field instanceof OneToManyAssociation) { InternalCollection collection =
+     * (InternalCollection) object.getField(field); collection.setOid(createOid(collection));
+     * collection.setResolved(); for (int j = 0; j < collection.size(); j++) {
+     * persist(collection.elementAt(j)); } } else { Object fieldValue = object.getField(field); if (fieldValue ==
+     * null) { continue; } if (!(fieldValue instanceof NakedObject)) { throw new
+     * NakedObjectRuntimeException(); } persist((NakedObject) fieldValue); } }
+     * 
+     * try { createObject(object); } catch (ObjectStoreException e) { throw new
+     * NakedObjectRuntimeException(e); } }
+     */
 
     /**
      * A count of the number of instances matching the specified pattern.
      */
     public int numberOfInstances(NakedObjectSpecification specification) {
         LOG.info("numberOfInstances like " + specification.getShortName());
-        try {
-            return objectStore.numberOfInstances(specification, false);
-        } catch (ObjectManagerException e) {
-            throw new NakedObjectRuntimeException(e);
-        }
-
+        return objectStore.numberOfInstances(specification, false);
     }
 
     public void objectChanged(NakedObject object) {
@@ -329,38 +275,37 @@ public class LocalObjectManager extends AbstractNakedObjectManager implements Pe
             objectsToRefreshViewsFor.addDirty(object);
         }
     }
-  
+
     public void reset() {
         objectStore.reset();
     }
 
     public void resolveField(NakedObject object, NakedObjectField field) {
-        if(field.isValue()) {
+        if (field.isValue()) {
             return;
         }
         NakedReference reference = (NakedReference) object.getField(field);
-        if(reference.getResolveState().isResolved()) {
+        if (reference.getResolveState().isResolved()) {
             return;
         }
-        if (! reference.getResolveState().isPersistent()) {
+        if (!reference.getResolveState().isPersistent()) {
             return;
         }
-        
+
         LOG.info("resolve-field" + object + "/" + field.getName());
         objectStore.resolveField(object, field);
     }
-    
-    public void reload(NakedObject object) {}
 
+    public void reload(NakedObject object) {}
 
     public void resolveImmediately(NakedObject object) {
         ResolveState resolveState = object.getResolveState();
         if (resolveState.isResolvable(ResolveState.RESOLVING)) {
-	        Assert.assertFalse("only resolve object that is not yet resolved", object, object.getResolveState().isResolved());
-	        Assert.assertTrue("only resolve object that is persistent", object, object.getResolveState().isPersistent());
-	
-	        LOG.info("resolve-immediately: " + object);
-	        objectStore.resolveImmediately(object);
+            Assert.assertFalse("only resolve object that is not yet resolved", object, object.getResolveState().isResolved());
+            Assert.assertTrue("only resolve object that is persistent", object, object.getResolveState().isPersistent());
+
+            LOG.info("resolve-immediately: " + object);
+            objectStore.resolveImmediately(object);
         }
     }
 
@@ -422,44 +367,39 @@ public class LocalObjectManager extends AbstractNakedObjectManager implements Pe
     }
 
     public void shutdown() {
-        try {
-            LOG.info("shutting down " + this);
-            if (transaction != null) {
-                try {
-                    abortTransaction();
-                } catch (Exception e2) {
-                    LOG.error("failure during abort", e2);
-                }
-    		}
-            objectsToBeSaved.shutdown();
-            objectsToRefreshViewsFor.shutdown();
-             objectStore.shutdown();
-            objectStore = null;
-            nakedClasses.clear();
-        } catch (ObjectManagerException e) {
-            throw new NakedObjectRuntimeException(e);
+        LOG.info("shutting down " + this);
+        if (transaction != null) {
+            try {
+                abortTransaction();
+            } catch (Exception e2) {
+                LOG.error("failure during abort", e2);
+            }
         }
+        objectsToBeSaved.shutdown();
+        objectsToRefreshViewsFor.shutdown();
+        objectStore.shutdown();
+        objectStore = null;
+        nakedClasses.clear();
     }
 
     public void startTransaction() {
-        if(transaction == null) {
+        if (transaction == null) {
             transaction = new Transaction();
             transactionLevel = 0;
-        } 
+        }
         transactionLevel++;
     }
 
     public String toString() {
         ToString toString = new ToString(this);
-        if(objectStore != null) {
+        if (objectStore != null) {
             toString.append("objectStore", objectStore.name());
         }
-        if(persistAlgorithm != null) {
+        if (persistAlgorithm != null) {
             toString.append("oidGenerator", persistAlgorithm.name());
         }
         return toString.toString();
     }
-
 
     /**
      * Expose as a .NET property
@@ -474,34 +414,27 @@ public class LocalObjectManager extends AbstractNakedObjectManager implements Pe
         this.persistAlgorithm = persistAlgorithm;
     }
 
-    
     public void tempResetDirty() {
         objectsToBeSaved.dirtyObjects();
     }
-    
-    
+
 }
 
 /*
- * Naked Objects - a framework that exposes behaviourally complete business
- * objects directly to the user. Copyright (C) 2000 - 2005 Naked Objects Group
- * Ltd
+ * Naked Objects - a framework that exposes behaviourally complete business objects directly to the user.
+ * Copyright (C) 2000 - 2005 Naked Objects Group Ltd
  * 
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
  * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
  * 
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  * 
- * The authors can be contacted via www.nakedobjects.org (the registered address
- * of Naked Objects Group is Kingsway House, 123 Goldworth Road, Woking GU21
- * 1NR, UK).
+ * The authors can be contacted via www.nakedobjects.org (the registered address of Naked Objects Group is
+ * Kingsway House, 123 Goldworth Road, Woking GU21 1NR, UK).
  */
