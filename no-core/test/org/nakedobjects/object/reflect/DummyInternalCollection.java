69a5fbbb9637d8990f748e861141f37e235d3f67
rcm <dev-null@apache.org>
2005-09-27 14:58:37 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1007715 13f79535-47bb-0310-9956-ffa450edef68

---
+++ no-core/test/org/nakedobjects/object/reflect/DummyInternalCollection.java
@@ -0,0 +1,114 @@
+package org.nakedobjects.object.reflect;
+
+import org.nakedobjects.object.DummyNakedObjectSpecification;
+import org.nakedobjects.object.NakedCollection;
+import org.nakedobjects.object.NakedObject;
+import org.nakedobjects.object.NakedObjectSpecification;
+import org.nakedobjects.object.Persistable;
+import org.nakedobjects.object.ResolveState;
+import org.nakedobjects.object.defaults.AbstractNakedReference;
+import org.nakedobjects.object.persistence.Oid;
+import org.nakedobjects.utility.Assert;
+
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Vector;
+
+
+public class DummyInternalCollection extends AbstractNakedReference implements NakedCollection {
+    private Vector collection = new Vector();
+    private Oid oid;
+    private NakedObjectSpecification specification;
+
+    public DummyInternalCollection() {}
+
+    public DummyInternalCollection(Vector vector) {
+        this.collection = vector;
+    }
+
+    public void checkLock(long version) {}
+
+    public boolean contains(NakedObject object) {
+        return collection.contains(object);
+    }
+
+    public void debugClearResolved() {}
+
+    public void destroyed() {}
+
+    public NakedObject elementAt(int index) {
+        return (NakedObject) collection.elementAt(index);
+    }
+
+    public Enumeration elements() {
+        return collection.elements();
+    }
+
+    public String getIconName() {
+        return null;
+    }
+
+    public Object getObject() {
+        return collection;
+    }
+
+    public Oid getOid() {
+        return oid;
+    }
+
+    public ResolveState getResolveState() {
+        return null;
+    }
+
+    public NakedObjectSpecification getSpecification() {
+        return specification;
+    }
+
+    public long getVersion() {
+        return 0;
+    }
+
+    public void init(Object[] initElements) {
+        Assert.assertEquals("Collection not empty", 0, this.collection.size());
+        for (int i = 0; i < initElements.length; i++) {
+            collection.addElement(initElements[i]);
+        }
+    }
+
+    public Persistable persistable() {
+        return Persistable.USER_PERSISTABLE;
+    }
+
+    public void setOptimisticLock(long version, String user, Date time) {}
+
+    public void setupSpecification(NakedObjectSpecification specification) {
+        this.specification = specification;
+    }
+
+    public int size() {
+        return collection.size();
+    }
+
+    public String titleString() {
+        return "title";
+    }
+}
+
+/*
+ * Naked Objects - a framework that exposes behaviourally complete business objects directly to the user.
+ * Copyright (C) 2000 - 2005 Naked Objects Group Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * The authors can be contacted via www.nakedobjects.org (the registered address of Naked Objects Group is
+ * Kingsway House, 123 Goldworth Road, Woking GU21 1NR, UK).
+ */
