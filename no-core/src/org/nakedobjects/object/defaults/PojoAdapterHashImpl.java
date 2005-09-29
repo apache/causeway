8c3e90e1dced7910add4cf31076f6e63e2dbfabc
rcm <dev-null@apache.org>
2005-09-29 13:40:44 +0000

    Added logging to show changes to the adapter hash
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1007746 13f79535-47bb-0310-9956-ffa450edef68

--- no-core/src/org/nakedobjects/object/defaults/PojoAdapterHashImpl.java
+++ no-core/src/org/nakedobjects/object/defaults/PojoAdapterHashImpl.java
@@ -12,9 +12,11 @@ import org.apache.log4j.Logger;
 
 
 public class PojoAdapterHashImpl implements PojoAdapterHash {
+    private static final Logger LOG = Logger.getLogger(PojoAdapterHashImpl.class);
     protected Hashtable pojos = new Hashtable();
 
     public void add(Object pojo, Naked adapter) {
+        LOG.debug("add " + pojo + " as " + adapter);
         pojos.put(pojo, adapter);
     }
 
@@ -24,7 +26,7 @@ public class PojoAdapterHashImpl implements PojoAdapterHash {
 
     protected void finalize() throws Throwable {
         super.finalize();
-        Logger.getLogger(PojoAdapterHashImpl.class).info("finalizing hash of pojos");
+        LOG.info("finalizing hash of pojos");
     }
 
     public String getDebugData() {
@@ -52,38 +54,36 @@ public class PojoAdapterHashImpl implements PojoAdapterHash {
     }
 
     public void reset() {
+        LOG.debug("reset");
         pojos.clear();
     }
 
     public void shutdown() {
+        LOG.debug("shutdown");
         pojos.clear();
     }
 
     public void remove(NakedObject object) {
+        LOG.debug("remove " + object);
         pojos.remove(object.getObject());
     }
 }
 
 /*
- * Naked Objects - a framework that exposes behaviourally complete business
- * objects directly to the user. Copyright (C) 2000 - 2005 Naked Objects Group
- * Ltd
+ * Naked Objects - a framework that exposes behaviourally complete business objects directly to the user.
+ * Copyright (C) 2000 - 2005 Naked Objects Group Ltd
  * 
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
  * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
  * 
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  * 
- * The authors can be contacted via www.nakedobjects.org (the registered address
- * of Naked Objects Group is Kingsway House, 123 Goldworth Road, Woking GU21
- * 1NR, UK).
+ * The authors can be contacted via www.nakedobjects.org (the registered address of Naked Objects Group is
+ * Kingsway House, 123 Goldworth Road, Woking GU21 1NR, UK).
  */
