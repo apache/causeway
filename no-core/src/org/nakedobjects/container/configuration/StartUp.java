2093043810591db630301077c807441208b9f594
rcm <dev-null@apache.org>
2005-10-10 12:23:55 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1007790 13f79535-47bb-0310-9956-ffa450edef68

--- no-core/src/org/nakedobjects/container/configuration/StartUp.java
+++ no-core/src/org/nakedobjects/container/configuration/StartUp.java
@@ -11,7 +11,6 @@ import java.lang.reflect.Method;
 import java.util.Enumeration;
 import java.util.Properties;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -21,6 +20,7 @@ import org.apache.log4j.PropertyConfigurator;
 public class StartUp {
     private static final Logger LOG = Logger.getLogger(StartUp.class);
     private static final String CONTAINER = "container";
+    private static final String VIEWER = "viewer";
 
     public static void main(String[] args) {
         LogManager.getRootLogger().setLevel(Level.OFF);
@@ -35,31 +35,44 @@ public class StartUp {
         AboutNakedObjects.logVersion();
 
         LOG.debug("Configuring system using " + name);
-        
-        
-        String containerName = configuration.getString(CONTAINER);
-        LOG.debug("loading core component " + containerName);
-        NakedObjects container = (NakedObjects) loadComponent(containerName, NakedObjects.class);
-        setProperty(container, "configuration", configuration);
-        setProperties(container, "nakedobjects." + CONTAINER, properties);
 
-        container.init();
+
+        try {
+            String containerName = configuration.getString(CONTAINER);
+            LOG.debug("loading core component " + containerName);
+            NakedObjects container = (NakedObjects) loadComponent(containerName, NakedObjects.class);
+            setProperty(container, "configuration", configuration);
+            setProperties(container, "nakedobjects." + CONTAINER, properties);
+            
+            
+            // TODO need to load viewer, server listener etc
+    //        setProperties(container, "nakedobjects." + VIEWER, properties);
+      //      loadComponent(containerName, NakedObjects.class);
+            
+            container.init();
+        } catch(StartupException e) {
+            LOG.error("Failed to start NOF: " + e.getMessage(), e);
+        }
     }
 
     private static void setProperties(Object object, String prefix, Properties properties) {
+        LOG.debug("looking for properties starting with " + prefix);
         Enumeration e = properties.propertyNames();
         while (e.hasMoreElements()) {
             String key = (String) e.nextElement();
-            if (key.startsWith(prefix) && key.length() > prefix.length()) {
-      /*          if(key.substring(0, prefix.length() + 1).)
-                {
-                    
+            if (key.startsWith(prefix) && key.length() > prefix.length() && key.substring(prefix.length() + 1).indexOf('.') == -1) {
+                LOG.debug("  property " + key + " (of prefix " + prefix + ")");
+                String className = properties.getProperty(key).trim();
+                
+                Object value;
+                if(className.equalsIgnoreCase("true")) {
+                   value = Boolean.valueOf(true);
+                } else if (className.equalsIgnoreCase("false")) {
+                    value = Boolean.valueOf(false);
+                } else {
+                    value = load(className, key, properties);
                 }
-         */       
-                String className = properties.getProperty(key);
-                Object field = load(className, prefix + "." + key, properties);
-
-                setProperty(object, key, field);
+                setProperty(object, key, value);
             }
         }
     }
@@ -67,26 +80,25 @@ public class StartUp {
     private static void setProperty(Object object, String fieldName, Object value) {
         String field = fieldName.substring(fieldName.lastIndexOf(".") + 1);
         field = Character.toUpperCase(field.charAt(0)) + field.substring(1);
-        LOG.debug("setting " + field + " on " + object);
+        LOG.debug("    setting " + field + " on " + object);
 
         Class c = object.getClass();
-        LOG.debug("  getting set method for " + field + "/" +  value.getClass());
-        Method setter;
+        LOG.debug("    getting set method for " + field);
+        Method setter = null;
         try {
             PropertyDescriptor property = new PropertyDescriptor(field, c, null, "set" + field);
             setter = property.getWriteMethod();
             
             //setter = c.getMethod("set" + field, new Class[] { value.getClass() });
             setter.invoke(object, new Object[] { value });
+            LOG.debug("  set " + field + " with " +  value.getClass());
         } catch (SecurityException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
-         } catch (IllegalArgumentException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (IllegalAccessException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
+         } catch (IllegalArgumentException e) {
+             throw new StartupException(e.getMessage() + ": can't invoke " + setter.getName() + " with instance of " + value.getClass().getName());
+        } catch (IllegalAccessException e) {
+            throw new StartupException(e.getMessage() + ": can't access " + setter.getName());
         } catch (InvocationTargetException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
@@ -96,10 +108,10 @@ public class StartUp {
         }
     }
 
-    private static Object load(String key, String string, Properties properties) {
-        LOG.debug("loading component " + key);
-        Object object = loadComponent(key);
-        setProperties(object, key, properties);
+    private static Object load(String className, String prefix, Properties properties) {
+        LOG.debug("loading component " + className + " for " + prefix);
+        Object object = loadComponent(className);
+        setProperties(object, prefix, properties);
         return object;
     }
 
