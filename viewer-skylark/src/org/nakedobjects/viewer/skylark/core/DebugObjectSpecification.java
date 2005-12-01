2a0aace616e35a04340a4dc7a400136f48f5d384
rcm <dev-null@apache.org>
2005-12-01 11:56:13 +0000

    Split out the debug details, and provided one method to view them all.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1009490 13f79535-47bb-0310-9956-ffa450edef68

---
+++ viewer-skylark/src/org/nakedobjects/viewer/skylark/core/DebugObjectSpecification.java
@@ -0,0 +1,62 @@
+package org.nakedobjects.viewer.skylark.core;
+
+import org.nakedobjects.object.Dump;
+import org.nakedobjects.object.Naked;
+import org.nakedobjects.object.NakedCollection;
+import org.nakedobjects.object.NakedObject;
+import org.nakedobjects.utility.DebugInfo;
+import org.nakedobjects.utility.DebugString;
+
+
+public class DebugObjectSpecification implements DebugInfo {
+    private final Naked object;
+
+    public DebugObjectSpecification(final Naked object) {
+        this.object = object;
+    }
+
+    public String getDebugData() {
+        DebugString debug = new DebugString();
+
+        if (object instanceof NakedObject) {
+            NakedObject obj = (NakedObject) object;
+            dumpObject(obj, debug);
+
+        } else if (object instanceof NakedCollection) {
+            NakedCollection collection = (NakedCollection) object;
+            dumpObject(collection, debug);
+        }
+        
+        return debug.toString();
+    }
+
+    public String getDebugTitle() {
+        return "Object Specification";
+    }
+
+    private void dumpObject(final Naked object, DebugString info) {
+        if (object != null) {
+            Dump.specification(object, info);
+        }
+    }
+}
+
+/*
+ * Naked Objects - a framework that exposes behaviourally complete business objects directly to the
+ * user. Copyright (C) 2000 - 2005 Naked Objects Group Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ * 
+ * The authors can be contacted via www.nakedobjects.org (the registered address of Naked Objects
+ * Group is Kingsway House, 123 Goldworth Road, Woking GU21 1NR, UK).
+ */
