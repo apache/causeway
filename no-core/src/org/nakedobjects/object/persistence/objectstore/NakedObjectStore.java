e2a87be6dfbfcb97e304428434ddc5b24a73e44f
rcm <dev-null@apache.org>
2005-12-05 16:04:43 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1009562 13f79535-47bb-0310-9956-ffa450edef68

--- no-core/src/org/nakedobjects/object/persistence/objectstore/NakedObjectStore.java
+++ no-core/src/org/nakedobjects/object/persistence/objectstore/NakedObjectStore.java
@@ -10,7 +10,6 @@ import org.nakedobjects.object.NakedObjectsComponent;
 import org.nakedobjects.object.ObjectNotFoundException;
 import org.nakedobjects.object.ObjectPerstsistenceException;
 import org.nakedobjects.object.Oid;
-import org.nakedobjects.object.UnsupportedFindException;
 import org.nakedobjects.object.transaction.CreateObjectCommand;
 import org.nakedobjects.object.transaction.DestroyObjectCommand;
 import org.nakedobjects.object.transaction.PersistenceCommand;
