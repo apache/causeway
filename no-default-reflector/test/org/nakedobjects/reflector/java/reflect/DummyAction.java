477835fc5c6673ea2c546beba4d5a1e05f73df1f
rcm <dev-null@apache.org>
2005-12-08 11:13:09 +0000

    Added flag to Action interface to indicate an action runs on the instance rather than the class
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1009704 13f79535-47bb-0310-9956-ffa450edef68

--- no-default-reflector/test/org/nakedobjects/reflector/java/reflect/DummyAction.java
+++ no-default-reflector/test/org/nakedobjects/reflector/java/reflect/DummyAction.java
@@ -9,6 +9,7 @@ import org.nakedobjects.object.control.Consent;
 import org.nakedobjects.object.reflect.ActionPeer;
 import org.nakedobjects.object.reflect.MemberIdentifier;
 
+
 class DummyAction implements Action {
 
     private final ActionPeer peer;
@@ -17,48 +18,24 @@ class DummyAction implements Action {
         this.peer = peer;
     }
 
-    public int getParameterCount() {
-        return 0;
+    public Naked execute(NakedObject object, Naked[] parameters) {
+        return null;
     }
 
-    public Type getType() {
-        return peer.getType();
-    }
-
-    public Target getTarget() {
-        return peer.getTarget();
+    public String getDescription() {
+        return null;
     }
 
     public Object getExtension(Class cls) {
         return null;
     }
-    
+
     public Class[] getExtensions() {
         return new Class[0];
     }
 
-    public boolean hasReturn() {
-        return false;
-    }
-
-    public NakedObjectSpecification[] getParameterTypes() {
-        return null;
-    }
-
-    public Naked[] parameterStubs() {
-        return null;
-    }
-
-    public NakedObjectSpecification getReturnType() {
-        return null;
-    }
-
-    public ActionParameterSet getParameterSet(NakedObject object) {
-        return null;
-    }
-
-    public Naked execute(NakedObject object, Naked[] parameters) {
-        return null;
+    public String getId() {
+        return peer.getIdentifier().getName();
     }
 
     public MemberIdentifier getIdentifier() {
@@ -69,23 +46,35 @@ class DummyAction implements Action {
         return null;
     }
 
-    public String getId() {
-        return peer.getIdentifier().getName();
+    public int getParameterCount() {
+        return 0;
     }
 
-    public Consent isUsable(NakedObject target) {
+    public ActionParameterSet getParameterSet(NakedObject object) {
         return null;
+    }
+
+    public NakedObjectSpecification[] getParameterTypes() {
+        return null;
+    }
+
+    public NakedObjectSpecification getReturnType() {
+        return null;
+    }
+
+    public Target getTarget() {
+        return peer.getTarget();
+    }
+
+    public Type getType() {
+        return peer.getType();
+    }
+
+    public boolean hasReturn() {
+        return false;
     }
 
     public Consent hasValidParameters(NakedObject object, Naked[] parameters) {
-        return null;
-    }
-
-    public String getDescription() {
-        return null;
-    }
-
-    public Consent isVisible(NakedObject target) {
         return null;
     }
 
@@ -93,8 +82,23 @@ class DummyAction implements Action {
         return false;
     }
 
-}
+    public boolean isOnInstance() {
+        return true;
+    }
 
+    public Consent isUsable(NakedObject target) {
+        return null;
+    }
+
+    public Consent isVisible(NakedObject target) {
+        return null;
+    }
+
+    public Naked[] parameterStubs() {
+        return null;
+    }
+
+}
 
 /*
  * Naked Objects - a framework that exposes behaviourally complete business objects directly to the user.
