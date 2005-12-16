742f05bdbdb2adecd8b86e5e216cd0b20cb7f2dc
rcm <dev-null@apache.org>
2005-12-16 14:04:10 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1009925 13f79535-47bb-0310-9956-ffa450edef68

--- no-distribution-command/test/org/nakedobjects/distribution/ObjectEncoderTest.java
+++ no-distribution-command/test/org/nakedobjects/distribution/ObjectEncoderTest.java
@@ -99,14 +99,13 @@ public class ObjectEncoderTest extends TestCase {
         field1_1.setupResolveState(ResolveState.RESOLVED);
         field1_1.setupSpecification(field1_1Specification);
         field1.setupFieldValue("field5", field1_1);
-        
+
         // value field
         field2 = new DummyNakedValue();
         value = new Date();
         field2.setupObject(value);
         valueSpecification = field2.getSpecification();
 
-        
         // collection field - with two elements
         DummyNakedObjectSpecification element1Specification = new DummyNakedObjectSpecification();
         element1Specification.fields = new NakedObjectField[1];
@@ -114,26 +113,25 @@ public class ObjectEncoderTest extends TestCase {
         element1.setupVersion(new DummyVersion(17));
         element1.setupResolveState(ResolveState.RESOLVED);
         element1.setupSpecification(element1Specification);
-        
+
         DummyNakedObjectSpecification elementSpecification = new DummyNakedObjectSpecification();
         element2 = new DummyNakedObject();
         element2.setupVersion(new DummyVersion(19));
         element2.setupResolveState(ResolveState.RESOLVED);
         element2.setupSpecification(elementSpecification);
-        
+
         field3Specification = new DummyNakedObjectSpecification();
         field3Specification.setupIsCollection();
         field3 = new DummyNakedCollection();
         field3.setupResolveState(ResolveState.RESOLVED);
         field3.setupSpecification(field3Specification);
         field3.setupVersion(new DummyVersion(27));
-        field3.init(new Object[] {element1, element2});
-        
+        field3.init(new Object[] { element1, element2 });
+
         // empty reference field
         field4Specification = new DummyNakedObjectSpecification();
         field4Specification.fields = new NakedObjectField[0];
 
-        
         // root object
         rootSpecification = new DummyNakedObjectSpecification();
         rootSpecification.fields = new NakedObjectField[4];
@@ -150,7 +148,6 @@ public class ObjectEncoderTest extends TestCase {
         rootObject.setupFieldValue("field2", field2);
         rootObject.setupFieldValue("field3", field3);
 
-        
         // cyclic references
         DummyNakedObject field1_1_1 = rootObject;
         field1_1Specification.fields[0] = new DummyField("backref1", rootSpecification);
@@ -177,7 +174,7 @@ public class ObjectEncoderTest extends TestCase {
         ServerActionResultData data = encoder.createActionResult(collection, null, null, null, new String[0], new String[0]);
         assertTrue(data.getReturn() instanceof CollectionData);
         assertEquals(type.getFullName(), data.getReturn().getType());
-   }
+    }
 
     public void testCreateActionResultWithObject() {
         DummyNakedObject object = new DummyNakedObject();
@@ -187,7 +184,7 @@ public class ObjectEncoderTest extends TestCase {
         ServerActionResultData data = encoder.createActionResult(object, null, null, null, new String[0], new String[0]);
         assertTrue(data.getReturn() instanceof ObjectData);
         assertEquals(type.getFullName(), data.getReturn().getType());
-     }
+    }
 
     public void testCreateCompletePersistentGraph() {
         DummyOid rootOid = new DummyOid(1);
@@ -226,16 +223,15 @@ public class ObjectEncoderTest extends TestCase {
         NullData emptyFieldData = (NullData) rootData.getFieldContent()[3];
         assertNotNull(emptyFieldData);
 
-        CollectionData collectionData =  (CollectionData) rootData.getFieldContent()[2];
+        CollectionData collectionData = (CollectionData) rootData.getFieldContent()[2];
         assertEquals(2, collectionData.getElements().length);
-          
 
         // check back references
         ObjectData fieldOfField = (ObjectData) referencedFieldData2.getFieldContent()[0];
         assertSame(fieldOfField, rootData);
-        
+
         ObjectData fieldOfElement = (ObjectData) collectionData.getElements()[0].getFieldContent()[0];
-        assertSame(fieldOfElement, rootData);        
+        assertSame(fieldOfElement, rootData);
 
     }
 
@@ -299,7 +295,7 @@ public class ObjectEncoderTest extends TestCase {
     public void testCreateMakePersistentGraphWhereAllReferencesArePersistent() {
         rootObject.setupResolveState(ResolveState.TRANSIENT);
 
-        ObjectData rootData = (ObjectData) encoder.createMakePersistentGraph(rootObject);
+        ObjectData rootData = (ObjectData) encoder.createMakePersistentGraph(rootObject, ObjectEncoder.createKnownTransients());
         assertEquals(ResolveState.TRANSIENT, rootObject.getResolveState());
 
         assertEquals(null, rootData.getOid());
@@ -326,7 +322,7 @@ public class ObjectEncoderTest extends TestCase {
         field1_1.setupResolveState(ResolveState.TRANSIENT);
         element1.setupResolveState(ResolveState.TRANSIENT);
 
-        ObjectData rootData = (ObjectData) encoder.createMakePersistentGraph(rootObject);
+        ObjectData rootData = (ObjectData) encoder.createMakePersistentGraph(rootObject, ObjectEncoder.createKnownTransients());
         assertEquals(ResolveState.TRANSIENT, rootObject.getResolveState());
 
         assertEquals(null, rootData.getOid());
@@ -350,100 +346,94 @@ public class ObjectEncoderTest extends TestCase {
 
         NullData emptyFieldData = (NullData) rootData.getFieldContent()[3];
         assertNotNull(emptyFieldData);
-        
-        CollectionData collectionData =  (CollectionData) rootData.getFieldContent()[2];
+
+        CollectionData collectionData = (CollectionData) rootData.getFieldContent()[2];
         assertEquals(2, collectionData.getElements().length);
-          
-        
+
         // check back references
         ObjectData fieldOfField = (ObjectData) referencedFieldData2.getFieldContent()[0];
-        assertSame(fieldOfField, rootData);        
+        assertSame(fieldOfField, rootData);
 
         ObjectData fieldOfElement = (ObjectData) collectionData.getElements()[0].getFieldContent()[0];
-        assertSame(fieldOfElement, rootData);        
+        assertSame(fieldOfElement, rootData);
     }
-    
-
 
     public void testCreateMadePersistentGraphWhereAllTransient() {
         // complete setup
         DummyObjectData reference2 = new DummyObjectData(null, "ref2", false, null);
         field1_1.setupResolveState(ResolveState.TRANSIENT);
         field1_1.setupOid(new DummyOid(192));
-    
+
         DummyValueData value = new DummyValueData(null, "val");
-    
+
         DummyObjectData el1 = new DummyObjectData(null, "element1", false, null);
         element1.setupResolveState(ResolveState.TRANSIENT);
         element1.setupOid(new DummyOid(34));
-        
+
         DummyObjectData el2 = new DummyObjectData(null, "element2", false, null);
         element2.setupResolveState(ResolveState.TRANSIENT);
         element2.setupOid(new DummyOid(29));
-        
-        
+
         DummyObjectData reference1 = new DummyObjectData(null, "ref1", false, null);
-        reference1.setFieldContent(new Data[] {reference2});
+        reference1.setFieldContent(new Data[] { reference2 });
         field1.setupResolveState(ResolveState.TRANSIENT);
         field1.setupFields(field1Specification.getFields());
         field1.setupOid(new DummyOid(583));
-        
-    
-        DummyCollectionData coll = new DummyCollectionData(null, "coll", new ObjectData[] {el1, el2}, null);
-    
-        ObjectData rootData = new DummyObjectData(null, "root",false, null);
-        rootData.setFieldContent( new Data[] {reference1, value, coll, null});
+
+        DummyCollectionData coll = new DummyCollectionData(null, "coll", new ObjectData[] { el1, el2 }, null);
+
+        ObjectData rootData = new DummyObjectData(null, "root", false, null);
+        rootData.setFieldContent(new Data[] { reference1, value, coll, null });
         rootObject.setupResolveState(ResolveState.TRANSIENT);
         rootObject.setupFields(rootSpecification.getFields());
         rootObject.setupOid(new DummyOid(712));
-        
-        reference2.setFieldContent(new Data[] {rootData});
 
-        el1.setFieldContent(new Data[] {rootData});
+        reference2.setFieldContent(new Data[] { rootData });
+
+        el1.setFieldContent(new Data[] { rootData });
 
         // test
         ObjectData transientGraph = encoder.createMadePersistentGraph(rootData, rootObject, new SingleResponseUpdateNotifier());
         assertEquals(new DummyOid(712), transientGraph.getOid());
         assertEquals(new DummyVersion(11), transientGraph.getVersion());
         assertEquals(4, transientGraph.getFieldContent().length);
-        
+
         // check root's 3 fields
         ObjectData field1 = (ObjectData) transientGraph.getFieldContent()[0];
         assertEquals("ref1", field1.getType());
         assertEquals(new DummyOid(583), field1.getOid());
         assertEquals(new DummyVersion(12), field1.getVersion());
         assertEquals(1, field1.getFieldContent().length);
-        
-        Data field2 =transientGraph.getFieldContent()[1];
+
+        Data field2 = transientGraph.getFieldContent()[1];
         assertEquals(null, field2);
-    
-        CollectionData field3 =(CollectionData) transientGraph.getFieldContent()[2];
+
+        CollectionData field3 = (CollectionData) transientGraph.getFieldContent()[2];
         assertNotNull(field3);
         assertEquals("coll", field3.getType());
         assertEquals(null, field3.getOid());
         assertEquals(new DummyVersion(27), field3.getVersion());
         assertEquals(2, field3.getElements().length);
-    
-        Data field4 =transientGraph.getFieldContent()[3];
+
+        Data field4 = transientGraph.getFieldContent()[3];
         assertEquals(null, field4);
-    
+
         // check the 3rd level object
         ObjectData field = (ObjectData) field1.getFieldContent()[0];
         assertEquals("ref2", field.getType());
         assertEquals(new DummyOid(192), field.getOid());
         assertEquals(new DummyVersion(13), field.getVersion());
-    
+
         // check elements of collection
         ObjectData element1 = (ObjectData) field3.getElements()[0];
         assertEquals("element1", element1.getType());
         assertEquals(new DummyOid(34), element1.getOid());
         assertEquals(new DummyVersion(17), element1.getVersion());
-        
+
         ObjectData element2 = (ObjectData) field3.getElements()[1];
         assertEquals("element2", element2.getType());
         assertEquals(new DummyOid(29), element2.getOid());
         assertEquals(new DummyVersion(19), element2.getVersion());
-        
 
     }
 
@@ -458,59 +448,57 @@ public class ObjectEncoderTest extends TestCase {
         DummyObjectData el1 = new DummyObjectData(new DummyOid(34), "element1", false, null);
         element1.setupResolveState(ResolveState.TRANSIENT);
         element1.setupOid(new DummyOid(34));
-        
+
         DummyObjectData el2 = new DummyObjectData(null, "element2", false, null);
         element2.setupResolveState(ResolveState.TRANSIENT);
         element2.setupOid(new DummyOid(29));
-        
-        
+
         DummyObjectData reference1 = new DummyObjectData(new DummyOid(583), "ref1", false, null);
-        reference1.setFieldContent( new Data[] {reference2});
+        reference1.setFieldContent(new Data[] { reference2 });
         field1.setupResolveState(ResolveState.RESOLVED);
         field1.setupFields(field1Specification.getFields());
         field1.setupOid(new DummyOid(583));
 
-        DummyCollectionData coll = new DummyCollectionData(null, "coll", new ObjectData[] {el1, el2}, null);
+        DummyCollectionData coll = new DummyCollectionData(null, "coll", new ObjectData[] { el1, el2 }, null);
 
         ObjectData data = new DummyObjectData(null, "root", false, null);
-        data.setFieldContent(new Data[] {reference1, value, coll, null});
+        data.setFieldContent(new Data[] { reference1, value, coll, null });
         rootObject.setupResolveState(ResolveState.TRANSIENT);
         rootObject.setupFields(rootSpecification.getFields());
         rootObject.setupOid(new DummyOid(712));
-        
+
         // test
         ObjectData updates = encoder.createMadePersistentGraph(data, rootObject, new SingleResponseUpdateNotifier());
         assertEquals(new DummyOid(712), updates.getOid());
         assertEquals(new DummyVersion(11), updates.getVersion());
         assertEquals(4, updates.getFieldContent().length);
-        
+
         // check root's 3 fields
         ObjectData field1 = (ObjectData) updates.getFieldContent()[0];
         assertEquals("first field was persistent, no data sent back", null, field1);
-        
-        Data field2 =updates.getFieldContent()[1];
+
+        Data field2 = updates.getFieldContent()[1];
         assertEquals(null, field2);
 
-        CollectionData field3 =(CollectionData) updates.getFieldContent()[2];
+        CollectionData field3 = (CollectionData) updates.getFieldContent()[2];
         assertNotNull(field3);
         assertEquals("coll", field3.getType());
         assertEquals(null, field3.getOid());
         assertEquals(new DummyVersion(27), field3.getVersion());
         assertEquals(2, field3.getElements().length);
 
-        Data field4 =updates.getFieldContent()[3];
+        Data field4 = updates.getFieldContent()[3];
         assertEquals(null, field4);
 
         // check elements of collection
         ObjectData element1 = (ObjectData) field3.getElements()[0];
         assertEquals("first element was persistent, no data sent back", null, element1);
-        
+
         ObjectData element2 = (ObjectData) field3.getElements()[1];
         assertEquals("element2", element2.getType());
         assertEquals(new DummyOid(29), element2.getOid());
         assertEquals(new DummyVersion(19), element2.getVersion());
     }
-
 
     public void testPersistentObjectParameter() {
         assertEquals(ResolveState.RESOLVED, rootObject.getResolveState());
@@ -518,7 +506,8 @@ public class ObjectEncoderTest extends TestCase {
         DummyOid rootOid = new DummyOid(1);
         rootObject.setupOid(rootOid);
 
-        Data[] results = encoder.createDataForParameters(new NakedObjectSpecification[] {new DummyNakedObjectSpecification()}, new Naked[] {rootObject});
+        Data[] results = encoder.createDataForParameters(new NakedObjectSpecification[] { new DummyNakedObjectSpecification() },
+                new Naked[] { rootObject });
         ObjectData rootData = (ObjectData) results[0];
         assertEquals(ResolveState.RESOLVED, rootObject.getResolveState());
 
@@ -551,7 +540,8 @@ public class ObjectEncoderTest extends TestCase {
         rootObject.setupResolveState(ResolveState.TRANSIENT);
         field1.setupResolveState(ResolveState.TRANSIENT);
 
-        Data[] results = encoder.createDataForParameters(new NakedObjectSpecification[] {new DummyNakedObjectSpecification()}, new Naked[] {rootObject});
+        Data[] results = encoder.createDataForParameters(new NakedObjectSpecification[] { new DummyNakedObjectSpecification() },
+                new Naked[] { rootObject });
         ObjectData rootData = (ObjectData) results[0];
         assertEquals(ResolveState.TRANSIENT, rootObject.getResolveState());
 
@@ -582,7 +572,8 @@ public class ObjectEncoderTest extends TestCase {
         DummyNakedValue dummyNakedValue = new DummyNakedValue();
         dummyNakedValue.setupObject(new Integer(123));
 
-        Data[] data = (Data[]) encoder.createDataForParameters(new NakedObjectSpecification[] {new DummyNakedObjectSpecification()}, new Naked[] {dummyNakedValue});
+        Data[] data = (Data[]) encoder.createDataForParameters(
+                new NakedObjectSpecification[] { new DummyNakedObjectSpecification() }, new Naked[] { dummyNakedValue });
 
         assertEquals(dummyNakedValue.getSpecification().getFullName(), data[0].getType());
         assertEquals(new Integer(123), ((ValueData) data[0]).getValue());
@@ -590,21 +581,20 @@ public class ObjectEncoderTest extends TestCase {
 }
 
 /*
- * Naked Objects - a framework that exposes behaviourally complete business objects directly to the
- * user. Copyright (C) 2000 - 2005 Naked Objects Group Ltd
+ * Naked Objects - a framework that exposes behaviourally complete business objects directly to the user.
+ * Copyright (C) 2000 - 2005 Naked Objects Group Ltd
  * 
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
  * 
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
  * 
- * You should have received a copy of the GNU General Public License along with this program; if
- * not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- * 02111-1307 USA
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  * 
- * The authors can be contacted via www.nakedobjects.org (the registered address of Naked Objects
- * Group is Kingsway House, 123 Goldworth Road, Woking GU21 1NR, UK).
+ * The authors can be contacted via www.nakedobjects.org (the registered address of Naked Objects Group is
+ * Kingsway House, 123 Goldworth Road, Woking GU21 1NR, UK).
  */
