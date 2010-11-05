9713512d7a1aa343dcff72ce363ae9d62e3f49d5
Daniel Keir Haywood <danhaywood@apache.org>
2010-11-05 12:16:06 +0000

    tidying up pom for viewer-bdd in support/prototype; removed dummy test for surefire
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1031531 13f79535-47bb-0310-9956-ffa450edef68

--- support/prototype/viewer-bdd/src/test/java/org/apache/isis/support/prototype/stories/NewClaimDefaultsOkStory.java
+++ support/prototype/viewer-bdd/src/test/java/org/apache/isis/support/prototype/stories/NewClaimDefaultsOkStory.java
@@ -3,13 +3,11 @@ package org.apache.isis.support.prototype.stories;
 import org.apache.isis.support.prototype.CustomCssPackage;
 import org.apache.isis.viewer.bdd.concordion.AbstractIsisConcordionTest;
 
-
 public class NewClaimDefaultsOkStory extends AbstractIsisConcordionTest {
 
+    @Override
+    protected Class<?> customCssPackage() {
+        return CustomCssPackage.class;
+    }
 
-	@Override
-	protected Class<?> customCssPackage() {
-		return CustomCssPackage.class;
-	}
-	
 }
