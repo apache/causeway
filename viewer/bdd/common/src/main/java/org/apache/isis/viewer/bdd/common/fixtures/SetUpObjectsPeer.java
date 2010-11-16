3dbde61f0537161ba877ea912cf44995b04da865
Daniel Keir Haywood <danhaywood@apache.org>
2010-11-16 14:25:59 +0000

    aligning package names with maven groupId/artifactId for core/runtime
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1035641 13f79535-47bb-0310-9956-ffa450edef68

--- viewer/bdd/common/src/main/java/org/apache/isis/viewer/bdd/common/fixtures/SetUpObjectsPeer.java
+++ viewer/bdd/common/src/main/java/org/apache/isis/viewer/bdd/common/fixtures/SetUpObjectsPeer.java
@@ -17,264 +17,260 @@ import org.apache.isis.viewer.bdd.common.StoryBoundValueException;
 import org.apache.isis.viewer.bdd.common.util.Strings;
 
 public class SetUpObjectsPeer extends AbstractFixturePeer {
-	
-	public static enum Mode {
-		PERSIST, DO_NOT_PERSIST;
 
-		public boolean isPersist() {
-			return this == PERSIST;
-		}
-	}
+    public static enum Mode {
+        PERSIST, DO_NOT_PERSIST;
 
-	public static enum PropertyResult {
-		ALIAS, NO_SUCH_PROPERTY, NOT_A_PROPERTY, OK;
-		public boolean isOk() {
-			return this == OK;
-		}
-	}
-
-    public static enum SetUpObjectResult {
-    	OK, 
-    	NO_ASSOCIATION, 
-    	CLEARED, 
-    	CANNOT_CLEAR("(cannot clear)"), 
-    	CANNOT_SET ("(cannot set"), 
-    	CANNOT_PARSE("(cannot parse)"), 
-    	UNKNOWN_REFERENCE("(unknown reference)");
-    	private String errorMessage;
-		private SetUpObjectResult() {
-    		this(null);
-    	}
-		private SetUpObjectResult(String errorMessage) {
-    		this.errorMessage = errorMessage;
-    	}
-    	public boolean isHandled() { return !isError(); }
-    	public boolean isError() { return errorMessage != null; }
-    	public String getErrorMessage() { return errorMessage; }
+        public boolean isPersist() {
+            return this == PERSIST;
+        }
     }
 
-	public interface AssociationVisitor {
-		void visit(OneToOneAssociation association, int colNum);
-	}
+    public static enum PropertyResult {
+        ALIAS, NO_SUCH_PROPERTY, NOT_A_PROPERTY, OK;
+        public boolean isOk() {
+            return this == OK;
+        }
+    }
 
-	private final ObjectSpecification spec;
+    public static enum SetUpObjectResult {
+        OK, NO_ASSOCIATION, CLEARED, CANNOT_CLEAR("(cannot clear)"), CANNOT_SET("(cannot set"),
+        CANNOT_PARSE("(cannot parse)"), UNKNOWN_REFERENCE("(unknown reference)");
+        private String errorMessage;
 
-	private final List<OneToOneAssociation> properties = new ArrayList<OneToOneAssociation>();
-	private final CellBinding aliasBinding;
-	private final SetUpObjectsPeer.Mode mode;
+        private SetUpObjectResult() {
+            this(null);
+        }
 
-	private final List<String> cellTextList = new ArrayList<String>();
-	private String alias;
+        private SetUpObjectResult(String errorMessage) {
+            this.errorMessage = errorMessage;
+        }
 
-	public SetUpObjectsPeer(AliasRegistry aliasesRegistry,
-			final String className, final SetUpObjectsPeer.Mode mode,
-			final CellBinding aliasBinding) {
-		super(aliasesRegistry, aliasBinding);
+        public boolean isHandled() {
+            return !isError();
+        }
 
-		ObjectSpecification spec;
-		try {
-			spec = getSpecificationLoader().loadSpecification(className);
-		} catch (final IsisException ex) {
-			spec = null;
-		}
-		this.spec = spec;
+        public boolean isError() {
+            return errorMessage != null;
+        }
 
-		this.mode = mode;
-		this.aliasBinding = aliasBinding;
-	}
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+    }
 
-	public boolean isSpecOk() {
-		return spec != null;
-	}
+    public interface AssociationVisitor {
+        void visit(OneToOneAssociation association, int colNum);
+    }
 
-	public List<OneToOneAssociation> getProperties() {
-		return properties;
-	}
+    private final ObjectSpecification spec;
 
-	public CellBinding getAliasBinding() {
-		return aliasBinding;
-	}
+    private final List<OneToOneAssociation> properties = new ArrayList<OneToOneAssociation>();
+    private final CellBinding aliasBinding;
+    private final SetUpObjectsPeer.Mode mode;
 
-	public PropertyResult definePropertyOrAlias(String heading, int colNum) {
+    private final List<String> cellTextList = new ArrayList<String>();
+    private String alias;
 
-		OneToOneAssociation otoa = null;
+    public SetUpObjectsPeer(AliasRegistry aliasRegistry, final String className, final SetUpObjectsPeer.Mode mode,
+        final CellBinding aliasBinding) {
+        super(aliasRegistry, aliasBinding);
 
-		try {
-			final int aliasColumn = getAliasBinding().getColumn();
-			if (colNum == aliasColumn) {
-				return PropertyResult.ALIAS;
-			}
+        this.spec = loadSpecIfValid(className);
 
-			ObjectAssociation association = null;
-			try {
-				final String memberName = Strings.memberIdFor(heading);
-				association = spec.getAssociation(memberName);
-			} catch (final Exception ex) {
-				return PropertyResult.NO_SUCH_PROPERTY;
-			}
+        this.mode = mode;
+        this.aliasBinding = aliasBinding;
+    }
 
-			if (!association.isOneToOneAssociation()) {
-				return PropertyResult.NOT_A_PROPERTY;
-			}
+    private ObjectSpecification loadSpecIfValid(final String className) {
+        try {
+            return getSpecificationLoader().loadSpecification(className);
+        } catch (final IsisException ex) {
+            return null;
+        }
+    }
 
-			otoa = (OneToOneAssociation) association;
+    public boolean isSpecOk() {
+        return spec != null;
+    }
 
-			return PropertyResult.OK;
-		} finally {
-			// add an association if OK, add null otherwise
-			getProperties().add(otoa);
-		}
-	}
+    public List<OneToOneAssociation> getProperties() {
+        return properties;
+    }
 
-	public void resetForNextObject() {
-		cellTextList.clear();
-		this.alias = null;
-	}
+    public CellBinding getAliasBinding() {
+        return aliasBinding;
+    }
 
-	/**
-	 * Used by Concordion only.
-	 * 
-	 * <p>
-	 * FitNesse, on the other hand, uses a more fine-grained approach, calling the underlying
-	 * methods.
-	 */
-	public void createObject() throws StoryBoundValueException {
-		ObjectAdapter adapter = createInstance();
-		
-		for (int colNum = 0; colNum < getProperties().size(); colNum++) {
-			SetUpObjectResult result = setUpProperty(adapter, colNum);
-			
-			if (!result.isHandled()) {
-				CellBinding cellBinding = getCellBindings().get(colNum);
-				throw StoryBoundValueException.current(cellBinding, result.getErrorMessage());
-			}
-		}
-			
-		persistIfNecessary(adapter);
+    public PropertyResult definePropertyOrAlias(String heading, int colNum) {
+
+        OneToOneAssociation otoa = null;
+
+        try {
+            final int aliasColumn = getAliasBinding().getColumn();
+            if (colNum == aliasColumn) {
+                return PropertyResult.ALIAS;
+            }
+
+            ObjectAssociation association = null;
+            try {
+                final String memberName = Strings.memberIdFor(heading);
+                association = spec.getAssociation(memberName);
+            } catch (final Exception ex) {
+                return PropertyResult.NO_SUCH_PROPERTY;
+            }
+
+            if (!association.isOneToOneAssociation()) {
+                return PropertyResult.NOT_A_PROPERTY;
+            }
+
+            otoa = (OneToOneAssociation) association;
+
+            return PropertyResult.OK;
+        } finally {
+            // add an association if OK, add null otherwise
+            getProperties().add(otoa);
+        }
+    }
+
+    public void resetForNextObject() {
+        cellTextList.clear();
+        this.alias = null;
+    }
+
+    /**
+     * Used by Concordion only.
+     * 
+     * <p>
+     * FitNesse, on the other hand, uses a more fine-grained approach, calling the underlying methods.
+     */
+    public void createObject() throws StoryBoundValueException {
+        ObjectAdapter adapter = createInstance();
+
+        for (int colNum = 0; colNum < getProperties().size(); colNum++) {
+            SetUpObjectResult result = setUpProperty(adapter, colNum);
+
+            if (!result.isHandled()) {
+                CellBinding cellBinding = getCellBindings().get(colNum);
+                throw StoryBoundValueException.current(cellBinding, result.getErrorMessage());
+            }
+        }
+
+        persistIfNecessary(adapter);
         alias(adapter);
         resetForNextObject();
-	}
+    }
 
+    public ObjectAdapter createInstance() {
+        if (spec == null) {
+            return null;
+        }
+        return getPersistenceSession().createInstance(spec);
+    }
 
-	public ObjectAdapter createInstance() {
-		if (spec == null) {
-			return null;
-		}
-		return getPersistenceSession().createInstance(spec);
-	}
+    public SetUpObjectResult setUpProperty(ObjectAdapter adapter, int colNum) {
 
-	public SetUpObjectResult setUpProperty(ObjectAdapter adapter, int colNum) {
+        final OneToOneAssociation association = getProperties().get(colNum);
+        if (association == null) {
+            return SetUpObjectResult.NO_ASSOCIATION;
+        }
 
-		final OneToOneAssociation association = getProperties().get(colNum);
-		if (association == null) {
-			return SetUpObjectResult.NO_ASSOCIATION;
-		}
+        final String cellText = cellTextList.get(colNum);
 
-		final String cellText = cellTextList.get(colNum);
+        // handle empty cell as null
+        if (cellText == null || cellText.length() == 0) {
 
-		// handle empty cell as null
-		if (cellText == null || cellText.length() == 0) {
+            // use clear facet if available
+            final PropertyClearFacet clearFacet = association.getFacet(PropertyClearFacet.class);
 
-			// use clear facet if available
-			final PropertyClearFacet clearFacet = association
-					.getFacet(PropertyClearFacet.class);
+            if (clearFacet != null) {
+                clearFacet.clearProperty(adapter);
+                return SetUpObjectResult.CLEARED;
+            }
 
-			if (clearFacet != null) {
-				clearFacet.clearProperty(adapter);
-				return SetUpObjectResult.CLEARED;
-			}
+            // use setter facet otherwise
+            final PropertySetterFacet setterFacet = association.getFacet(PropertySetterFacet.class);
 
-			// use setter facet otherwise
-			final PropertySetterFacet setterFacet = association
-					.getFacet(PropertySetterFacet.class);
+            if (setterFacet != null) {
+                setterFacet.setProperty(adapter, null);
+                return SetUpObjectResult.CLEARED;
+            }
 
-			if (setterFacet != null) {
-				setterFacet.setProperty(adapter, null);
-				return SetUpObjectResult.CLEARED;
-			}
+            return SetUpObjectResult.CANNOT_CLEAR;
+        }
 
-			return SetUpObjectResult.CANNOT_CLEAR;
-		}
+        // non-empty, will need a setter
+        final PropertySetterFacet setterFacet = association.getFacet(PropertySetterFacet.class);
+        if (setterFacet == null) {
+            return SetUpObjectResult.CANNOT_SET;
+        }
 
-		// non-empty, will need a setter
-		final PropertySetterFacet setterFacet = association
-				.getFacet(PropertySetterFacet.class);
-		if (setterFacet == null) {
-			return SetUpObjectResult.CANNOT_SET;
-		}
+        final ObjectSpecification fieldSpecification = association.getSpecification();
+        final ParseableFacet parseableFacet = fieldSpecification.getFacet(ParseableFacet.class);
 
-		final ObjectSpecification fieldSpecification = association
-				.getSpecification();
-		final ParseableFacet parseableFacet = fieldSpecification
-				.getFacet(ParseableFacet.class);
+        ObjectAdapter referencedAdapter = null;
+        if (parseableFacet != null) {
+            // handle as parseable value
+            try {
+                referencedAdapter = parseableFacet.parseTextEntry(adapter, cellText);
+            } catch (final IllegalArgumentException ex) {
+                return SetUpObjectResult.CANNOT_PARSE;
+            }
 
-		ObjectAdapter referencedAdapter = null;
-		if (parseableFacet != null) {
-			// handle as parseable value
-			try {
-				referencedAdapter = parseableFacet.parseTextEntry(adapter,
-						cellText);
-			} catch (final IllegalArgumentException ex) {
-				return SetUpObjectResult.CANNOT_PARSE;
-			}
+        } else {
+            // handle as reference to known object
+            referencedAdapter = getAliasRegistry().getAliased(cellText);
+            if (referencedAdapter == null) {
+                return SetUpObjectResult.UNKNOWN_REFERENCE;
+            }
+        }
 
-		} else {
-			// handle as reference to known object
-			referencedAdapter = getAliasRegistry().getAliased(cellText);
-			if (referencedAdapter == null) {
-				return SetUpObjectResult.UNKNOWN_REFERENCE;
-			}
-		}
+        setterFacet.setProperty(adapter, referencedAdapter);
+        return SetUpObjectResult.OK;
+    }
 
-		setterFacet.setProperty(adapter, referencedAdapter);
-		return SetUpObjectResult.OK;
-	}
+    public void persistIfNecessary(ObjectAdapter adapter) {
+        if (mode.isPersist()) {
+            // xactn mgmt now done by PersistenceSession#makePersistent()
+            // getTransactionManager().startTransaction();
+            getPersistenceSession().makePersistent(adapter);
+            // getTransactionManager().endTransaction();
+        }
+    }
 
-	public void persistIfNecessary(ObjectAdapter adapter) {
-		if (mode.isPersist()) {
-			// xactn mgmt now done by PersistenceSession#makePersistent()
-			// getTransactionManager().startTransaction();
-			getPersistenceSession().makePersistent(adapter);
-			// getTransactionManager().endTransaction();
-		}
-	}
-
-	public void alias(ObjectAdapter adapter) {
+    public void alias(ObjectAdapter adapter) {
         final String alias = aliasFor(adapter);
         getAliasRegistry().aliasAs(alias, adapter);
-	}
+    }
 
-	public String aliasFor(ObjectAdapter adapter) {
-		if (alias != null) {
-			return alias;
-		} else {
-			String specShortName = Strings.lowerLeading(spec.getShortName());
-			return getAliasRegistry().aliasPrefixedAs(specShortName, adapter);
-		}
-	}
+    public String aliasFor(ObjectAdapter adapter) {
+        if (alias != null) {
+            return alias;
+        } else {
+            String specShortName = Strings.lowerLeading(spec.getShortName());
+            return getAliasRegistry().aliasPrefixedAs(specShortName, adapter);
+        }
+    }
 
-	public void forEachAssociation(AssociationVisitor visitor) {
-		for (int colNum = 0; colNum < getProperties().size(); colNum++) {
-			final OneToOneAssociation association = getProperties().get(
-					colNum);
-			if (association != null) {
-				visitor.visit(association, colNum);
-			}
-		}
-	}
+    public void forEachAssociation(AssociationVisitor visitor) {
+        for (int colNum = 0; colNum < getProperties().size(); colNum++) {
+            final OneToOneAssociation association = getProperties().get(colNum);
+            if (association != null) {
+                visitor.visit(association, colNum);
+            }
+        }
+    }
 
-	public boolean addPropertyValueOrAlias(String propertyValue) {
-		cellTextList.add(propertyValue);
+    public boolean addPropertyValueOrAlias(String propertyValue) {
+        cellTextList.add(propertyValue);
 
-		// capture alias if just added
-		int aliasColumn1based = getAliasBinding().getColumn() + 1;
-		if (cellTextList.size() == aliasColumn1based) {
-			alias = propertyValue;
-		}
+        // capture alias if just added
+        int aliasColumn1based = getAliasBinding().getColumn() + 1;
+        if (cellTextList.size() == aliasColumn1based) {
+            alias = propertyValue;
+        }
 
-		return true;
-	}
-
-
+        return true;
+    }
 
 }
