ce5f6c09baf6d8b51d915be3e558b023e10b88e1
Daniel Keir Haywood <danhaywood@apache.org>
2010-11-16 00:24:27 +0000

    adding svn:externals for docbkx dtd, styles, images so that can build docs locally (using mvn site -o)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1035506 13f79535-47bb-0310-9956-ffa450edef68

--- viewer/bdd/concordion/src/main/java/org/apache/isis/viewer/bdd/concordion/internal/fixtures/SetUpObjectsForConcordion.java
+++ viewer/bdd/concordion/src/main/java/org/apache/isis/viewer/bdd/concordion/internal/fixtures/SetUpObjectsForConcordion.java
@@ -6,58 +6,52 @@ import org.apache.isis.viewer.bdd.common.Constants;
 import org.apache.isis.viewer.bdd.common.StoryBoundValueException;
 import org.apache.isis.viewer.bdd.common.fixtures.SetUpObjectsPeer;
 import org.apache.isis.viewer.bdd.concordion.internal.fixtures.bindings.CellBindingForConcordion;
-import org.apache.isis.viewer.bdd.concordion.internal.fixtures.perform.StoryCellForConcordion;
 
 public class SetUpObjectsForConcordion extends AbstractFixture<SetUpObjectsPeer> {
 
-    public SetUpObjectsForConcordion(
-    		final AliasRegistry aliasesRegistry,
-            final String className, 
-            final SetUpObjectsPeer.Mode mode) {
-        this(aliasesRegistry, className, mode, 
-        		CellBindingForConcordion.builder(
-        				Constants.ALIAS_RESULT_NAME, 
-        				Constants.ALIAS_RESULT_HEAD_SET).autoCreate().build());
+    public SetUpObjectsForConcordion(final AliasRegistry aliasesRegistry, final String className,
+        final SetUpObjectsPeer.Mode mode) {
+        this(aliasesRegistry, className, mode, CellBindingForConcordion
+            .builder(Constants.ALIAS_RESULT_NAME, Constants.ALIAS_RESULT_HEAD_SET).autoCreate().build());
     }
 
-    private SetUpObjectsForConcordion(final AliasRegistry storyFixture,
-            final String className, final SetUpObjectsPeer.Mode mode,
-            final CellBinding aliasBinding) {
+    private SetUpObjectsForConcordion(final AliasRegistry storyFixture, final String className,
+        final SetUpObjectsPeer.Mode mode, final CellBinding aliasBinding) {
         super(new SetUpObjectsPeer(storyFixture, className, mode, aliasBinding));
     }
 
-	public String executeHeader(String alias, String... propertyNames) {
-		
-		// create bindings (there's only one)
-		getPeer().getAliasBinding().setHeadColumn(0);
-		
-		// define properties and the alias column
-		int colNum=0;
-		getPeer().definePropertyOrAlias(alias, colNum++);
-		
-		for (String propertyName: propertyNames) {
-			getPeer().definePropertyOrAlias(propertyName, colNum++);
-		}
-		
-		return ""; // ok
-	}
+    public String executeHeader(String alias, String... propertyNames) {
 
-	public String executeRow(String alias, String... propertyValues) {
-		
-		// set property values and the alis
-		getPeer().addPropertyValueOrAlias(alias);
-		for(String propertyValue: propertyValues){
-			getPeer().addPropertyValueOrAlias(propertyValue);
-		}
-		
-		// create the object
-		try {
-			getPeer().createObject();
-			return "ok";
-		} catch(StoryBoundValueException ex) {
-			return ex.toString();
-		}
-		
-	}
+        // create bindings (there's only one)
+        getPeer().getAliasBinding().setHeadColumn(0);
+
+        // define properties and the alias column
+        int colNum = 0;
+        getPeer().definePropertyOrAlias(alias, colNum++);
+
+        for (String propertyName : propertyNames) {
+            getPeer().definePropertyOrAlias(propertyName, colNum++);
+        }
+
+        return ""; // ok
+    }
+
+    public String executeRow(String alias, String... propertyValues) {
+
+        // set property values and the alis
+        getPeer().addPropertyValueOrAlias(alias);
+        for (String propertyValue : propertyValues) {
+            getPeer().addPropertyValueOrAlias(propertyValue);
+        }
+
+        // create the object
+        try {
+            getPeer().createObject();
+            return "ok";
+        } catch (StoryBoundValueException ex) {
+            return ex.toString();
+        }
+
+    }
 
 }
