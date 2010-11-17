01e953064fae82df7d3745b8271d73d769cd3d66
Daniel Keir Haywood <danhaywood@apache.org>
2010-11-17 23:10:06 +0000

    introducing profilestore as defaults/alternatives API
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1036274 13f79535-47bb-0310-9956-ffa450edef68

---
+++ alternatives/profilestore/xml/src/test/java/org/apache/isis/alternatives/objectstore/xml/internal/data/ObjectDataVectorTest.java
@@ -0,0 +1,68 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+
+package org.apache.isis.alternatives.objectstore.xml.internal.data;
+
+import static org.junit.Assert.assertTrue;
+
+import org.apache.isis.alternatives.objectstore.xml.internal.version.FileVersion;
+import org.apache.isis.core.metamodel.spec.feature.ObjectAssociation;
+import org.apache.isis.core.metamodel.testspec.TestProxySpecification;
+import org.apache.isis.core.runtime.persistence.oidgenerator.simple.SerialOid;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ObjectDataVectorTest {
+	private ObjectDataVector objectDataVector;
+	private ObjectData objectData;
+    private TestProxySpecification spec;
+	private SerialOid oid;
+	private FileVersion version;
+
+	@Before
+	public void setUp() throws Exception {
+		boolean isTransient = true;
+		long serialNum = Long.parseLong("1", 16);
+		oid = isTransient ? SerialOid.createTransient(serialNum) : SerialOid.createPersistent(serialNum);
+
+		spec = new TestProxySpecification(this.getClass());
+        spec.fields = new ObjectAssociation[0];
+
+        version = new FileVersion("", System.currentTimeMillis());
+        objectData = new ObjectData(spec, oid, version);
+		objectDataVector = new ObjectDataVector();
+	}
+	
+	@Test 
+	public void validatesObjectDataIsStored()throws Exception {
+		objectDataVector.addElement(objectData);
+		assertTrue(objectDataVector.contains(objectData));
+		assertTrue(objectDataVector.element(0).equals(objectData));
+	}
+	
+	@Test 
+	public void validatesObjectDataVectorSize()throws Exception {
+		objectDataVector.addElement(objectData);
+		assertTrue(objectDataVector.size() == 1);
+	}
+	
+}
+
+
