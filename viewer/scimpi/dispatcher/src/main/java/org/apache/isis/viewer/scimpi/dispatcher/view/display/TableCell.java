b5157859a59e68cf45756302a2128a44933d2aec
Robert Charles Matthews <rmatthews@apache.org>
2011-01-07 18:59:32 +0000

    Class in table cell is now settable
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1056456 13f79535-47bb-0310-9956-ffa450edef68

--- viewer/scimpi/dispatcher/src/main/java/org/apache/isis/viewer/scimpi/dispatcher/view/display/TableCell.java
+++ viewer/scimpi/dispatcher/src/main/java/org/apache/isis/viewer/scimpi/dispatcher/view/display/TableCell.java
@@ -36,7 +36,7 @@ public class TableCell extends AbstractElementProcessor {
         String id = request.getOptionalProperty(OBJECT);
         String fieldName = request.getRequiredProperty(FIELD);
         String className = request.getOptionalProperty(CLASS);
-        className = className == null ? "cell" : className;
+        className = className == null ? "" : " class=\"" + className + "\"";
         ObjectAdapter object = request.getContext().getMappedObjectOrVariable(id, ELEMENT);
         ObjectAssociation field = object.getSpecification().getAssociation(fieldName);
         if (field == null) {
@@ -45,7 +45,7 @@ public class TableCell extends AbstractElementProcessor {
         if (field.isVisible(IsisContext.getAuthenticationSession(), object).isVetoed()) {
             throw new ForbiddenException("Field " + fieldName + " in " + object + " is not visible");
         }
-        request.appendHtml("<td " + className + ">");
+        request.appendHtml("<td" + className + ">");
         ObjectAdapter fieldReference = field.get(object);
         String source = fieldReference == null ? "" : request.getContext().mapObject(fieldReference, Scope.REQUEST);
         String name = request.getOptionalProperty(RESULT_NAME, fieldName);
