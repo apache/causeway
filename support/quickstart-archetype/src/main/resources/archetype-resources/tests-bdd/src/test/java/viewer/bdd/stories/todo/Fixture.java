1d530b9f18dfdefc03e525543f2558bdc84eab62
Daniel Keir Haywood <danhaywood@apache.org>
2011-02-25 14:42:50 +0000

    archetype: recreated from 'quickstart' prototype app
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1074545 13f79535-47bb-0310-9956-ffa450edef68

---
+++ support/quickstart-archetype/src/main/resources/archetype-resources/tests-bdd/src/test/java/viewer/bdd/stories/todo/Fixture.java
@@ -0,0 +1,12 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package viewer.bdd.stories.todo;
+
+import viewer.bdd.AbstractApplicationStory;
+
+
+public class Fixture extends AbstractApplicationStory {
+
+
+}
