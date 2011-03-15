8b15a212dd4a3a7c376cec5464d18b6a152118a9
Daniel Keir Haywood <danhaywood@apache.org>
2011-03-15 23:02:38 +0000

    added IsisWicketUnsecuredApplication to allow anonymous logins
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1081982 13f79535-47bb-0310-9956-ffa450edef68

--- viewer/wicket/viewer/src/main/java/org/apache/isis/viewer/wicket/viewer/integration/wicket/WebRequestCycleForIsis.java
+++ viewer/wicket/viewer/src/main/java/org/apache/isis/viewer/wicket/viewer/integration/wicket/WebRequestCycleForIsis.java
@@ -20,17 +20,18 @@
 
 package org.apache.isis.viewer.wicket.viewer.integration.wicket;
 
-import org.apache.isis.core.commons.authentication.AuthenticationSession;
-import org.apache.isis.runtimes.dflt.runtime.context.IsisContext;
-import org.apache.isis.runtimes.dflt.runtime.session.IsisSession;
-import org.apache.isis.runtimes.dflt.runtime.transaction.IsisTransaction;
-import org.apache.isis.runtimes.dflt.runtime.transaction.IsisTransactionManager;
 import org.apache.log4j.Logger;
 import org.apache.wicket.Response;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.protocol.http.WebRequest;
 import org.apache.wicket.protocol.http.WebRequestCycle;
 import org.apache.wicket.protocol.http.request.WebClientInfo;
+
+import org.apache.isis.core.commons.authentication.AuthenticationSession;
+import org.apache.isis.runtimes.dflt.runtime.context.IsisContext;
+import org.apache.isis.runtimes.dflt.runtime.session.IsisSession;
+import org.apache.isis.runtimes.dflt.runtime.transaction.IsisTransaction;
+import org.apache.isis.runtimes.dflt.runtime.transaction.IsisTransactionManager;
 
 /**
  * Isis-specific implementation of the Wicket's {@link WebRequestCycle}, automatically
@@ -54,7 +55,7 @@ public class WebRequestCycleForIsis extends WebRequestCycle {
 	@Override
 	public AuthenticatedWebSessionForIsis getWebSession()
 	{
-		return (AuthenticatedWebSessionForIsis)super.getWebSession();
+	    return (AuthenticatedWebSessionForIsis)super.getWebSession();
 	}
 
 	@Override
