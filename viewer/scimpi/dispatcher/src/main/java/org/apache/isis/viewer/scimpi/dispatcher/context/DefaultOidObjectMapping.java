04d983d580d1f59c74fd042eac38e3f46f7b337b
Robert Charles Matthews <rmatthews@apache.org>
2011-04-13 15:44:53 +0000

    Using JSON directly to store transient data on the page (instead of trying to bend Memento and binary data to to task).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1091830 13f79535-47bb-0310-9956-ffa450edef68

--- viewer/scimpi/dispatcher/src/main/java/org/apache/isis/viewer/scimpi/dispatcher/context/DefaultOidObjectMapping.java
+++ viewer/scimpi/dispatcher/src/main/java/org/apache/isis/viewer/scimpi/dispatcher/context/DefaultOidObjectMapping.java
@@ -20,11 +20,6 @@
 
 package org.apache.isis.viewer.scimpi.dispatcher.context;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -32,22 +27,26 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.isis.core.commons.debug.DebugBuilder;
-import org.apache.isis.core.commons.encoding.DataInputStreamExtended;
-import org.apache.isis.core.commons.encoding.DataOutputExtended;
-import org.apache.isis.core.commons.encoding.DataOutputStreamExtended;
 import org.apache.isis.core.commons.exceptions.IsisException;
 import org.apache.isis.core.metamodel.adapter.ObjectAdapter;
 import org.apache.isis.core.metamodel.adapter.oid.AggregatedOid;
 import org.apache.isis.core.metamodel.adapter.oid.Oid;
+import org.apache.isis.core.metamodel.facets.collections.modify.CollectionFacet;
+import org.apache.isis.core.metamodel.facets.object.encodeable.EncodableFacet;
 import org.apache.isis.core.metamodel.spec.ObjectSpecification;
 import org.apache.isis.core.metamodel.spec.ObjectSpecification.CreationMode;
+import org.apache.isis.core.metamodel.spec.feature.ObjectAssociation;
+import org.apache.isis.core.metamodel.spec.feature.OneToManyAssociation;
+import org.apache.isis.core.metamodel.spec.feature.OneToOneAssociation;
 import org.apache.isis.runtimes.dflt.runtime.context.IsisContext;
-import org.apache.isis.runtimes.dflt.runtime.memento.Memento;
 import org.apache.isis.runtimes.dflt.runtime.persistence.oidgenerator.simple.SerialOid;
 import org.apache.isis.viewer.scimpi.dispatcher.ScimpiException;
 import org.apache.isis.viewer.scimpi.dispatcher.context.RequestContext.Scope;
 import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class DefaultOidObjectMapping implements ObjectMapping {
@@ -94,20 +93,57 @@ public class DefaultOidObjectMapping implements ObjectMapping {
     }
 
     public String encodedObject(ObjectAdapter object) {
-         Memento memento = new Memento(object);
-        
-        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        final DataOutputStreamExtended outputImpl = new DataOutputStreamExtended(baos);
         try {
-            // TODO record the OID for the this object so it can be re-reference without being rewriting the data
-            memento.encodedData(outputImpl);
-            final byte[] byteArray = baos.toByteArray();
-            String data = new String(byteArray);
-            return "D" + mapObject(object, Scope.INTERACTION) + "|" + URLEncoder.encode(data, "UTF-8");
-        } catch (IOException e) {
-            throw new IsisException("Failed to write object", e);
+            List<ObjectAdapter> savedObject = new ArrayList<ObjectAdapter>();
+            JSONObject data = saveData(object, savedObject);
+            return "D" + StringEscapeUtils.escapeHtml(data.toString(4));
+        } catch (JSONException e) {
+            throw new ScimpiException(e);
         }
     }
+
+    protected JSONObject saveData(ObjectAdapter object, List<ObjectAdapter> savedObject) throws JSONException {
+        if (savedObject.contains(object)) {
+            return null;
+        }
+        savedObject.add(object);
+
+        JSONObject data = new JSONObject();
+        ObjectSpecification specification = object.getSpecification();
+        data.put("_class", specification.getFullIdentifier());
+        data.put("_id", encodedOid(object.getOid()));
+        
+        for (ObjectAssociation association : specification.getAssociations()) {
+            ObjectAdapter fieldValue = association.get(object);
+            String fieldName = association.getId();
+            if (fieldValue == null) {
+                data.put(fieldName, (Object) null);
+            } else if (association.getSpecification().isEncodeable()) {
+                EncodableFacet encodeableFacet = fieldValue.getSpecification().getFacet(EncodableFacet.class);
+                data.put(fieldName, encodeableFacet.toEncodedString(fieldValue));
+            } else if (association instanceof OneToManyAssociation) {
+                List<JSONObject> collection = new ArrayList<JSONObject>();
+                CollectionFacet facet = fieldValue.getSpecification().getFacet(CollectionFacet.class);
+                for (ObjectAdapter element : facet.iterable(fieldValue)) {
+                   collection.add(saveData(element, savedObject));
+                }
+                data.put(fieldName, collection);
+            } else {
+                if (fieldValue.isTransient()) {
+                    JSONObject saveData = saveData(fieldValue, savedObject);
+                    if (saveData == null) {
+                        data.put(fieldName, mapObject(fieldValue, Scope.INTERACTION));
+                    } else {
+                        data.put(fieldName, saveData);
+                    }
+                } else {
+                    data.put(fieldName, mapObject(fieldValue, Scope.INTERACTION));
+                }
+            }
+        }
+        return data;
+    }
+    
     
     public String mapObject(ObjectAdapter inObject, Scope scope) {
         // TODO need to ensure that transient objects are remapped each time so that any changes are added to
@@ -115,74 +151,111 @@ public class DefaultOidObjectMapping implements ObjectMapping {
         // continue work here.....here
 
         ObjectAdapter object = inObject;
-        try {
-            Oid oid = object.getOid();
-            if (oidType == null) {
-                oidType = oid.getClass();
-            }
+        Oid oid = object.getOid();
+        if (oidType == null) {
+            oidType = oid.getClass();
+        }
+        
+        String encodedOid;
+        if (oid instanceof AggregatedOid) {
+            AggregatedOid aoid = (AggregatedOid) oid;
+            Oid parentOid = aoid.getParentOid();
+            object = IsisContext.getPersistenceSession().getAdapterManager().getAdapterFor(parentOid);
+        }
+        encodedOid = encodedOid(oid);
+        boolean isTransient = object.isTransient();
+        String id = (isTransient ? "T" : "P") + object.getSpecification().getFullIdentifier() + "@" + encodedOid;
+        LOG.debug("encoded " + oid + " as " + id + " ~ " + encodedOid);
+        if (isTransient) {
             
-            String encodedOid;
-            if (oid instanceof AggregatedOid) {
-                AggregatedOid aoid = (AggregatedOid) oid;
-                Oid parentOid = aoid.getParentOid();
-                String fieldName = aoid.getId();
-                int element = -1; // aoid.getElement();
-                
-                object = IsisContext.getPersistenceSession().getAdapterManager().getAdapterFor(parentOid);
-                encodedOid = Long.toString(((SerialOid) parentOid).getSerialNo(), 16) + "@" + fieldName + (element == -1 ? "" : "@" + element);
-            } else  if (oid instanceof SerialOid) {
-                encodedOid = Long.toString(((SerialOid) oid).getSerialNo(), 16);
+            // TODO cache these in requests so that Mementos are only created once.
+            
+             // TODO if Transient/Interaction then return state; other store state in session an return OID string 
+            TransientObjectMapping mapping = new TransientObjectMapping((ObjectAdapter) inObject);
+            if (scope == Scope.REQUEST) {
+                requestTransients.put(id, mapping);
+            } else if (scope ==Scope.INTERACTION || scope == Scope.SESSION) {
+                sessionTransients.put(id, mapping);
             } else {
-                ByteArrayOutputStream output = new ByteArrayOutputStream();
-                DataOutputExtended outputStream = new DataOutputStreamExtended(output);
-                oid.encode(outputStream);
-                //  encodedOid = URLEncoder.encode(output.toString(), "UTF-8");
-                encodedOid = IsisContext.getPersistenceSession().getOidGenerator().getOidStringifier().enString(oid);
-                // outputStream.flush();
+                throw new ScimpiException("Can't hold globally transient object");
             }
-            boolean isTransient = object.isTransient();
-            String id = (isTransient ? "T" : "P") + object.getSpecification().getFullIdentifier() + "@" + encodedOid;
-            LOG.debug("encoded " + oid + " as " + id + " ~ " + encodedOid);
-            if (isTransient) {
-                
-                // TODO cache these in requests so that Mementos are obly created once.
-                
-                 // TODO if Transient/Interaction then return state; other store state in session an return OID string 
-                TransientObjectMapping mapping = new TransientObjectMapping((ObjectAdapter) inObject);
-                if (scope == Scope.REQUEST) {
-                    requestTransients.put(id, mapping);
-                } else if (scope ==Scope.INTERACTION || scope == Scope.SESSION) {
-                    sessionTransients.put(id, mapping);
-                } else {
-                    throw new ScimpiException("Can't hold globally transient object");
-                }
-            }
-            return id;
-        } catch (IOException e) {
-            throw new IsisException(e);
+        }
+        return id;
+    }
+    
+    static String encodedOid(Oid oid) {
+        if (oid instanceof AggregatedOid) {
+            AggregatedOid aoid = (AggregatedOid) oid;
+            Oid parentOid = aoid.getParentOid();
+            String fieldName = aoid.getId();
+            int element = -1; // aoid.getElement();
+            
+            return Long.toString(((SerialOid) parentOid).getSerialNo(), 16) + "@" + fieldName + (element == -1 ? "" : "@" + element);
+        } else  if (oid instanceof SerialOid) {
+            return Long.toString(((SerialOid) oid).getSerialNo(), 16);
+        } else {
+            return IsisContext.getPersistenceSession().getOidGenerator().getOidStringifier().enString(oid);
         }
     }
 
     public ObjectAdapter decodeObject(String data) {
-        byte[] oidBytes;
+        String objectData = StringEscapeUtils.unescapeHtml(data);
+        LOG.debug("data" + objectData);
+
         try {
-            int x = data.indexOf('|');
-            String id = data.substring(0, x);
-            String objectData = data.substring(x + 1);
-            String state = URLDecoder.decode(objectData, "UTF-8");
-            
-            ObjectAdapter object = mappedObject(id);
-            Memento memento = new Memento(object);
-            
-            oidBytes = state.getBytes();
-            final ByteArrayInputStream bais = new ByteArrayInputStream(oidBytes);
-            final DataInputStreamExtended inputImpl = new DataInputStreamExtended(bais);
-            memento.restore(inputImpl);
-            memento.recreateObject();
+            JSONObject jsonObject = new JSONObject(objectData);
+            ObjectAdapter object = readObject(jsonObject);
             return object;
-        } catch (IOException ex) {
-            throw new IsisException("Failed to read object", ex);
+        } catch (JSONException e) {
+            throw new ScimpiException("Problem reading data: " + data, e);
         }
+    }
+
+    protected ObjectAdapter readObject(JSONObject jsonObject) throws JSONException {
+        String cls = (String) jsonObject.get("_class");
+        String oid = jsonObject.getString("_id");
+        
+        ObjectAdapter object = mappedObject("T" + cls + "@" + oid);
+        
+        ObjectSpecification specification = object.getSpecification();
+        for (ObjectAssociation association : specification.getAssociations()) {
+            String fieldName = association.getId();
+
+            
+            Object fieldValue = jsonObject.has(fieldName) ? jsonObject.get(fieldName) : null;
+
+            if (association.getSpecification().isEncodeable()) {
+                if (fieldValue == null) {
+                    ((OneToOneAssociation) association).initAssociation(object, null);
+                } else {
+                    EncodableFacet encodeableFacet = association.getSpecification().getFacet(EncodableFacet.class);
+                    ObjectAdapter fromEncodedString = encodeableFacet.fromEncodedString((String) fieldValue);
+                    ((OneToOneAssociation) association).initAssociation(object, fromEncodedString);
+                }
+            } else if (association instanceof OneToManyAssociation) {
+                /*
+                List<JSONObject> collection = new ArrayList<JSONObject>();
+                CollectionFacet facet = fieldValue.getSpecification().getFacet(CollectionFacet.class);
+                for (ObjectAdapter element : facet.iterable(fieldValue)) {
+                   collection.add(saveData(element, savedObject));
+                }
+                data.put(fieldName, collection);
+                */
+            } else {
+                if (fieldValue == null) {
+                    ((OneToOneAssociation) association).initAssociation(object, null);
+                } else {
+                    if (fieldValue instanceof JSONObject) {
+                        ObjectAdapter fieldObject = readObject((JSONObject) fieldValue);
+                        ((OneToOneAssociation) association).initAssociation(object, fieldObject);
+                    } else {
+                        ObjectAdapter field = mappedObject((String) fieldValue);
+                        ((OneToOneAssociation) association).initAssociation(object, field);
+                    }
+                }
+            }
+        }
+        return object;
     }
     
     public ObjectAdapter mappedObject(String id) {
@@ -199,8 +272,6 @@ public class DefaultOidObjectMapping implements ObjectMapping {
                 String oidData = split[1];
                 SerialOid oid = SerialOid.createTransient(Long.valueOf(oidData, 16).longValue());
                 return IsisContext.getPersistenceSession().recreateAdapter(oid, pojo);
-                
-//                return null;
             }
             ObjectAdapter mappedTransientObject = mapping.getObject();
             LOG.debug("retrieved " + mappedTransientObject.getOid() + " for " + id);
@@ -208,68 +279,34 @@ public class DefaultOidObjectMapping implements ObjectMapping {
         } else {
             String[] split = id.split("@");
             ObjectSpecification spec = IsisContext.getSpecificationLoader().loadSpecification(split[0].substring(1));
-            
-           try {
-               String oidData = split[1];
-               LOG.debug("decoding " + oidData);
-               
-               ObjectAdapter loadObject;
-               Oid oid;
-               // HACK - to remove after fix!
-               if (oidType == null) {
-                   oidType = IsisContext.getPersistenceSession().getServices().get(0).getOid().getClass();
-               }
-               if (split.length > 2) {
-                   SerialOid parentOid = SerialOid.createPersistent(Long.parseLong(oidData, 16));
-                  // int element = (split.length == 3) ? -1 : Integer.valueOf(split[3]);
-                   //oid = new AggregatedOid(parentOid, split[2], element);
-                   oid = new AggregatedOid(parentOid, split[2]);
-                   IsisContext.getPersistenceSession().loadObject(parentOid, spec);
-                   loadObject = IsisContext.getPersistenceSession().getAdapterManager().getAdapterFor(oid); 
-               } else if (oidType.isAssignableFrom(SerialOid.class)) {
-                   oid = SerialOid.createPersistent(Long.parseLong(oidData, 16));
-                   loadObject = IsisContext.getPersistenceSession().loadObject(oid, spec);
-               } else {
-                   oid = IsisContext.getPersistenceSession().getOidGenerator().getOidStringifier().deString(oidData);
-/*                   String decodedOid = URLDecoder.decode(oidData, "UTF-8");
-                   DataInputExtended stream = new DataInputStreamExtended(new ByteArrayInputStream(decodedOid.getBytes()));
-                   
-                    Constructor<? extends Oid> constructor = oidType.getConstructor(DataInputExtended.class);
-                    oid = constructor.newInstance(stream);         
-                    LOG.debug("decoded " + id + " as " + oid + ": " + oidData + " ~ " + decodedOid);
-   */
-                   loadObject = IsisContext.getPersistenceSession().loadObject(oid, spec);
 
-               }
+            try {
+                String oidData = split[1];
+                LOG.debug("decoding " + oidData);
 
-               
-          
-                    
+                ObjectAdapter loadObject;
+                Oid oid;
+                // HACK - to remove after fix!
+                if (oidType == null) {
+                    oidType = IsisContext.getPersistenceSession().getServices().get(0).getOid().getClass();
+                }
+                if (split.length > 2) {
+                    SerialOid parentOid = SerialOid.createPersistent(Long.parseLong(oidData, 16));
+                    oid = new AggregatedOid(parentOid, split[2]);
+                    IsisContext.getPersistenceSession().loadObject(parentOid, spec);
+                    loadObject = IsisContext.getPersistenceSession().getAdapterManager().getAdapterFor(oid); 
+                } else if (oidType.isAssignableFrom(SerialOid.class)) {
+                    oid = SerialOid.createPersistent(Long.parseLong(oidData, 16));
+                    loadObject = IsisContext.getPersistenceSession().loadObject(oid, spec);
+                } else {
+                    oid = IsisContext.getPersistenceSession().getOidGenerator().getOidStringifier().deString(oidData);
+                    loadObject = IsisContext.getPersistenceSession().loadObject(oid, spec);
+                }
+
                 return loadObject;
             } catch (SecurityException e) {
                 throw new IsisException(e);
-                /*
-            } catch (NoSuchMethodException e) {
-                throw new IsisException(e);
-            } catch (IllegalArgumentException e) {
-                throw new IsisException(e);
-            } catch (InstantiationException e) {
-                throw new IsisException(e);
-            } catch (IllegalAccessException e) {
-                throw new IsisException(e);
-            } catch (InvocationTargetException e) {
-                throw new IsisException(e);
-            } catch (UnsupportedEncodingException e) {
-                throw new IsisException(e);
-                */
             }
-            
-            /*
-     //       long serialNumber = Long.parseLong(id.substring(pos + 1), 36);
-   //         *** Oid oid = isTransient ? SerialOid.createTransient(serialNumber) : SerialOid.createPersistent(serialNumber);
-            Oid oid = null;
-            return IsisContext.getPersistenceSession().loadObject(oid, spec);
-            */
         }
     }
 
