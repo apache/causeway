d74606b233f4d1a81c63c88c38972af1d8f2edfc
Daniel Keir Haywood <danhaywood@apache.org>
2011-05-13 14:32:08 +0000

    ISIS-20 adding missing licenses to .properties files (including .allow and .passwords files)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1102757 13f79535-47bb-0310-9956-ffa450edef68

--- addmissinglicenses.groovy
+++ addmissinglicenses.groovy
@@ -86,9 +86,27 @@ def license_using_hash_comments="""#  Licensed to the Apache Software Foundation
 #  under the License.
 """
 
+def license_using_rem_comments="""REM  Licensed to the Apache Software Foundation (ASF) under one
+REM  or more contributor license agreements.  See the NOTICE file
+REM  distributed with this work for additional information
+REM  regarding copyright ownership.  The ASF licenses this file
+REM  to you under the Apache License, Version 2.0 (the
+REM  "License"); you may not use this file except in compliance
+REM  with the License.  You may obtain a copy of the License at
+REM  
+REM         http://www.apache.org/licenses/LICENSE-2.0
+REM         
+REM  Unless required by applicable law or agreed to in writing,
+REM  software distributed under the License is distributed on an
+REM  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+REM  KIND, either express or implied.  See the License for the
+REM  specific language governing permissions and limitations
+REM  under the License.
+"""
 
-def fileEndings = [".shtml"]
-//def fileEndings = [".xml", ".html", ".shtml", ".css", ".java", ".sh", ".properties", ".groovy", ".allow", ".passwords"]
+
+def fileEndings = [".groovy"]
+//def fileEndings = [".xml", ".html", ".shtml", ".css", ".java", ".sh", ".properties", ".groovy", ".allow", ".passwords", ".bat"]
 
 def licenseTextByFileEnding = [
 	".java": license_using_c_style_comments,
@@ -101,6 +119,7 @@ def licenseTextByFileEnding = [
     ".allow": license_using_hash_comments,
     ".passwords": license_using_hash_comments,
 	".sh": license_using_hash_comments,
+	".bat": license_using_rem_comments,
 	]
 
 
