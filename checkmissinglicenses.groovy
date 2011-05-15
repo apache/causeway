98980cc700ac2beae67bf07dad4052c305591ab3
Daniel Keir Haywood <danhaywood@apache.org>
2011-05-15 00:11:28 +0000

    ISIS-20: adding checkmissinglicenses.groovy script
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1103255 13f79535-47bb-0310-9956-ffa450edef68

---
+++ checkmissinglicenses.groovy
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//
+// prereq:
+//
+// run mvn license:download-licenses
+//
+// this will create target/generated-resources/license.xml for each module,
+// which this script then parses through.
+//
+
+import groovy.xml.StreamingMarkupBuilder
+import groovy.xml.XmlUtil
+import javax.xml.transform.*
+import javax.xml.transform.stream.*
+
+def currentDir=new File(".")
+
+currentDir.eachFileRecurse { file ->
+  if ( file.name == "licenses.xml" ) {
+
+    println( file.canonicalPath )
+
+    def fileXml = new XmlSlurper().parseText(file.text)
+
+
+    fileXml.dependencies.dependency.each { dependency ->
+
+      def url = dependency.licenses.license.url.text()
+
+      if(! url) {
+	println( dependency.groupId.text() + " : " + dependency.artifactId.text() + " : " + dependency.version.text() )
+      }
+
+    }
+  }
+}
