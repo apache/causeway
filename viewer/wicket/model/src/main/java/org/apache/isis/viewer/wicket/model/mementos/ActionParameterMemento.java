8ffbc17cd37ee9013e2233c13265535c6df70286
Daniel Keir Haywood <danhaywood@apache.org>
2011-05-17 06:42:55 +0000

    ISIS-20 fixing Javadoc formatting issue
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1104016 13f79535-47bb-0310-9956-ffa450edef68

--- viewer/wicket/model/src/main/java/org/apache/isis/viewer/wicket/model/mementos/ActionParameterMemento.java
+++ viewer/wicket/model/src/main/java/org/apache/isis/viewer/wicket/model/mementos/ActionParameterMemento.java
@@ -26,8 +26,9 @@ import org.apache.isis.core.metamodel.spec.feature.ObjectAction;
 import org.apache.isis.core.metamodel.spec.feature.ObjectActionParameter;
 
 /**
- * {@link Serializable} representation of a {@link ObjectActioneter parameter} of a {@link ObjecObjectActionsee
- * ActionMemento
+ * {@link Serializable} representation of a {@link ObjectActioneter parameter} of a {@link ObjecObjectAction}.
+ * 
+ * @see ActionMemento
  */
 public class ActionParameterMemento implements Serializable {
 
