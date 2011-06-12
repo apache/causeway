f84fefd5d036a8e591e4d753ea342cb9677358eb
Daniel Keir Haywood <danhaywood@apache.org>
2011-06-12 10:23:28 +0000

    reapplying further changes made in trunk (rat tool) since copy
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/branches/0.1.2-RC4-incubating/trunk@1134903 13f79535-47bb-0310-9956-ffa450edef68

--- updateiconlibrary.groovy
+++ updateiconlibrary.groovy
@@ -104,11 +104,19 @@ iconsByCategoryByPage.each { pageName, iconsByCategory ->
   def numCols = colsPerRow[pageName]
   def buf = new StringBuilder()
 
-  buf.append("<html><head><title>${pageName}</title></head><body>\n")
-  buf.append("<!-- START SNIPPET: icon-table -->\n")
-
-  //def numIcons = 0
-  //buf.append("<table>\n")
+  buf.append("""<!-- Licensed to the Apache Software Foundation (ASF) under one or more contributor 
+    license agreements. See the NOTICE file distributed with this work for additional 
+    information regarding copyright ownership. The ASF licenses this file to 
+    you under the Apache License, Version 2.0 (the "License"); you may not use 
+    this file except in compliance with the License. You may obtain a copy of 
+    the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required 
+    by applicable law or agreed to in writing, software distributed under the 
+    License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS 
+    OF ANY KIND, either express or implied. See the License for the specific 
+    language governing permissions and limitations under the License. -->
+<html><head><title>${pageName}</title></head><body>
+<!-- START SNIPPET: icon-table -->
+""")
 
   iconsByCategory.each { categoryName, iconsByName ->
 
@@ -147,19 +155,12 @@ iconsByCategoryByPage.each { pageName, iconsByCategory ->
         buf.append("<td/>")
       }
       buf.append("</tr>\n")
-      //numIcons=0
     }
-    //buf.append("<tr>\n")
-    //numCols.each {
-      //buf.append("<td/>")
-    //}
-    //buf.append("</tr>\n")
     buf.append("</table>\n")
   }
 
-  //buf.append("</table>\n")
-  buf.append("<!-- END SNIPPET: icon-table -->\n")
-  buf.append("</body></html>\n")
+  buf.append("""<!-- END SNIPPET: icon-table -->
+</body></html>\n""")
 
   def pageFileText = buf.toString()
   pageFile.text = pageFileText
