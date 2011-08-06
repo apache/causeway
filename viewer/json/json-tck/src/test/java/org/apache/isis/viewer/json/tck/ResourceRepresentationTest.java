536940ee803401ef384af701c948b02dd71d8cf1
Daniel Keir Haywood <danhaywood@apache.org>
2011-08-06 18:15:23 +0000

    ISIS-109: Json viewer tests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk/framework@1154565 13f79535-47bb-0310-9956-ffa450edef68

--- viewer/json/json-tck/src/test/java/org/apache/isis/viewer/json/tck/ResourceRepresentationTest.java
+++ viewer/json/json-tck/src/test/java/org/apache/isis/viewer/json/tck/ResourceRepresentationTest.java
@@ -1,14 +1,12 @@
+package org.apache.isis.viewer.json.tck;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
-import java.net.URI;
-
 import javax.ws.rs.core.Response;
 
 import org.apache.isis.runtimes.dflt.webserver.WebServer;
-import org.apache.isis.viewer.json.applib.JsonRepresentation;
 import org.apache.isis.viewer.json.applib.RestfulClient;
 import org.apache.isis.viewer.json.applib.blocks.Method;
 import org.apache.isis.viewer.json.applib.domain.DomainObjectRepresentation;
@@ -18,14 +16,13 @@ import org.apache.isis.viewer.json.applib.homepage.HomePageRepresentation;
 import org.apache.isis.viewer.json.applib.homepage.HomePageResource;
 import org.apache.isis.viewer.json.applib.util.HttpStatusCode;
 import org.apache.isis.viewer.json.applib.util.JsonResponse;
-import org.codehaus.jackson.JsonNode;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 
-public class JsonViewerEndToEndTest {
+public class ResourceRepresentationTest {
 
     @BeforeClass
     public static void setUpClass() throws Exception {
@@ -38,17 +35,17 @@ public class JsonViewerEndToEndTest {
         webServer.stop();
     }
 
-    private static WebServer webServer;
-    private RestfulClient client;
+    protected static WebServer webServer;
+    protected RestfulClient client;
 
     @Before
     public void setUp() throws Exception {
         client = new RestfulClient(webServer.getBase());
     }
-    
-    
+
+
     @Test
-    public void homePageResources() throws Exception {
+    public void homePageResource_returnsHomePageRepresentation() throws Exception {
         // given
         HomePageResource homePageResource = client.getHomePageResource();
         
@@ -68,24 +65,8 @@ public class JsonViewerEndToEndTest {
     }
 
     @Test
-    public void homePageResource_canMarshallToJsonNode() throws Exception {
-        // given
-        HomePageResource homePageResource = client.getHomePageResource();
-        
-        // when
-        Response resourcesResp = homePageResource.resources();
-        JsonResponse<JsonNode> homePageJsonResp = JsonResponse.of(resourcesResp, JsonNode.class);
-        
-        // then
-        assertThat(homePageJsonResp.getStatus(), is(HttpStatusCode.OK));
-        JsonNode homePage = homePageJsonResp.getEntity();
-
-        assertThat(homePage.isArray(), is(false));
-    }
-
-
-    @Test
     public void homePageResource_linksToServicesResource() throws Exception {
+        
         // given
         HomePageResource homePageResource = client.getHomePageResource();
 
@@ -107,9 +88,8 @@ public class JsonViewerEndToEndTest {
         assertThat(applibValuesEntityRepoRep, is(not(nullValue())));
     }
 
-
     @Test
-    public void servicesResource_canBeCalledDirectly() throws Exception {
+    public void servicesResource_returnsServicesRepresentation() throws Exception {
         // given
         ServicesResource servicesResource = client.getServicesResource();
         
@@ -124,62 +104,5 @@ public class JsonViewerEndToEndTest {
         assertThat(applibValuesEntityRepoRep, is(not(nullValue())));
     }
 
-
-    @Test
-    public void jsonResponse_canFollow_usingGet() throws Exception {
-        // given
-        HomePageResource homePageResource = client.getHomePageResource();
-
-        // when
-        Response resourcesResp = homePageResource.resources();
-        JsonResponse<HomePageRepresentation> homePageJsonResp = JsonResponse.of(resourcesResp, HomePageRepresentation.class);
-        
-        // then
-        HomePageRepresentation homePageRepr = homePageJsonResp.getEntity();
-        
-        // and when
-        Response servicesResp = client.follow(homePageRepr.getServices());
-        JsonResponse<ServicesRepresentation> servicesJsonResp = JsonResponse.of(servicesResp, ServicesRepresentation.class);
-        
-        // then
-        assertThat(servicesJsonResp.getStatus(), is(HttpStatusCode.OK));
-        ServicesRepresentation servicesRepr = servicesJsonResp.getEntity();
-        
-        assertThat(servicesRepr, is(not(nullValue())));
-        assertThat(servicesRepr.get(0), is(not(nullValue())));
-    }
-
-    @Test
-    public void jsonRepresentation_deserializeFromMap() throws Exception {
-        // given
-        HomePageResource homePageResource = client.getHomePageResource();
-
-        // when
-        Response resourcesResp = homePageResource.resources();
-        JsonResponse<JsonRepresentation> homePageJsonResp = JsonResponse.of(resourcesResp, JsonRepresentation.class);
-        
-        // then
-        assertThat(homePageJsonResp.getStatus(), is(HttpStatusCode.OK));
-        JsonRepresentation jsonRepr = homePageJsonResp.getEntity();
-
-        assertThat(jsonRepr, is(not(nullValue())));
-        assertThat(jsonRepr.getJsonNode().isArray(), is(false));
-    }
-
-    @Test
-    public void jsonRepresentation_deserializeFromList() throws Exception {
-        // given
-        ServicesResource servicesResource = client.getServicesResource();
-        
-        // when
-        Response servicesResp = servicesResource.services();
-        JsonResponse<JsonRepresentation> servicesJsonResp = JsonResponse.of(servicesResp, JsonRepresentation.class);
-        
-        // then
-        JsonRepresentation jsonRepr = servicesJsonResp.getEntity();
-
-        assertThat(jsonRepr, is(not(nullValue())));
-        assertThat(jsonRepr.getJsonNode().isArray(), is(true));
-    }
 
 }
