781bf32f275531ecf31e7055d0c7bf9d6553c33b
Daniel Keir Haywood <danhaywood@apache.org>
2012-01-22 17:33:39 +0000

    ISIS-165: RAT, license check for 0.2.0-incubating
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1234556 13f79535-47bb-0310-9956-ffa450edef68

--- framework/viewer/junit/src/main/java/org/apache/isis/viewer/junit/internal/AnnotationInstaller.java
+++ framework/viewer/junit/src/main/java/org/apache/isis/viewer/junit/internal/AnnotationInstaller.java
@@ -1,3 +1,21 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package org.apache.isis.viewer.junit.internal;
 
 import org.apache.isis.core.runtime.authentication.AuthenticationManagerInstaller;
@@ -6,7 +24,7 @@ import org.apache.isis.runtimes.dflt.runtime.installerregistry.installerapi.Pers
 import org.apache.isis.security.dflt.authentication.NoopAuthenticationManagerInstaller;
 import org.apache.isis.security.dflt.authorization.NoopAuthorizationManagerInstaller;
 import org.apache.isis.viewer.junit.Authenticator;
-import org.apache.isis.viewer.junit.Authorizer;
+import org.apache.isis.viewer.junit.Authorizor;
 import org.apache.isis.viewer.junit.Persistor;
 
 public class AnnotationInstaller {
@@ -39,7 +57,7 @@ public class AnnotationInstaller {
 
     // {{ AuthorizationManagerInstaller
     public AuthorizationManagerInstaller addAuthorizerAnnotatedOn(final Class<?> javaClass) throws InstantiationException, IllegalAccessException {
-        final Authorizer authorizorAnnotation = javaClass.getAnnotation(Authorizer.class);
+        final Authorizor authorizorAnnotation = javaClass.getAnnotation(Authorizor.class);
         if (authorizorAnnotation != null) {
             return addAuthorizerRepresentedBy(authorizorAnnotation);
         } else {
@@ -48,7 +66,7 @@ public class AnnotationInstaller {
 
     }
 
-    private AuthorizationManagerInstaller addAuthorizerRepresentedBy(final Authorizer authorizorAnnotation) throws InstantiationException, IllegalAccessException {
+    private AuthorizationManagerInstaller addAuthorizerRepresentedBy(final Authorizor authorizorAnnotation) throws InstantiationException, IllegalAccessException {
         final Class<?> fixtureClass = authorizorAnnotation.value();
         return (AuthorizationManagerInstaller) fixtureClass.newInstance();
     }
