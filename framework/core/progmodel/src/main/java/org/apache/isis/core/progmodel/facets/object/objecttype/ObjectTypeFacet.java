94d427703621c0b8edd277c04d83453bee89a23b
Daniel Keir Haywood <danhaywood@apache.org>
2012-03-15 11:03:11 +0000

    ISIS-48: added JPA metamodel into the main build
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1300908 13f79535-47bb-0310-9956-ffa450edef68

--- framework/core/progmodel/src/main/java/org/apache/isis/core/progmodel/facets/object/objecttype/ObjectTypeFacet.java
+++ framework/core/progmodel/src/main/java/org/apache/isis/core/progmodel/facets/object/objecttype/ObjectTypeFacet.java
@@ -1,22 +1,12 @@
 package org.apache.isis.core.progmodel.facets.object.objecttype;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
 
 import org.apache.isis.core.metamodel.facets.SingleStringValueFacet;
 
 
 /**
- * Corresponds to annotating the class with the {@link DiscriminatorValue}
- * annotation.
- * <p>
- * Maps onto the information in {@link Entity} as follows:
- * <ul>
- * <li>{@link DiscriminatorValue#value()} ->
- * {@link ObjectTypeFacet#value()}</li>
- * </ul>
+ * Identifies the type of entity, such that OIDs are self-describing.
  */
 public interface ObjectTypeFacet extends SingleStringValueFacet {
-
 
 }
