3251decdca06ff1689901b26260bb3566d7af908
Robert Charles Matthews <rmatthews@apache.org>
2012-07-23 11:28:52 +0000

    ISIS-162 - Expand element to display graph of specifications.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/isis/trunk@1364587 13f79535-47bb-0310-9956-ffa450edef68

--- framework/viewer/scimpi/scimpi-dispatcher/src/main/java/org/apache/isis/viewer/scimpi/dispatcher/view/debug/Specification.java
+++ framework/viewer/scimpi/scimpi-dispatcher/src/main/java/org/apache/isis/viewer/scimpi/dispatcher/view/debug/Specification.java
@@ -19,14 +19,17 @@
 
 package org.apache.isis.viewer.scimpi.dispatcher.view.debug;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.isis.core.metamodel.adapter.ObjectAdapter;
 import org.apache.isis.core.metamodel.spec.ObjectSpecification;
 import org.apache.isis.core.metamodel.spec.feature.ObjectAssociation;
 import org.apache.isis.viewer.scimpi.dispatcher.AbstractElementProcessor;
 import org.apache.isis.viewer.scimpi.dispatcher.context.RequestContext;
 import org.apache.isis.viewer.scimpi.dispatcher.processor.Request;
+
 
 public class Specification extends AbstractElementProcessor {
 
@@ -44,15 +47,67 @@ public class Specification extends AbstractElementProcessor {
             final String id = request.getOptionalProperty("object");
             final ObjectAdapter object = context.getMappedObjectOrResult(id);
             final ObjectSpecification specification = object.getSpecification();
-            request.appendHtml(specification.getSingularName() + " (" + specification.getFullIdentifier() + ") \n");
-            final List<ObjectAssociation> fields = specification.getAssociations();
-            for (int i = 0; i < fields.size(); i++) {
-                request.appendHtml("    " + fields.get(i).getName() + " (" + fields.get(i).getSpecification().getSingularName() + ") \n");
+            String type = request.getOptionalProperty(TYPE, "details");
+
+            if (type.equals("graph")) {
+                specificationGraph(request, specification, null, new ArrayList<ObjectSpecification>(), 0);
+            } else if (type.equals("details")) {
+                specificationDetails(request, specification);
+            } else {
+                request.appendHtml("invalid type: " + type);
             }
 
             request.appendHtml("</pre>");
             request.appendHtml("</div>");
         }
+    }
+
+    private void specificationDetails(final Request request, final ObjectSpecification specification) {
+        renderName(request, specification);
+        final List<ObjectAssociation> fields = specification.getAssociations();
+        for (int i = 0; i < fields.size(); i++) {
+            request.appendHtml("    " + fields.get(i).getName() + " (" + fields.get(i).getSpecification().getSingularName()
+                    + ") \n");
+        }
+    }
+
+    private void specificationGraph(
+            Request request,
+            ObjectSpecification specification,
+            String fieldName,
+            List<ObjectSpecification> processed,
+            int level) {
+        if (processed.contains(specification)) {
+            return;
+        }
+
+        request.appendHtml(StringUtils.repeat("    ", level));
+        if (processed.contains(specification)) {
+            request.appendHtml("* ");
+        }
+        request.appendHtml(specification.getFullIdentifier());
+        if (fieldName != null) {
+            request.appendHtml(" (" + fieldName + ")");
+        }
+        request.appendHtml("\n");
+
+        if (processed.contains(specification)) {
+            return;
+        }
+        processed.add(specification);
+
+        final List<ObjectAssociation> fields = specification.getAssociations();
+        for (int i = 0; i < fields.size(); i++) {
+            ObjectSpecification fieldSpecification = fields.get(i).getSpecification();
+            if (fieldSpecification.isValue()) {
+                continue;
+            }
+            specificationGraph(request, fieldSpecification, fields.get(i).getName(), processed, level + 1);
+        }
+    }
+
+    private void renderName(final Request request, final ObjectSpecification specification) {
+        request.appendHtml(specification.getSingularName() + " (" + specification.getFullIdentifier() + ") \n");
     }
 
     @Override
