f4ccac6c082b1cab0ed99ac47f9ee9dfa98c771f
Daniel Keir Haywood <danhaywood@apache.org>
2012-11-21 19:22:46 +0000

    ISIS-232: made wicket viewer's LinkAndLabel serializable...
    ... to avoid stacktraces.
    
    git-svn-id: https://svn.apache.org/repos/asf/isis/trunk@1412236 13f79535-47bb-0310-9956-ffa450edef68

--- framework/viewer/wicket/wicket-model/src/main/java/org/apache/isis/viewer/wicket/model/links/LinkAndLabel.java
+++ framework/viewer/wicket/wicket-model/src/main/java/org/apache/isis/viewer/wicket/model/links/LinkAndLabel.java
@@ -1,8 +1,13 @@
 package org.apache.isis.viewer.wicket.model.links;
 
+import java.io.Serializable;
+
 import org.apache.wicket.markup.html.link.AbstractLink;
 
-public class LinkAndLabel {
+public class LinkAndLabel implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
     private final AbstractLink link;
     private final String label;
 
