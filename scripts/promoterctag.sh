bca6bcc43d3879027d62b640eac20ef288ad03f8
Dan Haywood <danhaywood@apache.org>
2013-05-30 08:40:37 +0100

    ISIS-406: new promoterctag.sh helper script

---
+++ scripts/promoterctag.sh
@@ -0,0 +1,9 @@
+#
+# Removes an RC tag and replaces with final one
+#
+# usage: 
+# promoterctag isis-1.2.0 RC1
+#
+git tag -d $1-$2
+git push origin :refs/tags/$1-$2
+git push origin refs/tags/$1:refs/tags/$1
