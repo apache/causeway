28e68a736937c9c180113d2c6cd8903361dfa2d3
Dan Haywood <danhaywood@apache.org>
2013-07-31 01:02:29 +0100

    ISIS-484: new contract test for inject service methods not overridable

---
+++ core/unittestsupport/src/test/java/org/apache/isis/core/unittestsupport/inject/InjectServiceMethodMustBeFinalContractTestAll.java
@@ -0,0 +1,11 @@
+package org.apache.isis.core.unittestsupport.inject;
+
+
+public class InjectServiceMethodMustBeFinalContractTestAll extends InjectServiceMethodMustBeFinalContractTestAbstract {
+
+    public InjectServiceMethodMustBeFinalContractTestAll() {
+        super("org.apache.isis.core.unittestsupport.inject");
+        withLoggingTo(System.out);
+    }
+
+}
