c2f4d8cb5678d596480c171374fa2141d4dadbd1
Dan Haywood <danhaywood@apache.org>
2013-11-20 00:00:45 +0000

    ISIS-592: removing accidental dependency on JDK7.

--- core/applib/src/main/java/org/apache/isis/applib/services/xmlsnapshot/XmlSnapshotService.java
+++ core/applib/src/main/java/org/apache/isis/applib/services/xmlsnapshot/XmlSnapshotService.java
@@ -44,10 +44,6 @@ public interface XmlSnapshotService {
             super();
         }
 
-        public Exception(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-            super(message, cause, enableSuppression, writableStackTrace);
-        }
-
         public Exception(String message, Throwable cause) {
             super(message, cause);
         }
