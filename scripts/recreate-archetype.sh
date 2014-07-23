3fca9990d03c970e5b17f2af8464d80e54cdc526
Dan Haywood <dan@haywood-associates.co.uk>
2014-07-23 22:29:55 +0100

    ISIS-839: updating example app .launch files and out-of-date welcome.html and the about/index.html files.
    Also new recreate-archetype.sh script.

---
+++ scripts/recreate-archetype.sh
@@ -0,0 +1,70 @@
+export ISISART=$(basename $(pwd))-archetype
+
+TOFIX=""
+
+env | grep ISISTMP >/dev/null
+if [ $? -ne 0 ]; then
+    echo "\$ISISTMP not set!"
+    TOFIX="$TOFIX\nexport ISISTMP=/c/tmp"
+fi
+
+env | grep ISISDEV >/dev/null
+if [ $? -ne 0 ]; then
+    echo "\$ISISDEV not set!"
+    TOFIX="$TOFIX\nexport ISISDEV=1.7.0-SNAPSHOT"
+fi
+
+env | grep ISISREL >/dev/null
+if [ $? -ne 0 ]; then
+    echo "\$ISISREL not set!"
+    TOFIX="$TOFIX\nexport ISISDEV=1.6.0"
+fi
+
+env | grep ISISRC >/dev/null
+if [ $? -ne 0 ]; then
+    echo "\$ISISRC  not set!"
+    TOFIX="$TOFIX\nexport ISISRC=RC1"
+fi
+
+env | grep ISISPAR >/dev/null
+if [ $? -ne 0 ]; then
+    echo "\$ISISPAR not set! (Isis parent release, usually same as ISISREL)"
+    TOFIX="$TOFIX\nexport ISISPAR=$ISISREL"
+fi
+
+if [ "$TOFIX" != "" ]; then
+    echo -e $TOFIX
+    exit 1
+fi
+
+export ISISCPT=$(echo $ISISART | cut -d- -f2)
+export ISISCPN=$(echo $ISISART | cut -d- -f1)
+
+#
+#
+#
+env | grep ISIS | sort
+
+
+echo "mvn clean ..."
+mvn clean
+
+echo "removing other non-source files ..."
+for a in .project .classpath .settings bin .idea target-ide; do /bin/find . -name $a -exec rm -r {} \;; done
+/bin/find . -name "*.iml" -exec rm {} \;
+/bin/find . -name "*.log" -exec rm {} \;
+/bin/find . -name "pom.xml.*" -exec rm {} \;
+
+echo "mvn archetype:create-from-project ..."
+mvn archetype:create-from-project
+
+echo "groovy script to update archetypes ..."
+groovy ../../../scripts/updateGeneratedArchetypeSources.groovy -n $ISISCPN -v $ISISPAR
+
+echo "deleting old archetype ..."
+git rm -rf ../../archetype/$ISISCPN
+rm -rf ../../archetype/$ISISCPN
+
+echo "adding new archetype ..."
+mv target/generated-sources/archetype ../../archetype/$ISISCPN
+git add ../../archetype/$ISISCPN
