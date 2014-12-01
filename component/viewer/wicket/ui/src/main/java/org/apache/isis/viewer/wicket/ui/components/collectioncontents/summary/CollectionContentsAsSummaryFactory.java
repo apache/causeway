c05621aa6e29deffa3608f8b2ac6983ee7aa1270
Dan Haywood <dan@haywood-associates.co.uk>
2014-12-01 21:08:07 +0000

    ISIS-537: CollectionContentsAsSummary icon now uses fa- icon.

--- component/viewer/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/components/collectioncontents/summary/CollectionContentsAsSummaryFactory.java
+++ component/viewer/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/components/collectioncontents/summary/CollectionContentsAsSummaryFactory.java
@@ -23,6 +23,7 @@ import java.util.List;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.resource.CssResourceReference;
 
 import org.apache.isis.applib.filter.Filter;
@@ -31,6 +32,7 @@ import org.apache.isis.core.metamodel.spec.feature.Contributed;
 import org.apache.isis.core.metamodel.spec.feature.ObjectAssociation;
 import org.apache.isis.core.metamodel.facets.value.bigdecimal.BigDecimalValueFacet;
 import org.apache.isis.viewer.wicket.model.models.EntityCollectionModel;
+import org.apache.isis.viewer.wicket.ui.CollectionContentsAsFactory;
 import org.apache.isis.viewer.wicket.ui.ComponentFactory;
 import org.apache.isis.viewer.wicket.ui.ComponentFactoryAbstract;
 import org.apache.isis.viewer.wicket.ui.ComponentType;
@@ -38,7 +40,7 @@ import org.apache.isis.viewer.wicket.ui.ComponentType;
 /**
  * {@link ComponentFactory} for {@link CollectionContentsAsSummary}.
  */
-public class CollectionContentsAsSummaryFactory extends ComponentFactoryAbstract {
+public class CollectionContentsAsSummaryFactory extends ComponentFactoryAbstract implements CollectionContentsAsFactory {
 
     private static final long serialVersionUID = 1L;
 
@@ -78,4 +80,15 @@ public class CollectionContentsAsSummaryFactory extends ComponentFactoryAbstract
         // do not bundle, because of relative CSS images...
         return null;
     }
+
+    @Override
+    public IModel<String> getTitleLabel() {
+        return Model.of("Summary");
+    }
+
+    @Override
+    public IModel<String> getCssClass() {
+        return Model.of("fa fa-fw fa-usd");
+    }
+
 }
