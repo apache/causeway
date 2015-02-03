5e39335f67678cff53d9c99c5c347dbed4c259e6
Dan Haywood <dan@haywood-associates.co.uk>
2015-02-03 22:30:03 +0000

    ISIS-1025: support different logo URLs for signin page vs header page.

--- component/viewer/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/components/header/HeaderPanel.java
+++ component/viewer/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/components/header/HeaderPanel.java
@@ -1,3 +1,21 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package org.apache.isis.viewer.wicket.ui.components.header;
 
 import java.util.Locale;
@@ -18,6 +36,7 @@ import org.apache.isis.viewer.wicket.model.models.ServiceActionsModel;
 import org.apache.isis.viewer.wicket.ui.ComponentType;
 import org.apache.isis.viewer.wicket.ui.components.widgets.navbar.BrandLogo;
 import org.apache.isis.viewer.wicket.ui.components.widgets.navbar.BrandName;
+import org.apache.isis.viewer.wicket.ui.components.widgets.navbar.Placement;
 import org.apache.isis.viewer.wicket.ui.pages.error.ErrorPage;
 import org.apache.isis.viewer.wicket.ui.panels.PanelAbstract;
 import org.apache.isis.viewer.wicket.ui.util.Components;
@@ -71,8 +90,9 @@ public class HeaderPanel extends PanelAbstract<Model<String>> {
         Class<? extends Page> homePage = getApplication().getHomePage();
         final BookmarkablePageLink<Void> applicationNameLink = new BookmarkablePageLink<>("applicationName", homePage);
 
-        final BrandLogo brandImage = new BrandLogo("brandLogo");
-        final BrandName brandName = new BrandName("brandText");
+        final Placement placement = Placement.HEADER;
+        final BrandLogo brandImage = new BrandLogo("brandLogo", placement);
+        final BrandName brandName = new BrandName("brandText", Placement.HEADER);
         applicationNameLink.add(brandName, brandImage);
 
         add(applicationNameLink);
--- component/viewer/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/components/widgets/navbar/BrandLogo.java
+++ component/viewer/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/components/widgets/navbar/BrandLogo.java
@@ -1,3 +1,21 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package org.apache.isis.viewer.wicket.ui.components.widgets.navbar;
 
 import com.google.inject.name.Named;
@@ -9,30 +27,43 @@ import org.apache.wicket.markup.html.WebComponent;
  */
 public class BrandLogo extends WebComponent {
 
+    private final Placement placement;
 
     @com.google.inject.Inject(optional = true)
-    @Named("brandLogo")
-    private String logoUrl;
+    @Named("brandLogoHeader")
+    private String logoHeaderUrl;
+
+    @com.google.inject.Inject(optional = true)
+    @Named("brandLogoSignin")
+    private String logoSigninUrl;
 
     /**
      * Constructor.
      *
      * @param id The component id
+     * @param placement
      */
-    public BrandLogo(final String id) {
+    public BrandLogo(final String id, final Placement placement) {
         super(id);
+        this.placement = placement;
     }
 
     @Override
     protected void onComponentTag(final ComponentTag tag) {
         super.onComponentTag(tag);
 
-        tag.put("src", logoUrl);
+        tag.put("src", url());
     }
 
     @Override
     protected void onConfigure() {
         super.onConfigure();
-        setVisible(logoUrl != null);
+        setVisible(url() != null);
     }
+
+    private String url() {
+        return placement.urlFor(logoHeaderUrl, logoSigninUrl);
+    }
+
+
 }
