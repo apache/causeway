69284c63d605c6e22959909dd12e29a475279521
Dan Haywood <dan@haywood-associates.co.uk>
2015-02-04 07:34:32 +0000

    ISIS-987: adding some missing javadoc

--- core/applib/src/main/java/org/apache/isis/applib/services/email/EmailService.java
+++ core/applib/src/main/java/org/apache/isis/applib/services/email/EmailService.java
@@ -9,14 +9,25 @@ import javax.annotation.PostConstruct;
 import org.apache.isis.applib.annotation.Programmatic;
 
 /**
- * TODO ISIS-987 Javadoc
+ * Provides the ability to send emails to one or more recipients.
+ *
+ * <p>
+ * The core framework also provides a default implementation <tt>EmailServiceDefault</tt> that sends email as an
+ * HTML message, using an external SMTP provider.  See the Isis website for further details.
+ * </p>
  */
 public interface EmailService extends Serializable {
 
+    /**
+     * Always called by the framework, and allows the implementation to read configuration properties and initialize itself
+     */
     @PostConstruct
     @Programmatic
     public void init() ;
 
+    /**
+     * Main API to send email and optional attachments.
+     */
     @Programmatic
     boolean send(List<String> to, List<String> cc, List<String> bcc, String subject, String body, DataSource... attachments);
 
