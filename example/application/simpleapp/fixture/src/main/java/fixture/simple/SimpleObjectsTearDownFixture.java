540a108721304582982d03e3a8fe750167a0aeb0
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2015-02-09 00:48:54 +0200

    Use the non-deprecated IsisJdoSupport class

--- example/application/simpleapp/fixture/src/main/java/fixture/simple/SimpleObjectsTearDownFixture.java
+++ example/application/simpleapp/fixture/src/main/java/fixture/simple/SimpleObjectsTearDownFixture.java
@@ -20,7 +20,7 @@
 package fixture.simple;
 
 import org.apache.isis.applib.fixturescripts.FixtureScript;
-import org.apache.isis.objectstore.jdo.applib.service.support.IsisJdoSupport;
+import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
 
 public class SimpleObjectsTearDownFixture extends FixtureScript {
 
