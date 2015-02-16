337d05555dea4fc81a1223b4864e39fdb0035fb7
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2015-02-16 17:05:30 +0200

    ISIS-1014 Vertically center the modal window with JavaScript
    This way it is also possible to re-center it on window resize

--- component/viewer/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/pages/jquery.isis.wicket.viewer.js
+++ component/viewer/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/pages/jquery.isis.wicket.viewer.js
@@ -31,6 +31,21 @@ $(function() {
         copyModalShown: false
     };
 
+    /**
+     * taken from http://www.minimit.com/articles/solutions-tutorials/vertical-center-bootstrap-3-modals
+     */
+    function centerModals() {
+        $('.modal').each(function() {
+            var $clone = $(this).clone().css('display', 'block').appendTo('body');
+            var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2);
+            top = top > 0 ? top : 0;
+            $clone.remove();
+            $(this).find('.modal-content').css("margin-top", top);
+        });
+    }
+    $(document, '.modal').on('show.bs.modal', centerModals);
+    $(window).on('resize', centerModals);
+
     var isisVeilTimeoutId;
     
     var isisShowVeil = function() {
