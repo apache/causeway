a4f924b11cf614535e2faf2fb4f7aacdb4c7e1e0
Dan Haywood <dan@haywood-associates.co.uk>
2015-04-24 10:29:16 +0100

    ISIS-1137: Now cache a ProxyFactory instance by class (allowing reuse of the enhanced class for all instances of that type) rather than creating a newly enhanced class as currently.

--- core/wrapper/src/main/java/org/apache/isis/core/wrapper/proxy/ProxyInstantiatorForJavassist.java
+++ core/wrapper/src/main/java/org/apache/isis/core/wrapper/proxy/ProxyInstantiatorForJavassist.java
@@ -19,20 +19,37 @@
 
 package org.apache.isis.core.wrapper.proxy;
 
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.Map;
 
-import javassist.util.proxy.MethodFilter;
-import javassist.util.proxy.MethodHandler;
-import javassist.util.proxy.ProxyFactory;
-import javassist.util.proxy.ProxyObject;
+import com.google.common.collect.MapMaker;
 
 import org.apache.isis.applib.services.wrapper.WrapperObject;
+import org.apache.isis.applib.services.wrapper.WrappingObject;
 import org.apache.isis.core.commons.lang.ArrayExtensions;
+import org.apache.isis.core.metamodel.specloader.classsubstitutor.JavassistEnhanced;
 import org.apache.isis.core.wrapper.handlers.DelegatingInvocationHandler;
 import org.apache.isis.core.wrapper.internal.util.Util;
 
+import javassist.util.proxy.MethodFilter;
+import javassist.util.proxy.MethodHandler;
+import javassist.util.proxy.Proxy;
+import javassist.util.proxy.ProxyFactory;
+
 public class ProxyInstantiatorForJavassist implements ProxyInstantiator {
+
+    /**
+     * Lazily constructed cache.
+     */
+    private final Map<Class, ProxyFactory> proxyFactoryByClass;
+
+    public ProxyInstantiatorForJavassist() {
+        this(new MapMaker().concurrencyLevel(10).<Class, ProxyFactory>makeMap());
+    }
+
+    public ProxyInstantiatorForJavassist(Map<Class, ProxyFactory> proxyFactoryByClass) {
+        this.proxyFactoryByClass = proxyFactoryByClass;
+    }
 
     @SuppressWarnings("unchecked")
     public <T> T instantiateProxy(final DelegatingInvocationHandler<T> handler) {
@@ -44,24 +61,40 @@ public class ProxyInstantiatorForJavassist implements ProxyInstantiator {
         if (clazz.isInterface()) {
             return Util.createInstance(clazz, handler, WrapperObject.class);
         } else {
-            final Class<T> enhancedClass = createEnhancedClass(clazz, handler, WrapperObject.class);
-            ProxyObject proxyObject = (ProxyObject) Util.createInstance(enhancedClass);
-            proxyObject.setHandler(new MethodHandler() {
+            final ProxyFactory proxyFactory = proxyFactoryFor(clazz);
+
+            final Class<T> enhancedClass = proxyFactory.createClass();
+            final Proxy proxy = (Proxy) Util.createInstance(enhancedClass);
+
+            proxy.setHandler(new MethodHandler() {
                 @Override
                 public Object invoke(Object self, Method thisMethod, Method proceed, Object[] args) throws Throwable {
                     return handler.invoke(self, thisMethod, args);
                 }
             });
-            return (T) proxyObject;
+
+            return (T) proxy;
         }
     }
-    
-    @SuppressWarnings("unchecked")
-    private static <T> Class<T> createEnhancedClass(final Class<T> toProxyClass, final InvocationHandler handler, final Class<?>... auxiliaryTypes) {
-        
+
+    private <T> ProxyFactory proxyFactoryFor(final Class<T> toProxyClass) {
+        ProxyFactory proxyFactory = proxyFactoryByClass.get(toProxyClass);
+        if(proxyFactory == null) {
+            proxyFactory = createProxyFactoryFor(toProxyClass);
+            proxyFactoryByClass.put(toProxyClass, proxyFactory);
+        }
+        return proxyFactory;
+    }
+
+    private <T> ProxyFactory createProxyFactoryFor(final Class<T> toProxyClass) {
+
         final ProxyFactory proxyFactory = new ProxyFactory();
+
         proxyFactory.setSuperclass(toProxyClass);
-        proxyFactory.setInterfaces(ArrayExtensions.combine(toProxyClass.getInterfaces(), new Class<?>[]{org.apache.isis.core.metamodel.specloader.classsubstitutor.JavassistEnhanced.class}, auxiliaryTypes));
+        final Class[] types = ArrayExtensions.combine(
+                toProxyClass.getInterfaces(),
+                new Class<?>[] { JavassistEnhanced.class, WrappingObject.class });
+        proxyFactory.setInterfaces(types);
 
         proxyFactory.setFilter(new MethodFilter() {
             @Override
@@ -70,8 +103,14 @@ public class ProxyInstantiatorForJavassist implements ProxyInstantiator {
                 return !m.getName().equals("finalize") || m.isBridge();
             }
         });
-        
-        return proxyFactory.createClass();
+
+        // this is the default, I believe
+        // calling it here only because I know that it will throw an exception if the code were
+        // in the future changed such that caching is invalid
+        // (ie fail fast if future change could introduce a bug)
+        proxyFactory.setUseCache(true);
+
+        return proxyFactory;
     }
 
 }
