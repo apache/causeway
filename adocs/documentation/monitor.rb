fb3edc90199469449bdffb1ad80552f60b8e76d8
Dan Haywood <dan@haywood-associates.co.uk>
2015-04-27 21:09:24 +0100

    ISIS-1133: some tidy-up, abortive attempt to also launch web browser.

--- adocs/documentation/monitor.rb
+++ adocs/documentation/monitor.rb
@@ -5,12 +5,20 @@ require 'pathname'
 require 'fileutils'
 require 'bundler/setup'
 
+#require 'launchy'
+
 require 'webrick'
+
+#include Launchy
 include WEBrick
+
 
 Bundler.require(:default)
 
-# uses: https://github.com/guard/listen
+
+
+#prime = false
+
 
 # to suppress some debugs
 $CELLULOID_DEBUG=false
@@ -24,15 +32,9 @@ targetBaseDir = File.absolute_path 'target/site'
 srcBasePath = Pathname.new srcBaseDir
 targetBasePath = Pathname.new targetBaseDir
 
-puts ""
-puts ""
-puts ""
-puts "now monitoring..."
-puts ""
-
 i=0
 
-def process(file,srcBasePath,targetBasePath,templateDir,i)
+def process(file,srcBasePath,targetBasePath,templateDir,i,priming)
 
     workingDir = Dir.pwd
 
@@ -55,26 +57,41 @@ def process(file,srcBasePath,targetBasePath,templateDir,i)
         if srcSplit[0].length==0 then
             # handle include files of form
             # _xxx-xxx_yyy-yyy_zzz  => xxx-xxx.adoc
-            regenerate = srcSplit[1] + ".adoc"
+            if priming then
+                regenerate = ""
+            else
+                regenerate = srcSplit[1] + ".adoc"
+            end
         else
             regenerate = srcBase
         end
 
-        cmd = "asciidoctor #{regenerate} --backend html --eruby erb --template-dir '#{templateDir}' --destination-dir='#{targetRelDir}' -a imagesdir='' -a toc=right -a icons=font -a source-highlighter=coderay"
+        unless regenerate == "" then
 
-        puts ""
-        puts "#{i}: #{cmd}"
+            cmd = "asciidoctor #{regenerate} --backend html --eruby erb --template-dir '#{templateDir}' --destination-dir='#{targetRelDir}' -a imagesdir='' -a toc=right -a icons=font -a source-highlighter=coderay"
 
-        system cmd
+            #unless priming then
+                puts ""
+                puts "#{i}: #{cmd}"
+            #end
+
+            system cmd
+        end
 
     else
 
-        cmd = "cp #{srcBase} #{targetRelDir}"
+        unless File.directory?(srcBase) then
 
-        puts ""
-        puts "#{i}: #{cmd}"
+            cmd = "cp #{srcBase} #{targetRelDir}"
 
-        system cmd
+            #unless priming then
+                puts ""
+                puts "#{i}: #{cmd}"
+            #end
+
+            system cmd
+
+        end
 
     end
 
@@ -84,36 +101,66 @@ def process(file,srcBasePath,targetBasePath,templateDir,i)
 
 end
 
-# process all files
-files = Dir.glob("src/main/asciidoc/**/*")
-files.each { |file|
-    i = process file, srcBasePath, targetBasePath, templateDir, i
-}
 
+puts ""
+puts ""
+puts ""
+puts "priming (processing all files)..."
+puts ""
+
+
+#
+# priming: process all files
+#
+files = Dir.glob("src/main/asciidoc/**/*")
+#if prime then
+    files.each { |file|
+        absFile = File.absolute_path file
+        i = process absFile, srcBasePath, targetBasePath, templateDir, i, true
+    }
+#end
+
+
+puts ""
+puts ""
+puts ""
+puts "now monitoring..."
+puts ""
+
+
+#
 # then continue monitoring all directories
+#
 directories = Dir.glob("src/main/asciidoc/**/*/")
 listener = Listen.to(directories) do |modified, added, removed|
     unless modified.length==0
         modified.each { |file|
-            i = process file, srcBasePath, targetBasePath, templateDir, i
+            i = process file, srcBasePath, targetBasePath, templateDir, i, false
         }
     end
     unless added.length==0
         added.each { |file|
-            i = process file, srcBasePath, targetBasePath, templateDir, i
+            i = process file, srcBasePath, targetBasePath, templateDir, i, false
         }
     end
     unless removed.length==0
         removed.each { |file|
-            puts "removed #{file}"
-      
+            #puts "removed #{file}"
         }
     end
 end
 listener.start
-#listener.only(/.*\.adoc$/)
 
 s = HTTPServer.new(:Port => 4000,  :DocumentRoot => 'target/site')
-trap("INT"){ s.shutdown }
+trap("INT"){
+    s.shutdown
+    listener.stop
+}
 s.start
+
+
+#puts ""
+#puts "opening web browser @ http://localhost:4000/"
+#puts
+#Launchy.open("http://localhost:4000")
 
