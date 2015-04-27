072eab469beab0d0b79c0d0fbd0919b81fdbda9e
Dan Haywood <dan@haywood-associates.co.uk>
2015-04-27 19:48:50 +0100

    ISIS-1133: tidying up listen.rb, adding dependency for coderay

--- adocs/documentation/listen.rb
+++ adocs/documentation/listen.rb
@@ -7,33 +7,19 @@ require 'bundler/setup'
 
 Bundler.require(:default)
 
-# https://github.com/guard/listen
+# uses: https://github.com/guard/listen
 
-
-# to suppress
+# to suppress some debugs
 $CELLULOID_DEBUG=false
 $CELLULOID_TEST=false
-
 
 #scriptDir = File.absolute_path File.dirname(__FILE__)
 templateDir = File.absolute_path '../template'
 srcBaseDir = File.absolute_path 'src/main/asciidoc'
 targetBaseDir = File.absolute_path 'target/site'
-workingDir = Dir.pwd
 
-#templatePath = Pathname.new templateDir
 srcBasePath = Pathname.new srcBaseDir
 targetBasePath = Pathname.new targetBaseDir
-workingPath = Pathname.new workingDir
-
-#puts "workingDir: #{workingDir}"
-#puts "workingPath: #{workingPath}"
-
-#puts "srcBaseDir: #{srcBaseDir}"
-#puts "srcBasePath: #{srcBasePath}"
-
-#puts "targetBaseDir: #{targetBaseDir}"
-#puts "targetBasePath: #{targetBasePath}"
 
 puts ""
 puts ""
@@ -41,62 +27,62 @@ puts ""
 puts "now monitoring..."
 puts ""
 
-def process(file)
-    puts "regenerating #{file}..."
+i=0
+
+def process(file,srcBasePath,targetBasePath,templateDir,i)
+
+    workingDir = Dir.pwd
+
+    srcDir = File.dirname file
+    srcBase = File.basename file
+
+    srcSplit = srcBase.split('_')
+    if srcSplit[0].length==0 then
+        # handle include files of form
+        # _xxx-xxx_yyy-yyy_zzz  => xxx-xxx.adoc
+        regenerate = srcSplit[1] + ".adoc"
+    else
+        regenerate = srcBase
+    end
+
+    srcPath = Pathname.new srcDir
+    srcRel = srcPath.relative_path_from srcBasePath
+
+    targetRelPath = targetBasePath + srcRel
+    targetRelDir = File.absolute_path targetRelPath.to_s
+
+    Dir.chdir srcDir
+    FileUtils.mkdir_p targetRelDir
+
+    cmd = "asciidoctor #{regenerate} --backend html --eruby erb --template-dir '#{templateDir}' --destination-dir='#{targetRelDir}' -a imagesdir='' -a toc=right -a icons=font -a source-highlighter=coderay"
+
+    puts ""
+    puts "#{i}: #{cmd}"
+
+    system cmd
+
+    Dir.chdir workingDir
+
+    return i+1
 end
 
-i=0
 listener = Listen.to('src/main/asciidoc/user-guide') do |modified, added, removed|
-  unless modified.length==0
-    modified.each { |file|
-
-      srcDir = File.dirname file
-      srcBase = File.basename file
-
-      srcSplit = srcBase.split('_')
-      if srcSplit[0].length==0 then
-          regenerate = srcSplit[1] + ".adoc"
-      else
-          regenerate = srcBase
-      end
-
-      srcPath = Pathname.new srcDir
-      srcRel = srcPath.relative_path_from srcBasePath
-      targetRelPath = targetBasePath + srcRel
-      targetRelDir = File.absolute_path targetRelPath.to_s
-
-      #puts "regenerate: #{regenerate}"
-      #puts "targetRelPath: #{targetRelPath}"
-      #puts "targetRelDir: #{targetRelDir}"
-
-      process regenerate
-      Dir.chdir srcDir
-      FileUtils.mkdir_p targetRelDir
-
-      #cmd = "asciidoctor #{regenerate} --backend html --eruby erb --template-dir '#{templateDir}' --destination-dir='#{targetRelDir}' -a imagesdir='' -a toc=right -a icons=font -a source-highlighter=coderay"
-      cmd = "asciidoctor #{regenerate} --backend html --eruby erb --template-dir '#{templateDir}' --destination-dir='#{targetRelDir}' -a imagesdir='' -a toc=right -a icons=font"
-
-      i=i+1
-      puts ""
-      puts "#{i}: #{cmd}"
-      system cmd
-
-      Dir.chdir workingDir
-    }
-  end
-  unless added.length==0
-    puts "added absolute path: #{added}"
-    added.each { |file|
-      puts "added #{file}"
+    unless modified.length==0
+        modified.each { |file|
+            i = process file, srcBasePath, targetBasePath, templateDir, i
+        }
+    end
+    unless added.length==0
+        added.each { |file|
+            i = process file, srcBasePath, targetBasePath, templateDir, i
+        }
+    end
+    unless removed.length==0
+        removed.each { |file|
+            puts "removed #{file}"
       
-    }
-  end
-  unless removed.length==0
-    removed.each { |file|
-      puts "removed #{file}"
-      
-    }
-  end
+        }
+    end
 end
 listener.start
 listener.only(/.*\.adoc$/)
