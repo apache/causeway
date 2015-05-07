9afd147c8d2678fe4e9ee64037a3ffd7bcb7d9fc
Dan Haywood <dan@haywood-associates.co.uk>
2015-05-07 23:09:21 +0100

    ISIS-1143: initial take-on of MetaModelService (support for lookup/reverse lookup of objectType)

---
+++ core/applib/src/main/java/org/apache/isis/applib/services/metamodel/MetaModelService.java
@@ -0,0 +1,46 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.isis.applib.services.metamodel;
+
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.Programmatic;
+
+/**
+ * This service provides a formal API into Isis' metamodel.
+ *
+ * <p>
+ * This API is currently extremely limited, but the intention is to extend it gradually as use cases emerge.
+ * </p>
+ */
+public interface MetaModelService {
+
+    /**
+     * Provides a reverse lookup of a domain class' object type, as defined by {@link DomainObject#objectType()} or the (now-deprecated) {@link org.apache.isis.applib.annotation.ObjectType} annotation, or any other mechanism that corresponds to Isis' <code>ObjectSpecIdFacet</code>.
+     */
+    @Programmatic
+    Class<?> fromObjectType(final String objectType);
+
+    /**
+     * Provides a lookup of a domain class' object type, as defined by {@link DomainObject#objectType()} or the (now-deprecated) {@link org.apache.isis.applib.annotation.ObjectType} annotation, or any other mechanism that corresponds to Isis' <code>ObjectSpecIdFacet</code>.
+     */
+    @Programmatic
+    String toObjectType(final Class<?> domainType);
+
+
+}
---
+++ core/metamodel/src/main/java/org/apache/isis/core/metamodel/services/metamodel/MetaModelServiceDefault.java
@@ -0,0 +1,62 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.isis.core.metamodel.services.metamodel;
+
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.annotation.Programmatic;
+import org.apache.isis.applib.services.metamodel.MetaModelService;
+import org.apache.isis.core.metamodel.facets.object.objectspecid.ObjectSpecIdFacet;
+import org.apache.isis.core.metamodel.spec.ObjectSpecId;
+import org.apache.isis.core.metamodel.spec.ObjectSpecification;
+import org.apache.isis.core.metamodel.spec.SpecificationLoader;
+import org.apache.isis.core.metamodel.spec.SpecificationLoaderAware;
+
+@DomainService(
+        nature = NatureOfService.DOMAIN
+)
+public class MetaModelServiceDefault implements MetaModelService, SpecificationLoaderAware {
+
+    private SpecificationLoader specificationLookup;
+
+    @Programmatic
+    public Class<?> fromObjectType(final String objectType) {
+        if(objectType == null) {
+            return null;
+        }
+        final ObjectSpecId objectSpecId = new ObjectSpecId(objectType);
+        final ObjectSpecification objectSpecification = specificationLookup.lookupBySpecId(objectSpecId);
+        return objectSpecification != null? objectSpecification.getCorrespondingClass(): null;
+    }
+
+    @Override public String toObjectType(final Class<?> domainType) {
+        if(domainType == null) {
+            return null;
+        }
+        final ObjectSpecification objectSpecification = specificationLookup.loadSpecification(domainType);
+        final ObjectSpecIdFacet objectSpecIdFacet = objectSpecification.getFacet(ObjectSpecIdFacet.class);
+        final ObjectSpecId objectSpecId = objectSpecIdFacet.value();
+        return objectSpecId.asString();
+    }
+
+    @Override
+    public void setSpecificationLookup(final SpecificationLoader specificationLookup) {
+        this.specificationLookup = specificationLookup;
+    }
+}
