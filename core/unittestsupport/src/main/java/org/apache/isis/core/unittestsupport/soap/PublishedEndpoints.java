1d27060fa6cae298e4cbb6227279af239373d136
Dan Haywood <dan@haywood-associates.co.uk>
2015-07-04 12:53:39 +0100

    ISIS-1166: further improvements/simplifications to SoapEndpointPublishingRule

---
+++ core/unittestsupport/src/main/java/org/apache/isis/core/unittestsupport/soap/PublishedEndpoints.java
@@ -0,0 +1,54 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.isis.core.unittestsupport.soap;
+
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+/**
+ * Collection of SOAP endpoints that have been published; will automatically assign a unique address to any
+ * that have not been {@link SoapEndpoint}s whose {@link SoapEndpointSpec} does not specify an {@link SoapEndpointSpec#getEndpointAddress() address}.
+ */
+class PublishedEndpoints {
+
+    private int port = SoapEndpointPublishingRule.INITIAL_PORT;
+    private Map<Class<?>, SoapEndpoint> soapEndpointByType = Maps.newLinkedHashMap();
+
+    void publishEndpointIfRequired(final List<SoapEndpointSpec> soapEndpointSpecs) {
+        // merge in any new endpoints to static cache
+        for (SoapEndpointSpec soapEndpointSpec : soapEndpointSpecs) {
+            final Class<?> endpointClass = soapEndpointSpec.getEndpointClass();
+            SoapEndpoint soapEndpoint = this.soapEndpointByType.get(endpointClass);
+            if (soapEndpoint == null) {
+                // instantiate and publish,automatically assigning an address to any that don't specify one
+                soapEndpoint = new SoapEndpoint(soapEndpointSpec);
+                soapEndpointByType.put(endpointClass, soapEndpoint);
+                port = soapEndpoint.publish(port);
+            }
+        }
+    }
+
+    String getEndpointAddress(Class<?> endpointClass) {
+        return soapEndpointByType.get(endpointClass).getSpec().getEndpointAddress();
+    }
+
+    <T> T getEndpointImplementor(Class<T> endpointClass) {
+        return (T) soapEndpointByType.get(endpointClass).getImplementor();
+    }
+}
--- core/unittestsupport/src/main/java/org/apache/isis/core/unittestsupport/soap/SoapEndpointPublishingRule.java
+++ core/unittestsupport/src/main/java/org/apache/isis/core/unittestsupport/soap/SoapEndpointPublishingRule.java
@@ -18,12 +18,9 @@ package org.apache.isis.core.unittestsupport.soap;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Supplier;
 
-import javax.xml.ws.Endpoint;
-
-import com.google.common.collect.Maps;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
@@ -35,101 +32,48 @@ import org.junit.runners.model.Statement;
  */
 public class SoapEndpointPublishingRule implements TestRule {
 
-    private static Map<Class<?>,SoapEndpoint> cachedSoapEndpointByType = Maps.newLinkedHashMap();
+    /**
+     * For any endpoints where the address is not specified, ports are assigned starting from this.
+     */
+    static final int INITIAL_PORT = 54345;
 
-    public static class SoapEndpointSpec {
-        private final Class<?> endpointClass;
-        private final String endpointAddress;
-        private final Supplier<?> endpointImplementorFactory;
+    private static PublishedEndpoints publishedEndpoints = new PublishedEndpoints();
 
-        public SoapEndpointSpec(final Class<?> endpointClass, final String endpointAddress) {
-            this(endpointClass, endpointAddress, new SupplierUsingDefaultConstructor(endpointClass));
-        }
-        public SoapEndpointSpec(final Class<?> endpointClass, final String endpointAddress, final Supplier<?> endpointImplementorFactory) {
-            this.endpointClass = endpointClass;
-            this.endpointAddress = endpointAddress;
-            this.endpointImplementorFactory = endpointImplementorFactory;
-        }
-
-    }
-
-    private static class SoapEndpoint {
-        private final SoapEndpointSpec spec;
-        /**
-         * lazily populated first time
-         */
-        private Object implementor;
-        public SoapEndpoint(final SoapEndpointSpec spec) {
-            this.spec = spec;
-        }
-    }
-
-    private final Map<Class<?>,SoapEndpoint> soapEndpointByType = Maps.newLinkedHashMap();
+    private final List<SoapEndpointSpec> soapEndpointSpecs = Lists.newArrayList();
 
     public SoapEndpointPublishingRule(final Class<?> endpointClass, final String endpointAddress) {
-        this(new SoapEndpointSpec(endpointClass, endpointAddress, new SupplierUsingDefaultConstructor(endpointClass)));
+        this(new SoapEndpointSpec(endpointClass, endpointAddress));
+    }
+
+    public SoapEndpointPublishingRule(Class<?>... endpointClasses) {
+        this(Arrays.asList(endpointClasses));
+    }
+
+    public SoapEndpointPublishingRule(final List<Class<?>> endpointClasses) {
+        this(Iterables.transform(endpointClasses, SoapEndpointSpec.asSoapEndpointSpec()));
     }
 
     public SoapEndpointPublishingRule(SoapEndpointSpec... soapEndpointSpecs) {
         this(Arrays.asList(soapEndpointSpecs));
     }
 
-    public SoapEndpointPublishingRule(final List<SoapEndpointSpec> soapEndpointSpecs) {
-        for (SoapEndpointSpec soapEndpointSpec : soapEndpointSpecs) {
-            soapEndpointByType.put(soapEndpointSpec.endpointClass, new SoapEndpoint(soapEndpointSpec));
-        }
+    public SoapEndpointPublishingRule(final Iterable<SoapEndpointSpec> soapEndpointSpecs) {
+        Iterables.addAll(this.soapEndpointSpecs, soapEndpointSpecs);
     }
 
     @Override
     public Statement apply(final Statement base, final Description description) {
-        publishEndpointIfRequired();
+        publishedEndpoints.publishEndpointIfRequired(soapEndpointSpecs);
         return base;
     }
 
-    private void publishEndpointIfRequired() {
-        // merge in any new endpoints to static cache
-        for (Class<?> type : soapEndpointByType.keySet()) {
-            final SoapEndpoint soapEndpoint = soapEndpointByType.get(type);
-            final Class<?> endpointClass = soapEndpoint.spec.endpointClass;
-            final SoapEndpoint cachedSoapEndpoint = cachedSoapEndpointByType.get(endpointClass);
-            if(cachedSoapEndpoint == null) {
-                cachedSoapEndpointByType.put(endpointClass, soapEndpoint);
-            }
-        }
-        // ensure all endpoints (including any new ones) are instantiated and published
-        for (Class<?> type : cachedSoapEndpointByType.keySet()) {
-            final SoapEndpoint cachedSoapEndpoint = cachedSoapEndpointByType.get(type);
-            if(cachedSoapEndpoint.implementor == null) {
-                cachedSoapEndpoint.implementor = cachedSoapEndpoint.spec.endpointImplementorFactory.get();
-                Endpoint.publish(cachedSoapEndpoint.spec.endpointAddress, cachedSoapEndpoint.implementor);
-            }
-        }
-    }
-
     public String getEndpointAddress(Class<?> endpointClass) {
-        return cachedSoapEndpointByType.get(endpointClass).spec.endpointAddress;
+        return publishedEndpoints.getEndpointAddress(endpointClass);
     }
 
     public <T> T getEndpointImplementor(Class<T> endpointClass) {
-        return (T)cachedSoapEndpointByType.get(endpointClass).implementor;
+        return publishedEndpoints.getEndpointImplementor(endpointClass);
     }
 
-
-    private static class SupplierUsingDefaultConstructor implements Supplier<Object> {
-        private final Class<?> endpointClass;
-
-        public SupplierUsingDefaultConstructor(final Class<?> endpointClass) {
-            this.endpointClass = endpointClass;
-        }
-
-        @Override
-        public Object get() {
-            try {
-                return endpointClass.newInstance();
-            } catch (InstantiationException | IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
 
 }
