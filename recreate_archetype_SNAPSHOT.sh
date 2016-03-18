c32ab6b4e9f5ca7b263a7ef65f22e74dca9a5767
Dan Haywood <dan@haywood-associates.co.uk>
2016-03-18 06:23:06 +0000

    ISIS-1287: updating the recreate_archetype scripts

--- recreate_archetype_SNAPSHOT.sh
+++ recreate_archetype_SNAPSHOT.sh
@@ -1,6 +1,6 @@
-export ISISREL=1.10.0-SNAPSHOT
-export ISISDEV=1.10.0-SNAPSHOT
-export JIRA=ISIS-1195
+export ISISREL=1.12.0-SNAPSHOT
+export ISISDEV=1.12.0-SNAPSHOT
+export JIRA=ISIS-1287
 
 export ISISTMP=/c/tmp   # or as required
 export ISISPAR=$ISISREL
