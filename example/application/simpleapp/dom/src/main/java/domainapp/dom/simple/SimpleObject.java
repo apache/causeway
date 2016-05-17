f60d201faba6bdd0346d2f5a6caadfa3513f1e68
Dan Haywood <dan@haywood-associates.co.uk>
2016-05-17 07:58:37 +0100

    ISIS-1355: minor extension to SimpleObject example.

--- example/application/simpleapp/dom/src/main/java/domainapp/dom/simple/SimpleObject.java
+++ example/application/simpleapp/dom/src/main/java/domainapp/dom/simple/SimpleObject.java
@@ -22,7 +22,10 @@ import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.VersionStrategy;
 
 import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.CommandReification;
 import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.ParameterLayout;
 import org.apache.isis.applib.annotation.Property;
 import org.apache.isis.applib.annotation.Publishing;
 import org.apache.isis.applib.annotation.SemanticsOf;
@@ -69,12 +72,15 @@ public class SimpleObject implements Comparable<SimpleObject> {
     }
 
 
+
     public static class NameDomainEvent extends PropertyDomainEvent<SimpleObject,String> {}
     @javax.jdo.annotations.Column(
             allowsNull="false",
             length = NAME_LENGTH
     )
     @Property(
+        command = CommandReification.ENABLED,
+        publishing = Publishing.ENABLED,
         domainEvent = NameDomainEvent.class
     )
     private String name;
@@ -88,6 +94,22 @@ public class SimpleObject implements Comparable<SimpleObject> {
     public TranslatableString validateName(final String name) {
         return name != null && name.contains("!")? TranslatableString.tr("Exclamation mark is not allowed"): null;
     }
+
+
+
+    public static class UpdateNameDomainEvent extends ActionDomainEvent<SimpleObject> {}
+    @Action(
+            command = CommandReification.ENABLED,
+            publishing = Publishing.ENABLED,
+            semantics = SemanticsOf.IDEMPOTENT,
+            domainEvent = UpdateNameDomainEvent.class
+    )
+    @MemberOrder(name="name", sequence = "1") // associate with 'name' property
+    public SimpleObject updateName(@ParameterLayout(named="Name") final String name) {
+        setName(name);
+        return this;
+    }
+
 
 
 
