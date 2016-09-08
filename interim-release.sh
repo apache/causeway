f2f7cedca09ac0fef0aebafd1aa8aaa7684a3b47
Dan Haywood <dan@haywood-associates.co.uk>
2016-09-09 00:15:31 +0200

    ISIS-1465: enhancing interim-release.sh to automatically cleanrremote branches and push new branch (and tag)

--- interim-release.sh
+++ interim-release.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 VERSION_BASE=$1
+REMOTE=$2
 
-if [ ! "$VERSION_BASE" ]; then
-    echo "usage: $(basename $0) [base version]"
-    echo "   eg: $(basename $0) 1.13.0"
+if [ -z "$VERSION_BASE" -o -z "$REMOTE" ]; then
+    echo "usage: $(basename $0) [base version] [remote]"
+    echo "   eg: $(basename $0) 1.13.0 incodehq"
     exit 1
 fi
 
@@ -12,6 +14,13 @@ VERSION="$VERSION_BASE.$DATE"
 BRANCH="interim/$VERSION"
 TAG="isis-$VERSION"
 
+echo "removing any earlier (local) 'interim' branches"
+for a in `git branch --list | grep interim`
+do
+    git branch -D $a
+done
+
+echo "checking out new branch $BRANCH"
 git checkout -b "$BRANCH"
 
 echo "updating version in all pom.xml files..."
@@ -24,3 +33,16 @@ git commit -am "bumping to $VERSION"
 
 echo "tagging"
 git tag $TAG
+
+echo "removing any earlier remote branches"
+for a in `git ls-remote --heads $REMOTE  | sed 's?.*refs/heads/??' | grep interim`
+do
+    git push $REMOTE --delete $a
+done
+
+echo "pushing tag"
+git push $REMOTE $TAG
+
+echo "pushing branch"
+git push $REMOTE $BRANCH
+
