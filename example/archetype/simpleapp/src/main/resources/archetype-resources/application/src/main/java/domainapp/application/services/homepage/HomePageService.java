c221ea9f55810d52a145164d0032484f875b4454
Dan Haywood <dan@haywood-associates.co.uk>
2016-11-24 11:25:53 +0000

    ISIS-1538: recreating simpleapp archetype

--- example/archetype/simpleapp/src/main/resources/archetype-resources/application/src/main/java/domainapp/application/services/homepage/HomePageService.java
+++ example/archetype/simpleapp/src/main/resources/archetype-resources/application/src/main/java/domainapp/application/services/homepage/HomePageService.java
@@ -26,29 +26,20 @@ import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.HomePage;
 import org.apache.isis.applib.annotation.NatureOfService;
 import org.apache.isis.applib.annotation.SemanticsOf;
-import org.apache.isis.applib.services.registry.ServiceRegistry;
+import org.apache.isis.applib.services.factory.FactoryService;
 
 @DomainService(
         nature = NatureOfService.VIEW_CONTRIBUTIONS_ONLY // trick to suppress the actions from the top-level menu
 )
 public class HomePageService {
 
-    //region > homePage (action)
-
-    @Action(
-            semantics = SemanticsOf.SAFE
-    )
+    @Action(semantics = SemanticsOf.SAFE)
     @HomePage
     public HomePageViewModel homePage() {
-        return serviceRegistry.injectServicesInto(new HomePageViewModel());
+        return factoryService.instantiate(HomePageViewModel.class);
     }
 
-    //endregion
-
-    //region > injected services
 
     @javax.inject.Inject
-    ServiceRegistry serviceRegistry;
-
-    //endregion
+    FactoryService factoryService;
 }
