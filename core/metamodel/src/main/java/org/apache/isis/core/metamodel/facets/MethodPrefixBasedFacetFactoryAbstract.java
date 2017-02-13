85ff5c07f504065af59acd4f1df0d915a593083b
Dan Haywood <dan@haywood-associates.co.uk>
2017-02-13 13:03:34 +0000

    ISIS-1578: changes the existing {Hide/Disable}ForContextFacetViaMethodFactory to (optionally, but by default) no longer search for supporting methods that have anything other than no-args.
    Can be disabled using configuration parameter.

--- core/metamodel/src/main/java/org/apache/isis/core/metamodel/facets/MethodPrefixBasedFacetFactoryAbstract.java
+++ core/metamodel/src/main/java/org/apache/isis/core/metamodel/facets/MethodPrefixBasedFacetFactoryAbstract.java
@@ -22,9 +22,11 @@ package org.apache.isis.core.metamodel.facets;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import org.apache.isis.core.commons.config.IsisConfiguration;
 import org.apache.isis.core.metamodel.facetapi.FeatureType;
+import org.apache.isis.core.metamodel.facets.members.hidden.method.HideForContextFacetViaMethodFactory;
 import org.apache.isis.core.metamodel.spec.ObjectSpecification;
 import org.apache.isis.core.metamodel.spec.feature.Contributed;
 import org.apache.isis.core.metamodel.spec.feature.ObjectAction;
@@ -35,6 +37,11 @@ import org.apache.isis.core.metamodel.specloader.validator.ValidationFailures;
 public abstract class MethodPrefixBasedFacetFactoryAbstract
         extends FacetFactoryAbstract
         implements MethodPrefixBasedFacetFactory {
+
+    public static final String ISIS_REFLECTOR_VALIDATOR_NO_PARAMS_ONLY_KEY =
+            "isis.reflector.validator.noParamsOnly";
+    public static final boolean ISIS_REFLECTOR_VALIDATOR_NO_PARAMS_ONLY_DEFAULT = true;
+
 
     private final List<String> prefixes;
 
@@ -68,16 +75,31 @@ public abstract class MethodPrefixBasedFacetFactoryAbstract
 
             @Override
             public boolean visit(final ObjectSpecification objectSpec, final ValidationFailures validationFailures) {
+
+                boolean noParamsOnly = configuration.getBoolean(
+                        MethodPrefixBasedFacetFactoryAbstract.ISIS_REFLECTOR_VALIDATOR_NO_PARAMS_ONLY_KEY,
+                        MethodPrefixBasedFacetFactoryAbstract.ISIS_REFLECTOR_VALIDATOR_NO_PARAMS_ONLY_DEFAULT);
+
                 final List<ObjectAction> objectActions = objectSpec.getObjectActions(Contributed.EXCLUDED);
                 for (final ObjectAction objectAction : objectActions) {
                     for (final String prefix : prefixes) {
                         String actionId = objectAction.getId();
+
                         if (actionId.startsWith(prefix) && prefix.length() < actionId.length()) {
+
+                            final String explanation =
+                                    objectAction.getParameterCount() > 0 && noParamsOnly &&
+                                    (Objects.equals(prefix, MethodPrefixConstants.HIDE_PREFIX) || Objects.equals(prefix, MethodPrefixConstants.DISABLE_PREFIX))
+                                    ? " (note that such methods must have no parameters)"
+                                    : "";
+
+                            String message = "%s#%s: has prefix %s, is probably intended as a supporting method for a property, collection or action%s.  If the method is intended to be an action, then rename and use @ActionLayout(named=\"...\") or ignore completely using @Programmatic";
                             validationFailures.add(
-                                    "%s#%s: has prefix %s, is probably a supporting method for a property, collection or action.  If the method is intended to be an action, then rename and use @ActionLayout(named=\"...\") or ignore completely using @Programmatic",
+                                    message,
                                     objectSpec.getIdentifier().getClassName(),
                                     actionId,
-                                    prefix);
+                                    prefix,
+                                    explanation);
                         }
                     }
                 }
