c5b0bc3bb71fd38d7ad23667de6bcb7f70552789
Dan Haywood <dan@haywood-associates.co.uk>
2017-03-24 07:19:49 +0000

    ISIS-1521: simplifies previewing of asciidoc websites

--- adocs/documentation/monitor.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-mvn compile -o && ruby monitor.rb -b
--- adocs/documentation/preview-html.sh
+++ adocs/documentation/preview-html.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn compile -o && python -m webbrowser -t http://localhost:8000 && pushd target/site && python -m http.server 8000 && popd
+mvn compile -o && python -m webbrowser -t http://localhost:4000 && pushd target/site && python -m http.server 4000 && popd
