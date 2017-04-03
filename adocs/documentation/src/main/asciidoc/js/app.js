f01ef83e2445d6cac89dfc72f4e36b95d067c517
Dan Haywood <dan@haywood-associates.co.uk>
2017-04-03 23:04:20 +0100

    ISIS-1521: adds search to asciidoc template

---
+++ adocs/documentation/src/main/asciidoc/js/app.js
@@ -0,0 +1,59 @@
+$('#hello-world').submit(function(ev) {
+    ev.preventDefault(); // to stop the form from submitting
+    /* Validations go here */
+
+    var searchField = $('#searchfield');
+    var searchText = searchField.val()
+
+
+    $.getJSON('/elasticlunr/index.json', function (data) {
+
+        var index = elasticlunr.Index.load(data);
+        var searchResults = index.search(searchText 
+        /*,{
+            fields: {
+                title: {boost: 3},
+                description: {boost: 2},
+                body: {boost: 1}
+        }}
+        */
+        );
+
+        $('#search-results').empty();
+        if(searchResults.length === 0) {
+
+            $('#search-results').append("<br/>No matches found for '" + searchText + "'<br/><br/>");
+
+        } else {
+            for (var i = 0; i < searchResults.length; i++) {
+
+                var searchResult = searchResults[i];
+                var ref = searchResult['ref'];
+
+                var doc = index.documentStore.docs[ref];
+
+                var title = doc.title;
+                var description = doc.description;
+                var url = doc.url;
+                var score = searchResult['score'];
+                var percentScore = score.toFixed(2);
+
+                if(description) {
+                    $('#search-results').append("<br/><span class='searchLink'><a href='/" + url + "'>" + title + " <span class='searchScore'>" + percentScore + "</span>" + "</a></span><p class='searchDescription'>" + description + "</p>");
+                }
+            };
+        }
+        $('#search-results').focus();
+        $('html,body').animate({
+            scrollTop: $("#search-results").offset().top - 80
+        });
+
+    });
+
+    $(searchField).val('');
+});
+
+$(document).ready(function(){
+    var searchField = $('#searchfield');
+    $(searchField).focus();
+});
