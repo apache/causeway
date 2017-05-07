c9c720d1bfe176f32b4392ce26a01db440daf3e1
Dan Haywood <dan@haywood-associates.co.uk>
2017-05-07 09:46:43 +0100

    ISIS-1579: wip, now using DN as base; half-way converted.
    Still need to:
    - sort out search bar
    - colour/theme
    - headings
    - index.adoc

--- adocs/documentation/preview-html.sh
+++ adocs/documentation/preview-html.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-mvn compile -Dreindex -o && python -m webbrowser -t http://localhost:4000 && pushd target/site && python -m http.server 4000 && popd
+mvn compile -o && python -m webbrowser -t http://localhost:4000/downloads.html && pushd target/site && python -m http.server 4000 && popd
+#mvn compile -Dreindex -o && python -m webbrowser -t http://localhost:4000 && pushd target/site && python -m http.server 4000 && popd
