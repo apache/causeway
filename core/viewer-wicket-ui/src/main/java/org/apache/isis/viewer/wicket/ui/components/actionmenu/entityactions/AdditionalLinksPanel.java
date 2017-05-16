431294388ed4ea52a00bcf6f44b3fb778f05b997
Andi Huber <hobrom@gmx.at>
2017-05-16 13:20:55 +0200

    Fix ISIS-1615
    https://issues.apache.org/jira/browse/ISIS-1615

--- core/viewer-wicket-ui/src/main/java/org/apache/isis/viewer/wicket/ui/components/actionmenu/entityactions/AdditionalLinksPanel.java
+++ core/viewer-wicket-ui/src/main/java/org/apache/isis/viewer/wicket/ui/components/actionmenu/entityactions/AdditionalLinksPanel.java
@@ -108,6 +108,9 @@ public class AdditionalLinksPanel extends PanelAbstract<ListOfLinksModel> {
                 final String itemTitle = first(linkAndLabel.getDisabledReasonIfAny(), linkAndLabel.getDescriptionIfAny());
                 if(itemTitle != null) {
                     item.add(new AttributeAppender("title", itemTitle));
+                    // ISIS-1615, prevent bootstrap from changing the HTML link's 'title' attribute on client-side;
+                    // bootstrap will not touch the 'title' attribute once the HTML link has a 'data-original-title' attribute
+                    link.add(new AttributeAppender("data-original-title", ""));
                 }
 
                 final Label viewTitleLabel = new Label(ID_ADDITIONAL_LINK_TITLE, linkAndLabel.getLabel());
