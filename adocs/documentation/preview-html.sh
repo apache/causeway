06a469f36140cd770dbc0c859b3c3b4ba28b5fe5
Dan Haywood <dan@haywood-associates.co.uk>
2017-07-20 19:59:14 +0100

    ISIS-1521: fixing bad links

--- adocs/documentation/preview-html.sh
+++ adocs/documentation/preview-html.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-mvn compile -Dreindex -o && python -m webbrowser -t http://localhost:4000 && pushd target/site && python -m http.server 4000 && popd
+mvn compile -o && python -m webbrowser -t http://localhost:4000 && pushd target/site && python -m http.server 4000 && popd
+#mvn compile -Dreindex -o && python -m webbrowser -t http://localhost:4000 && pushd target/site && python -m http.server 4000 && popd
 
 #mvn compile -o && python -m webbrowser -t http://localhost:4000/guides/ugvw/ugvw.html && pushd target/site && python -m http.server 4000 && popd
 #mvn compile -o -Dreindex && python -m webbrowser -t http://localhost:4000/guides/ugvw/ugvw.html && pushd target/site && python -m http.server 4000 && popd
