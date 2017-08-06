4cc914bb11c7a1c747aa5e878f16599cf9fc95f7
Dan Haywood <dan@haywood-associates.co.uk>
2017-08-06 19:38:57 +0100

    ISIS-1521: update to script

--- scripts/clean-up-tags-local.sh
+++ scripts/clean-up-tags-local.sh
@@ -1,9 +1,19 @@
+for a in `git tag --list | grep origin`
+do
+    git tag -d $a
+done
+
 for a in `git tag --list | grep upstream`
 do
     git tag -d $a
 done
 
 for a in `git tag --list | grep ^isis-`
+do
+    git tag -d $a
+done
+
+for a in `git tag --list | grep ^todo`
 do
     git tag -d $a
 done
