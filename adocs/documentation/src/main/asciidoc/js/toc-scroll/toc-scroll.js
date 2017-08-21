fd0f82557139c58975da0fb5324252f3877a0390
Dan Haywood <dan@haywood-associates.co.uk>
2017-08-21 12:40:21 +0100

    ISIS-1465: updates migration notes for 1.15.0
    also fix javascript issue and markup issue

--- adocs/documentation/src/main/asciidoc/js/toc-scroll/toc-scroll.js
+++ adocs/documentation/src/main/asciidoc/js/toc-scroll/toc-scroll.js
@@ -2,22 +2,29 @@ $( document ).ready(function() {
 
     $("ul.nav li a").click(function (event) {
         var $target = $(event.currentTarget),
-            $scrollToTarget = $($target.attr("href")),
-            $header = $("nav.navbar-static-top"),
-            prop = {
-                // subtracting the offset results in the incorrect entry in the TOC from being highlighted.
-                // since the sticky-header keeps the header out of the way, this isn't a problem for us.
-                scrollTop:   $scrollToTarget.offset().top /*- $header.outerHeight(true)*/
-            },
-            speed = 500;
+            targetHref = $target.attr("href");
+
+        if(targetHref && targetHref.startsWith("#")) {
+            var $scrollToTarget = $(targetHref),
+                $header = $("nav.navbar-static-top");
+
+            if($scrollToTarget && $scrollToTarget.offset() && $scrollToTarget.offset().top) {
+                var prop = {
+                        // subtracting the offset results in the incorrect entry in the TOC from being highlighted.
+                        // since the sticky-header keeps the header out of the way, this isn't a problem for us.
+                        scrollTop:   $scrollToTarget.offset().top
+                    },
+                    speed = 500;
 
 
-        $($header).attr("data-toc-scrolling", "true")
-        $('html, body').animate(prop, speed,
-            function() {
-                var x = 2;
-                $($header).removeAttr("data-toc-scrolling")
-            });
+                $($header).attr("data-toc-scrolling", "true")
+                $('html, body').animate(prop, speed,
+                    function() {
+                        var x = 2;
+                        $($header).removeAttr("data-toc-scrolling")
+                    });
+            }
+        }
     });
 
 });
