bb537f96d2e2280730e5873555df0b9cb87ddbd9
Dan Haywood <dan@haywood-associates.co.uk>
2017-10-16 12:09:32 +0100

    ISIS-11742: deletes unused methods from IsisMatchers

--- core/metamodel/src/main/java/org/apache/isis/core/commons/matchers/IsisMatchers.java
+++ core/metamodel/src/main/java/org/apache/isis/core/commons/matchers/IsisMatchers.java
@@ -19,15 +19,18 @@
 
 package org.apache.isis.core.commons.matchers;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
-import org.hamcrest.*;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.hamcrest.core.IsEqual;
 import org.hamcrest.core.StringContains;
 import org.hamcrest.core.StringEndsWith;
 import org.hamcrest.core.StringStartsWith;
+
 import org.apache.isis.core.commons.lang.StringExtensions;
 
 import static org.hamcrest.CoreMatchers.nullValue;
@@ -164,11 +167,6 @@ public final class IsisMatchers {
     }
 
     @Factory
-    public static <T extends Comparable<T>> Matcher<T> greaterThan(final T c) {
-        return Matchers.greaterThan(c);
-    }
-
-    @Factory
     public static Matcher<Class<?>> classEqualTo(final Class<?> operand) {
 
         class ClassEqualsMatcher extends TypeSafeMatcher<Class<?>> {
@@ -190,23 +188,6 @@ public final class IsisMatchers {
         }
 
         return new ClassEqualsMatcher(operand);
-    }
-
-    @Factory
-    public static Matcher<File> existsAndNotEmpty() {
-
-        return new TypeSafeMatcher<File>() {
-
-            @Override
-            public void describeTo(final Description arg0) {
-                arg0.appendText("exists and is not empty");
-            }
-
-            @Override
-            public boolean matchesSafely(final File f) {
-                return f.exists() && f.length() > 0;
-            }
-        };
     }
 
     @Factory
@@ -258,60 +239,6 @@ public final class IsisMatchers {
     }
 
     @Factory
-    public static <T> Matcher<List<T>> listContaining(final T t) {
-        return new TypeSafeMatcher<List<T>>() {
-    
-            @Override
-            public void describeTo(Description arg0) {
-                arg0.appendText("list containing ").appendValue(t);
-            }
-    
-            @Override
-            public boolean matchesSafely(List<T> arg0) {
-                return arg0.contains(t);
-            }
-        };
-    }
-
-    @Factory
-    public static <T> Matcher<List<T>> listContainingAll(final T... items) {
-        return new TypeSafeMatcher<List<T>>() {
-
-            @Override
-            public void describeTo(Description arg0) {
-                arg0.appendText("has items ").appendValue(items);
-                
-            }
-
-            @Override
-            public boolean matchesSafely(List<T> arg0) {
-                return arg0.containsAll(Arrays.asList(items));
-            }
-        };
-    }
-
-    @Factory
-    public static Matcher<List<Object>> containsObjectOfType(final Class<?> cls) {
-        return new TypeSafeMatcher<List<Object>>() {
-
-            @Override
-            public void describeTo(final Description desc) {
-                desc.appendText("contains instance of type " + cls.getName());
-            }
-
-            @Override
-            public boolean matchesSafely(final List<Object> items) {
-                for (final Object object : items) {
-                    if (cls.isAssignableFrom(object.getClass())) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-        };
-    }
-
-    @Factory
     public static Matcher<String> startsWith(final String expected) {
         return new TypeSafeMatcher<String>() {
 
@@ -343,26 +270,5 @@ public final class IsisMatchers {
         };
     }
 
-    
-    @Factory
-    public static Matcher<File> equalsFile(final File file) throws IOException {
-        final String canonicalPath = file.getCanonicalPath();
-        return new TypeSafeMatcher<File>() {
-
-            @Override
-            public void describeTo(Description arg0) {
-                arg0.appendText("file '" + canonicalPath + "'");
-            }
-
-            @Override
-            public boolean matchesSafely(File arg0) {
-                try {
-                    return arg0.getCanonicalPath().equals(canonicalPath);
-                } catch (IOException e) {
-                    return false;
-                }
-            }
-        };
-    }
 
 }
