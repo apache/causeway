9b81c2ef40ab3fc8e35dcdbf0dd2e46f8e710d23
Dan Haywood <dan@haywood-associates.co.uk>
2017-10-16 10:44:29 +0100

    ISIS-1742: deletes unused constants and methods from ObjectAssociation.Predicates

--- core/metamodel/src/main/java/org/apache/isis/core/metamodel/spec/feature/ObjectAssociation.java
+++ core/metamodel/src/main/java/org/apache/isis/core/metamodel/spec/feature/ObjectAssociation.java
@@ -156,24 +156,15 @@ public interface ObjectAssociation extends ObjectMember, CurrentHolder {
     // //////////////////////////////////////////////////////
 
     public static class Predicates {
-        /**
-         * Filters only fields that are for properties (ie 1:1 associations)
-         *
-         * @deprecated -use {@link Predicate equivalent}
-         */
-        @Deprecated
+
+        private Predicates(){}
+
         public final static Predicate<ObjectAssociation> PROPERTIES = new Predicate<ObjectAssociation>() {
             @Override
             public boolean apply(final ObjectAssociation association) {
                 return association.isOneToOneAssociation();
             }
         };
-        /**
-         * Filters only fields that are for reference properties (ie 1:1 associations)
-         *
-         * @deprecated -use {@link Predicate equivalent}
-         */
-        @Deprecated
         public final static Predicate<ObjectAssociation> REFERENCE_PROPERTIES = new Predicate<ObjectAssociation>() {
             @Override
             public boolean apply(final ObjectAssociation association) {
@@ -181,83 +172,12 @@ public interface ObjectAssociation extends ObjectMember, CurrentHolder {
                        !association.getSpecification().containsDoOpFacet(ValueFacet.class);
             }
         };
-        /**
-         * Filters only fields that are for properties (ie 1:1 associations)
-         *
-         * @deprecated -use {@link Predicate equivalent}
-         */
-        @Deprecated
-        public final static Predicate<ObjectAssociation> WHERE_VISIBLE_IN_COLLECTION_TABLE = new Predicate<ObjectAssociation>() {
-            @Override
-            public boolean apply(final ObjectAssociation association) {
-                final HiddenFacet hiddenFacet = association.getFacet(HiddenFacet.class);
-                return hiddenFacet == null || !hiddenFacet.where().inParentedTable();
-            }
-        };
-        /**
-         * Filters only fields that are for properties (ie 1:1 associations)
-         *
-         * @deprecated -use {@link Predicate equivalent}
-         */
-        @Deprecated
-        public final static Predicate<ObjectAssociation> WHERE_VISIBLE_IN_STANDALONE_TABLE = new Predicate<ObjectAssociation>() {
-            @Override
-            public boolean apply(final ObjectAssociation association) {
-                final HiddenFacet hiddenFacet = association.getFacet(HiddenFacet.class);
-                return hiddenFacet == null || !hiddenFacet.where().inStandaloneTable();
-            }
-        };
-        /**
-         * Returns all fields (that is, filters out nothing).
-         *
-         * @deprecated -use {@link Predicate equivalent}
-         */
-        @Deprecated
-        public final static Predicate<ObjectAssociation> ALL = new Predicate<ObjectAssociation>() {
-            @Override
-            public boolean apply(final ObjectAssociation property) {
-                return true;
-            }
-        };
-        /**
-         * Filters only fields that are for collections (ie 1:m associations)
-         *
-         * @deprecated -use {@link Predicate equivalent}
-         */
-        @Deprecated
         public final static Predicate<ObjectAssociation> COLLECTIONS = new Predicate<ObjectAssociation>() {
             @Override
             public boolean apply(final ObjectAssociation property) {
                 return property.isOneToManyAssociation();
             }
         };
-        /**
-         * Filters only properties that are visible statically, ie have not been
-         * unconditionally hidden at compile time.
-         *
-         * @deprecated -use {@link Predicate equivalent}
-         */
-        @Deprecated
-        public static final Predicate<ObjectAssociation> VISIBLE_AT_LEAST_SOMETIMES = new Predicate<ObjectAssociation>() {
-            @Override
-            public boolean apply(final ObjectAssociation property) {
-                final HiddenFacet hiddenFacet = property.getFacet(HiddenFacet.class);
-                return hiddenFacet == null || hiddenFacet.where() != Where.ANYWHERE;
-            }
-        };
-
-        private Predicates(){}
-        
-        public static com.google.common.base.Predicate being(final Contributed contributed) {
-            return new com.google.common.base.Predicate<ObjectAssociation>(){
-                @Override
-                public boolean apply(final ObjectAssociation t) {
-                    return contributed.isIncluded() || 
-                           !(t instanceof ContributeeMember);
-                }
-            };
-        }
-
 
         public static final Predicate<ObjectAssociation> staticallyVisible(final Where where) {
             return new Predicate<ObjectAssociation>() {
@@ -275,23 +195,6 @@ public interface ObjectAssociation extends ObjectMember, CurrentHolder {
                         }
                     }
                     return true;
-                }
-            };
-        }
-
-        /**
-         * @deprecated -use {@link Predicate equivalent}
-         */
-        @Deprecated
-        public static Predicate<ObjectAssociation> dynamicallyVisible(
-                final ObjectAdapter target,
-                final InteractionInitiatedBy interactionInitiatedBy,
-                final Where where) {
-            return new Predicate<ObjectAssociation>() {
-                @Override
-                public boolean apply(final ObjectAssociation objectAssociation) {
-                    final Consent visible = objectAssociation.isVisible(target, interactionInitiatedBy, where);
-                    return visible.isAllowed();
                 }
             };
         }
