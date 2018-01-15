a0ac44c4e1dd74f5faf4cb59541101d3b42be88b
Andi Huber <ahuber@apache.org>
2018-01-15 22:01:29 +0100

    ISIS-1743 fa-Icon mapping for mixins using '$$' method names

--- core/metamodel/src/main/java/org/apache/isis/core/metamodel/facets/members/cssclassfa/annotprop/CssClassFaFacetOnMemberFactory.java
+++ core/metamodel/src/main/java/org/apache/isis/core/metamodel/facets/members/cssclassfa/annotprop/CssClassFaFacetOnMemberFactory.java
@@ -69,7 +69,7 @@ public class CssClassFaFacetOnMemberFactory extends FacetFactoryAbstract impleme
     private CssClassFaFacet createFromConfiguredRegexIfPossible(final ProcessMethodContext processMethodContext) {
         final Method method = processMethodContext.getMethod();
 
-        String value = faIconIfAnyFor(method);
+        String value = faIconIfAnyFor(MixinInterceptor.intendedNameOf(method));
         CssClassFaPosition position = CssClassFaPosition.LEFT;
         if (value != null) {
             int idxOfSeparator = value.indexOf(':');
@@ -84,12 +84,7 @@ public class CssClassFaFacetOnMemberFactory extends FacetFactoryAbstract impleme
         }
     }
 
-    private String faIconIfAnyFor(Method method) {
-        final String name = method.getName();
-        return faIconIfAnyFor(name);
-    }
-
-    private String faIconIfAnyFor(String name) {
+	private String faIconIfAnyFor(String name) {
         final Map<Pattern, String> faIconByPattern = getFaIconByPattern();
 
         for (Map.Entry<Pattern, String> entry : faIconByPattern.entrySet()) {
