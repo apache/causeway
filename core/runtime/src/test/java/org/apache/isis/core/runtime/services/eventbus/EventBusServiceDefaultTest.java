5ff764f2d3e67dd6479608b504cb8cd57667063b
Andi Huber <ahuber@apache.org>
2018-03-04 15:53:52 +0100

    ISIS-1893 fixes runtime test dependencies and tests

--- core/runtime/src/test/java/org/apache/isis/core/runtime/services/eventbus/EventBusServiceDefaultTest.java
+++ core/runtime/src/test/java/org/apache/isis/core/runtime/services/eventbus/EventBusServiceDefaultTest.java
@@ -17,7 +17,7 @@
 package org.apache.isis.core.runtime.services.eventbus;
 
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.isIn;
 import static org.junit.Assert.assertThat;
 
 import java.util.Collections;
@@ -25,12 +25,9 @@ import java.util.List;
 
 import org.apache.isis.applib.services.registry.ServiceRegistry;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.eventbus.Subscribe;
 
 public class EventBusServiceDefaultTest {
 
@@ -56,7 +53,7 @@ public class EventBusServiceDefaultTest {
         public void emptyMap() throws Exception {
             eventBusService.init(Collections.<String, String>emptyMap());
 
-            assertThat(eventBusService.getImplementation(), is("guava"));
+            assertThat(eventBusService.getImplementation(), isIn(new String[] {"auto", "plugin"}));
             assertThat(eventBusService.isAllowLateRegistration(), is(false));
         }
 
@@ -117,7 +114,7 @@ public class EventBusServiceDefaultTest {
         @Test
         public void implementation_setToEmptyString() throws Exception {
             eventBusService.init(ImmutableMap.of(EventBusServiceDefault.KEY_EVENT_BUS_IMPLEMENTATION, ""));
-            assertThat(eventBusService.getImplementation(), is("guava"));
+            assertThat(eventBusService.getImplementation(), isIn(new String[] {"auto", "plugin"}));
         }
 
         @Test
@@ -128,82 +125,5 @@ public class EventBusServiceDefaultTest {
         }
 
     }
-
-    public static class Post extends EventBusServiceDefaultTest {
-
-        public static class Subscriber {
-            Object obj;
-            @Subscribe
-            public void on(Object obj) {
-                this.obj = obj;
-            }
-        }
-
-        @Rule
-        public ExpectedException expectedException = ExpectedException.none();
-
-        Subscriber subscriber;
-
-        @Before
-        public void setUp() throws Exception {
-            super.setUp();
-            subscriber = new Subscriber();
-        }
-
-        @Test
-        public void allow_late_registration_means_can_register_after_post() throws Exception {
-            // given
-            eventBusService.init(ImmutableMap.of(
-                    EventBusServiceDefault.KEY_ALLOW_LATE_REGISTRATION, "true",
-                    EventBusServiceDefault.KEY_EVENT_BUS_IMPLEMENTATION, "guava"));
-            assertThat(eventBusService.isAllowLateRegistration(), is(true));
-            assertThat(eventBusService.getImplementation(), is("guava"));
-
-            eventBusService.post(new Object());
-
-            // when
-            eventBusService.register(subscriber);
-
-            // then
-            assertThat(subscriber.obj, is(nullValue()));
-        }
-
-        @Test
-        public void disallow_late_registration_means_cannot_register_after_post() throws Exception {
-            // given
-            eventBusService.init(ImmutableMap.of(
-                    EventBusServiceDefault.KEY_ALLOW_LATE_REGISTRATION, "false",
-                    EventBusServiceDefault.KEY_EVENT_BUS_IMPLEMENTATION, "guava"));
-            assertThat(eventBusService.isAllowLateRegistration(), is(false));
-            assertThat(eventBusService.getImplementation(), is("guava"));
-
-            eventBusService.post(new Object());
-
-            // expect
-            expectedException.expect(IllegalStateException.class);
-
-            // when
-            eventBusService.register(new Subscriber());
-        }
-
-        @Test
-        public void disallow_late_registration_means_can_register_before_post() throws Exception {
-            // given
-            eventBusService.init(ImmutableMap.of(
-                    EventBusServiceDefault.KEY_ALLOW_LATE_REGISTRATION, "false",
-                    EventBusServiceDefault.KEY_EVENT_BUS_IMPLEMENTATION, "guava"));
-            assertThat(eventBusService.isAllowLateRegistration(), is(false));
-            assertThat(eventBusService.getImplementation(), is("guava"));
-
-            eventBusService.register(subscriber);
-
-            // when
-            final Object event = new Object();
-            eventBusService.post(event);
-
-            // then
-            assertThat(subscriber.obj, is(event));
-        }
-
-    }
+   
 }
