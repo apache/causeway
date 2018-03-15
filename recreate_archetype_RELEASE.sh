9cc64a3cbbbd3d8d0d03cb3c5a5b1c250dbbd7a9
Dan Haywood <dan@haywood-associates.co.uk>
2018-03-15 00:05:10 +0000

    ISIS-1916: recreating simpleapp archetype

--- recreate_archetype_RELEASE.sh
+++ recreate_archetype_RELEASE.sh
@@ -1,17 +1,18 @@
-export ISISREL=1.13.0
-export ISISDEV=1.14.0-SNAPSHOT
-export JIRA=ISIS-1335
+#!/usr/bin/env bash
+export ISISREL=1.16.3
+export ISISDEV=1.16.4-SNAPSHOT
+export JIRA=ISIS-1916
 
 export ISISTMP=/c/tmp   # or as required
 export ISISPAR=$ISISREL
 export ISISRC=RC1
-export ISISCPT=$(echo $ISISART | cut -d- -f2)
-export ISISCPN=$(echo $ISISART | cut -d- -f1)
 
 for a in simpleapp helloworld
 do
 	pushd example/application/$a
 	export ISISART=$a-archetype
+    export ISISCPT=$(echo $ISISART | cut -d- -f2)
+    export ISISCPN=$(echo $ISISART | cut -d- -f1)
 
 	env | grep ISIS | sort
 	sh ../../../scripts/recreate-archetype.sh $JIRA
--- recreate_archetype_SNAPSHOT.sh
+++ recreate_archetype_SNAPSHOT.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
-export ISISREL=1.16.3.SNAPSHOT
+export ISISREL=1.16.3-SNAPSHOT
 export ISISDEV=1.16.3-SNAPSHOT
 export JIRA=ISIS-1916
 
 export ISISTMP=/c/tmp   # or as required
 export ISISPAR=$ISISREL
 export ISISRC=RC1
-export ISISCPT=$(echo $ISISART | cut -d- -f2)
-export ISISCPN=$(echo $ISISART | cut -d- -f1)
 
 #for a in simpleapp helloworld
 for a in simpleapp
 do
 	pushd example/application/$a
 	export ISISART=$a-archetype
+    export ISISCPT=$(echo $ISISART | cut -d- -f2)
+    export ISISCPN=$(echo $ISISART | cut -d- -f1)
 
 	env | grep ISIS | sort
 	sh ../../../scripts/recreate-archetype.sh $JIRA
