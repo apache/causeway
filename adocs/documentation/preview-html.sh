63f7419f2a1bddb20937b94ed7ac7d16d6be3684
danhaywood <dan@haywood-associates.co.uk>
2018-09-19 00:12:53 +0100

    ISIS-1899: updates asciidoc versions
    and makes a start on a custom theme for PDF

--- adocs/documentation/preview-html.sh
+++ adocs/documentation/preview-html.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-mvn compile -o $* && python -m webbrowser -t http://localhost:4000 && pushd target/site && python -m http.server 4000 && popd
+mvn compile  $* && python -m webbrowser -t http://localhost:4000 && pushd target/site && python -m http.server 4000 && popd
 #mvn compile -Dreindex -o && python -m webbrowser -t http://localhost:4000 && pushd target/site && python -m http.server 4000 && popd
 
 #mvn compile -o && python -m webbrowser -t http://localhost:4000/guides/ugvw/ugvw.html && pushd target/site && python -m http.server 4000 && popd
