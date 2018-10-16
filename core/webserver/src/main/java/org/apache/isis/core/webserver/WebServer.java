f67a4a7db2a15d223420c8736808f0fc70b93b69
Andi Huber <ahuber@apache.org>
2018-10-17 00:39:37 +0200

    ISIS-1895: fixes context parameters not picked up during bootstrapping (part 2/2)
    Task-Url: https://issues.apache.org/jira/browse/ISIS-1895

--- core/webserver/src/main/java/org/apache/isis/core/webserver/WebServer.java
+++ core/webserver/src/main/java/org/apache/isis/core/webserver/WebServer.java
@@ -58,7 +58,6 @@ import org.apache.isis.core.runtime.runner.opts.OptionHandlerFixture;
 import org.apache.isis.core.runtime.runner.opts.OptionHandlerFixtureFromEnvironmentVariable;
 import org.apache.isis.core.runtime.runner.opts.OptionHandlerHelp;
 import org.apache.isis.core.runtime.runner.opts.OptionHandlerSystemProperties;
-import org.apache.isis.core.webapp.WebAppConstants;
 import org.apache.isis.core.webserver.internal.OptionHandlerPort;
 import org.apache.isis.core.webserver.internal.OptionHandlerStartupMode;
 
@@ -164,8 +163,6 @@ public class WebServer {
         final Server jettyServer = new Server(port);
         final WebAppContext context = new WebAppContext(SRC_MAIN_WEBAPP, webappContextPath);
         jettyServer.setHandler(context);
-
-        context.setAttribute(WebAppConstants.CONFIGURATION_BUILDER_KEY, configurationBuilder);
 
         return jettyServer;
     }
