18aab827b8c5e7fd73da287861d8db8bed7a8625
danhaywood <dan@haywood-associates.co.uk>
2018-12-31 10:31:22 +0000

    ISIS-1899: fixes version-bump.sh and version-reset.sh scripts
    ... they were wrong way around

--- scripts/version-reset.sh
+++ scripts/version-reset.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 JIRA=$1
 shift
-REL=$1
-shift
+REL="1.0.0-SNAPSHOT"
 
-if [ ! "$JIRA" -o ! "$REL" ]; then
+
+if [ ! "$JIRA"  ]; then
     echo "" >&2
     echo "usage: " >&2
     echo "" >&2
-    echo "$(basename $0) \${ISISJIRA} \${ISISREL} " >&2
+    echo "$(basename $0) \${ISISJIRA} " >&2
     echo "" >&2
     exit 1
 fi
@@ -16,4 +16,4 @@ fi
 sed -i -E "s|<revision>[^<]+<|<revision>${REL}<|g" core/pom.xml
 sed -i -E "s|<revision>[^<]+<|<revision>${REL}<|g" example/application/simpleapp/pom.xml
 sed -i -E "s|<revision>[^<]+<|<revision>${REL}<|g" example/application/helloworld/pom.xml
-git commit -am "${JIRA}: bumps revision property across all pom.xml's to ${REL}"
+git commit -am "${JIRA}: bumps revision property across all pom.xml's back to ${REL}"
