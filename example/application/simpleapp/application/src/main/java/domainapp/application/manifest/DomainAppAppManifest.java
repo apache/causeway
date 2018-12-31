33389ddbeb111fa3bedf8a3c08558ec0d28ca942
danhaywood <dan@haywood-associates.co.uk>
2018-12-31 09:15:44 +0000

    ISIS-2023: for simpleapp archetype, removes now-empty non-changing properties, just isis-non-changing.propertise

--- example/application/simpleapp/application/src/main/java/domainapp/application/manifest/DomainAppAppManifest.java
+++ example/application/simpleapp/application/src/main/java/domainapp/application/manifest/DomainAppAppManifest.java
@@ -29,12 +29,8 @@ public class DomainAppAppManifest extends AppManifestAbstract2 {
 
     public static final Builder BUILDER = Builder
             .forModule(new DomainAppApplicationModule())
-            .withConfigurationPropertiesFile(DomainAppAppManifest.class,
-                    "isis-non-changing.properties",
-                    "authentication_shiro.properties",
-                    "persistor_datanucleus.properties",
-                    "viewer_restfulobjects.properties",
-                    "viewer_wicket.properties")
+            .withConfigurationPropertiesFile(
+                    DomainAppAppManifest.class, "isis-non-changing.properties")
             .withAuthMechanism("shiro");
 
     public DomainAppAppManifest() {
