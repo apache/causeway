5a964f241243daeb5c8948b13ed1b8808310bcbd
danhaywood <dan@haywood-associates.co.uk>
2019-06-02 18:03:10 +0100

    ISIS-2062: reorg dg pages

--- antora/components/support/modules/dg/pages/_partials/x.sh
+++ antora/components/support/modules/dg/pages/_partials/x.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-for a in `ls | grep _cgcom`
+for a in `ls | grep _dg`
 do
-	b=$(echo $a | cut -c8- | sed 's|_|/|g')
+	b=$(echo $a | cut -c5- | sed 's|_|/|g')
 	d=$(dirname $b)
 	f=$(basename $b)
 	mkdir -p ../$d
