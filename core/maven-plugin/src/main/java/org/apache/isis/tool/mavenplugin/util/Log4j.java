417e81dac6ad55a8f660b4255bd896515c54e514
Andi Huber <ahuber@apache.org>
2019-06-03 12:39:58 +0200

    ISIS-2112: remove log4j (v1) from the classpath
    Task-Url: https://issues.apache.org/jira/browse/ISIS-2112

--- core/maven-plugin/src/main/java/org/apache/isis/tool/mavenplugin/util/Log4j.java
+++ core/maven-plugin/src/main/java/org/apache/isis/tool/mavenplugin/util/Log4j.java
@@ -20,28 +20,38 @@ package org.apache.isis.tool.mavenplugin.util;
 
 import java.util.Enumeration;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configurator;
+
+
 public final class Log4j {
 
     private Log4j(){}
 
     public static void configureIfRequired() {
         if(isConfigured()) return;
-        org.apache.log4j.BasicConfigurator.configure();
-        org.apache.log4j.LogManager.getRootLogger().setLevel(org.apache.log4j.Level.INFO);
+        //org.apache.log4j.BasicConfigurator.configure(); //log4j v1
+        Configurator.setLevel(LogManager.getRootLogger().getName(), Level.INFO);
     }
 
     private static boolean isConfigured() {
-        Enumeration<?> appenders = org.apache.log4j.LogManager.getRootLogger().getAllAppenders();
-        if (appenders.hasMoreElements()) {
-            return true;
-        }
-        Enumeration<?> loggers = org.apache.log4j.LogManager.getCurrentLoggers();
-        while (loggers.hasMoreElements()) {
-            org.apache.log4j.Logger c = (org.apache.log4j.Logger) loggers.nextElement();
-            if (c.getAllAppenders().hasMoreElements())
-                return true;
-        }
-        return false;
+    	return LoggerContext.getContext().isInitialized();
+    	
+//log4j v1    	
+//        Enumeration<?> appenders = LogManager.getRootLogger(). getAllAppenders();
+//        if (appenders.hasMoreElements()) {
+//            return true;
+//        }
+//        Enumeration<?> loggers = LogManager.getCurrentLoggers();
+//        while (loggers.hasMoreElements()) {
+//            Logger c = (Logger) loggers.nextElement();
+//            if (c.getAllAppenders().hasMoreElements())
+//                return true;
+//        }
+//        return false;
     }
 
 
