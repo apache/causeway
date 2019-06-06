65834b42de51730628a7a22cf49a7065d36670f9
Andi Huber <ahuber@apache.org>
2019-06-06 13:32:56 +0200

    ISIS-2112: smoke testing the AuditerService
    Task-Url: https://issues.apache.org/jira/browse/ISIS-2112

--- example/misc/smoketest/src/test/java/org/apache/isis/testdomain/tests/AuditerServiceTest.java
+++ example/misc/smoketest/src/test/java/org/apache/isis/testdomain/tests/AuditerServiceTest.java
@@ -18,14 +18,12 @@
  */
 package org.apache.isis.testdomain.tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.sql.Timestamp;
 import java.util.UUID;
 
 import javax.inject.Inject;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 
 import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.DomainServiceLayout;
@@ -33,21 +31,44 @@ import org.apache.isis.applib.annotation.NatureOfService;
 import org.apache.isis.applib.fixturescripts.FixtureScripts;
 import org.apache.isis.applib.services.audit.AuditerService;
 import org.apache.isis.applib.services.bookmark.Bookmark;
+import org.apache.isis.applib.services.fixturespec.FixtureScriptsDefault;
 import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.applib.services.xactn.TransactionService;
 import org.apache.isis.core.runtime.system.context.IsisContext;
+import org.apache.isis.incubator.IsisTransactionManagerForJdo;
+import org.apache.isis.runtime.spring.IsisBoot;
 import org.apache.isis.testdomain.jdo.Book;
+import org.apache.isis.testdomain.jdo.JdoTestDomainModule;
 import org.apache.isis.testdomain.jdo.JdoTestDomainPersona;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.val;
 
 /**
  * Depends on {@link JdoBootstrappingTest_usingFixtures} to succeed.
  */
-@Disabled // TODO[2112] activate tests
+@SpringBootTest(
+		classes = { 
+				IsisBoot.class, 
+				FixtureScriptsDefault.class, 
+				JdoTestDomainModule.class, 
+				IsisTransactionManagerForJdo.class,
+				AuditerServiceTest.AuditerServiceStub.class
+		}, 
+		properties = {
+				"logging.config=log4j2-test.xml",
+				// "isis.reflector.introspector.parallelize=false",
+				// "logging.level.org.apache.isis.core.metamodel.specloader.specimpl.ObjectSpecificationAbstract=TRACE"
+	})
+@Transactional
 class AuditerServiceTest {
 
+	@Inject private RepositoryService repository;
+	@Inject private TransactionService transactionService;
 	@Inject private FixtureScripts fixtureScripts;
 
 	@BeforeEach
@@ -60,27 +81,31 @@ class AuditerServiceTest {
 		fixtureScripts.runBuilderScript(JdoTestDomainPersona.InventoryWith1Book.builder());
 	}
 
-	@Test
+	@Test @Rollback(false)
 	void auditerServiceShouldBeAwareOfInventoryChanges() {
 
-		val auditerServiceAny = IsisContext.getServiceRegistry().lookupServiceElseFail(AuditerService.class);
+		val auditerServiceAny = IsisContext.getServiceRegistry()
+				.select(AuditerServiceStub.class)
+				.getFirst()
+				.orElse(null);
 
-		// priority based service lookup (could be a test on its own)
 		assertEquals(AuditerServiceStub.class, auditerServiceAny.getClass());
 
 		// given
 		val auditerService = (AuditerServiceStub) auditerServiceAny;
-		val repository = IsisContext.getServiceRegistry().lookupServiceElseFail(RepositoryService.class);
-		val txMan = IsisContext.getPersistenceSession().get().getTransactionManager();
 		val book = repository.allInstances(Book.class).listIterator().next();
 
 		// when
 		auditerService.clearHistory();
 		book.setName("Book #2");
 		repository.persist(book);
-		txMan.endTransaction();
-
-		// then
+		
+		// then - before the commit
+		assertEquals("", auditerService.getHistory());
+		
+		transactionService.nextTransaction();
+		
+		// then - after the commit
 		assertEquals("targetClassName=Book,propertyName=name,preValue=Sample Book,postValue=Book #2;",
 				auditerService.getHistory());
 	}
