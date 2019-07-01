8f57de5054ed79b21cc5c92e461dada47106d2f2
Andi Huber <ahuber@apache.org>
2019-07-01 12:41:53 +0200

    ISIS-2126 brings in new main folder 'extensions'
    - adding extension 'viewer-wicket-excel' (from incode)
    - moving module 'smoketest' one up in the filesystem
    - also fixes smoke-test setups

---
+++ example/smoketest/src/test/java/org/apache/isis/testdomain/tests/JdoBootstrappingTest_usingFixtures.java
@@ -0,0 +1,79 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.isis.testdomain.tests;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.inject.Inject;
+
+import org.apache.isis.applib.fixturescripts.FixtureScripts;
+import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.testdomain.jdo.Inventory;
+import org.apache.isis.testdomain.jdo.JdoTestDomainModule;
+import org.apache.isis.testdomain.jdo.JdoTestDomainPersona;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.val;
+
+@SpringBootTest(
+	classes = { 
+			JdoTestDomainModule.class, 
+	}, 
+	properties = {
+			"logging.config=log4j2-test.xml",
+			// "isis.reflector.introspector.parallelize=false",
+			// "logging.level.org.apache.isis.metamodel.specloader.specimpl.ObjectSpecificationAbstract=TRACE"
+})
+@Transactional
+class JdoBootstrappingTest_usingFixtures {
+
+	@Inject private FixtureScripts fixtureScripts;
+	@Inject private RepositoryService repository;
+
+	void setUp() {
+		// cleanup
+		fixtureScripts.runBuilderScript(JdoTestDomainPersona.PurgeAll.builder());
+
+		// given
+		fixtureScripts.runBuilderScript(JdoTestDomainPersona.InventoryWith1Book.builder());
+	}
+
+	@Test @Rollback(false)
+	void sampleInventoryShouldBeSetUp() {
+
+		setUp();
+
+		val inventories = repository.allInstances(Inventory.class);
+		assertEquals(1, inventories.size());
+
+		val inventory = inventories.get(0);
+		assertNotNull(inventory);
+		assertNotNull(inventory.getProducts());
+		assertEquals(1, inventory.getProducts().size());
+
+		val product = inventory.getProducts().iterator().next();
+		assertEquals("Sample Book", product.getName());
+
+	}
+
+}
---
+++ extensions/viewer-wicket-excel/src/main/java/org/apache/isis/extensions/viewer/wicket/excel/ExcelFileDownloadLink.java
@@ -0,0 +1,97 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.isis.extensions.viewer.wicket.excel;
+
+import java.io.File;
+
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.link.DownloadLink;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.request.IRequestCycle;
+import org.apache.wicket.request.handler.resource.ResourceStreamRequestHandler;
+import org.apache.wicket.request.resource.ContentDisposition;
+import org.apache.wicket.util.encoding.UrlEncoder;
+import org.apache.wicket.util.file.Files;
+import org.apache.wicket.util.resource.FileResourceStream;
+import org.apache.wicket.util.resource.IResourceStream;
+
+class ExcelFileDownloadLink extends DownloadLink {
+
+    private static final long serialVersionUID = 1L;
+    
+    private final String xlsxFileName;
+
+    public ExcelFileDownloadLink(String id, LoadableDetachableModel<File> model, String xlsxFileName) {
+        super(id, model, xlsxFileName);
+        this.xlsxFileName = xlsxFileName;
+    }
+    
+    
+
+    @Override
+    public void onClick()
+    {
+        final File file = getModelObject();
+        if (file == null)
+        {
+            throw new IllegalStateException(getClass().getName() +
+                " failed to retrieve a File object from model");
+        }
+
+        String fileName = encodedFileName();
+
+        final IResourceStream resourceStream = new FileResourceStream(
+            new org.apache.wicket.util.file.File(file)) {
+            
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public String getContentType() {
+                return "application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml";
+            }
+        };
+        
+        getRequestCycle().scheduleRequestHandlerAfterCurrent(
+            new ResourceStreamRequestHandler(resourceStream)
+            {
+                @Override
+                public void respond(IRequestCycle requestCycle)
+                {
+                    super.respond(requestCycle);
+                    Files.remove(file);
+                }
+            }.setFileName(fileName)
+                .setContentDisposition(ContentDisposition.ATTACHMENT));
+    }
+
+	@Override
+	protected void onComponentTag(ComponentTag tag) {
+		super.onComponentTag(tag);
+		tag.put("download", encodedFileName());
+	}
+
+    private String encodedFileName() {
+        return encoded(this.xlsxFileName);
+    }
+
+    private String encoded(final String xlsxFileName) {
+        return UrlEncoder.QUERY_INSTANCE.encode(xlsxFileName, getRequest().getCharset());
+    }
+
+}
