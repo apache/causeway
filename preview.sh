582698610a5dd3d6f99df2ce60d35498345508a4
danhaywood <dan@haywood-associates.co.uk>
2019-07-04 07:18:28 +0100

    ISIS-2062: updates docs for Spring @EventListener

--- preview.sh
+++ preview.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 rm -rf antora/target/site \
 && antora site.yml $@ \
-&& serve antora/target/site
-
-#rm -rf antora/target/site
-#antora site.yml $@
-##cp -R antora/hp-overlay
-#serve antora/target/site
+&& serve --local antora/target/site
