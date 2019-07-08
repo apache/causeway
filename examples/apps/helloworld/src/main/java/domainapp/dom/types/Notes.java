6819e976b0214b770907d38db1f576df48725fcb
danhaywood <dan@haywood-associates.co.uk>
2019-07-08 14:06:15 +0100

    ISIS-2148: change helloworld app's @Notes, should be multiline=5

--- examples/apps/helloworld/src/main/java/domainapp/dom/types/Notes.java
+++ examples/apps/helloworld/src/main/java/domainapp/dom/types/Notes.java
@@ -7,15 +7,13 @@ import java.lang.annotation.Target;
 
 import javax.jdo.annotations.Column;
 
-import org.apache.isis.applib.annotation.Editing;
-import org.apache.isis.applib.annotation.Parameter;
-import org.apache.isis.applib.annotation.ParameterLayout;
-import org.apache.isis.applib.annotation.Property;
+import org.apache.isis.applib.annotation.*;
 
 @Column(length = Notes.MAX_LEN, allowsNull = "true")
 @Property(editing = Editing.ENABLED, maxLength = Notes.MAX_LEN)
+@PropertyLayout(multiLine = 5)
 @Parameter(maxLength = Notes.MAX_LEN)
-@ParameterLayout(named = "Notes")
+@ParameterLayout(multiLine = 5)
 @Target({ ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Notes {
