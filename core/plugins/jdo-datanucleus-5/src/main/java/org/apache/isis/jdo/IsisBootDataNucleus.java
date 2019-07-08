eebb41dccfee8ba5bbd8ac11bba332f30507f0d5
danhaywood <dan@haywood-associates.co.uk>
2019-07-08 13:42:25 +0100

    ISIS-2144: scans the package that includes the Persistable_ mixins

--- core/plugins/jdo-datanucleus-5/src/main/java/org/apache/isis/jdo/IsisBootDataNucleus.java
+++ core/plugins/jdo-datanucleus-5/src/main/java/org/apache/isis/jdo/IsisBootDataNucleus.java
@@ -1,9 +1,13 @@
 package org.apache.isis.jdo;
 
+import org.apache.isis.config.beans.IsisBeanScanInterceptorForSpring;
 import org.apache.isis.jdo.jdosupport.IsisJdoSupportDN5;
+import org.apache.isis.jdo.jdosupport.mixins.Persistable_datanucleusIdLong;
 import org.apache.isis.jdo.metrics.MetricsServiceDefault;
 import org.apache.isis.jdo.transaction.IsisPlatformTransactionManagerForJdo;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 
 @Configuration
@@ -13,6 +17,14 @@ import org.springframework.context.annotation.Import;
 	IsisJdoSupportDN5.class,
 	IsisPlatformTransactionManagerForJdo.class,
 })
+@ComponentScan(
+		basePackageClasses= {
+				// bring in the mixins
+				Persistable_datanucleusIdLong.class,
+		},
+		includeFilters= {
+				@ComponentScan.Filter(type = FilterType.CUSTOM, classes= {IsisBeanScanInterceptorForSpring.class})
+		})
 public class IsisBootDataNucleus {
 
 }
