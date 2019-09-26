8dbcc64f7dbbb715300069a6f73025d7c3aa0425
Jörg Rade <joerg.rade@kuehne-nagel.com>
2019-09-26 13:33:30 +0200

    #35 Measure Code Coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,27 +1,68 @@
+// see https://github.com/mkraynov/kfsad/blob/master/karma.conf.js
 module.exports = function (config) {
   config.set({
- //   basePath: '.',
-    frameworks: ['mocha', 'browserify'],
-    reporters: ['mocha'],
+    basePath: '',
+    // all plugins have to be installed before via
+    // npm install <plugin> --save-dev
     plugins: [
+      'karma-webpack',
       'karma-browserify',
+      'karma-jasmine',
       'karma-mocha',
       'karma-mocha-reporter',
-      'karma-chrome-launcher'],
+      'karma-chrome-launcher',
+      'karma-phantomjs-launcher',
+      'karma-coverage'],
+    frameworks: ['mocha'],
     files: [
-      'build/node_modules/*.js',
-      'build/classes/kotlin/main/*.js',
-      'build/classes/kotlin/test/*.js'
+      'build/js-tests/kroviz-tests.js'
     ],
     exclude: [],
+    preprocessors: {
+      'build/**/*_test.js': ['webpack']
+    },
+//    webpack: require('./webpack.karma'),
+    webpack: {
+      entry: './build/js-tests/kroviz-tests.js',
+      output: {
+        filename: 'bundle.js'
+      }
+    },
+    webpackMiddleware: {
+      noInfo: true
+    },
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['coverage'],
+    port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      // https://github.com/karma-runner/karma-chrome-launcher/issues/158#issuecomment-339265457
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    singleRun: true,
     concurrency: Infinity,
-    preprocessors: {
-      'build/**/*.js': ['browserify'],
+    client: {
+      mocha: {
+        timeout: 6000
+      }
     }
+
+   /* plugins: [
+      'karma-browserify',
+      'karma-mocha',
+      'karma-mocha-reporter',
+      'karma-chrome-launcher',
+      'karma-phantomjs-launcher',
+      'karma-coverage'],
+    browserify: ({
+      entries: ['./build/js/kroviz.js'], */
+//      noParse: ['./node_modules_imported/**/*js']
+//    }),
   })
-}
+};
