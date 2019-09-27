24ab4a0b6cf1e9e2b2cbbb920eda60c08c5ab2a6
Jörg Rade <joerg.rade@kuehne-nagel.com>
2019-09-27 16:38:00 +0200

    #35 Measure Code Coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,68 +1,34 @@
-// see https://github.com/mkraynov/kfsad/blob/master/karma.conf.js
+// Example: see https://github.com/mkraynov/kfsad/blob/master/karma.conf.js
+// Doc: see http://karma-runner.github.io/4.0/config/configuration-file.html
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    // all plugins have to be installed before via
-    // npm install <plugin> --save-dev
-    plugins: [
-      'karma-webpack',
-      'karma-browserify',
-      'karma-jasmine',
-      'karma-mocha',
-      'karma-mocha-reporter',
-      'karma-chrome-launcher',
-      'karma-phantomjs-launcher',
-      'karma-coverage'],
-    frameworks: ['mocha'],
-    files: [
-      'build/js-tests/kroviz-tests.js'
-    ],
-    exclude: [],
-    preprocessors: {
-      'build/**/*_test.js': ['webpack']
-    },
-//    webpack: require('./webpack.karma'),
-    webpack: {
-      entry: './build/js-tests/kroviz-tests.js',
-      output: {
-        filename: 'bundle.js'
-      }
-    },
-    webpackMiddleware: {
-      noInfo: true
-    },
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['coverage'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
-    customLaunchers: {
-      // https://github.com/karma-runner/karma-chrome-launcher/issues/158#issuecomment-339265457
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
-    singleRun: true,
-    concurrency: Infinity,
-    client: {
-      mocha: {
-        timeout: 6000
-      }
-    }
+      files: ['./build/**/*.js'],
+      excludes: ['./**/*.css'],
+      preprocessors: {
+//        'build/**/*_test.js': ['webpack']
+      },
+      frameworks: ['jasmine'],
+      plugins: [
+        require('karma-chrome-launcher'),
+        require('karma-coverage-istanbul-reporter'),
+        require('istanbul-instrumenter-loader'),
+        require('karma-jasmine'),
+        require('karma-jasmine-html-reporter'),
+        require('karma-webpack')
+      ],
+      reporters: ['coverage-istanbul'],
+      coverageIstanbulReporter: {
+        reports: ['html', 'lcovonly', 'text-summary'],
+        // if using webpack and pre-loaders, work around webpack breaking the source path
+        fixWebpackSourcePaths: true
+      },
+      browsers: ['ChromeHeadless'],
 
-   /* plugins: [
-      'karma-browserify',
-      'karma-mocha',
-      'karma-mocha-reporter',
-      'karma-chrome-launcher',
-      'karma-phantomjs-launcher',
-      'karma-coverage'],
-    browserify: ({
-      entries: ['./build/js/kroviz.js'], */
-//      noParse: ['./node_modules_imported/**/*js']
-//    }),
-  })
+//      logLevel: config.LOG_DEBUG,
+      autoWatch: true,
+      singleRun: true,
+      concurrency: Infinity,
+      color: false
+    }
+  )
 };
