9f2773238f8e61fa546fc2ea54ef5c616bb25cc8
danhaywood <dan@haywood-associates.co.uk>
2019-09-28 14:46:01 +0100

    ISIS-2086: adds unit tests for isis.viewer.wicket.maxTitleLengthIn{Parented/Standalone}Tables

---
+++ core/config/src/test/java/org/apache/isis/config/IsisConfiguration_getMaxTitleLengthInParentedTables_Test.java
@@ -0,0 +1,63 @@
+package org.apache.isis.config;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IsisConfiguration_getMaxTitleLengthInParentedTables_Test {
+
+    private IsisConfiguration configuration;
+
+    @Before
+    public void setUp() throws Exception {
+        configuration = new IsisConfiguration();
+    }
+
+    @Test
+    public void when_not_set() throws Exception {
+        // when
+        int val = configuration.getViewer().getWicket().getMaxTitleLengthInParentedTables();
+
+        // then
+        Assertions.assertThat(val).isEqualTo(12);
+    }
+
+    @Test
+    public void when_not_set_explicitly_but_fallback_has_been() throws Exception {
+        // given
+        configuration.getViewer().getWicket().setMaxTitleLengthInTables(20);
+
+        // when
+        int val = configuration.getViewer().getWicket().getMaxTitleLengthInParentedTables();
+
+        // then
+        Assertions.assertThat(val).isEqualTo(20);
+    }
+
+    @Test
+    public void when_set_explicitly() throws Exception {
+        // given
+        configuration.getViewer().getWicket().setMaxTitleLengthInParentedTables(25);
+
+        // when
+        int val = configuration.getViewer().getWicket().getMaxTitleLengthInParentedTables();
+
+        // then
+        Assertions.assertThat(val).isEqualTo(25);
+    }
+
+    @Test
+    public void when_set_explicitly_ignores_fallback_has_been() throws Exception {
+        // given
+        configuration.getViewer().getWicket().setMaxTitleLengthInTables(20);
+        configuration.getViewer().getWicket().setMaxTitleLengthInParentedTables(25);
+
+        // when
+        int val = configuration.getViewer().getWicket().getMaxTitleLengthInParentedTables();
+
+        // then
+        Assertions.assertThat(val).isEqualTo(25);
+    }
+}
