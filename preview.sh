a18ff6bc60bd93743431ee744d98f4ebc5ed6cc9
danhaywood <dan@haywood-associates.co.uk>
2019-10-23 00:19:50 +0100

    ISIS-2062: improves formatting

--- preview.sh
+++ preview.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm -rf antora/target/site \
-&& rm -rf core/config/_adoc/modules/cfg/examples/generated/spring-configuration-metadata \
-&& groovy scripts/generateConfigDocs -f "./core/config/target/classes/META-INF/spring-configuration-metadata.json" -o ./core/config/_adoc/modules/cfg/examples/generated/spring-configuration-metadata \
+&& rm -rf core/config/_adoc/modules/cfg/examples/generated \
+&& groovy scripts/generateConfigDocs -f "./core/config/target/classes/META-INF/spring-configuration-metadata.json" -o ./core/config/_adoc/modules/cfg/examples/generated \
 && antora site.yml $@ \
 && serve antora/target/site
