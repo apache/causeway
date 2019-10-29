5f3a37f799bba7aaf363f3460ecdedd92ec8cfb4
danhaywood <dan@haywood-associates.co.uk>
2019-10-29 04:31:54 +0000

    ISIS-2094: removes simpleapp docker build for now

--- build-example-apps.sh
+++ build-example-apps.sh
@@ -59,34 +59,34 @@ cd ..
 #
 #cd ../../..
 
-export APP_NAME=simpleapp
-cd examples/apps/$APP_NAME
-
-mvn --batch-mode \
-    install \
-    -Drevision=$REVISION \
-    -Disis.version=$REVISION \
-    -Dmavenmixin-docker \
-    -Ddocker-plugin.imageName=$ORG_NAME/$APP_NAME
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-mvn -s ../../../.m2/settings.xml \
-    --batch-mode \
-    docker:push@push-image-tagged \
-    -pl webapp \
-    -DskipTests \
-    -Dskip.isis-validate \
-    -Dskip.isis-swagger \
-    -Drevision=$REVISION \
-    -Disis.version=$REVISION \
-    -Dmavenmixin-docker \
-    -Ddocker-plugin.imageName=$ORG_NAME/$APP_NAME \
-    -Ddocker-plugin.serverId=docker-registry \
-    -Ddocker.registryUrl=$DOCKER_REGISTRY_URL
-if [ $? -ne 0 ]; then
-  exit 1
-fi
-
-cd ../../..
+#export APP_NAME=simpleapp
+#cd examples/apps/$APP_NAME
+#
+#mvn --batch-mode \
+#    install \
+#    -Drevision=$REVISION \
+#    -Disis.version=$REVISION \
+#    -Dmavenmixin-docker \
+#    -Ddocker-plugin.imageName=$ORG_NAME/$APP_NAME
+#if [ $? -ne 0 ]; then
+#  exit 1
+#fi
+#
+#mvn -s ../../../.m2/settings.xml \
+#    --batch-mode \
+#    docker:push@push-image-tagged \
+#    -pl webapp \
+#    -DskipTests \
+#    -Dskip.isis-validate \
+#    -Dskip.isis-swagger \
+#    -Drevision=$REVISION \
+#    -Disis.version=$REVISION \
+#    -Dmavenmixin-docker \
+#    -Ddocker-plugin.imageName=$ORG_NAME/$APP_NAME \
+#    -Ddocker-plugin.serverId=docker-registry \
+#    -Ddocker.registryUrl=$DOCKER_REGISTRY_URL
+#if [ $? -ne 0 ]; then
+#  exit 1
+#fi
+#
+#cd ../../..
