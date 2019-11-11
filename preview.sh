234e8cf4a154f6793be30f09286a5af80d30c7da
danhaywood <dan@haywood-associates.co.uk>
2019-11-11 18:08:03 +0100

    ISIS-2062: moves docs from _adoc to src/main/doc
    for all modules

--- preview.sh
+++ preview.sh
@@ -2,5 +2,5 @@
 rm -rf antora/target/site \
 && rm -rf core/config/_adoc/modules/config/examples/generated \
 && groovy scripts/generateConfigDocs -f "./core/config/target/classes/META-INF/spring-configuration-metadata.json" -o ./core/config/_adoc/modules/config/examples/generated \
-&& antora  $@ \
+&& antora site.yml $@ \
 && serve antora/target/site
