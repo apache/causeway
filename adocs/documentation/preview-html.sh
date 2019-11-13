c9727e6fa8d39de90cf74cde70eb11107e8bc841
danhaywood <dan@haywood-associates.co.uk>
2019-11-13 21:03:50 +0000

    ISIS-2062: cleans up some docs, bumps the UI bundle

--- adocs/documentation/preview-html.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mvn compile  $* && python -m webbrowser -t http://localhost:4000 && pushd target/site && python -m http.server 4000 && popd
-#mvn compile -Dreindex -o && python -m webbrowser -t http://localhost:4000 && pushd target/site && python -m http.server 4000 && popd
-
-#mvn compile -o && python -m webbrowser -t http://localhost:4000/guides/ugvw/ugvw.html && pushd target/site && python -m http.server 4000 && popd
-#mvn compile -o -Dreindex && python -m webbrowser -t http://localhost:4000/guides/ugvw/ugvw.html && pushd target/site && python -m http.server 4000 && popd
