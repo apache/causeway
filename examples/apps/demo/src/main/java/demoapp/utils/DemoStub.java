fbfa9930e429654f9c785f0f78a4708f05146061
Andi Huber <ahuber@apache.org>
2019-11-13 14:55:42 +0100

    ISIS-2158: fixes demo-app links to sources

--- examples/apps/demo/src/main/java/demoapp/utils/DemoStub.java
+++ examples/apps/demo/src/main/java/demoapp/utils/DemoStub.java
@@ -49,7 +49,7 @@ public abstract class DemoStub {
     private static Map<String, String> createConstants() {
         val map = new HashMap<String, String>();
         map.put("SOURCES_ISIS", "https://github.com/apache/isis/blob/master/core/applib/src/main/java");
-        map.put("SOURCES_DEMO", "https://github.com/apache/isis/blob/master/examples/apps/demo/src/main/java");
+        map.put("SOURCES_DEMO", "https://github.com/apache/isis/tree/master/examples/apps/demo/src/main/java");
         map.put("ISSUES_DEMO", "https://issues.apache.org/jira/"); 
         return map;
     }
