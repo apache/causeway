8d5a40805b9c2ff16e2415d6860fb269302a9bd9
Andi Huber <ahuber@apache.org>
2019-11-14 12:04:04 +0100

    ISIS-2062: run groovy script only if groovy is installed; else noop
    allows to run with 'nightly' or on your workstation, but is skipped with
    the 'apache/isis' Site workflow

--- scripts/ci/build-site.sh
+++ scripts/ci/build-site.sh
@@ -2,17 +2,24 @@
 set -e
 
 SITE_CONFIG=$1
+GROOVY_CMD=`command -v groovy`
 
 bash $CI_SCRIPTS_PATH/print-environment.sh "build-site"
 
 echo ""
 echo "\$SITE_CONFIG: ${SITE_CONFIG}"
+echo "\$GROOVY_CMD: ${GROOVY_CMD}"
 echo ""
+ 
+# for now meant to run with nightly builds only 
+if [ ! -z "${GROOVY_CMD}" ]; then
 
-## generate automated site content (adoc files) 
-groovy $CI_SCRIPTS_PATH/../generateConfigDocs.groovy \
-  -f $PROJECT_ROOT_PATH/core/config/target/classes/META-INF/spring-configuration-metadata.json \
-  -o $PROJECT_ROOT_PATH/core/config/src/main/doc/modules/config/examples/generated
+  ## generate automated site content (adoc files) 
+  ${GROOVY_CMD} $CI_SCRIPTS_PATH/../generateConfigDocs.groovy \
+    -f $PROJECT_ROOT_PATH/core/config/target/classes/META-INF/spring-configuration-metadata.json \
+    -o $PROJECT_ROOT_PATH/core/config/src/main/doc/modules/config/examples/generated
+  
+fi
 
 ## run antora
 $(npm bin)/antora --stacktrace $SITE_CONFIG
