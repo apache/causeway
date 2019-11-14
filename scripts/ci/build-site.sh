aaf13a443aceee1cabab6c15f72828a2b1db1f14
danhaywood <dan@haywood-associates.co.uk>
2019-11-14 07:34:31 +0000

    ISIS-2062: adds TODO for running groovy script in CI pipeline

--- scripts/ci/build-site.sh
+++ scripts/ci/build-site.sh
@@ -9,6 +9,11 @@ echo ""
 echo "\$SITE_CONFIG: ${SITE_CONFIG}"
 echo ""
 
+# TODO: run groovy.
+#  However, this currently needs a local build of the metadata ... will also need to pull down from repo and unzip
+#groovy $CI_SCRIPTS_PATH/generateConfigDocs -f "./core/config/target/classes/META-INF/spring-configuration-metadata.json" -o ./core/config/_adoc/modules/config/examples/generated \
+#
+
 # run antora
 $(npm bin)/antora --stacktrace $SITE_CONFIG
 
