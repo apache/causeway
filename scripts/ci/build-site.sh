c1b8fdc1b7b84a603e3296b3f7f718caad66b3cb
Andi Huber <ahuber@apache.org>
2019-11-14 12:14:58 +0100

    ISIS-2062: fixes 'build-site' failing

--- scripts/ci/build-site.sh
+++ scripts/ci/build-site.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-set -e
 
 SITE_CONFIG=$1
 GROOVY_CMD=`command -v groovy`
+
+set -e
 
 bash $CI_SCRIPTS_PATH/print-environment.sh "build-site"
 
 echo ""
 echo "\$SITE_CONFIG: ${SITE_CONFIG}"
-echo "\$GROOVY_CMD: ${GROOVY_CMD}"
+echo "\$GROOVY_CMD:  ${GROOVY_CMD}"
 echo ""
  
 # for now meant to run with nightly builds only 
-if [ ! -z "${GROOVY_CMD}" ]; then
+if [ -z "${GROOVY_CMD}" ]; then
+
+  echo "no groovy: skipping automated site content script(s)"
+
+else
 
   ## generate automated site content (adoc files) 
   ${GROOVY_CMD} $CI_SCRIPTS_PATH/../generateConfigDocs.groovy \
