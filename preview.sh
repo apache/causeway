fe71b1068e2314591255c71f393ee5c6cfa73320
danhaywood <dan@haywood-associates.co.uk>
2019-11-18 12:10:36 +0000

    ISIS-2062: reorg extensions docs

--- preview.sh
+++ preview.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+FILE=site.yml
+if [[ $# -gt 0 ]]; then
+  FILE=site-$1.yml
+  shift
+fi
+
+#echo $FILE
+
 rm -rf antora/target/site \
 && rm -rf core/config/_adoc/modules/config/examples/generated \
 && groovy scripts/generateConfigDocs -f "./core/config/target/classes/META-INF/spring-configuration-metadata.json" -o ./core/config/src/main/doc/modules/config/examples/generated \
-&& antora site.yml $@ \
+&& antora $FILE $* \
 && serve antora/target/site
