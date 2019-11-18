8b71ae83511b56e2bfbb14a85800bedda574c3c6
danhaywood <dan@haywood-associates.co.uk>
2019-11-18 07:36:15 +0000

    ISIS-2062: tidying up refguide and userguide docs.

--- preview-scratch.sh
+++ preview-scratch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm -rf antora/target/site \
 && rm -rf core/config/_adoc/modules/config/examples/generated \
-&& groovy scripts/generateConfigDocs -f "./core/config/target/classes/META-INF/spring-configuration-metadata.json" -o ./core/config/_adoc/modules/config/examples/generated \
+&& groovy scripts/generateConfigDocs -f "./core/config/target/classes/META-INF/spring-configuration-metadata.json" -o ./core/config/src/main/doc/modules/config/examples/generated \
 && antora site-scratch.yml $@ \
 && serve antora/target/site
