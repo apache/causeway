1cb58be0563919a3c219a28f870cef1f3e596d74
danhaywood <dan@haywood-associates.co.uk>
2019-11-21 08:46:03 +0000

    ISIS-2181: renames preview2.sh to preview.sh

--- preview.sh
+++ preview.sh
@@ -1,27 +1,13 @@
 #!/usr/bin/env bash
-FILE=site.yml
+PLAYBOOK_FILE=site.yml
 if [[ $# -gt 0 ]]; then
-  FILE=antora/playbooks/site-$1.yml
+  PLAYBOOK_FILE=antora/playbooks/site-$1.yml
   shift
 fi
 
-echo "copying over examples ..."
-for examples_sh in $(find . -name examples.sh -print)
-do
-  echo $examples_sh
-  sh $examples_sh
-done
-
-
-echo "generating config docs ..."
-rm -rf core/config/src/main/doc/modules/config/examples/generated \
-&& groovy scripts/generateConfigDocs -f "./core/config/target/classes/META-INF/spring-configuration-metadata.json" -o ./core/config/src/main/doc/modules/config/examples/generated
-
-
-echo "generating site ..."
-rm -rf antora/target/site \
-&& antora $FILE $*
-
+echo "building ..."
+export ANTORA_CMD=antora
+sh build-site.sh $PLAYBOOK_FILE
 
 echo "serving ..."
 serve antora/target/site
