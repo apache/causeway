a170025398785c9f3dd4e9023114992ac5bc5d5f
danhaywood <dan@haywood-associates.co.uk>
2019-12-04 18:46:51 +0000

    ISIS-2213: moves jaxrs-resteasy-4 packages

--- core/viewers/restfulobjects/jaxrs-resteasy-4/src/main/java/org/apache/isis/viewer/restfulobjects/jaxrsresteasy4/conneg/RestfulObjectsJaxbWriterForXml.java
+++ core/viewers/restfulobjects/jaxrs-resteasy-4/src/main/java/org/apache/isis/viewer/restfulobjects/jaxrsresteasy4/conneg/RestfulObjectsJaxbWriterForXml.java
@@ -16,7 +16,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-package org.apache.isis.viewer.restfulobjects.jaxrs.resteasy.conneg;
+package org.apache.isis.viewer.restfulobjects.jaxrsresteasy4.conneg;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
