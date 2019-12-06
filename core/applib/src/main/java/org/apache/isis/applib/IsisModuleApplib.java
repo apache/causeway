66f416477a162bd930fabf9e0c5ffca7e9925e4d
danhaywood <dan@haywood-associates.co.uk>
2019-12-06 06:32:59 +0000

    ISIS-2215: TableColumnService is not a service, don't declare as such.
    Also move to applib.spi

--- core/applib/src/main/java/org/apache/isis/applib/IsisModuleApplib.java
+++ core/applib/src/main/java/org/apache/isis/applib/IsisModuleApplib.java
@@ -43,7 +43,6 @@ import org.apache.isis.applib.services.publish.PublisherServiceLogging;
 import org.apache.isis.applib.services.queryresultscache.QueryResultsCacheInternal;
 import org.apache.isis.applib.services.scratchpad.Scratchpad;
 import org.apache.isis.applib.services.swagger.SwaggerServiceMenu;
-import org.apache.isis.applib.services.tablecol.TableColumnOrderService;
 import org.apache.isis.applib.services.urlencoding.UrlEncodingServiceWithCompression;
 import org.apache.isis.schema.IsisModuleSchema;
 import org.springframework.context.annotation.Configuration;
@@ -76,7 +75,6 @@ import org.springframework.context.annotation.Import;
         QueryResultsCacheInternal.class,
         Scratchpad.class,
         SwaggerServiceMenu.class,
-        TableColumnOrderService.class,
         UrlEncodingServiceWithCompression.class,
 
         // @Component's
--- core/applib/src/main/java/org/apache/isis/applib/spi/tablecol/TableColumnOrderService.java
+++ core/applib/src/main/java/org/apache/isis/applib/spi/tablecol/TableColumnOrderService.java
@@ -16,7 +16,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-package org.apache.isis.applib.services.tablecol;
+package org.apache.isis.applib.spi.tablecol;
 
 import java.util.List;
 
