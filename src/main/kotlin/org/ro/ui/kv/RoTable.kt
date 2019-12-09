e29c93f31acdbb628cda1c8ddc43052eb8aeb381
Jörg Rade <joerg.rade@kuehne-nagel.com>
2019-12-09 16:28:10 +0100

    table column header styling & feature (moving)

--- src/main/kotlin/org/ro/ui/kv/RoTable.kt
+++ src/main/kotlin/org/ro/ui/kv/RoTable.kt
@@ -4,6 +4,7 @@ import org.ro.core.model.DisplayList
 import org.ro.core.model.Exposer
 import pl.treksoft.kvision.panel.SimplePanel
 import pl.treksoft.kvision.table.TableType
+import pl.treksoft.kvision.tabulator.Layout
 import pl.treksoft.kvision.tabulator.Tabulator
 import pl.treksoft.kvision.tabulator.TabulatorOptions
 import pl.treksoft.kvision.tabulator.tabulator
@@ -21,14 +22,15 @@ class RoTable(displayList: DisplayList) : SimplePanel() {
         val model = displayList.data
         val columns = ColumnFactory().buildColumns(displayList, true)
         val options = TabulatorOptions(
+                movableColumns = true,
                 height = "calc(100vh - 128px)",
-               // layout = Layout.FITDATAFILL,
+                layout = Layout.FITDATAFILL,
                 columns = columns,
                 persistenceMode = false//,
                 //selectable = true
         )
 
-        val tableTypes = setOf(TableType.BORDERED, TableType.STRIPED, TableType.HOVER)
+        val tableTypes = setOf(/*TableType.BORDERED,*/ TableType.STRIPED, TableType.HOVER)
 
         tabulator(model, options = options, types = tableTypes) {
             setEventListener<Tabulator<Exposer>> {
