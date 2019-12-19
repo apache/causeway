1729a0c6938e811458c0020add594ee2ff74e3ab
Jörg Rade <joerg.rade@kuehne-nagel.com>
2019-12-19 16:42:40 +0100

    #22 Create UML Diagrams (obey SOC)

--- src/main/kotlin/org/ro/core/aggregator/DiagramAggregator.kt
+++ src/main/kotlin/org/ro/core/aggregator/DiagramAggregator.kt
@@ -1,13 +1,18 @@
 package org.ro.core.aggregator
 
 import org.ro.core.event.LogEntry
-import org.ro.ui.FileAlert
 import org.ro.ui.Point
+import org.w3c.dom.parsing.DOMParser
+import kotlin.browser.document
 
 class DiagramAggregator(val at: Point = Point(100,100)) : BaseAggregator() {
 
     override fun update(logEntry: LogEntry) {
-        FileAlert(logEntry).open()
+        val response = logEntry.response
+        val p = DOMParser()
+        val svg = p.parseFromString(response, "image/svg+xml")
+        val dp = document.getElementById("diagramPanel")
+        dp.asDynamic().appendChild(svg.documentElement)
     }
 
 }
--- src/main/kotlin/org/ro/handler/DiagramHandler.kt
+++ src/main/kotlin/org/ro/handler/DiagramHandler.kt
@@ -1,20 +1,14 @@
 package org.ro.handler
 
-import org.w3c.dom.parsing.DOMParser
-import kotlin.browser.document
-
 class DiagramHandler : BaseHandler(), IResponseHandler {
 
     override fun doHandle() {
         update()
     }
 
-    override fun canHandle(xhrResponse: String): Boolean {
-        val p = DOMParser()
-        val svg = p.parseFromString(xhrResponse, "image/svg+xml")
-        val dp = document.getElementById("diagramPanel")
-        dp.asDynamic().appendChild(svg.documentElement)
-        return true
+    override fun canHandle(response: String): Boolean {
+        return response.startsWith("<") && response.endsWith(">")
     }
+
 
 }
