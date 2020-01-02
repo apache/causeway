fd362d2ab4cd5dcc087d3a936bc1135a200bd9fe
danhaywood <dan@haywood-associates.co.uk>
2020-01-02 10:04:11 +0000

    ISIS-2236: adds Cors WebModule.

--- extensions/vro/cors/src/main/java/org/apache/isis/extensions/cors/webmodule/WebModuleCors.java
+++ extensions/vro/cors/src/main/java/org/apache/isis/extensions/cors/webmodule/WebModuleCors.java
@@ -16,59 +16,85 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-package org.apache.isis.security.keycloak.webmodule;
+package org.apache.isis.extensions.cors.webmodule;
 
 import javax.inject.Named;
-import javax.servlet.FilterRegistration.Dynamic;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextListener;
 import javax.servlet.ServletException;
 
+import org.apache.isis.applib.annotation.OrderPrecedence;
+import org.ebaysf.web.cors.CORSFilter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
-import org.apache.isis.applib.annotation.OrderPrecedence;
 import org.apache.isis.webapp.modules.WebModule;
 import org.apache.isis.webapp.modules.WebModuleContext;
+
+import lombok.extern.log4j.Log4j2;
+import lombok.val;
 
 import static org.apache.isis.commons.internal.base._Casts.uncheckedCast;
 import static org.apache.isis.commons.internal.context._Context.getDefaultClassLoader;
 import static org.apache.isis.commons.internal.exceptions._Exceptions.unexpectedCodeReach;
 
-import lombok.val;
-import lombok.extern.log4j.Log4j2;
-
 /**
- * WebModule to enable support for Keycloak.
+ * WebModule providing support for CORS
+ * 
+ * @since 2.0
  */
 @Service
-@Named("isisSecurityKeycloak.WebModuleKeycloak")
-@Order(OrderPrecedence.HIGHEST + 100)
-@Qualifier("Keycloak")
+@Named("isisExtCors.WebModuleServerCors")
+@Qualifier("CORS")
+@Order(OrderPrecedence.MIDPOINT-99)
 @Log4j2
-public final class WebModuleKeycloak implements WebModule  {
+public final class WebModuleCors implements WebModule  {
 
-    private final static String KEYCLOAK_FILTER_CLASS_NAME = KeycloakFilter.class.getName();
-    private final static String KEYCLOAK_FILTER_NAME = "KeycloakFilter";
+    private final static String CORS_FILTER_CLASS_NAME = CORSFilter.class.getName();
+    private final static String CORS_FILTER_NAME = "CORS Filter";
 
     @Override
     public String getName() {
-        return "Keycloak";
-    }
-    
-    @Override
-    public void prepare(WebModuleContext ctx) {
+        return "CORS";
     }
 
+    @Override
+    public void prepare(WebModuleContext ctx) {
+        // nothing special required
+    }
+
+    /*
+    <filter>
+        <filter-name>CORS Filter</filter-name>
+        <filter-class>org.ebaysf.web.cors.CORSFilter</filter-class>
+        <init-param>
+            <param-name>cors.allowed.origins</param-name>
+            <param-value>*</param-value>
+        </init-param>
+        <init-param>
+            <param-name>cors.allowed.headers</param-name>
+            <param-value>Content-Type,Accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization,Cache-Control,If-Modified-Since,Pragma</param-value>
+        </init-param>
+        <init-param>
+            <param-name>cors.exposed.headers</param-name>
+            <param-value>Authorization</param-value>
+        </init-param>
+    </filter>
+    <filter-mapping>
+        <filter-name>CORS Filter</filter-name>
+        <url-pattern>/*</url-pattern>
+    </filter-mapping>
+     */
     @Override
     public ServletContextListener init(ServletContext ctx) throws ServletException {
 
-        final Dynamic filter;
+        final FilterRegistration.Dynamic filter;
         try {
-            val filterClass = getDefaultClassLoader().loadClass(KEYCLOAK_FILTER_CLASS_NAME);
+            val filterClass = getDefaultClassLoader().loadClass(CORS_FILTER_CLASS_NAME);
             val filterInstance = ctx.createFilter(uncheckedCast(filterClass));
-            filter = ctx.addFilter(KEYCLOAK_FILTER_NAME, filterInstance);
+            filter = ctx.addFilter(CORS_FILTER_NAME, filterInstance);
             if(filter==null) {
                 return null; // filter was already registered somewhere else (eg web.xml)
             }
@@ -77,8 +103,12 @@ public final class WebModuleKeycloak implements WebModule  {
             throw unexpectedCodeReach();
         }
 
+        filter.setInitParameter("cors.allowed.origins", "*");
+        filter.setInitParameter("cors.allowed.headers", "Content-Type,Accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization,Cache-Control,If-Modified-Since,Pragma");
+        filter.setInitParameter("cors.exposed.headers", "Authorization");
+
         val urlPattern = "/*";
-        filter.addMappingForUrlPatterns(null, false, urlPattern); // filter is forced first
+        filter.addMappingForUrlPatterns(null, false, urlPattern);
 
         return null;
     }
@@ -87,4 +117,5 @@ public final class WebModuleKeycloak implements WebModule  {
     public boolean isApplicable(WebModuleContext ctx) {
         return true;
     }
+
 }
