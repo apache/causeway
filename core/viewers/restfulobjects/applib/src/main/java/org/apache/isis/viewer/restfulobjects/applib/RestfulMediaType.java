8da00370ceddce82da55c6f1576729fec8950e7f
Andi Huber <ahuber@apache.org>
2020-01-09 13:58:40 +0100

    ISIS-2158: put media-type profile string under double quotes

--- core/viewers/restfulobjects/applib/src/main/java/org/apache/isis/viewer/restfulobjects/applib/RestfulMediaType.java
+++ core/viewers/restfulobjects/applib/src/main/java/org/apache/isis/viewer/restfulobjects/applib/RestfulMediaType.java
@@ -33,38 +33,40 @@ public final class RestfulMediaType {
     private RestfulMediaType() {
     }
 
-    static final String PROFILE_PARAM_PREFIX = "urn:org.restfulobjects:repr-types/";
+    private static final String DQ = "\""; // double quotes, using abbreviation to reduce clutter
+    
+    private static final String PROFILE_PARAM_PREFIX = "urn:org.restfulobjects:repr-types/";
+    
+    private static final String JSON_BASE = "application/json" + ";profile=" + DQ + PROFILE_PARAM_PREFIX;
 
-    private static final String JSON_BASE = "application/json" + ";profile=" + PROFILE_PARAM_PREFIX;
+    public final static String APPLICATION_JSON_HOME_PAGE = JSON_BASE + "homepage" + DQ;
+    public final static String APPLICATION_JSON_USER = JSON_BASE + "user" + DQ;
+    public final static String APPLICATION_JSON_VERSION = JSON_BASE + "version" + DQ;
+    public final static String APPLICATION_JSON_HEALTH = JSON_BASE + "health" + DQ;
+    public final static String APPLICATION_JSON_LIST = JSON_BASE + "list" + DQ;
+    public final static String APPLICATION_JSON_OBJECT = JSON_BASE + "object" + DQ;
+    public final static String APPLICATION_JSON_OBJECT_PROPERTY = JSON_BASE + "object-property" + DQ;
+    public final static String APPLICATION_JSON_OBJECT_COLLECTION = JSON_BASE + "object-collection" + DQ;
+    public final static String APPLICATION_JSON_OBJECT_ACTION = JSON_BASE + "object-action" + DQ;
+    public final static String APPLICATION_JSON_ACTION_RESULT = JSON_BASE + "action-result" + DQ;
+    public final static String APPLICATION_JSON_TYPE_LIST = JSON_BASE + "type-list" + DQ;
+    public final static String APPLICATION_JSON_DOMAIN_TYPE = JSON_BASE + "domain-type" + DQ;
+    public final static String APPLICATION_JSON_PROPERTY_DESCRIPTION = JSON_BASE + "property-description" + DQ;
+    public final static String APPLICATION_JSON_COLLECTION_DESCRIPTION = JSON_BASE + "collection-description" + DQ;
+    public final static String APPLICATION_JSON_ACTION_DESCRIPTION = JSON_BASE + "action-description" + DQ;
+    public final static String APPLICATION_JSON_ACTION_PARAMETER_DESCRIPTION = JSON_BASE + "action-param-description" + DQ;
+    public final static String APPLICATION_JSON_ERROR = JSON_BASE + "error" + DQ;
+    public final static String APPLICATION_JSON_TYPE_ACTION_RESULT = JSON_BASE + "type-action-result" + DQ;
 
-    public final static String APPLICATION_JSON_HOME_PAGE = JSON_BASE + "homepage";
-    public final static String APPLICATION_JSON_USER = JSON_BASE + "user";
-    public final static String APPLICATION_JSON_VERSION = JSON_BASE + "version";
-    public final static String APPLICATION_JSON_HEALTH = JSON_BASE + "health";
-    public final static String APPLICATION_JSON_LIST = JSON_BASE + "list";
-    public final static String APPLICATION_JSON_OBJECT = JSON_BASE + "object";
-    public final static String APPLICATION_JSON_OBJECT_PROPERTY = JSON_BASE + "object-property";
-    public final static String APPLICATION_JSON_OBJECT_COLLECTION = JSON_BASE + "object-collection";
-    public final static String APPLICATION_JSON_OBJECT_ACTION = JSON_BASE + "object-action";
-    public final static String APPLICATION_JSON_ACTION_RESULT = JSON_BASE + "action-result";
-    public final static String APPLICATION_JSON_TYPE_LIST = JSON_BASE + "type-list";
-    public final static String APPLICATION_JSON_DOMAIN_TYPE = JSON_BASE + "domain-type";
-    public final static String APPLICATION_JSON_PROPERTY_DESCRIPTION = JSON_BASE + "property-description";
-    public final static String APPLICATION_JSON_COLLECTION_DESCRIPTION = JSON_BASE + "collection-description";
-    public final static String APPLICATION_JSON_ACTION_DESCRIPTION = JSON_BASE + "action-description";
-    public final static String APPLICATION_JSON_ACTION_PARAMETER_DESCRIPTION = JSON_BASE + "action-param-description";
-    public final static String APPLICATION_JSON_ERROR = JSON_BASE + "error";
-    public final static String APPLICATION_JSON_TYPE_ACTION_RESULT = JSON_BASE + "type-action-result";
+    private static final String XML_BASE = "application/xml" + ";profile=" + DQ + PROFILE_PARAM_PREFIX;
+    public final static String APPLICATION_XML_LAYOUT_BS3 = XML_BASE + "layout-bs3" + DQ;
+    public final static String APPLICATION_JSON_LAYOUT_BS3 = JSON_BASE + "layout-bs3" + DQ;
 
-    private static final String XML_BASE = "application/xml" + ";profile=" + PROFILE_PARAM_PREFIX;
-    public final static String APPLICATION_XML_LAYOUT_BS3 = XML_BASE + "layout-bs3";
-    public final static String APPLICATION_JSON_LAYOUT_BS3 = JSON_BASE + "layout-bs3";
+    public final static String APPLICATION_XML_OBJECT_LAYOUT_BS3 = XML_BASE + "object-layout-bs3" + DQ;
+    public final static String APPLICATION_JSON_OBJECT_LAYOUT_BS3 = JSON_BASE + "object-layout-bs3" + DQ;
 
-    public final static String APPLICATION_XML_OBJECT_LAYOUT_BS3 = XML_BASE + "object-layout-bs3";
-    public final static String APPLICATION_JSON_OBJECT_LAYOUT_BS3 = JSON_BASE + "object-layout-bs3";
-
-    public final static String APPLICATION_JSON_LAYOUT_MENUBARS = JSON_BASE + "layout-menubars";
-    public final static String APPLICATION_XML_LAYOUT_MENUBARS = XML_BASE + "layout-menubars";
+    public final static String APPLICATION_JSON_LAYOUT_MENUBARS = JSON_BASE + "layout-menubars" + DQ;
+    public final static String APPLICATION_XML_LAYOUT_MENUBARS = XML_BASE + "layout-menubars" + DQ;
 
 
     // currently only support a subset of representations in XML format
@@ -73,18 +75,18 @@ public final class RestfulMediaType {
     //    public final static String APPLICATION_XML_USER = XML_BASE + "user";
     //    public final static String APPLICATION_XML_VERSION = XML_BASE + "version";
     //    public final static String APPLICATION_XML_LIST = XML_BASE + "list";
-    public final static String APPLICATION_XML_OBJECT = XML_BASE + "object";
-    public final static String APPLICATION_XML_OBJECT_PROPERTY = XML_BASE + "object-property";
-    public final static String APPLICATION_XML_OBJECT_COLLECTION = XML_BASE + "object-collection";
-    public final static String APPLICATION_XML_OBJECT_ACTION = XML_BASE + "object-action";
-    public final static String APPLICATION_XML_ACTION_RESULT = XML_BASE + "action-result";
+    public final static String APPLICATION_XML_OBJECT = XML_BASE + "object" + DQ;
+    public final static String APPLICATION_XML_OBJECT_PROPERTY = XML_BASE + "object-property" + DQ;
+    public final static String APPLICATION_XML_OBJECT_COLLECTION = XML_BASE + "object-collection" + DQ;
+    public final static String APPLICATION_XML_OBJECT_ACTION = XML_BASE + "object-action" + DQ;
+    public final static String APPLICATION_XML_ACTION_RESULT = XML_BASE + "action-result" + DQ;
     //    public final static String APPLICATION_XML_TYPE_LIST = XML_BASE + "type-list";
     //    public final static String APPLICATION_XML_DOMAIN_TYPE = XML_BASE + "domain-type";
     //    public final static String APPLICATION_XML_PROPERTY_DESCRIPTION = XML_BASE + "property-description";
     //    public final static String APPLICATION_XML_COLLECTION_DESCRIPTION = XML_BASE + "collection-description";
     //    public final static String APPLICATION_XML_ACTION_DESCRIPTION = XML_BASE + "action-description";
     //    public final static String APPLICATION_XML_ACTION_PARAMETER_DESCRIPTION = XML_BASE + "action-param-description";
-    public final static String APPLICATION_XML_ERROR = XML_BASE + "error";
+    public final static String APPLICATION_XML_ERROR = XML_BASE + "error" + DQ;
     //    public final static String APPLICATION_XML_TYPE_ACTION_RESULT = XML_BASE + "type-action-result";
 
     public final static String IMAGE_PNG = "image/png";
