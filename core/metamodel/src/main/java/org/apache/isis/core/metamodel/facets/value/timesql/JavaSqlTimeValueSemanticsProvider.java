9acf62391d2a356665d5dc59a1524461bcd1d86e
danhaywood <dan@haywood-associates.co.uk>
2020-01-17 01:02:55 +0100

    ISIS-2264: pushes TimeValueSemanticsProviderAbstract down to its subclass, JavaSqlTimeValueSemanticsProvider

--- core/metamodel/src/main/java/org/apache/isis/core/metamodel/facets/value/timesql/JavaSqlTimeValueSemanticsProvider.java
+++ core/metamodel/src/main/java/org/apache/isis/core/metamodel/facets/value/timesql/JavaSqlTimeValueSemanticsProvider.java
@@ -21,8 +21,11 @@ package org.apache.isis.core.metamodel.facets.value.timesql;
 
 import java.sql.Time;
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.apache.isis.applib.adapters.EncoderDecoder;
@@ -30,18 +33,24 @@ import org.apache.isis.applib.adapters.Parser;
 import org.apache.isis.applib.clock.Clock;
 import org.apache.isis.core.commons.internal.collections._Maps;
 import org.apache.isis.core.metamodel.facetapi.FacetHolder;
-import org.apache.isis.core.metamodel.facets.value.time.TimeValueSemanticsProviderAbstract;
+import org.apache.isis.core.metamodel.facets.value.ValueSemanticsProviderAbstractTemporal;
+
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Treats {@link java.sql.Time} as a time-only value type.
  *
  */
-public class JavaSqlTimeValueSemanticsProvider extends TimeValueSemanticsProviderAbstract<java.sql.Time> {
-    private static Map<String, DateFormat> formats = _Maps.newHashMap();
+public class JavaSqlTimeValueSemanticsProvider extends ValueSemanticsProviderAbstractTemporal<Time> {
 
-    static {
-        initFormats(formats);
+    protected static void initFormats(final Map<String, DateFormat> formats) {
+        formats.put(ISO_ENCODING_FORMAT, createDateEncodingFormat("HHmmssSSS"));
+        formats.put("short", DateFormat.getTimeInstance(DateFormat.SHORT));
     }
+
+    @Getter @Setter
+    private String configuredFormat;
 
     /**
      * Required because implementation of {@link Parser} and
@@ -52,8 +61,80 @@ public class JavaSqlTimeValueSemanticsProvider extends TimeValueSemanticsProvide
     }
 
     public JavaSqlTimeValueSemanticsProvider(final FacetHolder holder) {
-        super(holder, java.sql.Time.class);
+        super("time", type(), holder, java.sql.Time.class, 8, Immutability.NOT_IMMUTABLE, EqualByContent.NOT_HONOURED, null);
+
+        configuredFormat = getConfiguration().getValue().getFormat().getOrDefault("time", "short").toLowerCase().trim();
+
+        buildFormat(configuredFormat);
+
+        final String formatRequired = getConfiguration().getValue().getFormat().get("time");
+        if (formatRequired == null) {
+            format = formats().get(defaultFormat());
+        } else {
+            setMask(formatRequired);
+        }
     }
+
+    @Override
+    protected void clearFields(final Calendar cal) {
+        cal.set(Calendar.YEAR, 1970);
+        cal.set(Calendar.MONTH, 0);
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+    }
+
+    @Override
+    protected String defaultFormat() {
+        return "short";
+    }
+
+    @Override
+    public String toString() {
+        return "TimeValueSemanticsProvider: " + format;
+    }
+
+    @Override
+    protected DateFormat format() {
+
+        final Locale locale = Locale.getDefault();
+        final DateFormat dateFormat = DateFormat.getTimeInstance(DateFormat.SHORT, locale);
+        dateFormat.setTimeZone(UTC_TIME_ZONE);
+        return dateFormat;
+    }
+
+    @Override
+    protected List<DateFormat> formatsToTry() {
+        List<DateFormat> formats = new ArrayList<DateFormat>();
+
+        final Locale locale = Locale.getDefault();
+
+        formats.add(DateFormat.getTimeInstance(DateFormat.LONG, locale));
+        formats.add(DateFormat.getTimeInstance(DateFormat.MEDIUM, locale));
+        formats.add(DateFormat.getTimeInstance(DateFormat.SHORT, locale));
+        formats.add(createDateFormat("HH:mm:ss.SSS"));
+        formats.add(createDateFormat("HHmmssSSS"));
+        formats.add(createDateFormat("HH:mm:ss"));
+        formats.add(createDateFormat("HHmmss"));
+
+        for (DateFormat format : formats) {
+            format.setTimeZone(UTC_TIME_ZONE);
+        }
+
+        return formats;
+    }
+
+    @Override
+    public void appendAttributesTo(Map<String, Object> attributeMap) {
+        super.appendAttributesTo(attributeMap);
+        attributeMap.put("configuredFormat", configuredFormat);
+    }
+
+
+    private static Map<String, DateFormat> formats = _Maps.newHashMap();
+
+    static {
+        initFormats(formats);
+    }
+
 
     @Override
     public Time add(final Time original, final int years, final int months, final int days, final int hours, final int minutes) {
