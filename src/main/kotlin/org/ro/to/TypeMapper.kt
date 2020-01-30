a665f11366de5c6408dfee6e8a599f35eb0de081
Jörg Rade <joerg.rade@kuehne-nagel.com>
2020-01-30 19:02:12 +0100

    #42 Cleanup

--- src/main/kotlin/org/ro/to/TypeMapper.kt
+++ src/main/kotlin/org/ro/to/TypeMapper.kt
@@ -33,15 +33,12 @@ class TypeMapper {
         }
     }
 
-    private val ISO_DATE = Regex("^([\\+-]?\\d{4}(?!\\d{2}\\b))((-?)((0[1-9]|1[0-2])(\\3([12]\\d|0[1-9]|3[01]))?|W([0-4]\\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\\d|[12]\\d{2}|3([0-5]\\d|6[1-6])))([T\\s]((([01]\\d|2[0-3])((:?)[0-5]\\d)?|24\\:?00)([\\.,]\\d+(?!:))?)?(\\17[0-5]\\d([\\.,]\\d+)?)?([zZ]|([\\+-])([01]\\d|2[0-3]):?([0-5]\\d)?)?)?)?\$")
-    private val JAVA_LANG_LONG = Regex("\\d{19}")  //9223372036854775807
-//    private val JAVA_OFFSET_DATE_TIME = Regex("\\d{19}")  //20200125T140705.356+0100
+    private val isoDate = Regex("^([+-]?\\d{4}(?!\\d{2}\\b))((-?)((0[1-9]|1[0-2])(\\3([12]\\d|0[1-9]|3[01]))?|W([0-4]\\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\\d|[12]\\d{2}|3([0-5]\\d|6[1-6])))([T\\s]((([01]\\d|2[0-3])((:?)[0-5]\\d)?|24:?00)([.,]\\d+(?!:))?)?(\\17[0-5]\\d([.,]\\d+)?)?([zZ]|([+-])([01]\\d|2[0-3]):?([0-5]\\d)?)?)?)?\$")
 
     private fun match(value: Value?): String {
         val contentStr = value?.content.toString()
         return when {
-            ISO_DATE.matches(contentStr) -> ValueType.DATE.type
-//            JAVA_LANG_LONG.matches(contentStr) -> TypeMapperType.NUMERIC.type
+            isoDate.matches(contentStr) -> ValueType.DATE.type
             Utils.isXml(contentStr) -> ValueType.HTML.type
             else -> ValueType.TEXT.type
         }
