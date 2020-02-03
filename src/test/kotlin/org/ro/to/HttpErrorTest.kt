3f31044a3d9b6efd1648292c66c45cb162020439
Jörg Rade <joerg.rade@kuehne-nagel.com>
2020-02-03 10:43:28 +0100

    #42 Html Div in Panel with css

--- src/test/kotlin/org/ro/to/HttpErrorTest.kt
+++ src/test/kotlin/org/ro/to/HttpErrorTest.kt
@@ -2,6 +2,7 @@ package org.ro.to
 
 import kotlinx.serialization.UnstableDefault
 import org.ro.handler.HttpErrorHandler
+import org.ro.snapshots.demo2_0_0.HTTP_ERROR_405
 import org.ro.snapshots.simpleapp1_16_0.HTTP_ERROR
 import org.ro.snapshots.simpleapp1_16_0.HTTP_ERROR_500_UNIQUE_CONSTRAINT_VIOLATION
 import kotlin.test.Test
@@ -11,6 +12,23 @@ import kotlin.test.assertTrue
 
 @UnstableDefault
 class HttpErrorTest {
+
+    @Test
+    fun test405() {
+        val jsonStr = HTTP_ERROR_405.str
+        val error = HttpErrorHandler().parse(jsonStr)  as HttpError
+        val code = error.httpStatusCode
+        assertEquals(405, code)
+        assertNotNull(error.message)
+
+        val detail = error.detail
+        assertNotNull(detail)
+        assertNotNull(detail.className)
+        assertNotNull(detail.message)
+        assertEquals(error.message, detail.message)
+        assertNotNull(detail.element)
+        assertTrue(detail.element.size > 0)
+    }
 
     @Test
     fun test400() {
