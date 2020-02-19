6e5cbdca916e08af9086548a60017384ef9c0f00
Andi Huber <ahuber@apache.org>
2020-02-19 13:29:10 +0100

    ISIS-2292: fallback to base64 if object identifier is not url safe

--- persistence/jdo/datanucleus-5/src/main/java/org/apache/isis/persistence/jdo/datanucleus5/datanucleus/persistence/spi/JdoObjectIdSerializer.java
+++ persistence/jdo/datanucleus-5/src/main/java/org/apache/isis/persistence/jdo/datanucleus5/datanucleus/persistence/spi/JdoObjectIdSerializer.java
@@ -25,8 +25,8 @@ import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Function;
 
-import javax.jdo.annotations.IdentityType;
 import javax.jdo.identity.ByteIdentity;
 import javax.jdo.identity.IntIdentity;
 import javax.jdo.identity.LongIdentity;
@@ -35,173 +35,171 @@ import javax.jdo.identity.StringIdentity;
 
 import org.datanucleus.identity.DatastoreId;
 
+import org.apache.isis.core.commons.handler.ChainOfResponsibility;
+import org.apache.isis.core.commons.internal.base._Strings;
 import org.apache.isis.core.commons.internal.context._Context;
+import org.apache.isis.core.commons.internal.exceptions._Exceptions;
 import org.apache.isis.core.metamodel.adapter.oid.RootOid;
-import org.apache.isis.core.metamodel.spec.ObjectSpecification;
 import org.apache.isis.core.metamodel.specloader.SpecificationLoader;
-import org.apache.isis.persistence.jdo.datanucleus5.metamodel.facets.object.persistencecapable.JdoPersistenceCapableFacet;
+import org.apache.isis.persistence.jdo.datanucleus5.datanucleus.persistence.spi.JdoObjectIdDecoder.JdoObjectIdDecodingRequest;
 
 import lombok.val;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 public final class JdoObjectIdSerializer {
 
-    private static final char SEPARATOR = '_';
-
-    private JdoObjectIdSerializer(){}
-
-    public static class Exception extends RuntimeException {
-        private static final long serialVersionUID = 1L;
-
-        public Exception(final java.lang.Exception ex) {
-            super(ex);
-        }
+    static final char SEPARATOR = '_';
+    
+    private static List<JdoObjectIdEncoder> encoders() {
+        
+        final List<String> nonSafeUrlChars = Arrays.asList("/", "\\");
+        
+        val encoders = Arrays.asList(
+                // Byte
+                JdoObjectIdEncoder.of(
+                        JdoObjectIdEncoder.filter(ByteIdentity.class),   
+                        JdoObjectIdEncoder.stringifier("b")),
+                // Int
+                JdoObjectIdEncoder.of(
+                        JdoObjectIdEncoder.filter(IntIdentity.class),   
+                        JdoObjectIdEncoder.stringifier("i")),
+                // Long
+                JdoObjectIdEncoder.of(
+                        JdoObjectIdEncoder.filter(LongIdentity.class),   
+                        JdoObjectIdEncoder.stringifier("l")),
+                // String
+                JdoObjectIdEncoder.of(
+                        JdoObjectIdEncoder.filter(StringIdentity.class),   
+                        jdoOid->{
+                            val stringified = "" + jdoOid;
+                            if(nonSafeUrlChars.stream()
+                                    .anyMatch(stringified::contains)) {
+                                return "base64" + SEPARATOR + _Strings.base64UrlEncode(stringified);
+                            }
+                            return "s" + SEPARATOR + stringified;
+                        }),
+                // UUID
+                JdoObjectIdEncoder.of(
+                        jdoOid->{
+                            if(jdoOid instanceof javax.jdo.identity.ObjectIdentity) {
+                                val id = (ObjectIdentity) jdoOid;
+                                return id.getKeyAsObject() instanceof UUID;
+                            }
+                            return false;            
+                        },
+                        jdoOid->{
+                            val id = (ObjectIdentity) jdoOid;
+                            val uuid = (UUID) id.getKeyAsObject();
+                            return "u" + SEPARATOR + uuid.toString();
+                        }),
+                // DatastoreId
+                JdoObjectIdEncoder.of( 
+                        jdoOid->{
+                            if(jdoOid instanceof DatastoreId) {
+                                final DatastoreId dnOid = (DatastoreId) jdoOid;
+                                final Object keyValue = dnOid.getKeyAsObject();
+                                // prettier handling of common datatypes if possible ?
+                                if( keyValue instanceof String ||
+                                        keyValue instanceof Long ||
+                                        keyValue instanceof BigDecimal || // 1.8.0 did not support BigDecimal
+                                        keyValue instanceof BigInteger ||
+                                        keyValue instanceof Integer) {
+                                    return true;
+                                }
+                            }
+                            return false;            
+                        },
+                        jdoOid->{
+                            final DatastoreId dnOid = (DatastoreId) jdoOid;
+                            // no separator
+                            return "" + dnOid.getKeyAsObject();
+                        })
+                );
+        return encoders;
+        
+    }
+    
+    private static List<JdoObjectIdDecoder> decoders() {
+        
+        final List<String> dnPrefixes = Arrays.asList("S", "I", "L", "M", "B");
+        
+        val decoders = Arrays.asList(
+                JdoObjectIdDecoder.of(
+                        JdoObjectIdDecoder.filter("b"),
+                        JdoObjectIdDecoder.parser(Byte::parseByte, ByteIdentity::new)),
+                JdoObjectIdDecoder.of(
+                        JdoObjectIdDecoder.filter("i"),
+                        JdoObjectIdDecoder.parser(Integer::parseInt, IntIdentity::new)),
+                JdoObjectIdDecoder.of(
+                        JdoObjectIdDecoder.filter("l"),
+                        JdoObjectIdDecoder.parser(Long::parseLong, LongIdentity::new)),
+                JdoObjectIdDecoder.of(
+                        JdoObjectIdDecoder.filter("s"),
+                        JdoObjectIdDecoder.parser(Function.identity(), StringIdentity::new)),
+                JdoObjectIdDecoder.of(
+                        JdoObjectIdDecoder.filter("base64"),
+                        JdoObjectIdDecoder.parser(
+                                _Strings::base64UrlDecode, 
+                                (type, keyStr)-> new StringIdentity(type, _Strings.base64UrlDecode(keyStr)) )),
+                JdoObjectIdDecoder.of(
+                        JdoObjectIdDecoder.filter("u"),
+                        JdoObjectIdDecoder.parser(
+                                UUID::fromString, 
+                                (type, keyStr)-> new ObjectIdentity(type, UUID.fromString(keyStr)) )),
+                // if there is no separator, the identifier is for
+                // @javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
+                JdoObjectIdDecoder.of(
+                        JdoObjectIdDecoder.filter(""),
+                        JdoObjectIdDecodingRequest::getKeyStr),
+                JdoObjectIdDecoder.of(
+                        request->dnPrefixes.contains(request.getDistinguisher()),
+                        request->request.getKeyStr() + "[OID]" + request.getSpec().getFullIdentifier())
+                
+                );
+        return decoders;
     }
 
+    private final static ChainOfResponsibility<Object, String> 
+        encodingChain = ChainOfResponsibility.of(encoders());
+    
+    private final static ChainOfResponsibility<JdoObjectIdDecodingRequest, Object> 
+        decodingChain = ChainOfResponsibility.of(decoders());
+    
     public static String toOidIdentifier(final Object jdoOid) {
 
-        //
-        // @javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.APPLICATION)
-        //
-        if(jdoOid instanceof javax.jdo.identity.ByteIdentity) {
-            return "b" + SEPARATOR + jdoOid;
-        }
-        if(jdoOid instanceof javax.jdo.identity.IntIdentity) {
-            return "i" + SEPARATOR + jdoOid;
-        }
-        if(jdoOid instanceof javax.jdo.identity.StringIdentity) {
-            return "s" + SEPARATOR + jdoOid;
-        }
-        if(jdoOid instanceof javax.jdo.identity.LongIdentity) {
-            return "l" + SEPARATOR + jdoOid;
-        }
-        if(jdoOid instanceof javax.jdo.identity.ObjectIdentity) {
-            final javax.jdo.identity.ObjectIdentity id = (ObjectIdentity) jdoOid;
-            final Object keyAsObject = id.getKeyAsObject();
-            // UUID support
-            if(keyAsObject instanceof UUID) {
-                final UUID uuid = (UUID) keyAsObject;
-                return "u" + SEPARATOR + uuid.toString();
-            }
-        }
-
-        if(jdoOid instanceof DatastoreId) {
-
-            //
-            // prettier handling of common datatypes if possible
-            //
-
-            final DatastoreId dnOid = (DatastoreId) jdoOid;
-            final Object keyValue = dnOid.getKeyAsObject();
-
-
-            if( keyValue instanceof String ||
-                    keyValue instanceof Long ||
-                    keyValue instanceof BigDecimal || // 1.8.0 did not support BigDecimal
-                    keyValue instanceof BigInteger ||
-                    keyValue instanceof Integer) {
-
-                // no separator
-                return "" + keyValue;
-            }
-
-        }
-
-        // the JDO spec (5.4.3) requires that OIDs are serializable toString and
-        // recreatable through the constructor
-        return jdoOid.getClass().getName() + SEPARATOR + jdoOid.toString();
+        return encodingChain.handle(jdoOid)
+        .orElseGet(()->
+            // the JDO spec (5.4.3) requires that OIDs are serializable toString and
+            // re-create-able through the constructor
+            jdoOid.getClass().getName() + SEPARATOR + jdoOid.toString());
     }
 
-    private static List<String> dnPrefixes = Arrays.asList("S", "I", "L", "M", "B");
-
+   
     public static Object toJdoObjectId(SpecificationLoader specificationLoader, RootOid oid) {
 
-        final String idStr = oid.getIdentifier();
-        final int separatorIdx = idStr.indexOf(SEPARATOR);
+        val spec = specificationLoader.lookupBySpecIdElseLoad(oid.getObjectSpecId());
+        val request = JdoObjectIdDecodingRequest.parse(spec, oid.getIdentifier());
+        
+        return decodingChain.handle(request)
+        .orElseGet(()->{
 
-        final ObjectSpecification spec = specificationLoader.lookupBySpecIdElseLoad(oid.getObjectSpecId());
-        final JdoPersistenceCapableFacet jdoPcFacet = spec.getFacet(JdoPersistenceCapableFacet.class);
+            val clsName = request.getDistinguisher();
+            val keyStr = request.getKeyStr();         
 
-
-        if(separatorIdx != -1) {
-
-            // behaviour for OIDs as of 1.8.0 and previously
-
-            final String distinguisher = idStr.substring(0, separatorIdx);
-            final String keyStr = idStr.substring(separatorIdx + 1);
-
-            final boolean isApplicationIdentity = isApplicationIdentity(jdoPcFacet);
-
-            if("s".equals(distinguisher)) {
-                if (isApplicationIdentity) {
-                    return keyStr;
-                } else {
-                    return new StringIdentity(objectTypeClassFor(specificationLoader, oid), keyStr);
-                }
-            } else if("i".equals(distinguisher)) {
-                if(isApplicationIdentity) {
-                    return Integer.parseInt(keyStr);
-                } else {
-                    return new IntIdentity(objectTypeClassFor(specificationLoader, oid), keyStr);
-                }
-            } else if("l".equals(distinguisher)) {
-                if(isApplicationIdentity) {
-                    return Long.parseLong(keyStr);
-                } else {
-                    return new LongIdentity(objectTypeClassFor(specificationLoader, oid), keyStr);
-                }
-            } else if("b".equals(distinguisher)) {
-                if(isApplicationIdentity) {
-                    return Byte.parseByte(keyStr);
-                } else {
-                    return new ByteIdentity(objectTypeClassFor(specificationLoader, oid), keyStr);
-                }
-            } else if("u".equals(distinguisher)) {
-                if(isApplicationIdentity) {
-                    return UUID.fromString(keyStr);
-                } else {
-                    return new ObjectIdentity(
-                            objectTypeClassFor(specificationLoader, oid), UUID.fromString(keyStr));
-                }
-            }
-
-            if(dnPrefixes.contains(distinguisher)) {
-                return keyStr + "[OID]" + spec.getFullIdentifier();
-            }
-
-            final String clsName = distinguisher;
             try {
                 final Class<?> cls = _Context.loadClass(clsName);
                 final Constructor<?> cons = cls.getConstructor(String.class);
                 final Object dnOid = cons.newInstance(keyStr);
                 return dnOid.toString();
             } catch (ClassNotFoundException | IllegalArgumentException | InstantiationException | IllegalAccessException | SecurityException | InvocationTargetException | NoSuchMethodException e) {
-                throw new JdoObjectIdSerializer.Exception(e);
+                throw _Exceptions.unrecoverableFormatted(
+                        "failed to instantiate %s with arg %s", clsName, keyStr, e);
             }
+            
+        });
 
-        } else {
-
-            // there was no separator, so this identifier must have been for
-            // @javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
-            // for one of the common types (prettier handling)
-
-            // in DN 4.1, we did this...
-            // return idStr + "[OID]" + spec.getFullIdentifier();
-
-            // in DN 5.1, we simply do this...
-            return idStr;
-        }
     }
 
-    protected static boolean isApplicationIdentity(final JdoPersistenceCapableFacet jdoPcFacet) {
-        return jdoPcFacet != null && jdoPcFacet.getIdentityType() == IdentityType.APPLICATION;
-    }
-
-    private static Class<?> objectTypeClassFor(SpecificationLoader specificationLoader, RootOid oid) {
-        
-        val objectSpecId = oid.getObjectSpecId();
-        val spec = specificationLoader.lookupBySpecIdElseLoad(objectSpecId);
-        val correspondingClass = spec.getCorrespondingClass();
-        return correspondingClass;
-    }
 
 }
