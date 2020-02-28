f1faaabc245b330736c2ae8e8e8696b4cc599e72
Andi Huber <ahuber@apache.org>
2020-02-28 14:02:41 +0100

    ISIS-2296: hotfixing BeanDescriptor to not eagerly instantiate all dependent beans

--- subdomains/spring/applib/src/main/java/org/apache/isis/subdomains/spring/applib/service/BeanDescriptor.java
+++ subdomains/spring/applib/src/main/java/org/apache/isis/subdomains/spring/applib/service/BeanDescriptor.java
@@ -4,29 +4,47 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.util.StringUtils;
 
+import org.apache.isis.core.commons.internal.base._NullSafe;
+
 import lombok.Getter;
 import lombok.ToString;
+import lombok.Value;
+import lombok.val;
+import lombok.extern.log4j.Log4j2;
 
 @Getter
 @ToString
+@Log4j2
 public final class BeanDescriptor {
 
     private final String beanName;
     private final List<String> aliases;
-    private final Object bean;
     private final String scope;
     private final Class<?> type;
     private final String resource;
-    private final Map<String, Object> dependenciesByName;
+    private final Map<String, BeanNameAndDescriptor> dependenciesByName;
+    
+    @Value(staticConstructor = "of")
+    private static class BeanNameAndDescriptor {
+        final String beanName;
+        final BeanDefinition beanDefinition;
+        final BeanFactory beanFactory;
+        Object getBean() {
+            return beanFactory.getBean(beanName);
+        }
+    }
+    
 
     BeanDescriptor(
             final String beanName,
@@ -38,7 +56,6 @@ public final class BeanDescriptor {
         final BeanDefinition definition = beanFactory.getBeanDefinition(beanName);
 
         this.aliases = Collections.unmodifiableList(Arrays.asList(beanFactory.getAliases(beanName)));
-        this.bean = beanFactory.getBean(beanName);
 
         final String scope = definition.getScope();
         this.scope = StringUtils.hasText(scope) ? scope : "singleton";
@@ -46,19 +63,25 @@ public final class BeanDescriptor {
         this.type = beanFactory.getType(beanName);
         this.resource = definition.getResourceDescription();
 
-        this.dependenciesByName = Arrays.stream(beanFactory.getDependenciesForBean(beanName))
-                                .filter(name -> {
-                                    try {
-                                        return beanFactory.getBean(name) != null;
-                                    } catch(NoSuchBeanDefinitionException ex) {
-                                        return false;
-                                    }
-                                })
-                                .collect(Collectors.toMap(Function.identity(), beanFactory::getBean));
+        val dependencies = beanFactory.getDependenciesForBean(beanName);
+        
+        this.dependenciesByName = _NullSafe.stream(dependencies)
+            .map(name -> {
+                try {
+                    return BeanNameAndDescriptor.of(name, beanFactory.getBeanDefinition(name), beanFactory);
+                } catch(NoSuchBeanDefinitionException ex) {
+                    return null;
+                }
+            })
+            .filter(Objects::nonNull)
+            .collect(Collectors.toMap(BeanNameAndDescriptor::getBeanName, Function.identity()));
+        
     }
-
+    
     <T> List<T> dependencies(Class<T> cls) {
+        log.warn("non optimzed implementation: creates a bean for each dependency, even if not returned with the result");
         return dependenciesByName.values().stream()
+                .map(BeanNameAndDescriptor::getBean)
                 .filter(cls::isInstance)
                 .map(cls::cast)
                 .collect(Collectors.toList());
