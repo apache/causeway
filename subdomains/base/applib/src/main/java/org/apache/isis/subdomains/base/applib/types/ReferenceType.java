0322fe80bd11c6f028bea2e50a02e8ce5c6bec48
danhaywood <dan@haywood-associates.co.uk>
2020-03-06 17:23:20 +0000

    ISIS-2094: adds missing license statements

--- subdomains/base/applib/src/main/java/org/apache/isis/subdomains/base/applib/types/ReferenceType.java
+++ subdomains/base/applib/src/main/java/org/apache/isis/subdomains/base/applib/types/ReferenceType.java
@@ -1,17 +1,35 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package org.apache.isis.subdomains.base.applib.types;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class ReferenceType {
 
-    private ReferenceType() {}
-
+    @UtilityClass
     public static class Meta {
 
         public static final int MAX_LEN = 24;
 
         public static final String REGEX = "[ -/_A-Z0-9]+";
         public static final String REGEX_DESCRIPTION = "Only capital letters, numbers and 3 symbols being: \"_\" , \"-\" and \"/\" are allowed";
-
-        private Meta() {}
 
     }
 
