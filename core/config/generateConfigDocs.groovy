f63896bce8f554ce1d1e5200ff8e5fc36a3a92bf
danhaywood <dan@haywood-associates.co.uk>
2020-03-08 14:06:01 +0000

    ISIS-2062: add license to generated _nav.adoc for config docs

--- core/config/generateConfigDocs.groovy
+++ core/config/generateConfigDocs.groovy
@@ -208,9 +208,13 @@ for (property in data.properties) {
     }
 }
 
+def license = ":Notice: Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the \"License\"); you may not use this file except in compliance with the License. You may obtain a copy of the License at. http://www.apache.org/licenses/LICENSE-2.0 . Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR  CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License."
 new File(outputDir).mkdirs()
 
 def bufNav = StringBuilder.newInstance()
+bufNav << """${license}"
+
+"""
 
 for (PropertyGroup group in groups) {
     def buf = StringBuilder.newInstance()
@@ -219,7 +223,7 @@ for (PropertyGroup group in groups) {
         bufNav << "** xref:refguide:config:sections/${group.fileName()}.adoc[${group.name}]\n"
 
         buf << """= ${group.name}
-:Notice: Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at. http://www.apache.org/licenses/LICENSE-2.0 . Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR  CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+${license}
 :page-role: -toc
 
 
@@ -346,7 +350,7 @@ static String anchor(String str) {
     return str;
 }
 
-static String format(String str, int len = 30) {
+static String format(String str, int len = 35) {
 
     String lineFeed = " +\n";
 
