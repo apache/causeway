60a7802d8fa8c6eb5a000d163b496e8a8a9deec6
Andi Huber <ahuber@apache.org>
2020-03-13 14:03:48 +0100

    ISIS-2303: Vaadin Demo: de-duplicate code

--- examples/demo-vaadin/src/main/java/demoapp/vaadin/webapp/DemoAppVaadin.java
+++ examples/demo-vaadin/src/main/java/demoapp/vaadin/webapp/DemoAppVaadin.java
@@ -18,57 +18,32 @@
  */
 package demoapp.vaadin.webapp;
 
-import java.util.Optional;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
 import org.apache.isis.core.config.presets.IsisPresets;
-import org.apache.isis.core.runtimeservices.IsisModuleCoreRuntimeServices;
-import org.apache.isis.extensions.cors.impl.IsisModuleExtCorsImpl;
-import org.apache.isis.extensions.secman.api.SecurityModuleConfig;
-import org.apache.isis.extensions.secman.api.permission.PermissionsEvaluationService;
-import org.apache.isis.extensions.secman.api.permission.PermissionsEvaluationServiceAllowBeatsVeto;
-import org.apache.isis.extensions.secman.encryption.jbcrypt.IsisModuleExtSecmanEncryptionJbcrypt;
-import org.apache.isis.extensions.secman.jdo.IsisModuleExtSecmanPersistenceJdo;
-import org.apache.isis.extensions.secman.model.IsisModuleExtSecmanModel;
-import org.apache.isis.extensions.secman.shiro.IsisModuleExtSecmanRealmShiro;
-import org.apache.isis.extensions.viewer.wicket.exceldownload.ui.IsisModuleExtExcelDownloadUi;
-import org.apache.isis.incubator.model.metamodel.IsisModuleIncModelMetaModel;
 import org.apache.isis.incubator.viewer.vaadin.viewer.IsisModuleIncViewerVaadinViewer;
-import org.apache.isis.persistence.jdo.datanucleus5.IsisModuleJdoDataNucleus5;
-import org.apache.isis.security.shiro.IsisModuleSecurityShiro;
-import org.apache.isis.testing.fixtures.applib.IsisModuleTestingFixturesApplib;
-import org.apache.isis.valuetypes.asciidoc.ui.IsisModuleValAsciidocUi;
-import org.apache.isis.valuetypes.sse.ui.IsisModuleValSseUi;
-import org.apache.isis.viewer.restfulobjects.jaxrsresteasy4.IsisModuleViewerRestfulObjectsJaxrsResteasy4;
-import org.apache.isis.viewer.restfulobjects.viewer.IsisModuleViewerRestfulObjectsViewer;
-import org.apache.isis.viewer.wicket.viewer.IsisModuleViewerWicketViewer;
 
-import lombok.val;
-import lombok.extern.log4j.Log4j2;
-
-import demoapp.dom.DemoModule;
-import demoapp.utils.DemoRequestScopedBean;
-import demoapp.utils.LibraryPreloadingService;
+import demoapp.webapp.DemoApp;
 
 /**
  * Bootstrap the application.
  */
 @SpringBootApplication
-@Import({
-    DemoAppVaadin.AppManifest.class,
+@PropertySources({
+    @PropertySource(IsisPresets.DebugVaadin),
 })
-@Log4j2
+@Import({
+    DemoApp.AppManifest.class,
+    
+    // EXPERIMENTAL
+    IsisModuleIncViewerVaadinViewer.class, // vaadin viewer
+  
+})
 public class DemoAppVaadin extends SpringBootServletInitializer {
 
     /**
@@ -80,94 +55,6 @@ public class DemoAppVaadin extends SpringBootServletInitializer {
     public static void main(String[] args) {
         //IsisPresets.prototyping();
         SpringApplication.run(new Class[] { DemoAppVaadin.class }, args);
-    }
-
-    /**
-     * Makes the integral parts of the 'demo' web application.
-     */
-    @Configuration
-    @PropertySources({
-        @PropertySource(IsisPresets.HsqlDbInMemory),
-        @PropertySource(IsisPresets.NoTranslations),
-        @PropertySource(IsisPresets.SilenceWicket),
-        @PropertySource(IsisPresets.DataNucleusAutoCreate),
-        @PropertySource(IsisPresets.DebugVaadin),
-    })
-    @Import({
-        IsisModuleCoreRuntimeServices.class,
-        IsisModuleSecurityShiro.class,
-        IsisModuleJdoDataNucleus5.class,
-        IsisModuleViewerWicketViewer.class, // wicket viewer
-        IsisModuleValSseUi.class, // server sent events
-        IsisModuleValAsciidocUi.class, // ascii-doc rendering support
-
-        // EXPERIMENTAL
-        IsisModuleIncViewerVaadinViewer.class, // vaadin viewer
-        
-        // REST
-        IsisModuleViewerRestfulObjectsViewer.class,
-        IsisModuleViewerRestfulObjectsJaxrsResteasy4.class,
-        
-        // CORS
-        IsisModuleExtCorsImpl.class,
-
-        // Security Manager Extension (secman)
-        IsisModuleExtSecmanModel.class,
-        IsisModuleExtSecmanRealmShiro.class,
-        IsisModuleExtSecmanPersistenceJdo.class,
-        IsisModuleExtSecmanEncryptionJbcrypt.class,
-
-        IsisModuleTestingFixturesApplib.class, // breaks demo app launch
-
-        IsisModuleIncModelMetaModel.class, // @Model support (incubator)
-        IsisModuleExtExcelDownloadUi.class, // allows for collection download as excel
-        
-        LibraryPreloadingService.class, // just a performance enhancement
-        
-        DemoRequestScopedBean.class // demo @RequestScope logging
-
-    })
-    @ComponentScan(
-            basePackageClasses= {
-                    DemoModule.class
-            }
-    )
-    public static class AppManifest {
-
-        @Bean
-        public SecurityModuleConfig securityModuleConfigBean() {
-            return SecurityModuleConfig.builder()
-                    .adminUserName("sven")
-                    .adminAdditionalPackagePermission("demoapp.dom")
-                    .adminAdditionalPackagePermission("org.apache.isis")
-                    .build();
-        }
-
-        @Bean
-        public PermissionsEvaluationService permissionsEvaluationService() {
-            return new PermissionsEvaluationServiceAllowBeatsVeto();
-        }
-        
-        /**
-         * If available from {@code System.getProperty("ContextPath")}
-         * or {@code System.getenv("ContextPath")},
-         * sets the context path for the web server. The context should start with a "/" character 
-         * but not end with a "/" character. The default context path can be
-         * specified using an empty string.
-         */
-        @Bean
-        public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
-            return factory -> {
-                val contextPath = Optional
-                        .ofNullable(System.getProperty("ContextPath"))
-                        .orElse(System.getenv("ContextPath")); // fallback
-                if(contextPath!=null) {
-                    factory.setContextPath(contextPath);
-                    log.info("Setting context path to '{}'", contextPath);
-                }
-            };
-        }
-        
     }
 
 }
