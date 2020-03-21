2ad44f3df2fee4ef58d3058147df837b794ebca1
Andi Huber <ahuber@apache.org>
2020-03-21 14:27:10 +0100

    ISIS-2318: DemoApp: add Mixin Demo (new mixin decl. style)

---
+++ examples/demo/src/main/java/demoapp/dom/mixins/MixinDemo.java
@@ -0,0 +1,61 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package demoapp.dom.mixins;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.Nature;
+import org.apache.isis.applib.annotation.Programmatic;
+
+import demoapp.dom.actions.assoc.DemoItem;
+import demoapp.utils.DemoStub;
+
+@XmlRootElement(name = "Demo")
+@XmlType
+@XmlAccessorType(XmlAccessType.FIELD)
+@DomainObject(nature=Nature.VIEW_MODEL, objectType = "demo.MixinDemo")
+public class MixinDemo extends DemoStub {
+
+    @Override
+    public String title() {
+        return "Mixin Demo";
+    }
+
+    // ---
+
+    List<DemoItem> collection;
+    
+    @Override @Programmatic
+    public void initDefaults() {
+
+        collection = new ArrayList<>();
+        collection.add(DemoItem.of("first"));
+        collection.add(DemoItem.of("second"));
+        collection.add(DemoItem.of("third"));
+
+    }
+
+}
