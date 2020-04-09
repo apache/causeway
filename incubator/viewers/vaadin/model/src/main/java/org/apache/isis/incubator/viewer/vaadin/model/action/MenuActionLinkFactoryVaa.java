576a32d68dad0c5f3c41e046bd29cc3bab6b655a
Andi Huber <ahuber@apache.org>
2020-04-09 19:55:02 +0200

    ISIS-2340: add font awesome support for Vaadin

--- incubator/viewers/vaadin/model/src/main/java/org/apache/isis/incubator/viewer/vaadin/model/action/MenuActionLinkFactoryVaa.java
+++ incubator/viewers/vaadin/model/src/main/java/org/apache/isis/incubator/viewer/vaadin/model/action/MenuActionLinkFactoryVaa.java
@@ -18,9 +18,14 @@
  */
 package org.apache.isis.incubator.viewer.vaadin.model.action;
 
+import java.util.Optional;
+
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
+import org.apache.isis.core.commons.internal.base._Strings;
 import org.apache.isis.core.metamodel.spec.feature.ObjectAction;
 import org.apache.isis.incubator.viewer.vaadin.model.entity.ObjectVaa;
 import org.apache.isis.viewer.common.model.link.ActionLinkFactory;
@@ -39,12 +44,46 @@ public class MenuActionLinkFactoryVaa implements ActionLinkFactory<Component> {
     public LinkAndLabelUiModel<Component> newLink(final ObjectAction objectAction) {
 
         val objectAdapter = serviceModel.getManagedObject();
-        val linkComponent = new Label(objectAction.getName());
         val whetherReturnsBlobOrClob = ObjectAction.Util.returnsBlobOrClob(objectAction);
+        val linkComponent = new HorizontalLayout();
         
-        //linkComponent.addClassName(className);
+        val model = LinkAndLabelUiModel.newLinkAndLabel(
+                (Component)linkComponent, 
+                objectAdapter, 
+                objectAction, 
+                whetherReturnsBlobOrClob);
         
-        return LinkAndLabelUiModel.newLinkAndLabel(linkComponent, objectAdapter, objectAction, whetherReturnsBlobOrClob);
+        addContent(linkComponent, model);
         
+        return model;
     }
+    
+    private void addContent(
+            final HorizontalLayout linkComponent, 
+            final LinkAndLabelUiModel<Component> model) {
+        
+        val faIcon = new Span();
+        
+        Optional.ofNullable(model.getCssClassFa())
+        .ifPresent(cssClassFa->{
+            _Strings.splitThenStreamTrimmed(cssClassFa, " ")
+            .forEach(faIcon::addClassName);
+            //faIcon.addClassNames("fa", cssClassFa, "fa-fw");    
+        });
+        
+        linkComponent.add(faIcon, new Label(model.getLabel()));
+    }
+    
+//    private Component createLinkComponent(final ObjectAction objectAction) {
+//        val label = new Label(objectAction.getName());
+//        label.addClassNames("fa", "fa-bolt");
+//        return label;
+//    }
+    
+//    private Component createLinkComponent(final ObjectAction objectAction) {
+//        val label = new Label(objectAction.getName());
+//        val icon = new Icon(VaadinIcon.BELL);
+//        icon.setSize("1.0em");
+//        return new HorizontalLayout(icon, label);
+//    }
 }
