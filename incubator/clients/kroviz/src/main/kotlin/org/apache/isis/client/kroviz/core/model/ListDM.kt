432da54fcafb1f907c5e5ba95b49f3904cfcf5e1
Jörg Rade <joerg.rade@kuehne-nagel.com>
2020-04-09 15:01:10 +0200

    ISIS-2344 check if object is already contained before adding

--- incubator/clients/kroviz/src/main/kotlin/org/apache/isis/client/kroviz/core/model/ListDM.kt
+++ incubator/clients/kroviz/src/main/kotlin/org/apache/isis/client/kroviz/core/model/ListDM.kt
@@ -6,10 +6,14 @@ import pl.treksoft.kvision.state.observableListOf
 
 class ListDM(override val title: String) : DisplayModelWithLayout() {
     var data = observableListOf<Exposer>()
+    private var rawData = observableListOf<TransferObject>()
 
     override fun addData(obj: TransferObject) {
-        val exo = Exposer(obj as TObject)
-        data.add(exo.dynamise())  //if exposer is not dynamised, data access in tables won't work
+        if (!rawData.contains(obj)) {
+            rawData.add(obj)
+            val exo = Exposer(obj as TObject)
+            data.add(exo.dynamise())  //if exposer is not dynamised, data access in tables won't work
+        }
     }
 
     override fun reset() {
