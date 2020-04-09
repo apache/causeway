706c4412c268fa2951dd3ffa27e6a0c3d4c67f95
Andi Huber <ahuber@apache.org>
2020-04-09 10:35:41 +0200

    ISIS-2343: java-doc and notes

--- core/security/src/main/java/org/apache/isis/core/security/authentication/logout/LogoutHandler.java
+++ core/security/src/main/java/org/apache/isis/core/security/authentication/logout/LogoutHandler.java
@@ -16,18 +16,35 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-
 package org.apache.isis.core.security.authentication.logout;
 
+/**
+ * 
+ * @since Apr 9, 2020
+ * TODO we are at early stages of the design, a better idea occurred:
+ * actually model the SignIn page as a true ViewModel similar to how we
+ * render the home-page; this should allow for the LogoutHandler to be called 
+ * from the framework more directly and not from within the LogoutMenu's 
+ * logout action, which is more complicated because, this happens within 
+ * the context of an IsisInteraction, where we cannot simply purge the 
+ * current session, when in the middle of an interaction
+ */
 public interface LogoutHandler {
 
     /**
+     * logout from the viewer's session, such that the application user has to sign in again
+     */
+    void logout();
+
+    /**
+     * 
+     * @return whether this handler feels responsible for the viewer, that corresponds 
+     * to the current servlet request
+     * 
      * @implNote currently the Wicket logout also triggers a Vaadin logout and vice versa;
      * hence implementations should check whether they are called within a thread
      * that belongs to a request cycle originating from the appropriate viewer/servlet
      */
-    void logout();
-    
     boolean isHandlingCurrentThread();
 
 }
