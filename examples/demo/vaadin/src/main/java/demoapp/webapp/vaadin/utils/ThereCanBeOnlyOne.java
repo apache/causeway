c438b2763d476b7bdea745993a8052ae59da6522
Andi Huber <ahuber@apache.org>
2020-06-19 22:36:29 +0200

    ISIS-2340: fix DemoAppVaadin build (2)

---
+++ examples/demo/vaadin/src/main/java/demoapp/webapp/vaadin/utils/ThereCanBeOnlyOne.java
@@ -0,0 +1,75 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package demoapp.webapp.vaadin.utils;
+
+import java.io.IOException;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicAuthCache;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import lombok.val;
+
+public class ThereCanBeOnlyOne {
+
+    public static void remoteShutdownOthersIfAny() {
+        try {
+            invokeRemoteShutdown();
+        } catch (Exception e) {
+            // ignore them all
+        }
+    }
+    
+    private static void invokeRemoteShutdown() throws IOException {
+        
+        val targetHost = new HttpHost("localhost", 8080, "http");
+        val credsProvider = new BasicCredentialsProvider();
+        credsProvider.setCredentials(
+                new AuthScope(targetHost.getHostName(), targetHost.getPort()),
+                new UsernamePasswordCredentials("sven", "pass"));
+        
+        // Create AuthCache instance
+        AuthCache authCache = new BasicAuthCache();
+        // Generate BASIC scheme object and add it to the local auth cache
+        BasicScheme basicAuth = new BasicScheme();
+        authCache.put(targetHost, basicAuth);
+
+        // Add AuthCache to the execution context
+        HttpClientContext context = HttpClientContext.create();
+        context.setCredentialsProvider(credsProvider);
+        context.setAuthCache(authCache);
+
+        val httpget = new HttpGet("/restful/services/demo.LineBreaker/actions/shutdown/invoke");
+        
+        try(val httpClient = HttpClientBuilder.create().build()){
+            try(val response = httpClient.execute(targetHost, httpget, context)) {
+                response.getEntity();
+            } 
+        }
+        
+    }
+    
+}
