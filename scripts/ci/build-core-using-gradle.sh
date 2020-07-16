8102822291ee235e81ba93b81fa59a726abcfb06
Andi Huber <ahuber@apache.org>
2020-07-16 09:23:42 +0200

    ISIS-2341: activate gradle build in CI build-script

--- scripts/ci/build-core-using-gradle.sh
+++ scripts/ci/build-core-using-gradle.sh
@@ -36,84 +36,8 @@ fi
 
 sh $SCRIPT_DIR/_print-environment.sh "build-core-using-gradle"
 
-if [ ! -z "$REVISION" ]; then
+cd $PROJECT_ROOT_PATH
 
-  cd $PROJECT_ROOT_PATH/core-parent
-  echo ""
-  echo ""
-  echo ">>> mvn versions:set -DnewVersion=$REVISION ..."
-  echo ""
-  echo ""
-#  mvn versions:set \
-#      -DnewVersion=$REVISION \
-#      | fgrep --line-buffered -v "^Progress (1)" \
-#      | fgrep --line-buffered -v "Downloading from central" \
-#      | fgrep --line-buffered -v "Downloaded from central" \
-#      | fgrep --line-buffered -v "Downloading from DataNucleus_2" \
-#      | fgrep --line-buffered -v "Downloaded from DataNucleus_2"
-
-  cd $PROJECT_ROOT_PATH/starters
-  echo ""
-  echo ""
-  echo ">>> sed'ing version in starters ..."
-  echo ""
-  echo ""
-#  CURR=$(grep "<version>" pom.xml | head -1 | cut -d'>' -f2 | cut -d'<' -f1)
-#  sed -i "s|<version>$CURR</version>|<version>$REVISION</version>|g" pom.xml
-fi
-
-cd $PROJECT_ROOT_PATH/core-parent
-echo ""
-echo ""
-echo ">>> mvn $MVN_STAGES $MVN_ADDITIONAL_OPTS"
-echo ""
-echo ""
-#mvn -s $SETTINGS_XML \
-#    $BATCH_MODE \
-#    -T1C \
-#    $MVN_STAGES \
-#    $MVN_ADDITIONAL_OPTS \
-#    $* \
-#    | fgrep --line-buffered -v "^Progress (1)" \
-#    | fgrep --line-buffered -v "Downloading from central" \
-#    | fgrep --line-buffered -v "Downloaded from central" \
-#    | fgrep --line-buffered -v "Downloading from DataNucleus_2" \
-#    | fgrep --line-buffered -v "Downloaded from DataNucleus_2" \
-#    | fgrep --line-buffered -v "Uploading from gcpappenginerepo" \
-#    | fgrep --line-buffered -v "Uploaded from gcpappenginerepo" \
-#    | fgrep --line-buffered -v "Downloading from gcpappenginerepo" \
-#    | fgrep --line-buffered -v "Downloaded from gcpappenginerepo" \
-#    | fgrep --line-buffered -v "[INFO] --- maven-enforcer-plugin" \
-#    | fgrep --line-buffered -v "[INFO] --- maven-site-plugin" \
-#    | fgrep --line-buffered -v "[INFO] <<< maven-source-plugin:" \
-#    | fgrep --line-buffered -v "[INFO] >>> maven-source-plugin" \
-#    | fgrep --line-buffered -v "[INFO] Installing" \
-#    | fgrep --line-buffered -v "[INFO] Copying" \
-#    | fgrep --line-buffered -v "[INFO] Using alternate deployment repository gcpappenginerepo" \
-#    | fgrep --line-buffered -v "[INFO] No site descriptor found: nothing to attach." \
-#    | fgrep --line-buffered -v "[INFO] Skipping because packaging 'jar' is not pom."
-
-if [ ! -z "$REVISION" ]; then
-  cd $PROJECT_ROOT_PATH/core-parent
-  echo ""
-  echo ""
-  echo ">>> mvn versions:revert ..."
-  echo ""
-  echo ""
-#  mvn versions:revert \
-#      | fgrep --line-buffered -v "^Progress (1)" \
-#      | fgrep --line-buffered -v "Downloading from central" \
-#      | fgrep --line-buffered -v "Downloaded from central" \
-#      | fgrep --line-buffered -v "Downloading from DataNucleus_2" \
-#      | fgrep --line-buffered -v "Downloaded from DataNucleus_2"
-
-  cd $PROJECT_ROOT_PATH/starters
-  echo ""
-  echo ""
-  echo ">>> sed'ing to revert version in starters ..."
-  echo ""
-  echo ""
-#  sed -i "s|<version>$REVISION</version>|<version>$CURR</version>|g" pom.xml
-fi
+gradle build
 
 cd $PROJECT_ROOT_PATH
