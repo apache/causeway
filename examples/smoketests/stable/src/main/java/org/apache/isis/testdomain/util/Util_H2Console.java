993281a8e407314003bd2bf49a8c4b6eac160c98
Andi Huber <ahuber@apache.org>
2020-09-05 07:00:27 +0200

    ISIS-2426: switch from PlantUml to Kroki (Antora site build) also adds some experimental diagrams to the site (wip)

--- examples/smoketests/stable/src/main/java/org/apache/isis/testdomain/util/Util_H2Console.java
+++ examples/smoketests/stable/src/main/java/org/apache/isis/testdomain/util/Util_H2Console.java
@@ -16,7 +16,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-package org.apache.isis.testdomain;
+package org.apache.isis.testdomain.util;
 
 import java.sql.SQLException;
 
