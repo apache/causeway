f7944d9f333cdf77507c5d16dd8bc9b7d382ff2a
Andi Huber <ahuber@apache.org>
2020-09-23 15:02:42 +0200

    ISIS-2341: Bumps [flexmark-all] from 0.34.32 to 0.62.2.
    (https://github.com/vsch/flexmark-java)

--- valuetypes/markdown/applib/src/main/java/org/apache/isis/valuetypes/markdown/applib/value/Converter.java
+++ valuetypes/markdown/applib/src/main/java/org/apache/isis/valuetypes/markdown/applib/value/Converter.java
@@ -24,7 +24,7 @@ import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
 import com.vladsch.flexmark.ext.tables.TablesExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
-import com.vladsch.flexmark.util.options.MutableDataSet;
+import com.vladsch.flexmark.util.data.MutableDataSet;
 
 public final class Converter {
 
