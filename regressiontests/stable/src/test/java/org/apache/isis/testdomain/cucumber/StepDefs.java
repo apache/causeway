3d76414b04485fe57164528e6133ecbb50f8199a
Andi Huber <ahuber@apache.org>
2020-10-12 08:36:29 +0200

    ISIS-2447: Add Regression Tests for Isis' Cucubmber Integration

---
+++ regressiontests/stable/src/test/java/org/apache/isis/testdomain/cucumber/StepDefs.java
@@ -0,0 +1,50 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.isis.testdomain.cucumber;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefs extends HeadlessCucumberTestAbstract {
+    private String today;
+    private String actualAnswer;
+
+//    @Given("today is Sunday")
+//    public void today_is_sunday() {
+//        this.today = "Sunday";
+//    }
+    
+    @Given("^today is (.+)$")
+    public void today_is(String today) {
+        this.today = today;
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+}
---
+++ regressiontests/stable/src/test/resources/org/apache/isis/testdomain/cucumber/is_it_friday_yet.feature
@@ -0,0 +1,8 @@
+Feature: Is it Friday yet?
+  Everybody wants to know when it's Friday
+
+  Scenario: Sunday isn't Friday
+    Given today is Sunday
+    When I ask whether it's Friday yet
+    Then I should be told "Nope"
+    
