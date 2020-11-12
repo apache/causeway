1bc65175e98dadebd78dcd2c9dd34ee571047d1f
Jörg Rade <joerg.rade@kuehne-nagel.com>
2020-11-12 16:55:48 +0100

    ISIS-2454 2/4 tests in CorsFilterTest still fail, 'succeeding' tests are rather false positives

--- regressiontests/stable/src/test/java/org/apache/isis/testdomain/rest/CorsFilterTest.java
+++ regressiontests/stable/src/test/java/org/apache/isis/testdomain/rest/CorsFilterTest.java
@@ -22,23 +22,25 @@ import org.apache.isis.core.config.IsisConfiguration;
 import org.apache.isis.core.metamodel._testing.MetaModelContext_forTesting;
 import org.apache.isis.core.metamodel.context.MetaModelContext;
 import org.apache.isis.extensions.cors.impl.IsisModuleExtCorsImpl;
-import org.apache.isis.testdomain.conf.Configuration_headlessButSecure;
+import org.apache.isis.testdomain.conf.Configuration_headless;
 import org.apache.isis.testdomain.rospec.Configuration_usingRoSpec;
 import org.apache.isis.testdomain.util.rest.RestEndpointService;
 import org.apache.isis.viewer.restfulobjects.jaxrsresteasy4.IsisModuleViewerRestfulObjectsJaxrsResteasy4;
-import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockFilterChain;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
-import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -48,15 +50,20 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CorsFilter;
 
-import javax.servlet.Filter;
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
@@ -64,29 +71,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         classes = {RestEndpointService.class, IsisModuleExtCorsImpl.class},
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import({
-        Configuration_headlessButSecure.class,
+        Configuration_headless.class,
         Configuration_usingRoSpec.class,
         IsisModuleViewerRestfulObjectsJaxrsResteasy4.class,
 })
+// see: https://github.com/predix/spring-cors-filter/blob/master/src/test/java/com/ge/predix/web/cors/test/CORSFilterTest.java
+// see: https://www.baeldung.com/intercepting-filter-pattern-in-java
 class CorsFilterTest {
 
     @LocalServerPort
     private int port;
 
     @Test
-    /** implies that:
-     * the CORS filter is registered, and
-     * the URI passes that filter
-     */
-    public void ensureUrlCanBeAccessed() {
-
+    // FALSE_POSITIVE:
+    // regardless what is set in IsisModuleExtCorsIml ("restful" vs. "restful/*)
+    // a (sensible) json response is returned
+    public void ensureUrlCanBeAccessed() throws Exception{
         final String url = "http://localhost:" + port + "/restful/user";
         final TestRestTemplate testRestTemplate = new TestRestTemplate();
         final ResponseEntity<String> response = testRestTemplate
                 //Credentials are not checked, only occurrence of HTTP/BA field in request header
                 .withBasicAuth("any", "any")
                 .getForEntity(url, String.class);
-
         assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
         assertTrue(response.getBody().length() > 0);
     }
@@ -97,35 +103,76 @@ class CorsFilterTest {
     @Test
     public void testCors() throws Exception {
         final RequestBuilder requestBuilder = MockMvcRequestBuilders
-                .get("/restful/user")
+                .get("http://localhost:" + port + "/restful/user")
                 .header("Access-Control-Request-Method", "GET")
                 .header("Origin", "http://localhost");
-        final CorsFilter corsFilter = createIsisCorsFilter().corsFilter();
+        final CorsFilter corsFilter = createIsisCorsFilter();
         final DefaultMockMvcBuilder builder = MockMvcBuilders
                 .webAppContextSetup(this.wac)
-                .addFilter(corsFilter, "/")
-                .dispatchOptions(true);
+                .addFilter(corsFilter, "/**");
         final MockMvc mockMvc = builder.build();
         mockMvc
                 .perform(requestBuilder)
-                .andDo(print());
-                //ServletResource not available.  Set BP in PathResourceResolver.getResource:186
-                //.andExpect(status().isOk())
-                //.andExpect(header().string("Access-Control-Allow-Methods", "GET,PUT,DELETE,POST,OPTIONS"));
+                .andDo(print())
+                //ServletResource not available.  //Set BP in PathResourceResolver.getResource:186
+                .andExpect(status().isOk())
+                .andExpect(header().string("Access-Control-Allow-Methods", "GET,PUT,DELETE,POST,OPTIONS"));
     }
 
-    private static IsisModuleExtCorsImpl createIsisCorsFilter() {
+    private CorsFilter createIsisCorsFilter() {
         final MetaModelContext metaModelContext = MetaModelContext_forTesting.buildDefault();
         final ConfigurableEnvironment environment = metaModelContext.getConfiguration().getEnvironment();
         final IsisConfiguration configuration = new IsisConfiguration(environment);
-        final Map<String, String> map = new HashMap<String, String>();
+        final Map<String, String> map = new HashMap<>();
 
         configuration.setIsisSettings(map);
 
-        final IsisModuleExtCorsImpl classUnderTest = new IsisModuleExtCorsImpl(configuration);
-        final FilterRegistrationBean<Filter> filterRegistration = classUnderTest.corsFilterRegistration();
-        return classUnderTest;
+        final IsisModuleExtCorsImpl isisExtCors = new IsisModuleExtCorsImpl(configuration);
+        isisExtCors.corsFilterRegistration();
+        return isisExtCors.corsFilter();
+    }
+
+    @Test
+    public void testRequestExpectStandardCorsResponse() throws ServletException, IOException {
+        final MockHttpServletRequest request = new MockHttpServletRequest("GET", "/invalid");
+        request.addHeader("Origin", "*");
+
+        final MockHttpServletResponse response = new MockHttpServletResponse();
+        final FilterChain filterChain = new MockFilterChain();
+        final Filter corsFilter = createIsisCorsFilter();
+        corsFilter.doFilter(request, response, filterChain);
+
+        assertEquals("*", response.getHeaderValue("Access-Control-Allow-Origin"));
+    }
+
+    @Test //requestUrl is not seen as invalid. Why?
+    // BP -> CorsFilter.doFilterInternal:87
+    public void testRequestWithForbiddenUri() throws ServletException, IOException {
+        final MockHttpServletRequest request = new MockHttpServletRequest("GET", "/invalid");
+        request.addHeader("Origin", "*");
+
+        final MockHttpServletResponse response = new MockHttpServletResponse();
+        final Filter corsFilter = createIsisCorsFilter();
+        final FilterChain filterChain = new FilterChainImpl(corsFilter);
+        corsFilter.doFilter(request, response, filterChain);
+
+        assertEquals(403, response.getStatus());
+    }
+
+    class FilterChainImpl implements FilterChain {
+        private Iterator<Filter> filters;
+
+        public FilterChainImpl(Filter... filters) {
+            this.filters = Arrays.asList(filters).iterator();
+        }
+
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response) throws IOException, ServletException {
+            if (filters.hasNext()) {
+                Filter filter = filters.next();
+                filter.doFilter(request, response, this);
+            }
+        }
     }
 
 }
-
