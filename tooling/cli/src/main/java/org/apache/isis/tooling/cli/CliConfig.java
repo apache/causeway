43b57b29aa5fc09f197e2d3309ae8b0876a28cf1
Andi Huber <ahuber@apache.org>
2020-12-12 00:42:42 +0100

    ISIS-2473: cli config: fix licenseHeader

--- tooling/cli/src/main/java/org/apache/isis/tooling/cli/CliConfig.java
+++ tooling/cli/src/main/java/org/apache/isis/tooling/cli/CliConfig.java
@@ -52,7 +52,7 @@ public class CliConfig {
     public static class ProjectDoc {
         private String description = "These tables summarize all Maven artifacts available with this project.";
         private String licenseHeader = 
-                ":Notice: Licensed to the Apache Software Foundation (ASF) under one or more contributor license "
+                "Licensed to the Apache Software Foundation (ASF) under one or more contributor license "
                 + "agreements. See the NOTICE file distributed with this work for additional information regarding "
                 + "copyright ownership. The ASF licenses this file to you under the Apache License, "
                 + "Version 2.0 (the \"License\"); you may not use this file except in compliance with the License. "
