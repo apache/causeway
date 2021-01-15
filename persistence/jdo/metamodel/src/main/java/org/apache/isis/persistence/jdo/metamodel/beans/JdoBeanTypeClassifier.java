ed122d4cd5bdc2316399363a685dabb3555d7e9f
Andi Huber <ahuber@apache.org>
2021-01-15 09:00:39 +0100

    ISIS-2445: remove debug code

--- persistence/jdo/metamodel/src/main/java/org/apache/isis/persistence/jdo/metamodel/beans/JdoBeanTypeClassifier.java
+++ persistence/jdo/metamodel/src/main/java/org/apache/isis/persistence/jdo/metamodel/beans/JdoBeanTypeClassifier.java
@@ -21,7 +21,6 @@ package org.apache.isis.persistence.jdo.metamodel.beans;
 import javax.jdo.annotations.EmbeddedOnly;
 
 import org.apache.isis.applib.services.metamodel.BeanSort;
-import org.apache.isis.commons.internal.debug._Probe;
 import org.apache.isis.core.config.beans.IsisBeanTypeClassifier;
 import org.apache.isis.core.metamodel.facets.Annotations;
 
@@ -38,15 +37,9 @@ public class JdoBeanTypeClassifier implements IsisBeanTypeClassifier {
     @Override
     public BeanClassification classify(Class<?> type) {
 
-        if(type.getSimpleName().startsWith("JdoInventory")) {
-            _Probe.errOut("classify %s", type);    
-        }
-        
         val persistenceCapableAnnot = findNearestAnnotation(type, javax.jdo.annotations.PersistenceCapable.class);
         if(persistenceCapableAnnot.isPresent()) {
         
-            _Probe.errOut("entity found %s", type);
-            
             val embeddedOnlyAttribute = persistenceCapableAnnot.get().embeddedOnly();
             // Whether objects of this type can only be embedded, 
             // hence have no ID that binds them to the persistence layer
