0f97d567cb356ffa02b7aed0d9c715cd4ff8f5a7
Andi Huber <ahuber@apache.org>
2021-02-19 09:33:07 +0100

    ISIS-2535: fixes jdo integration service loader configuration for the bean type classifier

--- persistence/jdo/datanucleus/src/main/java/org/apache/isis/persistence/jdo/datanucleus/config/JdoBeanTypeClassifier.java
+++ persistence/jdo/datanucleus/src/main/java/org/apache/isis/persistence/jdo/datanucleus/config/JdoBeanTypeClassifier.java
@@ -16,7 +16,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-package org.apache.isis.persistence.jdo.metamodel.beans;
+package org.apache.isis.persistence.jdo.datanucleus.config;
 
 import javax.jdo.annotations.EmbeddedOnly;
 
