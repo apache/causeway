4b506de8141381df040ac807e8aa5db432a0f55f
joonatan-r <joonatan.rantanen@student.lut.fi>
2021-04-09 18:41:13 +0300

    ISIS-2063: Entering a string such as 10:45 for a joda LocalTime causes stack trace
    Without this patch applied, input to a joda LocalTime field causes an error.
    This patch fixes the problem by adding a panel to be used by joda LocalTime.

---
+++ viewers/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/components/scalars/jodatime/JodaLocalTimePanel.java
@@ -0,0 +1,59 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package org.apache.isis.viewer.wicket.ui.components.scalars.jodatime;
+
+import org.apache.isis.viewer.wicket.model.models.ScalarModel;
+import org.apache.isis.viewer.wicket.ui.components.scalars.ScalarPanelTextFieldAbstract;
+import org.apache.isis.viewer.wicket.ui.components.scalars.TextFieldValueModel;
+import org.apache.wicket.markup.html.form.AbstractTextComponent;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.util.convert.IConverter;
+import org.joda.time.LocalTime;
+
+/**
+ * Panel for rendering scalars of type {@link LocalTime}.
+ */
+public class JodaLocalTimePanel extends ScalarPanelTextFieldAbstract<LocalTime> {
+    
+    private static final long serialVersionUID = 1L;
+    
+    public JodaLocalTimePanel(final String id, final ScalarModel scalarModel) {
+        super(id, scalarModel, LocalTime.class);
+    }
+    
+    @Override
+    protected AbstractTextComponent<LocalTime> createTextFieldForRegular(final String id) {
+        final TextFieldValueModel<LocalTime> textFieldValueModel = new TextFieldValueModel<>(this);
+        return new TextField<LocalTime>(id, textFieldValueModel, LocalTime.class) {
+            private static final long serialVersionUID = 1L;
+    
+            @SuppressWarnings("unchecked")
+            @Override
+            public <C> IConverter<C> getConverter(Class<C> type) {
+                return (IConverter<C>) (type == LocalTime.class ? new ConverterForJodaLocalTime() : super.getConverter(type));
+            }
+        };
+    }
+    
+    @Override
+    protected String getScalarPanelType() {
+        return "jodaLocalTimePanel";
+    }
+}
