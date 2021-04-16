6975c7d76f87a7034b2510f4b0017c377e238c66
danhaywood <dan@haywood-associates.co.uk>
2021-04-16 11:23:59 +0100

    ISIS-2484: adds some sketchy notes on jaxrsclient and restclient

--- mappings/jaxrsclient/impl/src/main/java/org/apache/isis/extensions/jaxrsclient/impl/client/JaxRsClientDefault.java
+++ mappings/jaxrsclient/impl/src/main/java/org/apache/isis/extensions/jaxrsclient/impl/client/JaxRsClientDefault.java
@@ -108,6 +108,9 @@ public class JaxRsClientDefault implements JaxRsClient {
     /**
      * Optional hook, eg allow timeouts to be set:
      *
+     * <p>
+     *     for example:
+     * </p>
      * <pre>
      *  HTTPConduit conduit = WebClient.getConfig(webTarget).getHttpConduit();
      *  conduit.getClient().setConnectionTimeout(1000 * 3);
@@ -123,12 +126,12 @@ public class JaxRsClientDefault implements JaxRsClient {
     }
 
     private MediaType mediaTypeFor(final Class<?> dtoClass, final String reprType) {
-        
+
      // application/xml;profile="urn~org.restfulobjects~repr-types/action-result";x-ro-domain-type="org.apache.isis.schema.cmd.v1.CommandsDto"
         val parameters = new HashMap<String,String>();
         parameters.put("profile", "urn:org.restfulobjects:repr-types/" + reprType);
         parameters.put("x-ro-domain-type", dtoClass.getName());
-        
+
         return new MediaType("application", "xml", parameters);
     }
 
