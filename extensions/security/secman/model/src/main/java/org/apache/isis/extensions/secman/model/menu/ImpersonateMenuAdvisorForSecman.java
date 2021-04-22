c9ae3d3e94789b3ba4a7c520effe3af60db5693a
Andi Huber <ahuber@apache.org>
2021-04-22 17:06:14 +0200

    ISIS-2614: explicitly register MeService

--- extensions/security/secman/model/src/main/java/org/apache/isis/extensions/secman/model/menu/ImpersonateMenuAdvisorForSecman.java
+++ extensions/security/secman/model/src/main/java/org/apache/isis/extensions/secman/model/menu/ImpersonateMenuAdvisorForSecman.java
@@ -1,4 +1,4 @@
-package org.apache.isis.core.runtimeservices.user;
+package org.apache.isis.extensions.secman.model.menu;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,14 +7,13 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
 import org.apache.isis.applib.annotation.OrderPrecedence;
 import org.apache.isis.applib.services.message.MessageService;
-import org.apache.isis.applib.services.user.UserService;
 import org.apache.isis.applib.services.user.ImpersonateMenuAdvisor;
+import org.apache.isis.applib.services.user.UserService;
 import org.apache.isis.extensions.secman.api.role.ApplicationRole;
 import org.apache.isis.extensions.secman.api.role.ApplicationRoleRepository;
 import org.apache.isis.extensions.secman.api.user.ApplicationUser;
@@ -25,7 +24,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.val;
 
 @Service
-@Named("isis.runtimeservices.ImpersonateMenuAdvisorDefault")
+@Named("isis.ext.secman.ImpersonateMenuAdvisorForSecman")
 @Order(OrderPrecedence.MIDPOINT)
 @Qualifier("Default")
 @RequiredArgsConstructor(onConstructor_ = {@Inject})
