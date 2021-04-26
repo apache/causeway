65b9045bf73237b1b4561f27670e726beaad47dc
Andi Huber <ahuber@apache.org>
2021-04-26 12:57:29 +0200

    ISIS-2617: make mouse-menu leave event special logic MDN conform
    However, there are still glitches on Chrome: wrong cursor on menu item
    hover - and also menu going into hiding despite this fix

--- viewers/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/pages/jquery.isis.wicket.viewer.js
+++ viewers/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/pages/jquery.isis.wicket.viewer.js
@@ -198,33 +198,35 @@ $(function() {
 	/*
 	 * Adapted from https://bootstrap-menu.com/detail-basic-hover.html
 	 * Changes:
-	 * - ignoring mouseleave events if hovering over a popover
+	 * - ignoring mouseleave events if hovering over a popover that belongs to a menuitem
 	 */
-	if (window.innerWidth > 992) {	
-		document.querySelectorAll('.navbar .nav-item').forEach(function(everyitem){	
-			everyitem.addEventListener('mouseover', function(e){
-				let el_link = this.querySelector('a[data-toggle]');
-				if(el_link != null){
-					let nextEl = el_link.nextElementSibling;
-					el_link.classList.add('show');
-					nextEl.classList.add('show');
-				}	
-			});
-			everyitem.addEventListener('mouseleave', function(e){
-				// do not hide the dropdown if hovering over a popover (tooltip) attached to the dropdown item
-				if($(e.toElement).hasClass('popover') || $(e.toElement).hasClass('popover-body')) {
-					e.preventDefault();
-					return;
-				}
-				let el_link = this.querySelector('a[data-toggle]');
-				if(el_link != null){
-					let nextEl = el_link.nextElementSibling;
-					el_link.classList.remove('show');
-					nextEl.classList.remove('show');
-				}
-			});
-		});	
-	}
+	document.querySelectorAll('.navbar .nav-item').forEach(function(everyitem){	
+		everyitem.addEventListener('mouseover', function(e){
+			let el_link = this.querySelector('a[data-toggle]');
+			if(el_link != null){
+				let nextEl = el_link.nextElementSibling;
+				el_link.classList.add('show');
+				nextEl.classList.add('show');
+			}	
+		});
+		everyitem.addEventListener('mouseleave', function(e){
+			// do not hide the dropdown if hovering over a popover (tooltip) attached to the dropdown item
+			// The MouseEvent.relatedTarget read-only property is the secondary target for the mouse event, 
+			// if there is one: That is, the EventTarget the pointing device entered to.
+			let relatedTarget = $(e.relatedTarget);
+			if(relatedTarget.hasClass('popover-body')
+					||relatedTarget.hasClass('popover')){
+				e.preventDefault();
+				return;
+			}
+			let el_link = this.querySelector('a[data-toggle]');
+			if(el_link != null){
+				let nextEl = el_link.nextElementSibling;
+				el_link.classList.remove('show');
+				nextEl.classList.remove('show');
+			}
+		});
+	});	
 
 });
 
