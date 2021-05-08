64bd15dfb6227f25ca4fe33c2062cd3d9071dbad
danhaywood <dan@haywood-associates.co.uk>
2021-05-08 12:53:40 +0100

    ISIS-2619: moves import statements of services etc from secman model module to secman api module

--- extensions/security/secman/model/src/main/java/org/apache/isis/extensions/secman/model/IsisModuleExtSecmanModel.java
+++ extensions/security/secman/model/src/main/java/org/apache/isis/extensions/secman/model/IsisModuleExtSecmanModel.java
@@ -22,62 +22,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import org.apache.isis.extensions.secman.api.IsisModuleExtSecmanApi;
-import org.apache.isis.extensions.secman.api.feature.dom.ApplicationFeatureViewModels;
-import org.apache.isis.extensions.secman.api.feature.dom.ApplicationNamespace;
-import org.apache.isis.extensions.secman.api.feature.dom.ApplicationType;
-import org.apache.isis.extensions.secman.api.feature.dom.ApplicationTypeAction;
-import org.apache.isis.extensions.secman.api.feature.dom.ApplicationTypeCollection;
-import org.apache.isis.extensions.secman.api.feature.dom.ApplicationTypeMember;
-import org.apache.isis.extensions.secman.api.feature.dom.ApplicationTypeProperty;
-import org.apache.isis.extensions.secman.api.permission.app.ApplicationOrphanedPermissionManager;
-import org.apache.isis.extensions.secman.api.permission.app.mixins.ApplicationOrphanedPermissionManager_relocateSelected;
-import org.apache.isis.extensions.secman.api.permission.menu.ApplicationPermissionMenu;
-import org.apache.isis.extensions.secman.api.permission.dom.mixins.ApplicationPermission_allow;
-import org.apache.isis.extensions.secman.api.permission.dom.mixins.ApplicationPermission_changing;
-import org.apache.isis.extensions.secman.api.permission.dom.mixins.ApplicationPermission_delete;
-import org.apache.isis.extensions.secman.api.permission.dom.mixins.ApplicationPermission_feature;
-import org.apache.isis.extensions.secman.api.permission.dom.mixins.ApplicationPermission_updateRole;
-import org.apache.isis.extensions.secman.api.permission.dom.mixins.ApplicationPermission_veto;
-import org.apache.isis.extensions.secman.api.permission.dom.mixins.ApplicationPermission_viewing;
-import org.apache.isis.extensions.secman.api.role.menu.ApplicationRoleMenu;
-import org.apache.isis.extensions.secman.api.role.dom.mixins.ApplicationRole_addPermission;
-import org.apache.isis.extensions.secman.api.role.dom.mixins.ApplicationRole_addUser;
-import org.apache.isis.extensions.secman.api.role.dom.mixins.ApplicationRole_delete;
-import org.apache.isis.extensions.secman.api.role.dom.mixins.ApplicationRole_removePermissions;
-import org.apache.isis.extensions.secman.api.role.dom.mixins.ApplicationRole_removeUsers;
-import org.apache.isis.extensions.secman.api.role.dom.mixins.ApplicationRole_updateDescription;
-import org.apache.isis.extensions.secman.api.role.dom.mixins.ApplicationRole_updateName;
-import org.apache.isis.extensions.secman.api.tenancy.menu.ApplicationTenancyMenu;
-import org.apache.isis.extensions.secman.api.tenancy.dom.mixins.ApplicationTenancy_addChild;
-import org.apache.isis.extensions.secman.api.tenancy.dom.mixins.ApplicationTenancy_addUser;
-import org.apache.isis.extensions.secman.api.tenancy.dom.mixins.ApplicationTenancy_delete;
-import org.apache.isis.extensions.secman.api.tenancy.dom.mixins.ApplicationTenancy_removeChild;
-import org.apache.isis.extensions.secman.api.tenancy.dom.mixins.ApplicationTenancy_removeUser;
-import org.apache.isis.extensions.secman.api.tenancy.dom.mixins.ApplicationTenancy_updateName;
-import org.apache.isis.extensions.secman.api.tenancy.dom.mixins.ApplicationTenancy_updateParent;
-import org.apache.isis.extensions.secman.api.tenancy.dom.mixins.ApplicationTenancy_users;
-import org.apache.isis.extensions.secman.api.user.app.ApplicationUserManager;
-import org.apache.isis.extensions.secman.api.user.menu.ApplicationUserMenu;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_addRole;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_delete;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_duplicate;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.perms.ApplicationUser_filterPermissions;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_lock;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.perms.ApplicationUser_permissions;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_removeRoles;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_resetPassword;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_unlock;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_updateAccountType;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_updateAtPath;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_updateEmailAddress;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_updateFaxNumber;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_updateName;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_updatePassword;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_updatePhoneNumber;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.ApplicationUser_updateUsername;
-import org.apache.isis.extensions.secman.api.user.contributions.HasUsername_open;
-import org.apache.isis.extensions.secman.api.user.menu.MeService;
-import org.apache.isis.extensions.secman.api.user.dom.mixins.perms.UserPermissionViewModel;
 import org.apache.isis.extensions.secman.model.facets.TenantedAuthorizationPostProcessor;
 import org.apache.isis.extensions.secman.model.spiimpl.TableColumnVisibilityServiceForSecman;
 
@@ -89,90 +33,12 @@ import org.apache.isis.extensions.secman.model.spiimpl.TableColumnVisibilityServ
         // Module
         IsisModuleExtSecmanApi.class,
 
-        // @DomainService
-        ApplicationFeatureViewModels.class,
-        ApplicationOrphanedPermissionManager.class,
-        ApplicationPermissionMenu.class,
-        ApplicationRoleMenu.class,
-        ApplicationTenancyMenu.class,
-        ApplicationUserMenu.class,
-
-        //ImpersonateMenuAdvisorForSecman.class, //not activated by default yet
-        MeService.class,
-
         // @Component
         TenantedAuthorizationPostProcessor.Register.class,
         TableColumnVisibilityServiceForSecman.class,
+        // ImpersonateMenuAdvisorForSecman.class, //not activated by default yet
 
-        // -- ViewModels
-
-        ApplicationNamespace.class,
-        ApplicationType.class,
-        ApplicationTypeAction.class,
-        ApplicationTypeCollection.class,
-        ApplicationTypeMember.class,
-        ApplicationTypeProperty.class,
-        ApplicationUserManager.class,
-        UserPermissionViewModel.class,
-
-        // -- Mixins
-
-        //ApplicationOrphanedPermissionManager
-        ApplicationOrphanedPermissionManager_relocateSelected.class,
-
-        //ApplicationPermission
-        ApplicationPermission_allow.class,
-        ApplicationPermission_changing.class,
-        ApplicationPermission_delete.class,
-        ApplicationPermission_feature.class,
-        ApplicationPermission_updateRole.class,
-        ApplicationPermission_veto.class,
-        ApplicationPermission_viewing.class,
-
-        //ApplicationRole
-        ApplicationRole_addPermission.class,
-        ApplicationRole_addUser.class,
-        ApplicationRole_delete.class,
-        ApplicationRole_removePermissions.class,
-        ApplicationRole_removeUsers.class,
-        ApplicationRole_updateDescription.class,
-        ApplicationRole_updateName.class,
-
-        //ApplicationTenancy
-        ApplicationTenancy_addChild.class,
-        ApplicationTenancy_addUser.class,
-        ApplicationTenancy_delete.class,
-        ApplicationTenancy_removeChild.class,
-        ApplicationTenancy_removeUser.class,
-        ApplicationTenancy_updateName.class,
-        ApplicationTenancy_updateParent.class,
-        ApplicationTenancy_users.class,
-
-        //ApplicationUser
-        ApplicationUser_addRole.class,
-        ApplicationUser_delete.class,
-        ApplicationUser_duplicate.class,
-        ApplicationUser_filterPermissions.class,
-        ApplicationUser_lock.class,
-        ApplicationUser_permissions.class,
-        ApplicationUser_removeRoles.class,
-        ApplicationUser_resetPassword.class,
-        ApplicationUser_unlock.class,
-        ApplicationUser_updateAccountType.class,
-        ApplicationUser_updateAtPath.class,
-        ApplicationUser_updateEmailAddress.class,
-        ApplicationUser_updateFaxNumber.class,
-        ApplicationUser_updateName.class,
-        ApplicationUser_updatePassword.class,
-        ApplicationUser_updatePhoneNumber.class,
-        ApplicationUser_updateUsername.class,
-        HasUsername_open.class,
-
-        //ApplicationUserManager (abstract, concrete classes are in JDO/JPA)
-        //ApplicationUserManager_allUsers.class,
-        //ApplicationUserManager_newDelegateUser.class,
-        //ApplicationUserManager_newLocalUser.class,
-    })
+})
 public class IsisModuleExtSecmanModel {
 
 }
