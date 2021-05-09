890c1ac0dff8c5a1edcb5f6bf7c1e174dea67d96
danhaywood <dan@haywood-associates.co.uk>
2021-05-09 11:30:17 +0100

    ISIS-2619: moves SecurityModuleAppUserRegistrationServiceAbstract from jdo/jpa to model

--- extensions/security/secman/model/src/main/java/org/apache/isis/extensions/secman/model/userreg/SecurityModuleAppUserRegistrationServiceAbstract.java
+++ extensions/security/secman/model/src/main/java/org/apache/isis/extensions/secman/model/userreg/SecurityModuleAppUserRegistrationServiceAbstract.java
@@ -16,7 +16,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-package org.apache.isis.extensions.secman.jpa.userreg;
+package org.apache.isis.extensions.secman.model.userreg;
 
 import java.util.Set;
 
