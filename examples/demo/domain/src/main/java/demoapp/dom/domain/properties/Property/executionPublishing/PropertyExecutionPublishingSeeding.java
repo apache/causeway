f72dddf61966b2ca5b54ec7b87759c84d991c01b
Andi Huber <ahuber@apache.org>
2021-05-27 13:27:25 +0200

    ISIS-2513: Demo: add JPA support for PropertyExecutionPublishing (2)

--- examples/demo/domain/src/main/java/demoapp/dom/domain/properties/Property/executionPublishing/PropertyExecutionPublishingSeeding.java
+++ examples/demo/domain/src/main/java/demoapp/dom/domain/properties/Property/executionPublishing/PropertyExecutionPublishingSeeding.java
@@ -22,34 +22,17 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
 
-import org.apache.isis.applib.services.repository.RepositoryService;
-import org.apache.isis.testing.fixtures.applib.fixturescripts.FixtureScript;
-
 import demoapp.dom._infra.seed.SeedServiceAbstract;
+import demoapp.dom._infra.values.ValueHolderRepository;
 import demoapp.dom.domain.properties.Property.executionPublishing.jdo.PropertyExecutionPublishingJdo;
-import demoapp.dom.types.Samples;
 
 @Service
-public class PropertyExecutionPublishingSeeding extends SeedServiceAbstract {
+public class PropertyExecutionPublishingSeeding
+extends SeedServiceAbstract {
 
-    public PropertyExecutionPublishingSeeding() {
-        super(PropertyPublishingJdoEntityFixture::new);
+    @Inject
+    public PropertyExecutionPublishingSeeding(ValueHolderRepository<String, ? extends PropertyExecutionPublishingJdo> entities) {
+        super(entities);
     }
 
-    static class PropertyPublishingJdoEntityFixture extends FixtureScript {
-
-        @Override
-        protected void execute(ExecutionContext executionContext) {
-            samples.stream()
-                    .map(PropertyExecutionPublishingJdo::new)
-                    .forEach(domainObject -> {
-                        repositoryService.persist(domainObject);
-                        executionContext.addResult(this, domainObject);
-                    });
-
-        }
-
-        @Inject RepositoryService repositoryService;
-        @Inject Samples<String> samples;
-    }
 }
