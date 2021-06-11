535548d262b1dc977b0434c526515f75fe71200c
danhaywood <dan@haywood-associates.co.uk>
2021-06-11 11:48:30 +0100

    ISIS-2732: removes methods to set clock from IsisIntegrationTestAbstractWithFixtures

--- testing/fixtures/applib/src/main/java/org/apache/isis/testing/fixtures/applib/IsisIntegrationTestAbstractWithFixtures.java
+++ testing/fixtures/applib/src/main/java/org/apache/isis/testing/fixtures/applib/IsisIntegrationTestAbstractWithFixtures.java
@@ -20,19 +20,14 @@ package org.apache.isis.testing.fixtures.applib;
 
 import javax.inject.Inject;
 
-import org.joda.time.LocalDate;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
-import org.apache.isis.applib.services.clock.ClockService;
-import org.apache.isis.testing.fixtures.applib.personas.PersonaWithBuilderScript;
-import org.apache.isis.testing.fixtures.applib.clock.clock.Clock;
-import org.apache.isis.testing.fixtures.applib.clock.clock.FixtureClock;
-import org.apache.isis.testing.fixtures.applib.clock.clock.TickingFixtureClock;
-import org.apache.isis.testing.fixtures.applib.personas.BuilderScriptAbstract;
 import org.apache.isis.testing.fixtures.applib.fixturescripts.FixtureScript;
 import org.apache.isis.testing.fixtures.applib.fixturescripts.FixtureScripts;
 import org.apache.isis.testing.fixtures.applib.modules.ModuleWithFixturesService;
+import org.apache.isis.testing.fixtures.applib.personas.BuilderScriptAbstract;
+import org.apache.isis.testing.fixtures.applib.personas.PersonaWithBuilderScript;
 import org.apache.isis.testing.integtestsupport.applib.IsisIntegrationTestAbstract;
 
 /**
@@ -54,41 +49,6 @@ public abstract class IsisIntegrationTestAbstractWithFixtures extends IsisIntegr
 
     protected <T> T runPersona(final PersonaWithBuilderScript<? extends BuilderScriptAbstract<T>> personaScript) {
         return this.fixtureScripts.runPersona(personaScript);
-    }
-
-    /**
-     * To use instead of {@link #getFixtureClock()}'s {@link FixtureClock#setDate(int, int, int)} ()}.
-     */
-    protected void setFixtureClockDate(final LocalDate date) {
-        if(date == null) {
-            return;
-        }
-        setFixtureClockDate(date.getYear(), date.getMonthOfYear(), date.getDayOfMonth());
-    }
-
-    /**
-     * To use instead of {@link #getFixtureClock()}'s {@link FixtureClock#setDate(int, int, int)} ()}.
-     */
-    protected void setFixtureClockDate(final int year, final int month, final int day) {
-
-        final Clock instance = Clock.getInstance();
-
-        if(instance instanceof TickingFixtureClock) {
-            TickingFixtureClock.reinstateExisting();
-            getFixtureClock().setDate(year, month, day);
-            TickingFixtureClock.replaceExisting();
-        }
-
-        if(instance instanceof FixtureClock) {
-            getFixtureClock().setDate(year, month, day);
-        }
-    }
-
-    /**
-     * If just require the current time, use {@link ClockService}
-     */
-    private FixtureClock getFixtureClock() {
-        return ((FixtureClock)FixtureClock.getInstance());
     }
 
     // -- DEPENDENCIES
