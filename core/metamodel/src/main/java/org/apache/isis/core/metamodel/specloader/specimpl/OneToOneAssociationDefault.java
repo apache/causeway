618fd7292fa644770a95a95a4580e5dcdcdf6439
danhaywood <dan@haywood-associates.co.uk>
2021-06-28 16:38:54 +0100

    ISIS-1720: fixes NPE when rendering collections (not valid to return a null supplier in Optiona;#orElseGet)

--- core/metamodel/src/main/java/org/apache/isis/core/metamodel/specloader/specimpl/OneToOneAssociationDefault.java
+++ core/metamodel/src/main/java/org/apache/isis/core/metamodel/specloader/specimpl/OneToOneAssociationDefault.java
@@ -94,7 +94,7 @@ implements OneToOneAssociation {
     public String getColumnDescription() {
         return lookupFacet(ColumnDescribedFacet.class)
         .map(ColumnDescribedFacet::translated)
-        .orElseGet(null);
+        .orElseGet(() -> null);
     }
 
     // -- VISIBLE, USABLE
