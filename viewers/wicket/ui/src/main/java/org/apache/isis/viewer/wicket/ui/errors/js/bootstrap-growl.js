a2e76f14430f2ecae43bad2c42f3a729eca48a75
andi-huber <ahuber@apache.org>
2021-07-01 10:16:43 +0200

    ISIS-2740: changing bootstrap growl z-index 1031 -> 999
    so we are lower that menu drop-downs which have z-index 1000

--- viewers/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/errors/js/bootstrap-growl.js
+++ viewers/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/errors/js/bootstrap-growl.js
@@ -19,7 +19,7 @@
 			},
 			offset: 20,
 			spacing: 10,
-			z_index: 1031,
+			z_index: 999, // modified for Apache Isis (origin: 1031), 
 			delay: 5000,
 			timer: 1000,
 			url_target: '_blank',
