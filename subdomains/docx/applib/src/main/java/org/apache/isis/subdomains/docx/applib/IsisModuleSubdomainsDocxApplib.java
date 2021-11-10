8fc41acfcc0ed362b1943e346a04181f0f44909f
Patrick Deenen <patrick.deenen@opencirclesolutions.nl>
2021-11-10 10:37:30 +0100

    Added @ComponentScan to IsisModuleSubdomainsDocxApplib so Docx beans are actually registered in the Spring application context

--- subdomains/docx/applib/src/main/java/org/apache/isis/subdomains/docx/applib/IsisModuleSubdomainsDocxApplib.java
+++ subdomains/docx/applib/src/main/java/org/apache/isis/subdomains/docx/applib/IsisModuleSubdomainsDocxApplib.java
@@ -18,9 +18,11 @@
  */
 package org.apache.isis.subdomains.docx.applib;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class IsisModuleSubdomainsDocxApplib {
 
 }
