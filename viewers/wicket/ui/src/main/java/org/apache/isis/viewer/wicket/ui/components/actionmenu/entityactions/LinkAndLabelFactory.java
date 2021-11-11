3dfc09e08026cacd929727858ca7d34194700f2c
Andi Huber <ahuber@apache.org>
2021-11-11 17:02:37 +0100

    ISIS-2877: just improves readability (of prev. commit)

--- viewers/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/components/actionmenu/entityactions/LinkAndLabelFactory.java
+++ viewers/wicket/ui/src/main/java/org/apache/isis/viewer/wicket/ui/components/actionmenu/entityactions/LinkAndLabelFactory.java
@@ -113,20 +113,21 @@ extends Function<ObjectAction, LinkAndLabel> {
             final ScalarParameterModel parameterModel) {
 
         // only supported, when parameter type is scalar and also is a value-type
-        if(!parameterModel.getMetaModel().isScalar()
-                || !parameterModel.getMetaModel().getElementType().isValue()) {
-            return action -> null;
+        if(parameterModel.getMetaModel().isScalar()
+                && parameterModel.getMetaModel().getElementType().isValue()) {
+
+            val linkFactory = new AdditionalLinkFactory();
+
+            return action -> LinkAndLabel.of(
+                    ActionModelImpl.forEntity(
+                            parameterModel.getParentUiModel(),
+                            action.getFeatureIdentifier(),
+                            Where.OBJECT_FORMS,
+                            null, parameterModel, null),
+                    linkFactory);
         }
 
-        val linkFactory = new AdditionalLinkFactory();
-
-        return action -> LinkAndLabel.of(
-                ActionModelImpl.forEntity(
-                        parameterModel.getParentUiModel(),
-                        action.getFeatureIdentifier(),
-                        Where.OBJECT_FORMS,
-                        null, parameterModel, null),
-                linkFactory);
+        return action -> null;
     }
 
     // -- HELPER
