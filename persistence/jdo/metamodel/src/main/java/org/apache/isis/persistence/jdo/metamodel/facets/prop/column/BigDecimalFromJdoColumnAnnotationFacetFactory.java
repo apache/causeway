dc07de53aac4c6fe1ee6e6cb3999fe892e168e19
Andi Huber <ahuber@apache.org>
2022-01-10 08:47:09 +0100

    ISIS-2856: remove MaxFractionalDigitsFacetForPersistentBigDecimalWhenUnspecified
    - was a no-op anyway

--- persistence/jdo/metamodel/src/main/java/org/apache/isis/persistence/jdo/metamodel/facets/prop/column/BigDecimalFromJdoColumnAnnotationFacetFactory.java
+++ persistence/jdo/metamodel/src/main/java/org/apache/isis/persistence/jdo/metamodel/facets/prop/column/BigDecimalFromJdoColumnAnnotationFacetFactory.java
@@ -19,21 +19,17 @@
 package org.apache.isis.persistence.jdo.metamodel.facets.prop.column;
 
 import java.math.BigDecimal;
-import java.util.Optional;
 
 import javax.inject.Inject;
 import javax.jdo.annotations.Column;
 import javax.jdo.annotations.IdentityType;
 
-import org.apache.isis.commons.internal.base._Optionals;
-import org.apache.isis.core.config.beans.IsisBeanTypeRegistry;
 import org.apache.isis.core.metamodel.context.MetaModelContext;
 import org.apache.isis.core.metamodel.facetapi.FeatureType;
 import org.apache.isis.core.metamodel.facetapi.MetaModelRefiner;
 import org.apache.isis.core.metamodel.facets.FacetFactoryAbstract;
 import org.apache.isis.core.metamodel.facets.FacetedMethod;
 import org.apache.isis.core.metamodel.facets.objectvalue.digits.MaxFractionalDigitsFacet;
-import org.apache.isis.core.metamodel.facets.objectvalue.digits.MaxFractionalDigitsFacetForPersistentBigDecimalWhenUnspecified;
 import org.apache.isis.core.metamodel.facets.objectvalue.digits.MaxTotalDigitsFacet;
 import org.apache.isis.core.metamodel.progmodel.ProgrammingModel;
 import org.apache.isis.core.metamodel.spec.feature.MixedIn;
@@ -48,12 +44,9 @@ public class BigDecimalFromJdoColumnAnnotationFacetFactory
 extends FacetFactoryAbstract
 implements MetaModelRefiner {
 
-    private Optional<IsisBeanTypeRegistry> beanTypeRegistryIfAny; // JUnit support (allowed to be empty)
-
     @Inject
     public BigDecimalFromJdoColumnAnnotationFacetFactory(final MetaModelContext mmc) {
         super(mmc, FeatureType.PROPERTIES_ONLY);
-        beanTypeRegistryIfAny = mmc.getServiceRegistry().lookupService(IsisBeanTypeRegistry.class);
     }
 
     @Override
@@ -74,13 +67,6 @@ implements MetaModelRefiner {
         addFacetIfPresent(
                 MaxFractionalDigitsFacetFromJdoColumn
                 .create(jdoColumnIfAny, holder));
-
-        // adds additional constraints if applicable
-        beanTypeRegistryIfAny.ifPresent(beanTypeRegistry->{
-            addFacetIfPresent(
-                    MaxFractionalDigitsFacetForPersistentBigDecimalWhenUnspecified
-                    .create(_Optionals.toInt(jdoColumnIfAny, Column::scale), processMethodContext, beanTypeRegistry));
-        });
 
     }
 
