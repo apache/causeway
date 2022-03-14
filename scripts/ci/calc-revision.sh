5275e627b727273bac49b34654fa126cee82e4af
Dan Haywood <dan@haywood-associates.co.uk>
2022-03-14 18:50:49 +0000

    ISIS-2975: experiments (17)

--- scripts/ci/calc-revision.sh
+++ scripts/ci/calc-revision.sh
@@ -4,7 +4,13 @@ echo ""
 echo ""
 echo ""
 
-DATE=$(date +%Y%m%d.%H%M)
+if [ ! -z "$TIMESTAMP" ]
+then
+  # TIMESTAMP: 2022-03-14T18:43:38Z
+  DATE="$(echo $TIMESTAMP | cut -c1-4)$(echo $TIMESTAMP | cut -c6-7)$(echo $TIMESTAMP | cut -c9-10).$(echo $TIMESTAMP | cut -c12-13)$(echo $TIMESTAMP | cut -c15-16)"
+else
+  DATE=$(date +%Y%m%d.%H%M)
+fi
 BRANCH=$(echo $GITHUB_REF_NAME)
 
 GIT_SHORT_COMMIT=$(echo $GITHUB_SHA | cut -c1-8)
