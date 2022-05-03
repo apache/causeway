4680573277f0ff79eadfe4ed28cd89c6550b98a3
andi-huber <ahuber@apache.org>
2022-05-03 10:48:31 +0200

    ISIS-2627: fixes UserPermissionViewModel serialization

--- extensions/security/secman/applib/src/main/java/org/apache/isis/extensions/secman/applib/user/dom/mixins/perms/UserPermissionViewModel.java
+++ extensions/security/secman/applib/src/main/java/org/apache/isis/extensions/secman/applib/user/dom/mixins/perms/UserPermissionViewModel.java
@@ -23,10 +23,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
@@ -46,12 +45,11 @@ import org.apache.isis.applib.services.appfeat.ApplicationFeatureId;
 import org.apache.isis.applib.services.appfeat.ApplicationFeatureRepository;
 import org.apache.isis.applib.services.appfeat.ApplicationFeatureSort;
 import org.apache.isis.applib.services.appfeatui.ApplicationFeatureViewModel;
-import org.apache.isis.applib.services.bookmark.Bookmark;
 import org.apache.isis.applib.services.factory.FactoryService;
 import org.apache.isis.applib.util.ObjectContracts;
 import org.apache.isis.applib.util.ToString;
-import org.apache.isis.commons.internal.base._NullSafe;
-import org.apache.isis.commons.internal.base._Strings;
+import org.apache.isis.commons.internal.base._Bytes;
+import org.apache.isis.commons.internal.resources._Serializables;
 import org.apache.isis.extensions.secman.applib.IsisModuleExtSecmanApplib;
 import org.apache.isis.extensions.secman.applib.permission.dom.ApplicationPermission;
 import org.apache.isis.extensions.secman.applib.permission.dom.ApplicationPermissionMode;
@@ -91,19 +89,12 @@ public class UserPermissionViewModel implements ViewModel {
 
     // -- constructors, factory methods
 
-    public static UserPermissionViewModel newViewModel(
+    public static String memento(
             final ApplicationFeatureId featureId,
             final ApplicationUser user,
             final ApplicationPermissionValueSet.Evaluation viewingEvaluation,
-            final ApplicationPermissionValueSet.Evaluation changingEvaluation,
-            final FactoryService factory) {
-
-        return factory
-                .viewModel(
-                        UserPermissionViewModel.class,
-                        Bookmark.forLogicalTypeNameAndIdentifier(
-                                UserPermissionViewModel.LOGICAL_TYPE_NAME,
-                                asEncodedString(featureId, user.getUsername(), viewingEvaluation, changingEvaluation)));
+            final ApplicationPermissionValueSet.Evaluation changingEvaluation) {
+        return asEncodedString(featureId, user.getUsername(), viewingEvaluation, changingEvaluation);
     }
 
     public UserPermissionViewModel() {
@@ -137,63 +128,54 @@ public class UserPermissionViewModel implements ViewModel {
             final String username,
             final ApplicationPermissionValueSet.Evaluation viewingEvaluation,
             final ApplicationPermissionValueSet.Evaluation changingEvaluation) {
-        return base64UrlEncode(asString(featureId, username, viewingEvaluation, changingEvaluation));
-    }
 
-    private static String asString(
-            final ApplicationFeatureId featureId,
-            final String username,
-            final ApplicationPermissionValueSet.Evaluation viewingEvaluation,
-            final ApplicationPermissionValueSet.Evaluation changingEvaluation) {
-
-        final boolean viewingEvaluationGranted = viewingEvaluation.isGranted();
         final ApplicationPermissionValue viewingEvaluationCause = viewingEvaluation.getCause();
-        final ApplicationFeatureId viewingEvaluationCauseFeatureId = viewingEvaluationCause != null? viewingEvaluationCause.getFeatureId(): null;
+        final ApplicationFeatureId viewingEvaluationCauseFeatureId = viewingEvaluationCause != null
+                ? viewingEvaluationCause.getFeatureId()
+                : null;
 
-        final boolean changingEvaluationGranted = changingEvaluation.isGranted();
         final ApplicationPermissionValue changingEvaluationCause = changingEvaluation.getCause();
-        final ApplicationFeatureId changingEvaluationCauseFeatureId = changingEvaluationCause != null? changingEvaluationCause.getFeatureId(): null;
+        final ApplicationFeatureId changingEvaluationCauseFeatureId = changingEvaluationCause != null
+                ? changingEvaluationCause.getFeatureId()
+                : null;
 
-        return join(
+        val payload = new String[] {
             username,
 
-            viewingEvaluationGranted,
-            viewingEvaluationCauseFeatureId != null? viewingEvaluationCauseFeatureId.getSort(): "",
-            viewingEvaluationCauseFeatureId != null? viewingEvaluationCauseFeatureId.getFullyQualifiedName(): "",
-            viewingEvaluationCause != null? viewingEvaluationCause.getRule(): "",
-            viewingEvaluationCause != null? viewingEvaluationCause.getMode(): "",
+            ""+viewingEvaluation.isGranted(),
+            viewingEvaluationCauseFeatureId != null? ""+viewingEvaluationCauseFeatureId.getSort(): "",
+            viewingEvaluationCauseFeatureId != null? ""+viewingEvaluationCauseFeatureId.getFullyQualifiedName(): "",
+            viewingEvaluationCause != null? ""+viewingEvaluationCause.getRule(): "",
+            viewingEvaluationCause != null? ""+viewingEvaluationCause.getMode(): "",
 
-            changingEvaluationGranted,
-            changingEvaluationCauseFeatureId != null? changingEvaluationCauseFeatureId.getSort(): "",
-            changingEvaluationCauseFeatureId != null? changingEvaluationCauseFeatureId.getFullyQualifiedName(): "",
-            changingEvaluationCause != null? changingEvaluationCause.getRule(): "",
-            changingEvaluationCause != null? changingEvaluationCause.getMode(): "",
+            ""+changingEvaluation.isGranted(),
+            changingEvaluationCauseFeatureId != null? ""+changingEvaluationCauseFeatureId.getSort(): "",
+            changingEvaluationCauseFeatureId != null? ""+changingEvaluationCauseFeatureId.getFullyQualifiedName(): "",
+            changingEvaluationCause != null? ""+changingEvaluationCause.getRule(): "",
+            changingEvaluationCause != null? ""+changingEvaluationCause.getMode(): "",
 
-            featureId.getSort(),
+            ""+featureId.getSort(),
             featureId.getFullyQualifiedName()
-        );
-    }
-
-    private static String join(final Object ... args) {
-        return _NullSafe.stream(args)
-                .map(arg->""+arg)
-                .collect(Collectors.joining(":"));
+        };
+        return new String(_Bytes.asUrlBase64.apply(_Serializables.write(payload)), StandardCharsets.US_ASCII);
     }
 
     private void parseEncoded(final String encodedString) {
-        parse(base64UrlDecode(encodedString));
+        val payload = _Serializables.read(String[].class, _Bytes.ofUrlBase64.apply(encodedString.getBytes(StandardCharsets.US_ASCII)));
+        parse(payload);
     }
 
-    private void parse(final String asString) {
-        final Iterator<String> iterator = _Strings.splitThenStream(asString, ":")
-                .collect(Collectors.toList())
-                .listIterator();
+    private void parse(final String[] payload) {
+        final Iterator<String> iterator = Arrays.asList(payload).iterator();
 
         this.username = iterator.next();
 
         this.viewingGranted = Boolean.parseBoolean(iterator.next());
         final String viewingEvaluationCauseFeatureIdType = iterator.next();
-        final ApplicationFeatureSort viewingEvaluationFeatureIdType =  !viewingEvaluationCauseFeatureIdType.isEmpty() ? ApplicationFeatureSort.valueOf(viewingEvaluationCauseFeatureIdType) : null;
+
+        final ApplicationFeatureSort viewingEvaluationFeatureIdType = !viewingEvaluationCauseFeatureIdType.isEmpty()
+                ? ApplicationFeatureSort.valueOf(viewingEvaluationCauseFeatureIdType)
+                : null;
         final String viewingEvaluationFeatureFqn = iterator.next();
         this.viewingFeatureId = viewingEvaluationFeatureIdType != null
                 ? ApplicationFeatureId.newFeature(viewingEvaluationFeatureIdType, viewingEvaluationFeatureFqn)
@@ -239,17 +221,6 @@ public class UserPermissionViewModel implements ViewModel {
             return new ApplicationPermissionValue(featureId, rule, mode);
         }
     }
-
-    private static String base64UrlDecode(final String str) {
-        val bytes = Base64.getUrlDecoder().decode(str);
-        return _Strings.ofBytes(bytes, StandardCharsets.UTF_8);
-    }
-
-    private static String base64UrlEncode(final String str) {
-        val bytes = str.getBytes(StandardCharsets.UTF_8);
-        return Base64.getUrlEncoder().encodeToString(bytes);
-    }
-
 
     // -- user (derived property, hidden in parented tables)
 
@@ -441,13 +412,11 @@ public class UserPermissionViewModel implements ViewModel {
             val permissionSet = user.getPermissionSet();
             val changingEvaluation = permissionSet.evaluate(featureId, ApplicationPermissionMode.CHANGING);
             val viewingEvaluation = permissionSet.evaluate(featureId, ApplicationPermissionMode.VIEWING);
-            return UserPermissionViewModel
-                    .newViewModel(
-                            featureId,
-                            user,
-                            viewingEvaluation,
-                            changingEvaluation,
-                            factoryService);
+            return factoryService.viewModel(new UserPermissionViewModel(memento(
+                    featureId,
+                    user,
+                    viewingEvaluation,
+                    changingEvaluation)));
         };
     }
 
