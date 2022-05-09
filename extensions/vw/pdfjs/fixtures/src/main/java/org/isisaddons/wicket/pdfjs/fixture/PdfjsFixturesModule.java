f071b975560bd92c6bef6812e3325d9839c8b90d
Andi Huber <ahuber@apache.org>
2022-05-09 11:49:32 +0200

    ISIS-3040: reorg. project structure
    - pdf.js and fullcalendar: gather the wicket specific sub artifacts in
    their own container artifact (wicket)

--- extensions/vw/pdfjs/fixtures/src/main/java/org/isisaddons/wicket/pdfjs/fixture/PdfjsFixturesModule.java
+++ extensions/vw/pdfjs/fixtures/src/main/java/org/isisaddons/wicket/pdfjs/fixture/PdfjsFixturesModule.java
@@ -20,13 +20,13 @@ package org.isisaddons.wicket.pdfjs.fixture;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.apache.isis.applib.ModuleAbstract;
 import org.apache.isis.applib.fixturescripts.FixtureScript;
+import org.apache.isis.testing.fixtures.applib.modules.ModuleWithFixtures;
 
 import org.isisaddons.wicket.pdfjs.fixture.demoapp.demomodule.fixturescripts.PdfJsDemoObjectWithBlob_tearDown;
 
 @XmlRootElement(name = "module")
-public class PdfjsFixturesModule extends ModuleAbstract {
+public class PdfjsFixturesModule implements ModuleWithFixtures {
 
     @Override public FixtureScript getTeardownFixture() {
         return new PdfJsDemoObjectWithBlob_tearDown();
