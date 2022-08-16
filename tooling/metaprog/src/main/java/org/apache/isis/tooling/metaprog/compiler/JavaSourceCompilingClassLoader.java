9b7fa805af6d84fbcbd64f58869a5b241269cf0b
andi-huber <ahuber@apache.org>
2022-08-16 08:51:15 +0200

    ISIS-3123: just restructures into packages

--- tooling/metaprog/src/main/java/org/apache/isis/tooling/metaprog/compiler/JavaSourceCompilingClassLoader.java
+++ tooling/metaprog/src/main/java/org/apache/isis/tooling/metaprog/compiler/JavaSourceCompilingClassLoader.java
@@ -16,7 +16,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-package org.apache.isis.tooling.metaprog;
+package org.apache.isis.tooling.metaprog.compiler;
 
 import java.io.File;
 import java.io.FileInputStream;
--- tooling/metaprog/src/main/java/org/apache/isis/tooling/metaprog/graphql/GraphQLToJavaSourceConverter.java
+++ tooling/metaprog/src/main/java/org/apache/isis/tooling/metaprog/graphql/GraphQLToJavaSourceConverter.java
@@ -16,7 +16,7 @@
  *  specific language governing permissions and limitations
  *  under the License.
  */
-package org.apache.isis.tooling.metaprog;
+package org.apache.isis.tooling.metaprog.graphql;
 
 import java.util.Collection;
 import java.util.List;
