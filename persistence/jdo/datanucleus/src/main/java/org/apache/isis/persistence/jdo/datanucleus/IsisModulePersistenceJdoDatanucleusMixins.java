8be0e478b87d25b80a3b8b94e09588c1fd089ae5
Dan Haywood <dan@haywood-associates.co.uk>
2022-08-29 10:31:31 +0100

    ISIS-3189: removes unused imports, is all.
    (cherry picked from commit 2f243f2c9243d4cc9717ff47af58821b4c542ff4)

--- persistence/jdo/datanucleus/src/main/java/org/apache/isis/persistence/jdo/datanucleus/IsisModulePersistenceJdoDatanucleusMixins.java
+++ persistence/jdo/datanucleus/src/main/java/org/apache/isis/persistence/jdo/datanucleus/IsisModulePersistenceJdoDatanucleusMixins.java
@@ -18,65 +18,15 @@
  */
 package org.apache.isis.persistence.jdo.datanucleus;
 
-import java.util.Collections;
-import java.util.List;
-
-import javax.jdo.JDOException;
-import javax.jdo.PersistenceManagerFactory;
-import javax.sql.DataSource;
-
-import org.datanucleus.api.jdo.JDOPersistenceManagerFactory;
-import org.datanucleus.metadata.PersistenceUnitMetaData;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
-import org.springframework.dao.support.PersistenceExceptionTranslator;
-import org.springframework.transaction.interceptor.TransactionInterceptor;
 
-import org.apache.isis.commons.internal.assertions._Assert;
-import org.apache.isis.commons.internal.base._NullSafe;
-import org.apache.isis.core.config.IsisConfiguration;
-import org.apache.isis.core.config.beans.IsisBeanTypeRegistry;
-import org.apache.isis.core.config.beans.aoppatch.TransactionInterceptorFactory;
-import org.apache.isis.core.metamodel.context.MetaModelContext;
-import org.apache.isis.core.metamodel.services.objectlifecycle.ObjectLifecyclePublisher;
-import org.apache.isis.persistence.jdo.datanucleus.changetracking.JdoLifecycleListener;
 import org.apache.isis.persistence.jdo.datanucleus.config.DatanucleusSettings;
-import org.apache.isis.persistence.jdo.datanucleus.dialect.DnJdoDialect;
-import org.apache.isis.persistence.jdo.datanucleus.entities.DnEntityStateProvider;
-import org.apache.isis.persistence.jdo.datanucleus.jdosupport.JdoSupportServiceDefault;
 import org.apache.isis.persistence.jdo.datanucleus.mixins.Persistable_datanucleusVersionLong;
 import org.apache.isis.persistence.jdo.datanucleus.mixins.Persistable_datanucleusVersionTimestamp;
 import org.apache.isis.persistence.jdo.datanucleus.mixins.Persistable_downloadJdoMetadata;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoByteIdValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoByteIdentityValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoCharIdValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoCharIdentityValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoDatastoreIdImplValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoDatastoreIdValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoDatastoreUniqueLongIdValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoIntIdValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoIntIdentityValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoLongIdValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoLongIdentityValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoObjectIdValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoObjectIdentityValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoShortIdValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoShortIdentityValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoStringIdValueSemantics;
-import org.apache.isis.persistence.jdo.datanucleus.valuetypes.JdoStringIdentityValueSemantics;
-import org.apache.isis.persistence.jdo.integration.IsisModulePersistenceJdoIntegration;
-import org.apache.isis.persistence.jdo.provider.config.JdoEntityDiscoveryListener;
-import org.apache.isis.persistence.jdo.spring.integration.JdoDialect;
-import org.apache.isis.persistence.jdo.spring.integration.JdoTransactionManager;
-import org.apache.isis.persistence.jdo.spring.integration.LocalPersistenceManagerFactoryBean;
-import org.apache.isis.persistence.jdo.spring.integration.TransactionAwarePersistenceManagerFactoryProxy;
 
-import lombok.SneakyThrows;
-import lombok.val;
 import lombok.extern.log4j.Log4j2;
 
 /**
