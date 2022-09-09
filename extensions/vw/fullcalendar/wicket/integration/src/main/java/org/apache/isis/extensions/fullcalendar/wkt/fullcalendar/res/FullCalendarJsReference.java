12566bc9220f1f24c8ac48155f4cf00150104a36
Andi Huber <ahuber@apache.org>
2022-09-09 06:38:14 +0200

    Bump fullcalendar from 5.10.2 to 5.11.3 (2)

--- extensions/vw/fullcalendar/wicket/integration/src/main/java/org/apache/isis/extensions/fullcalendar/wkt/fullcalendar/res/FullCalendarJsReference.java
+++ extensions/vw/fullcalendar/wicket/integration/src/main/java/org/apache/isis/extensions/fullcalendar/wkt/fullcalendar/res/FullCalendarJsReference.java
@@ -21,9 +21,10 @@ package org.apache.isis.extensions.fullcalendar.wkt.fullcalendar.res;
 import org.apache.wicket.markup.head.HeaderItem;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 
-import de.agilecoders.wicket.webjars.request.resource.WebjarsJavaScriptResourceReference;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+
+import de.agilecoders.wicket.webjars.request.resource.WebjarsJavaScriptResourceReference;
 
 public class FullCalendarJsReference
 extends WebjarsJavaScriptResourceReference {
@@ -35,7 +36,7 @@ extends WebjarsJavaScriptResourceReference {
         new FullCalendarJsReference();
 
     private FullCalendarJsReference() {
-        super("fullcalendar/5.10.2/main.min.js");
+        super("fullcalendar/5.11.3/main.min.js");
     }
 
     /**
