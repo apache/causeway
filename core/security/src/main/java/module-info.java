1afce7c31e6323f48d93fd96f0a7caac4bfd369e
Andi Huber <ahuber@apache.org>
2022-09-09 14:48:38 +0200

    ISIS-3207: migrate security (api)

---
+++ core/security/src/main/java/module-info.java
@@ -0,0 +1,27 @@
+module org.apache.isis.security.api {
+    exports org.apache.isis.core.security;
+    exports org.apache.isis.core.security._testing;
+    exports org.apache.isis.core.security.authentication.fixtures;
+    exports org.apache.isis.core.security.authentication.login;
+    exports org.apache.isis.core.security.authentication.logout;
+    exports org.apache.isis.core.security.authentication.manager;
+    exports org.apache.isis.core.security.authentication.singleuser;
+    exports org.apache.isis.core.security.authentication.standard;
+    exports org.apache.isis.core.security.authentication;
+    exports org.apache.isis.core.security.authorization.manager;
+    exports org.apache.isis.core.security.authorization.standard;
+    exports org.apache.isis.core.security.authorization;
+    exports org.apache.isis.core.security.util;
+
+    requires java.annotation;
+    requires java.desktop;
+    requires javax.inject;
+    requires lombok;
+    requires org.apache.isis.applib;
+    requires org.apache.isis.commons;
+    requires org.apache.isis.schema;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.core;
+    requires spring.tx;
+}
