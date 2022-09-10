e218ce11234f7e643c5ed17ad87ece2d8044c683
Andi Huber <ahuber@apache.org>
2022-09-10 04:39:33 +0200

    ISIS-3207: migrate runtime

---
+++ core/runtime/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module org.apache.isis.core.runtime {
+    exports org.apache.isis.core.runtime;
+    exports org.apache.isis.core.runtime.context;
+    exports org.apache.isis.core.runtime.idstringifier;
+    exports org.apache.isis.core.runtime.events;
+
+    requires java.annotation;
+    requires java.desktop;
+    requires javax.inject;
+    requires lombok;
+    requires org.apache.isis.applib;
+    requires org.apache.isis.commons;
+    requires org.apache.isis.core.config;
+    requires org.apache.isis.core.interaction;
+    requires org.apache.isis.core.metamodel;
+    requires org.apache.isis.core.transaction;
+    requires org.apache.isis.security.api;
+    requires org.apache.isis.valuetypes.jodatime.integration;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.core;
+    requires spring.tx;
+}
