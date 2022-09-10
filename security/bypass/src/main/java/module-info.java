9424a5216c8a01c6e765cf52ec85f363866f0181
Andi Huber <ahuber@apache.org>
2022-09-10 04:41:04 +0200

    ISIS-3207: security bypass fix

--- security/bypass/src/main/java/module-info.java
+++ security/bypass/src/main/java/module-info.java
@@ -3,11 +3,11 @@ module org.apache.isis.security.bypass {
     exports org.apache.isis.security.bypass.authentication;
     exports org.apache.isis.security.bypass.authorization;
 
-    requires isis.core.runtimeservices;
-    requires java.annotation;
-    requires javax.inject;
     requires org.apache.isis.applib;
     requires org.apache.isis.security.api;
+    requires org.apache.isis.core.runtimeservices;
+    requires java.annotation;
+    requires javax.inject;
     requires spring.beans;
     requires spring.context;
 }
