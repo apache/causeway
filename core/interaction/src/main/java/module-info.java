eb99ef85572c2b2d461dada1bbf0f0bad7115929
Andi Huber <ahuber@apache.org>
2022-09-10 03:58:39 +0200

    ISIS-3207: migrate to interaction

---
+++ core/interaction/src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module org.apache.isis.core.interaction {
+    exports org.apache.isis.core.interaction;
+    exports org.apache.isis.core.interaction.integration;
+    exports org.apache.isis.core.interaction.scope;
+    exports org.apache.isis.core.interaction.session;
+
+    requires java.annotation;
+    requires java.sql;
+    requires javax.inject;
+    requires lombok;
+    requires org.apache.isis.applib;
+    requires org.apache.isis.commons;
+    requires org.apache.isis.core.config;
+    requires org.apache.isis.core.metamodel;
+    requires org.apache.isis.schema;
+    requires org.apache.logging.log4j;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.core;
+    requires spring.tx;
+}
