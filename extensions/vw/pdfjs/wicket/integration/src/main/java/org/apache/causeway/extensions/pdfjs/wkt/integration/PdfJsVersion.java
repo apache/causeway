bb0f8b3116f034b42f67c3e0a6bb7c9c20319471
Andi Huber <ahuber@apache.org>
2023-03-31 06:55:39 +0200

    Bump pdfjs-dist from 3.3.122 to 3.4.120

--- extensions/vw/pdfjs/wicket/integration/src/main/java/org/apache/causeway/extensions/pdfjs/wkt/integration/PdfJsVersion.java
+++ extensions/vw/pdfjs/wicket/integration/src/main/java/org/apache/causeway/extensions/pdfjs/wkt/integration/PdfJsVersion.java
@@ -24,7 +24,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum PdfJsVersion {
     V2_X("v2plus", "pdfjs-dist/2.16.105"),
-    V3_X("v2plus", "pdfjs-dist/3.3.122")
+    V3_X("v2plus", "pdfjs-dist/3.4.120")
     ;
     @Getter private final String integrationScriptSuffix;
     @Getter private final String webjarPath;
