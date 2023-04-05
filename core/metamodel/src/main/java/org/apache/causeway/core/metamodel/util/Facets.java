fec428f1211a774e1f1344fbe374f0b9dc868e74
danhaywood <dan@haywood-associates.co.uk>
2023-04-05 23:34:01 +0200

    CAUSEWAY-3414: ignores TableDecorator.Default

--- core/metamodel/src/main/java/org/apache/causeway/core/metamodel/util/Facets.java
+++ core/metamodel/src/main/java/org/apache/causeway/core/metamodel/util/Facets.java
@@ -341,13 +341,15 @@ public final class Facets {
     }
     private Optional<Class<? extends TableDecorator>> parentedTableDecorator(
             final FacetHolder facetHolder) {
-        return facetHolder.lookupFacet(CollectionLayoutTableDecoratorFacet.class)
-                .map(CollectionLayoutTableDecoratorFacet::value);
+        return (Optional) facetHolder.lookupFacet(CollectionLayoutTableDecoratorFacet.class)
+                .map(CollectionLayoutTableDecoratorFacet::value)
+                .filter(x -> x != TableDecorator.Default.class);
     }
     private Optional<Class<? extends TableDecorator>> standaloneTableDecorator(
             final FacetHolder facetHolder) {
-        return facetHolder.lookupFacet(DomainObjectLayoutTableDecoratorFacet.class)
-                .map(DomainObjectLayoutTableDecoratorFacet::value);
+        return (Optional)facetHolder.lookupFacet(DomainObjectLayoutTableDecoratorFacet.class)
+                .map(DomainObjectLayoutTableDecoratorFacet::value)
+                .filter(x -> x != TableDecorator.Default.class);
     }
 
     public Optional<ObjectSpecification> elementSpec(final FacetHolder facetHolder) {
