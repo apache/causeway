305301088165f0e923799fba1c3a43505c8b5500
Andi Huber <ahuber@apache.org>
2023-04-18 17:39:13 +0200

    CAUSEWAY-3409: resurrect LayoutOrderTest

--- core/metamodel/src/test/java/org/apache/causeway/core/metamodel/facets/ordering/memberorder/LayoutOrderTest.java
+++ core/metamodel/src/test/java/org/apache/causeway/core/metamodel/facets/ordering/memberorder/LayoutOrderTest.java
@@ -18,10 +18,9 @@
  */
 package org.apache.causeway.core.metamodel.facets.ordering.memberorder;
 
-import java.lang.reflect.Method;
 import java.util.Collection;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -30,8 +29,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.apache.causeway.applib.annotation.ActionLayout;
 import org.apache.causeway.applib.annotation.CollectionLayout;
 import org.apache.causeway.applib.annotation.PropertyLayout;
-import org.apache.causeway.core.metamodel.facets.FacetFactory.ProcessMethodContext;
-import org.apache.causeway.core.metamodel.facets.FacetFactoryTestAbstract2;
+import org.apache.causeway.core.metamodel.facets.FacetFactoryTestAbstract;
 import org.apache.causeway.core.metamodel.facets.actions.layout.ActionLayoutFacetFactory;
 import org.apache.causeway.core.metamodel.facets.collections.layout.CollectionLayoutFacetFactory;
 import org.apache.causeway.core.metamodel.facets.members.layout.order.LayoutOrderFacet;
@@ -43,94 +41,69 @@ import org.apache.causeway.core.metamodel.facets.properties.propertylayout.Prope
 import lombok.val;
 
 class LayoutOrderTest
-extends FacetFactoryTestAbstract2 {
+extends FacetFactoryTestAbstract {
 
-    @BeforeEach
-    protected void setUp() {
-
-//FIXME
-//        context.checking(new Expectations() {{
-//            allowing(mockTranslationService).translate(with(any(TranslationContext.class)), with(any(String.class)));
-//            will(new Action() {
-//                @Override
-//                public Object invoke(final Invocation invocation) throws Throwable {
-//                    return invocation.getParameter(1);
-//                }
-//
-//                @Override
-//                public void describeTo(final Description description) {
-//                    description.appendText("Returns parameter #1");
-//                }
-//            });
-//        }});
-    }
-
-    public void testMemberOrderAnnotationPickedUpOnProperty() {
+    @Test
+    void memberOrderAnnotationPickedUpOnProperty() {
+        val facetFactory = new PropertyLayoutFacetFactory(getMetaModelContext());
         class Customer {
             @PropertyLayout(sequence = "1")
-            public String getFirstName() {
-                return null;
-            }
+            public String getFirstName() { return null; }
         }
-        final Method method = findMethodExactOrFail(Customer.class, "getFirstName");
+        propertyScenario(Customer.class, "firstName", (processMethodContext, facetHolder, facetedMethod, facetedMethodParameter)->{
+            // when
+            facetFactory.process(processMethodContext);
+            // then
+            val facet = facetedMethod.getFacet(LayoutOrderFacet.class);
+            assertNotNull(facet);
+            assertTrue(facet instanceof LayoutOrderFacetFromPropertyLayoutAnnotation);
+            assertEquals("1", facet.getSequence());
 
-        val facetFactory = new PropertyLayoutFacetFactory(getMetaModelContext());
-        facetFactory.process(ProcessMethodContext
-                .forTesting(Customer.class, null, method, methodRemover, facetedMethod));
-
-        val facet = facetedMethod.getFacet(LayoutOrderFacet.class);
-        assertNotNull(facet);
-        assertTrue(facet instanceof LayoutOrderFacetFromPropertyLayoutAnnotation);
-        assertEquals("1", facet.getSequence());
-
-        assertNoMethodsRemoved();
+            assertNoMethodsRemoved();
+        });
     }
 
-    public void testMemberOrderAnnotationPickedUpOnCollection() {
+    @Test
+    void memberOrderAnnotationPickedUpOnCollection() {
+        val facetFactory = new CollectionLayoutFacetFactory(getMetaModelContext());
         class Order {
         }
+        @SuppressWarnings("unused")
         class Customer {
             @CollectionLayout(sequence = "2")
-            public Collection<Order> getOrders() {
-                return null;
-            }
-
-            @SuppressWarnings("unused")
-            public void addToOrders(final Order o) {
-            }
+            public Collection<Order> getOrders() { return null;}
+            public void addToOrders(final Order o) {}
         }
-        final Method method = findMethodExactOrFail(Customer.class, "getOrders");
+        collectionScenario(Customer.class, "orders", (processMethodContext, facetHolder, facetedMethod, facetedMethodParameter)->{
+            // when
+            facetFactory.process(processMethodContext);
+            // then
+            val facet = facetedMethod.getFacet(LayoutOrderFacet.class);
+            assertNotNull(facet);
+            assertTrue(facet instanceof LayoutOrderFacetFromCollectionLayoutAnnotation);
+            assertEquals("2", facet.getSequence());
 
-        val facetFactory = new CollectionLayoutFacetFactory(getMetaModelContext());
-        facetFactory.process(ProcessMethodContext
-                .forTesting(Customer.class, null, method, methodRemover, facetedMethod));
-
-        val facet = facetedMethod.getFacet(LayoutOrderFacet.class);
-        assertNotNull(facet);
-        assertTrue(facet instanceof LayoutOrderFacetFromCollectionLayoutAnnotation);
-        assertEquals("2", facet.getSequence());
-
-        assertNoMethodsRemoved();
+            assertNoMethodsRemoved();
+        });
     }
 
-    public void testMemberOrderAnnotationPickedUpOnAction() {
+    @Test
+    void memberOrderAnnotationPickedUpOnAction() {
+        val facetFactory = new ActionLayoutFacetFactory(getMetaModelContext());
         class Customer {
             @ActionLayout(sequence = "3")
-            public void someAction() {
-            }
+            public void someAction() {}
         }
-        final Method method = findMethodExactOrFail(Customer.class, "someAction");
+        actionScenario(Customer.class, "someAction", (processMethodContext, facetHolder, facetedMethod, facetedMethodParameter) -> {
+            //when
+            facetFactory.process(processMethodContext);
+            //then
+            val facet = facetedMethod.getFacet(LayoutOrderFacet.class);
+            assertNotNull(facet);
+            assertTrue(facet instanceof LayoutOrderFacetFromActionLayoutAnnotation);
+            assertEquals("3", facet.getSequence());
 
-        val facetFactory = new ActionLayoutFacetFactory(getMetaModelContext());
-        facetFactory.process(ProcessMethodContext
-                .forTesting(Customer.class, null, method, methodRemover, facetedMethod));
-
-        val facet = facetedMethod.getFacet(LayoutOrderFacet.class);
-        assertNotNull(facet);
-        assertTrue(facet instanceof LayoutOrderFacetFromActionLayoutAnnotation);
-        assertEquals("3", facet.getSequence());
-
-        assertNoMethodsRemoved();
+            assertNoMethodsRemoved();
+        });
     }
-
 }
