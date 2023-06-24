03adcb4fcfe92c429ca9f7219b6dbef8c014e0b2
Andi Huber <ahuber@apache.org>
2023-06-24 08:49:13 +0200

    CAUSEWAY-3500: reverts accidental java-doc change in prev. commit

--- api/applib/src/main/java/org/apache/causeway/applib/value/semantics/TemporalValueSemantics.java
+++ api/applib/src/main/java/org/apache/causeway/applib/value/semantics/TemporalValueSemantics.java
@@ -223,7 +223,7 @@ extends
          * @apiNote Yet only tested with {@literal XXX}, as there needs to be a format correspondence with
          * <i>momentJs</i> for the <i>tempus-dominus</i> date/time-picker to work
          * (as used by the <i>Wicket Viewer</i>).
-         * {@link org.apache.causeway.viewer.wicket.ui.components.pops.datepicker._TimeFormatUtil}
+         * {@link org.apache.causeway.viewer.wicket.ui.components.scalars.datepicker._TimeFormatUtil}
          * does the format conversion.
          */
         @NonNull //@NotEmpty
