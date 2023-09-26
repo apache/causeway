c82fe8b4a15ddc2569e7af46764edf1dc4d3ff10
Andi Huber <ahuber@apache.org>
2023-09-26 11:33:42 +0200

    CAUSEWAY-3603: handle exceptions well with the Lazy type

--- commons/src/main/java/org/apache/causeway/commons/internal/binding/_Observables.java
+++ commons/src/main/java/org/apache/causeway/commons/internal/binding/_Observables.java
@@ -19,8 +19,8 @@
 package org.apache.causeway.commons.internal.binding;
 
 import java.util.function.BooleanSupplier;
-import java.util.function.Supplier;
 
+import org.apache.causeway.commons.functional.ThrowingSupplier;
 import org.apache.causeway.commons.internal.base._Lazy;
 
 /**
@@ -39,7 +39,7 @@ public class _Observables {
 
         private final _Lazy<T> lazyValue;
 
-        public LazyObservable(final Supplier<T> factory) {
+        public LazyObservable(final ThrowingSupplier<T> factory) {
             this.lazyValue = _Lazy.threadSafe(factory);
         }
 
@@ -60,7 +60,7 @@ public class _Observables {
 
     }
 
-    public static <T> LazyObservable<T> lazy(final Supplier<T> factory) {
+    public static <T> LazyObservable<T> lazy(final ThrowingSupplier<T> factory) {
         return new LazyObservable<T>(factory);
     }
 
