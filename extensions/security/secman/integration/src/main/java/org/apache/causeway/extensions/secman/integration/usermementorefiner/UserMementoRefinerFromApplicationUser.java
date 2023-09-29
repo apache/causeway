fd81e68e95db7bd06b6dad77febcac876da8b4d7
danhaywood <dan@haywood-associates.co.uk>
2023-09-29 11:01:06 +0100

    CAUSEWAY-3618 : refactored to preserve any original roles in the UserMemento - reorg imports

--- extensions/security/secman/integration/src/main/java/org/apache/causeway/extensions/secman/integration/usermementorefiner/UserMementoRefinerFromApplicationUser.java
+++ extensions/security/secman/integration/src/main/java/org/apache/causeway/extensions/secman/integration/usermementorefiner/UserMementoRefinerFromApplicationUser.java
@@ -18,22 +18,13 @@
  */
 package org.apache.causeway.extensions.secman.integration.usermementorefiner;
 
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import javax.inject.Inject;
 import javax.inject.Provider;
 
-import org.apache.causeway.extensions.secman.applib.user.dom.ApplicationUser;
-
 import org.springframework.stereotype.Service;
 
-import org.apache.causeway.applib.services.user.RoleMemento;
 import org.apache.causeway.applib.services.user.UserMemento;
-import org.apache.causeway.commons.collections.Can;
 import org.apache.causeway.core.security.authentication.manager.UserMementoRefiner;
-import org.apache.causeway.extensions.secman.applib.role.dom.ApplicationRole;
 import org.apache.causeway.extensions.secman.applib.user.dom.ApplicationUserRepository;
 
 import lombok.RequiredArgsConstructor;
