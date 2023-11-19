6b8bb5f8d123e3072cccaf8a5ba4ba891fd9f0f1
Andi Huber <ahuber@apache.org>
2023-11-19 17:52:48 +0100

    CAUSEWAY-3647: remove code in prev. commit thats not needed

--- testing/integtestsupport/applib/src/main/java/org/apache/causeway/testing/integtestsupport/applib/CausewayInteractionHandler.java
+++ testing/integtestsupport/applib/src/main/java/org/apache/causeway/testing/integtestsupport/applib/CausewayInteractionHandler.java
@@ -18,12 +18,9 @@
  */
 package org.apache.causeway.testing.integtestsupport.applib;
 
-import java.util.Optional;
-
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.springframework.context.ApplicationContext;
 
 import org.apache.causeway.applib.services.iactnlayer.InteractionService;
 import org.apache.causeway.commons.internal.assertions._Assert;
@@ -62,21 +59,6 @@ public class CausewayInteractionHandler implements BeforeEachCallback, AfterEach
         _Helper
             .getInteractionFactory(extensionContext)
             .ifPresent(InteractionService::closeInteractionLayers);
-    }
-
-
-    static Optional<ApplicationContext> getSpringContext(final ExtensionContext extensionContext) {
-        return extensionContext.getTestInstance()
-            .filter(CausewayIntegrationTestAbstract.class::isInstance)
-            .map(CausewayIntegrationTestAbstract.class::cast)
-            .map(causewayIntegrationTestAbstract -> causewayIntegrationTestAbstract.springContext)
-            .map(springContext->{
-                var mmc = springContext.getBean(MetaModelContext.class);
-                _Assert.assertNotNull(mmc, ()->
-                        "MetaModelContext not found on Spring's test context.");
-                MetaModelContext.setOrReplace(mmc);
-                return springContext;
-            });
     }
 
 }
