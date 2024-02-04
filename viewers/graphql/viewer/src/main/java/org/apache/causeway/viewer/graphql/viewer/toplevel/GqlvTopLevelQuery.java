d3c6d75e391a4e845d9b5e161ac83afcc3b03d37
andi-huber <ahuber@apache.org>
2024-02-04 17:30:13 +0100

    CAUSEWAY-3404: remove unused imports

--- viewers/graphql/viewer/src/main/java/org/apache/causeway/viewer/graphql/viewer/toplevel/GqlvTopLevelQuery.java
+++ viewers/graphql/viewer/src/main/java/org/apache/causeway/viewer/graphql/viewer/toplevel/GqlvTopLevelQuery.java
@@ -1,24 +1,19 @@
 package org.apache.causeway.viewer.graphql.viewer.toplevel;
 
+import static graphql.schema.GraphQLObjectType.newObject;
+
 import org.apache.causeway.applib.services.registry.ServiceRegistry;
 import org.apache.causeway.core.metamodel.spec.ObjectSpecification;
 import org.apache.causeway.viewer.graphql.model.context.Context;
 import org.apache.causeway.viewer.graphql.model.domain.GqlvDomainService;
 
-import lombok.Getter;
-
-import graphql.Scalars;
 import graphql.schema.DataFetcher;
 import graphql.schema.FieldCoordinates;
 import graphql.schema.GraphQLCodeRegistry;
 import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLObjectType;
-
+import lombok.Getter;
 import lombok.val;
-
-import static graphql.schema.FieldCoordinates.coordinates;
-import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
-import static graphql.schema.GraphQLObjectType.newObject;
 
 public class GqlvTopLevelQuery {
 
@@ -77,7 +72,7 @@ public class GqlvTopLevelQuery {
 
     }
 
-    public void addDomainServiceTo(ObjectSpecification objectSpec, Object servicePojo, Context context) {
+    public void addDomainServiceTo(final ObjectSpecification objectSpec, final Object servicePojo, final Context context) {
         val domainService = new GqlvDomainService(objectSpec, servicePojo, context);
 
         boolean actionsAdded = domainService.hasActions();
