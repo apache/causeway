c638d193cb02b82e05eb889f221cb5db990c28d2
Andi Huber <ahuber@apache.org>
2024-02-15 08:43:23 +0100

    CAUSEWAY-3404: removes unused import in prev. commit

--- regressiontests/stable-persistence-jpa/src/test/java/org/apache/causeway/testdomain/transactions/jpa/JpaTransactionScopeListenerTest.java
+++ regressiontests/stable-persistence-jpa/src/test/java/org/apache/causeway/testdomain/transactions/jpa/JpaTransactionScopeListenerTest.java
@@ -23,7 +23,6 @@ import javax.inject.Inject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
