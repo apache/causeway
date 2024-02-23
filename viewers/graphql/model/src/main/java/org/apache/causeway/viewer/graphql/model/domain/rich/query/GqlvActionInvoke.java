1f5959b6f07140ff9a7339e04565789823092b27
danhaywood <dan@haywood-associates.co.uk>
2024-02-23 06:37:39 +0000

    CAUSEWAY-3676: simplifies holders (6)

--- viewers/graphql/model/src/main/java/org/apache/causeway/viewer/graphql/model/domain/rich/query/GqlvActionInvoke.java
+++ viewers/graphql/model/src/main/java/org/apache/causeway/viewer/graphql/model/domain/rich/query/GqlvActionInvoke.java
@@ -42,8 +42,7 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class GqlvActionInvoke
         extends GqlvAbstractCustom
-        implements HolderActionInvokeResult,
-        HolderActionInvoke {
+        implements HolderActionInvoke {
 
     private final HolderActionInvoke holder;
     private final GqlvActionInvokeResult result;
--- viewers/graphql/model/src/main/java/org/apache/causeway/viewer/graphql/model/domain/rich/query/GqlvActionInvokeResult.java
+++ viewers/graphql/model/src/main/java/org/apache/causeway/viewer/graphql/model/domain/rich/query/GqlvActionInvokeResult.java
@@ -46,10 +46,10 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class GqlvActionInvokeResult extends GqlvAbstract {
 
-    private final HolderActionInvokeResult holder;
+    private final HolderActionInvoke holder;
 
     public GqlvActionInvokeResult(
-            final HolderActionInvokeResult holder,
+            final HolderActionInvoke holder,
             final Context context) {
         super(context);
 
