7b59496e3483ea525c18dbbc50fd1ab9828a19cc
danhaywood <dan@haywood-associates.co.uk>
2024-02-24 07:57:14 +0000

    CAUSEWAY-3676: fixes dupl integ test issue

--- viewers/graphql/test/src/test/java/org/apache/causeway/viewer/graphql/viewer/test/e2e/simple/SimpleDepartment_IntegTest.java
+++ viewers/graphql/test/src/test/java/org/apache/causeway/viewer/graphql/viewer/test/e2e/simple/SimpleDepartment_IntegTest.java
@@ -33,7 +33,7 @@ import org.apache.causeway.viewer.graphql.viewer.test.e2e.Abstract_IntegTest;
 //NOT USING @Transactional since we are running server within same transaction otherwise
 @Order(40)
 @ActiveProfiles("test")
-public class Department_IntegTest extends Abstract_IntegTest {
+public class SimpleDepartment_IntegTest extends Abstract_IntegTest {
 
     @Override
     @TestFactory
