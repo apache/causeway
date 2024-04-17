7f5cb4d517ee345f0617694e50defe52adfd51de
Andi Huber <ahuber@apache.org>
2024-04-17 15:12:58 +0200

    CAUSEWAY-3723: revert last commit, as it breaks PasswordEncoder provisioning

--- extensions/security/secman/integration/src/main/java/org/apache/causeway/extensions/secman/integration/authenticator/AuthenticatorSecmanAutoConfiguration.java
+++ extensions/security/secman/integration/src/main/java/org/apache/causeway/extensions/secman/integration/authenticator/AuthenticatorSecmanAutoConfiguration.java
@@ -20,11 +20,9 @@ package org.apache.causeway.extensions.secman.integration.authenticator;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.apache.causeway.applib.annotation.PriorityPrecedence;
@@ -39,29 +37,14 @@ import org.apache.causeway.extensions.secman.applib.user.dom.ApplicationUserRepo
 @Configuration
 public class AuthenticatorSecmanAutoConfiguration  {
 
-    /**
-     * The name of the bean that is required to be declared in order that
-     * {@link #authenticatorSecman(ApplicationUserRepository, PasswordEncoder)} can furnish an {@link Authenticator}.
-     */
-    public static final String PASSWORD_ENCODER_BEAN_NAME = CausewayModuleExtSecmanApplib.NAMESPACE + "PasswordEncoderForSecman";
-
     @Bean(CausewayModuleExtSecmanApplib.NAMESPACE + ".AuthenticatorSecman")
     @ConditionalOnMissingBean(Authenticator.class)
-    @ConditionalOnBean(value = PasswordEncoder.class,  name = PASSWORD_ENCODER_BEAN_NAME)
+    @Qualifier("Secman")
     public Authenticator authenticatorSecman(
             final ApplicationUserRepository applicationUserRepository,
-            final @Qualifier(PASSWORD_ENCODER_BEAN_NAME) PasswordEncoder passwordEncoder) {
+            final @Qualifier("Secman") PasswordEncoder passwordEncoder) {
         return new AuthenticatorSecman(
                 applicationUserRepository, passwordEncoder);
-    }
-
-    /**
-     * The {@link PasswordEncoder} that will be provided automatically unless the application chooses to provide some other implementation.
-     */
-    @Bean(PASSWORD_ENCODER_BEAN_NAME)
-    @ConditionalOnMissingBean(value = PasswordEncoder.class,  name = PASSWORD_ENCODER_BEAN_NAME)
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
 }
