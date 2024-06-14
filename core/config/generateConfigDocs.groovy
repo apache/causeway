c3c909abc7261474d53b194c57c5d7d890801552
Dan Haywood <dan@haywood-associates.co.uk>
2024-06-14 08:48:50 +0100

    CAUSEWAY-3774: wip on docs

--- core/config/generateConfigDocs.groovy
+++ core/config/generateConfigDocs.groovy
@@ -99,7 +99,7 @@ groups+= new PropertyGroup() {{
 
 groups+= new PropertyGroup() {{
     prefix = "causeway.core.meta-model.validator"
-    name = "MetaModel Validator"
+    name = "Core MetaModel Validator"
     searchOrder = 101
 }}
 
@@ -120,6 +120,13 @@ groups+= new PropertyGroup() {{
 groups+= new PropertyGroup() {{
     prefix = "causeway.persistence.schema"
     name = "Core Persistence Schema"
+    properties: []
+    searchOrder = 100
+}}
+
+groups+= new PropertyGroup() {{
+    prefix = "causeway.prototyping"
+    name = "Core Prototyping"
     properties: []
     searchOrder = 100
 }}
