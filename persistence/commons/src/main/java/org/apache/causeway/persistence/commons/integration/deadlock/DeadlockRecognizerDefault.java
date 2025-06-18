0e72eb982b9c04c5236a7d15be8626b2b3a9c6cd
Andi Huber <ahuber@apache.org>
2025-06-18 06:31:20 +0200

    CAUSEWAY-3883: replaces val w/ var

--- persistence/commons/src/main/java/org/apache/causeway/persistence/commons/integration/deadlock/DeadlockRecognizerDefault.java
+++ persistence/commons/src/main/java/org/apache/causeway/persistence/commons/integration/deadlock/DeadlockRecognizerDefault.java
@@ -20,7 +20,6 @@ package org.apache.causeway.persistence.commons.integration.deadlock;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import lombok.val;
 
 import jakarta.annotation.Priority;
 import jakarta.inject.Inject;
@@ -48,7 +47,7 @@ public class DeadlockRecognizerDefault implements DeadlockRecognizer {
 
     @Override
     public boolean isDeadlock(Throwable ex) {
-        val whetherDeadlock = ex instanceof DeadlockLoserDataAccessException || isMessage(ex, SQL_SERVER_DEADLOCK_MESSAGE);
+        var whetherDeadlock = ex instanceof DeadlockLoserDataAccessException || isMessage(ex, SQL_SERVER_DEADLOCK_MESSAGE);
         if (whetherDeadlock) {
             log.warn("Detected deadlock");
             log.debug("Detected deadlock details:", ex);
