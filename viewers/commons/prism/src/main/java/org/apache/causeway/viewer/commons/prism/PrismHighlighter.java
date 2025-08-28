120e92be3eb213541ca8c8330d188d97376bd933
Andi Huber <ahuber@apache.org>
2025-08-28 19:46:40 +0200

    CAUSEWAY-3892: fixes PrismHighlighter; due to some changes to how JSoup traverses document nodes

--- viewers/commons/prism/src/main/java/org/apache/causeway/viewer/commons/prism/PrismHighlighter.java
+++ viewers/commons/prism/src/main/java/org/apache/causeway/viewer/commons/prism/PrismHighlighter.java
@@ -18,6 +18,7 @@
  */
 package org.apache.causeway.viewer.commons.prism;
 
+import java.util.ArrayList;
 import java.util.function.UnaryOperator;
 
 import org.jsoup.Jsoup;
@@ -27,8 +28,7 @@ import org.jsoup.nodes.Node;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public record PrismHighlighter(
-        ) implements UnaryOperator<String> {
+public record PrismHighlighter() implements UnaryOperator<String> {
 
     /**
      * Returns the highlighted HTML.
@@ -40,6 +40,8 @@ public record PrismHighlighter(
         var doc = Jsoup.parseBodyFragment(htmlContent);
 
         //var tt = org.apache.causeway.commons.internal.base._Timing.now();
+
+        var replacements = new ArrayList<NodeReplacement>();
 
         doc.traverse((final Node node, final int depth)->{
             if(node instanceof Element element
@@ -63,13 +65,23 @@ public record PrismHighlighter(
                 //<pre class="highlight language-%s">
                 node.parent().attr("class", "highlight language-%s".formatted(prismLanguage.languageId()));
 
-                node.replaceWith(newNode);
+                replacements.add(new NodeReplacement(element, newNode));
             }
         });
+
+        replacements.forEach(NodeReplacement::apply);
 
         //tt.stop();System.err.printf("context took %s%n", tt);
 
         return doc.body().html();
+    }
+
+    private record NodeReplacement(
+            Element oldNode,
+            Element newNode) {
+        void apply() {
+            oldNode.replaceWith(newNode);
+        }
     }
 
 }
