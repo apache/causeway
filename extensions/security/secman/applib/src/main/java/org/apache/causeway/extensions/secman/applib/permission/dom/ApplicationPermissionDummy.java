4e995bc4eef5f14d51e2f26241557821e186d8dc
andi-huber <ahuber@apache.org>
2026-02-17 08:34:28 +0100

    CAUSEWAY-3968: [v2] prepare static weaving support (secman)

---
+++ extensions/security/secman/applib/src/main/java/org/apache/causeway/extensions/secman/applib/permission/dom/ApplicationPermissionDummy.java
@@ -0,0 +1,94 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.causeway.extensions.secman.applib.permission.dom;
+
+import javax.persistence.Entity;
+
+import org.apache.causeway.applib.services.appfeat.ApplicationFeatureSort;
+import org.apache.causeway.extensions.secman.applib.role.dom.ApplicationRole;
+
+/**
+ * Just a marker, to have the static-weaving processor pickup inherited abstract classes.
+ * @deprecated not required in 4.x (as abstract classes were refactored into interfaces) 
+ */
+@Deprecated
+@Entity
+public class ApplicationPermissionDummy extends ApplicationPermission {
+
+	@Override
+	public ApplicationRole getRole() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRole(ApplicationRole applicationRole) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ApplicationPermissionRule getRule() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRule(ApplicationPermissionRule rule) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ApplicationPermissionMode getMode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setMode(ApplicationPermissionMode mode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ApplicationFeatureSort getFeatureSort() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setFeatureSort(ApplicationFeatureSort featureSort) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getFeatureFqn() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setFeatureFqn(String featureFqn) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ extensions/security/secman/applib/src/main/java/org/apache/causeway/extensions/secman/applib/tenancy/dom/ApplicationTenancyDummy.java
@@ -0,0 +1,74 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.apache.causeway.extensions.secman.applib.tenancy.dom;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+
+/**
+ * Just a marker, to have the static-weaving processor pickup inherited abstract classes.
+ * @deprecated not required in 4.x (as abstract classes were refactored into interfaces) 
+ */
+@Deprecated
+@Entity
+public class ApplicationTenancyDummy extends ApplicationTenancy {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPath(String path) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ApplicationTenancy getParent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setParent(ApplicationTenancy parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Collection<ApplicationTenancy> getChildren() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
